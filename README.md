5a8ff77cb7f9db3d82fd7e7bcc274cd9d6d77f2c
TH Huang <tzhsn.huang@gmail.com>
2022-02-03 15:51:30 +0800

    [epi] alternating array

--- README.md
+++ README.md
@@ -32,3 +32,4 @@ This is my personal repository, including notes and resources related to competi
 | 5.5     | Sorted Array Remove Duplicates | [C++](./solutions/epi/sorted-array-remove-duplicates/main.cpp) |
 | 5.6     | Buy and Sell Stock             | [C++](./solutions/epi/buy-and-sell-stock/main.cpp)             |
 | 5.7     | Buy and Sell Stock Twice       | [C++](./solutions/epi/buy-and-sell-stock-twice/main.cpp)       |
+| 5.8     | Alternating Array              | [C++](./solutions/epi/alternating-array/main.cpp)              |
---
+++ solutions/epi/alternating-array/main.cpp
@@ -0,0 +1,68 @@
+#include <epi/fmt_print.h>
+#include <epi/generic_test.h>
+#include <epi/test_failure.h>
+#include <epi/timed_executor.h>
+using namespace std;
+
+void Rearrange(vector<int>* vec_ptr) {
+    auto& vec = *vec_ptr;
+    for (int i = 1; i < vec.size(); ++i) {
+        if (i & 1 && vec[i - 1] > vec[i]) {
+            swap(vec[i - 1], vec[i]);
+        }
+        if (!(i & 1) && vec[i - 1] < vec[i]) {
+            swap(vec[i - 1], vec[i]);
+        }
+    }
+    return;
+}
+void CheckAnswer(const vector<int>& A) {
+    for (int i = 0; i < A.size(); ++i) {
+        if (i % 2) {
+            if (A[i] < A[i - 1]) {
+                throw TestFailure()
+                    .WithProperty(PropertyName::RESULT, A)
+                    .WithMismatchInfo(i, FmtStr("A[{}] <= A[{}]", i - 1, i),
+                                      FmtStr("{} > {}", A[i - 1], A[i]));
+            }
+            if (i + 1 < A.size()) {
+                if (A[i] < A[i + 1]) {
+                    throw TestFailure()
+                        .WithProperty(PropertyName::RESULT, A)
+                        .WithMismatchInfo(i, FmtStr("A[{}] >= A[{}]", i, i + 1),
+                                          FmtStr("{} < {}", A[i], A[i + 1]));
+                }
+            }
+        } else {
+            if (i > 0) {
+                if (A[i - 1] < A[i]) {
+                    throw TestFailure()
+                        .WithProperty(PropertyName::RESULT, A)
+                        .WithMismatchInfo(i, FmtStr("A[{}] >= A[{}]", i - 1, i),
+                                          FmtStr("{} < {}", A[i - 1], A[i]));
+                }
+            }
+            if (i + 1 < A.size()) {
+                if (A[i + 1] < A[i]) {
+                    throw TestFailure()
+                        .WithProperty(PropertyName::RESULT, A)
+                        .WithMismatchInfo(i, FmtStr("A[{}] <= A[{}]", i, i + 1),
+                                          FmtStr("{} > {}", A[i], A[i + 1]));
+                }
+            }
+        }
+    }
+}
+
+void RearrangeWrapper(TimedExecutor& executor, vector<int> A) {
+    executor.Run([&] { Rearrange(&A); });
+
+    CheckAnswer(A);
+}
+
+int main(int argc, char* argv[]) {
+    vector<string> args{argv + 1, argv + argc};
+    vector<string> param_names{"executor", "A"};
+    return GenericTestMain(args, "main.cpp", "data.tsv", &RearrangeWrapper,
+                           DefaultComparator{}, param_names);
+}
