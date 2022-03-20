4f5877b6bf5b2e5fce701f5055953c974f0907ad
TH Huang <tzhsn.huang@gmail.com>
2022-03-20 09:53:38 +0800

    [epi] is tree a bst: check by range

--- solutions/epi/is-tree-a-bst/main.cpp
+++ solutions/epi/is-tree-a-bst/main.cpp
@@ -5,36 +5,36 @@
 #include <memory>
 using namespace std;
 
-/* bool IsInRange(const unique_ptr<BinaryTreeNode<int>>& node, int minimum, */
-/*                int maximum) { */
-/*     if (!node) return true; */
-/*     if (node->data < minimum || node->data > maximum) return false; */
-/*     return IsInRange(node->left, minimum, node->data) && */
-/*            IsInRange(node->right, node->data, maximum); */
-/* } */
-
-/* bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) { */
-/*     return IsInRange(tree, numeric_limits<int>::min(), */
-/*                      numeric_limits<int>::max()); */
-/* } */
+bool IsInRange(const unique_ptr<BinaryTreeNode<int>>& node, int minimum,
+               int maximum) {
+    if (!node) return true;
+    if (node->data < minimum || node->data > maximum) return false;
+    return IsInRange(node->left, minimum, node->data) &&
+           IsInRange(node->right, node->data, maximum);
+}
 
 bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) {
-    int curr_val = numeric_limits<int>::min();
-    function<bool(const unique_ptr<BinaryTreeNode<int>>&)> traverse =
-        [&](const unique_ptr<BinaryTreeNode<int>>& node) {
-            if (!node) return true;
-
-            if (!traverse(node->left)) return false;
-
-            if (node->data < curr_val) return false;
-            curr_val = node->data;
-
-            if (!traverse(node->right)) return false;
-
-            return true;
-        };
-    return traverse(tree);
+    return IsInRange(tree, numeric_limits<int>::min(),
+                     numeric_limits<int>::max());
 }
+
+/* bool IsBinaryTreeBST(const unique_ptr<BinaryTreeNode<int>>& tree) { */
+/*     int curr_val = numeric_limits<int>::min(); */
+/*     function<bool(const unique_ptr<BinaryTreeNode<int>>&)> traverse = */
+/*         [&](const unique_ptr<BinaryTreeNode<int>>& node) { */
+/*             if (!node) return true; */
+
+/*             if (!traverse(node->left)) return false; */
+
+/*             if (node->data < curr_val) return false; */
+/*             curr_val = node->data; */
+
+/*             if (!traverse(node->right)) return false; */
+
+/*             return true; */
+/*         }; */
+/*     return traverse(tree); */
+/* } */
 
 int main(int argc, char* argv[]) {
     vector<string> args{argv + 1, argv + argc};
