fb28cefeeb5f9036046831cb63a5c0fb9c1cc6bd
TH Huang <tzhsn.huang@gmail.com>
2022-03-21 07:55:52 +0800

    [epi] bst from preorder: optimized

--- solutions/epi/bst-from-preorder/main.cpp
+++ solutions/epi/bst-from-preorder/main.cpp
@@ -2,30 +2,56 @@
 #include <epi/generic_test.h>
 
 #include <algorithm>
+#include <limits>
 #include <memory>
 using namespace std;
 
+/* unique_ptr<BstNode<int>> ConstructNode(const vector<int>& preorder_sequence,
+ */
+/*                                        int l, int r) { */
+/*     if (r < l) return nullptr; */
+
+/*     int data = preorder_sequence[l]; */
+/*     auto node = make_unique<BstNode<int>>(data); */
+
+/*     auto it = upper_bound(preorder_sequence.begin() + l + 1, */
+/*                           preorder_sequence.begin() + r + 1, data); */
+/*     int m = it - begin(preorder_sequence); */
+
+/*     node->left = ConstructNode(preorder_sequence, l + 1, m - 1); */
+/*     node->right = ConstructNode(preorder_sequence, m, r); */
+
+/*     return node; */
+/* } */
+
+/* unique_ptr<BstNode<int>> RebuildBSTFromPreorder( */
+/*     const vector<int>& preorder_sequence) { */
+/*     if (empty(preorder_sequence)) return nullptr; */
+/*     return ConstructNode(preorder_sequence, 0, size(preorder_sequence) - 1);
+ */
+/* } */
+
 unique_ptr<BstNode<int>> ConstructNode(const vector<int>& preorder_sequence,
-                                       int l, int r) {
-    if (r < l) return nullptr;
+                                       int minimum, int maximum, int& pos) {
+    if (pos == size(preorder_sequence)) return nullptr;
 
-    int data = preorder_sequence[l];
+    int data = preorder_sequence[pos];
+    if (data < minimum || data > maximum) return nullptr;
+
     auto node = make_unique<BstNode<int>>(data);
+    ++pos;
 
-    auto it = upper_bound(preorder_sequence.begin() + l + 1,
-                          preorder_sequence.begin() + r + 1, data);
-    int m = it - begin(preorder_sequence);
-
-    node->left = ConstructNode(preorder_sequence, l + 1, m - 1);
-    node->right = ConstructNode(preorder_sequence, m, r);
+    node->left = ConstructNode(preorder_sequence, minimum, data, pos);
+    node->right = ConstructNode(preorder_sequence, data, maximum, pos);
 
     return node;
 }
 
 unique_ptr<BstNode<int>> RebuildBSTFromPreorder(
     const vector<int>& preorder_sequence) {
-    if (empty(preorder_sequence)) return nullptr;
-    return ConstructNode(preorder_sequence, 0, size(preorder_sequence) - 1);
+    int pos = 0;
+    return ConstructNode(preorder_sequence, numeric_limits<int>::min(),
+                         numeric_limits<int>::max(), pos);
 }
 
 int main(int argc, char* argv[]) {
