545d9518033ae48a69390c81ec0342bda7e1af38
TH Huang <tzhsn.huang@gmail.com>
2022-03-27 17:47:12 +0800

    [leetcode] the k weakest rows in a matrix: binary search

--- solutions/leetcode/the-k-weakest-rows-in-a-matrix/main.cpp
+++ solutions/leetcode/the-k-weakest-rows-in-a-matrix/main.cpp
@@ -28,3 +28,22 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> q;
+        for (int i = 0; i < size(mat); ++i) {
+            auto& r = mat[i];
+            auto it =
+                upper_bound(r.begin(), r.end(), 1,
+                            [](int value, int entry) { return value > entry; });
+            q.push({int(it - r.begin()), i});
+        }
+
+        vector<int> result(k);
+        for (int i = 0; i < k; ++i) result[i] = q.top()[1], q.pop();
+
+        return result;
+    }
+};
