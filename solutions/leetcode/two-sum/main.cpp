e950bb8fdf19c42714e119eaecfa27ed337b4596
TH Huang <tzhsn.huang@gmail.com>
2022-03-27 15:14:01 +0800

    [leetcode] two sum

--- solutions/leetcode/two-sum/main.cpp
+++ solutions/leetcode/two-sum/main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-using namespace std;
-
 class Solution {
    public:
-    vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            if (mp.count(num)) return {mp[num], i};
-            mp[target - num] = i;
+        for (int i = 0; i < size(nums); ++i) {
+            if (mp.count(nums[i])) return {mp[nums[i]], i};
+            mp[target - nums[i]] = i;
         }
-        return {-1, -1};
+        return {};
     }
 };
