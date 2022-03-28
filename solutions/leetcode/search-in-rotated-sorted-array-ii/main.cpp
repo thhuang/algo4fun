354e1d5ba391096c3c35e83a25a6f580afcf0b5a
TH Huang <tzhsn.huang@gmail.com>
2022-03-28 15:30:49 +0800

    [leetcode] search in rotated sorted array ii: iterative

--- solutions/leetcode/search-in-rotated-sorted-array-ii/main.cpp
+++ solutions/leetcode/search-in-rotated-sorted-array-ii/main.cpp
@@ -26,3 +26,35 @@ class Solution {
         return range_search(0, size(nums) - 1);
     }
 };
+
+class Solution {
+   public:
+    bool search(vector<int>& nums, int target) {
+        vector<array<int, 2>> vec = {{0, int(size(nums) - 1)}};
+        while (!empty(vec)) {
+            auto [l, r] = vec.back();
+            vec.pop_back();
+            if (l > r) continue;
+
+            int m = l + (r - l) / 2;
+            if (target == nums[l] || target == nums[m] || target == nums[r])
+                return true;
+            if (nums[l] == nums[m] || nums[m] == nums[r]) {
+                vec.push_back({l, m - 1});
+                vec.push_back({m + 1, r});
+                continue;
+            }
+
+            if (nums[l] < nums[m] && (target < nums[l] || nums[m] < target)) {
+                vec.push_back({m + 1, r});
+            } else if (nums[m] < nums[r] &&
+                       (nums[m] < target && target < nums[r])) {
+                vec.push_back({m + 1, r});
+            } else {
+                vec.push_back({l, m - 1});
+            }
+        }
+
+        return false;
+    }
+};
