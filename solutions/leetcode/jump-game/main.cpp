809f29c86201120b130fddaa73e45d085909ebb7
TH Huang <tzhsn.huang@gmail.com>
2022-03-29 10:32:43 +0800

    [leetcode] jump game: refactor

--- solutions/leetcode/jump-game/main.cpp
+++ solutions/leetcode/jump-game/main.cpp
@@ -5,8 +5,7 @@ class Solution {
         for (int i = 0; i < nums.size(); ++i) {
             if (i > max_dist) return false;
             max_dist = max(max_dist, i + nums[i]);
-            if (max_dist >= nums.size() - 1) return true;
         }
-        return false;
+        return true;
     }
 };
