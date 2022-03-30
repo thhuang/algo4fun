c4401fe838d93b85eaa2e6fbb394a388c8bb38ee
TH Huang <tzhsn.huang@gmail.com>
2022-03-30 13:55:24 +0800

    [leetcode] maximum subarray: refactor

--- solutions/leetcode/maximum-subarray/main.cpp
+++ solutions/leetcode/maximum-subarray/main.cpp
@@ -1,11 +1,11 @@
 class Solution {
    public:
     int maxSubArray(vector<int>& nums) {
-        int ans = nums[0];
-        int curr_max = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            curr_max = max(nums[i], curr_max + nums[i]);
-            ans = max(ans, curr_max);
+        int ans = numeric_limits<int>::min();
+        int local_max = numeric_limits<int>::min();
+        for (int v : nums) {
+            local_max = max(local_max, 0) + v;
+            ans = max(ans, local_max);
         }
         return ans;
     }
