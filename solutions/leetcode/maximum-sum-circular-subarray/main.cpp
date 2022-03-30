d9ace4951a29c9d9ac9a65e35d4a48fa0781b029
TH Huang <tzhsn.huang@gmail.com>
2022-03-30 15:08:45 +0800

    [leetcode] maximum sum circular subarray

---
+++ solutions/leetcode/maximum-sum-circular-subarray/main.cpp
@@ -0,0 +1,35 @@
+class Solution {
+   public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) return nums.front();
+
+        int sum = 0;
+        int ans1 = numeric_limits<int>::min();
+        int curr = numeric_limits<int>::min();
+        for (int i = 0; i < n; ++i) {
+            sum += nums[i];
+            curr = max(curr, 0) + nums[i];
+            ans1 = max(ans1, curr);
+        }
+
+        int ans2 = numeric_limits<int>::max();
+        curr = numeric_limits<int>::max();
+        for (int i = 1; i < n; ++i) {
+            curr = min(curr, 0) + nums[i];
+            ans2 = min(ans2, curr);
+        }
+        ans2 = sum - ans2;
+
+        int ans3 = numeric_limits<int>::max();
+        curr = numeric_limits<int>::max();
+        for (int i = 0; i < n - 1; ++i) {
+            curr = min(curr, 0) + nums[i];
+            ans3 = min(ans3, curr);
+        }
+        ans3 = sum - ans3;
+
+        return max({ans1, ans2, ans3});
+    }
+};
