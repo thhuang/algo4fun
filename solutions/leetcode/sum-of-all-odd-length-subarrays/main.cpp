936fa80539c82a9b2f58c9f8194d2bcbbeceb8f9
TH Huang <tzhsn.huang@gmail.com>
2022-03-31 10:08:52 +0800

    [leetcode] sum of all odd length subarrays: optimize

--- solutions/leetcode/sum-of-all-odd-length-subarrays/main.cpp
+++ solutions/leetcode/sum-of-all-odd-length-subarrays/main.cpp
@@ -12,3 +12,17 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+   public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n = size(arr);
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int l = i + 1;
+            int r = n - i;
+            ans += arr[i] * ((l * r + 1) / 2);
+        }
+        return ans;
+    }
+};
