931c7b7bb5439dc6fdfc7fde7ec070eb4045ee00
TH Huang <tzhsn.huang@gmail.com>
2022-04-03 21:36:38 +0800

    [leetcode] 01 matrix: optimize

--- solutions/leetcode/01-matrix/main.cpp
+++ solutions/leetcode/01-matrix/main.cpp
@@ -33,3 +33,32 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = size(mat);
+        int m = size(mat[0]);
+        vector<vector<int>> ans(n, vector<int>(m, n + m);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    continue;
+                }
+                if (i > 0) ans[i][j] = min(ans[i][j], ans[i - 1][j] + 1);
+                if (j > 0) ans[i][j] = min(ans[i][j], ans[i][j - 1] + 1);
+            }
+        }
+
+        for (int i = n - 1; ~i; --i) {
+            for (int j = m - 1; ~j; --j) {
+                if (i < n - 1) ans[i][j] = min(ans[i][j], ans[i + 1][j] + 1);
+                if (j < m - 1) ans[i][j] = min(ans[i][j], ans[i][j + 1] + 1);
+            }
+        }
+
+        return ans;
+    }
+};
