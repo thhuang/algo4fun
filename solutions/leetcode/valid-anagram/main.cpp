61a04b51b030f42c1a9815b49e80430502dde3d3
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 09:21:32 +0800

    [leetcode] valid anagram: with hash map

--- solutions/leetcode/valid-anagram/main.cpp
+++ solutions/leetcode/valid-anagram/main.cpp
@@ -14,3 +14,16 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        if (size(s) != size(t)) return false;
+        unordered_map<char, int> count;
+        for (auto c : s) ++count[c];
+        for (auto c : t) {
+            if (--count[c] < 0) return false;
+        }
+        return true;
+    }
+};
