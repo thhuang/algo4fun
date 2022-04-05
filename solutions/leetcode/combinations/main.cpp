bb57705de097e966814e71afe803aa66735e1af4
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 17:57:06 +0800

    [leetcode] combinations: optimize

--- solutions/leetcode/combinations/main.cpp
+++ solutions/leetcode/combinations/main.cpp
@@ -22,3 +22,29 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+
+        vector<int> combination;
+        function<void(int)> search = [&](int i) -> void {
+            if (combination.size() == k) {
+                result.push_back(combination);
+                return;
+            }
+
+            if (combination.size() + n - i + 1 < k) return;
+
+            search(i + 1);
+            combination.push_back(i);
+            search(i + 1);
+            combination.pop_back();
+        };
+
+        search(1);
+
+        return result;
+    }
+};
