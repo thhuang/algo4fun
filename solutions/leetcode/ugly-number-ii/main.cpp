dfe6feef9f005e847b52f28918462914713ee4f8
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 17:31:23 +0800

    [leetcode] ugly number ii: dp

--- solutions/leetcode/ugly-number-ii/main.cpp
+++ solutions/leetcode/ugly-number-ii/main.cpp
@@ -51,3 +51,21 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+   public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n);
+        dp[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for (int i = 1; i < n; ++i) {
+            int v = min({dp[p2] * 2, dp[p3] * 3, dp[p5] * 5});
+            dp[i] = v;
+            if (v == dp[p2] * 2) ++p2;
+            if (v == dp[p3] * 3) ++p3;
+            if (v == dp[p5] * 5) ++p5;
+        }
+
+        return dp.back();
+    }
+};
