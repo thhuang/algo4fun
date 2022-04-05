fb86390e2c47248fd7b9406ec57820606f74498e
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 09:29:07 +0800

    [leetcode] contains duplicate: sorting

--- solutions/leetcode/contains-duplicate/main.cpp
+++ solutions/leetcode/contains-duplicate/main.cpp
@@ -9,3 +9,14 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+   public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i - 1] == nums[i]) return true;
+        }
+        return false;
+    }
+};
