535679c34c80a707541b4c173ee3c25475b57bc8
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 16:44:17 +0800

    [leetcode] water and jug problem

---
+++ solutions/leetcode/water-and-jug-problem/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity,
+                         int targetCapacity) {
+        int n = jug1Capacity + jug2Capacity;
+        vector<bool> vis(n, false);
+        deque<int> q = {0};
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop_front();
+
+            if (i == targetCapacity) return true;
+            if (i < 0 || i >= n) continue;
+            if (vis[i]) continue;
+
+            vis[i] = true;
+
+            q.push_back(i + jug1Capacity);
+            q.push_back(i - jug1Capacity);
+            q.push_back(i + jug2Capacity);
+            q.push_back(i - jug2Capacity);
+        }
+
+        return false;
+    }
+};
