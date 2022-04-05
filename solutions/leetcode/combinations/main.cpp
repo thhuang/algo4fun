f144bc7ef4572d4d1323b1e2ab3610c568ba3992
TH Huang <tzhsn.huang@gmail.com>
2022-04-05 17:54:51 +0800

    [leetcode] combinations

---
+++ solutions/leetcode/combinations/main.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+
+        vector<int> combination;
+        function<void(int)> search = [&](int i) -> void {
+            if (combination.size() == k) {
+                result.push_back(combination);
+                return;
+            }
+
+            if (i > n) return;
+            search(i + 1);
+            combination.push_back(i);
+            search(i + 1);
+            combination.pop_back();
+        };
+
+        search(1);
+
+        return result;
+    }
+};
