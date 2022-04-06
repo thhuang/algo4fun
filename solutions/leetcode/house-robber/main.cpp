dad63f53aa4cd4142eee2564e7f2987618b9d21f
TH Huang <tzhsn.huang@gmail.com>
2022-04-06 17:52:07 +0800

    [leetcode] house robber: refactor

--- solutions/leetcode/house-robber/main.cpp
+++ solutions/leetcode/house-robber/main.cpp
@@ -1,14 +1,10 @@
 class Solution {
    public:
     int rob(vector<int>& nums) {
-        if (size(nums) == 0) return 0;
-        if (size(nums) == 1) return nums[0];
-
-        array<int, 2> dp = {0, nums[0]};
-        for (int i = 1; i < size(nums); ++i) {
+        array<int, 2> dp = {0, 0};
+        for (int i = 0; i < size(nums); ++i) {
             dp = {dp[1], max(dp[1], dp[0] + nums[i])};
         }
-
         return dp.back();
     }
 };
