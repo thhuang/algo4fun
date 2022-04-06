b54f47f5476c7406d52618a8b2752a5c0314ec97
TH Huang <tzhsn.huang@gmail.com>
2022-04-06 16:52:25 +0800

    [leetcode] pascals triangle ii: optimize

--- solutions/leetcode/pascals-triangle-ii/main.cpp
+++ solutions/leetcode/pascals-triangle-ii/main.cpp
@@ -13,3 +13,19 @@ class Solution {
         return row;
     }
 };
+
+class Solution {
+   public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> curr, prev = {1};
+        for (int i = 1; i <= rowIndex; ++i) {
+            curr.assign(i + 1, 0);
+            for (int j = 0; j < i + 1; ++j) {
+                if (j > 0) curr[j] += prev[j - 1];
+                if (j < i) curr[j] += prev[j];
+            }
+            prev = move(curr);
+        }
+        return prev;
+    }
+};
