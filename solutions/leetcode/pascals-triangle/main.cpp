c547403be02c986c7b706365689c324551bcbaca
TH Huang <tzhsn.huang@gmail.com>
2022-04-06 16:25:46 +0800

    [leetcode] pascals triangle: refactor

--- solutions/leetcode/pascals-triangle/main.cpp
+++ solutions/leetcode/pascals-triangle/main.cpp
@@ -3,11 +3,10 @@ class Solution {
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result = {{1}};
         for (int i = 1; i < numRows; ++i) {
-            result.push_back(vector<int>(i + 1));
-            for (int j = 0; j < result.back().size(); ++j) {
-                int a = j > 0 ? result[i - 1][j - 1] : 0;
-                int b = j < result[i - 1].size() ? result[i - 1][j] : 0;
-                result[i][j] = a + b;
+            result.push_back(vector<int>(i + 1, 0));
+            for (int j = 0; j < i + 1; ++j) {
+                if (j > 0) result[i][j] += result[i - 1][j - 1];
+                if (j < i) result[i][j] += result[i - 1][j];
             }
         }
         return result;
