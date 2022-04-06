01b7a00b32ac5e8198fd9cd72e490bac86d56260
TH Huang <tzhsn.huang@gmail.com>
2022-04-06 09:48:46 +0800

    [leetcode] path sum: refactor

--- solutions/leetcode/path-sum/main.cpp
+++ solutions/leetcode/path-sum/main.cpp
@@ -30,3 +30,13 @@ class Solution {
         return dfs(root);
     }
 };
+
+class Solution {
+   public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        if (!root->left && !root->right) return root->val == targetSum;
+        return hasPathSum(root->left, targetSum - root->val) ||
+               hasPathSum(root->right, targetSum - root->val);
+    }
+};
