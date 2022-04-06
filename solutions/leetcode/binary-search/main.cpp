6213dccfd2b706ff3b3a0247cf459e94869d1d91
TH Huang <tzhsn.huang@gmail.com>
2022-04-06 21:29:28 +0800

    [leetcode] binary search: refactor

--- solutions/leetcode/binary-search/main.cpp
+++ solutions/leetcode/binary-search/main.cpp
@@ -1,18 +1,15 @@
 class Solution {
    public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = size(nums) - 1;
+        int l = 0, r = nums.size() - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
             if (nums[m] < target) {
                 l = m + 1;
-                continue;
-            }
-            if (nums[m] > target) {
+            } else {
                 r = m - 1;
-                continue;
             }
-            return m;
         }
         return -1;
     }
