6e47fb555b71d5491f14c6bdfdd62308c9174ad0
TH Huang <tzhsn.huang@gmail.com>
2022-04-07 12:11:26 +0800

    [leetcode] find the town judge: optimize

--- solutions/leetcode/find-the-town-judge/main.cpp
+++ solutions/leetcode/find-the-town-judge/main.cpp
@@ -20,3 +20,18 @@ class Solution {
         return ~judge ? judge + 1 : -1;
     }
 };
+
+class Solution {
+   public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> count(n + 1);
+        for (auto p : trust) {
+            --count[p[0]];
+            ++count[p[1]];
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (count[i] == n - 1) return i;
+        }
+        return -1;
+    }
+};
