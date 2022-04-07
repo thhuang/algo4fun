4f48ca8b1e731f6c8968ca48fe2b9aa7362bb35f
TH Huang <tzhsn.huang@gmail.com>
2022-04-07 12:11:07 +0800

    [leetcode] find the town judge

---
+++ solutions/leetcode/find-the-town-judge/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<unordered_set<int>> graph(n);
+        vector<unordered_set<int>> rgraph(n);
+        for (auto edge : trust) {
+            --edge[0], --edge[1];
+            graph[edge[0]].insert(edge[1]);
+            rgraph[edge[1]].insert(edge[0]);
+        }
+
+        int judge = -1;
+        for (int i = 0; i < graph.size(); ++i) {
+            if (graph[i].size() > 0) continue;
+            if (~judge) return -1;
+            if (rgraph[i].size() != n - 1) return -1;
+            return i + 1;
+        }
+
+        return ~judge ? judge + 1 : -1;
+    }
+};
