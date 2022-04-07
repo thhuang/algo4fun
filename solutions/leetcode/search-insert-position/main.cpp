77e8e11e6e11106506a06002a3c448681cb997ef
TH Huang <tzhsn.huang@gmail.com>
2022-04-07 09:22:15 +0800

    [leetcode] search insert position: refactor

--- solutions/leetcode/search-insert-position/main.cpp
+++ solutions/leetcode/search-insert-position/main.cpp
@@ -17,3 +17,20 @@ class Solution {
         return l;
     }
 };
+
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        if (l > 0 && nums[l - 1] == target) return l - 1;
+        return l;
+    }
+};
