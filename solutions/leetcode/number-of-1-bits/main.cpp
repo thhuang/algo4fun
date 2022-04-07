f1bf4e8bee0c0269e50dc40e2a4bfddfffe8e625
TH Huang <tzhsn.huang@gmail.com>
2022-04-07 13:11:35 +0800

    [leetcode] number of 1 bits: refactor

--- solutions/leetcode/number-of-1-bits/main.cpp
+++ solutions/leetcode/number-of-1-bits/main.cpp
@@ -9,3 +9,12 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while (n) ++count, n &= (n - 1);
+        return count;
+    }
+};
