a2c90688dd9f5d23b027567e7bfcc3644dbe725b
TH Huang <tzhsn.huang@gmail.com>
2022-04-09 11:59:46 +0800

    [leetcode] unique paths ii

---
+++ solutions/leetcode/unique-paths-ii/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid.front().size();
+
+        vector<int> dp(m, 0);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                    continue;
+                }
+                if (j == 0) continue;
+                dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp.back();
+    }
+};
