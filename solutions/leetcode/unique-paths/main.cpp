8d91c1ae18f0cda0aaa1e3e919a62601f0742b52
TH Huang <tzhsn.huang@gmail.com>
2022-04-09 12:01:14 +0800

    [leetcode] unique paths: refactor

--- solutions/leetcode/unique-paths/main.cpp
+++ solutions/leetcode/unique-paths/main.cpp
@@ -17,8 +17,9 @@ class Solution {
 class Solution {
    public:
     int uniquePaths(int m, int n) {
-        vector<int> dp(n, 1);
-        for (int i = 1; i < m; ++i) {
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
                 dp[j] += dp[j - 1];
             }
