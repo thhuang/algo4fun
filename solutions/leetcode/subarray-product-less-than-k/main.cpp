a11b59019fafe8299555255ba30891b20a8b9145
TH Huang <tzhsn.huang@gmail.com>
2022-04-13 18:05:04 +0800

    [leetcode] subarray product less than k: refactor

--- solutions/leetcode/subarray-product-less-than-k/main.cpp
+++ solutions/leetcode/subarray-product-less-than-k/main.cpp
@@ -26,3 +26,20 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+
+        int product = 1;
+        int count = 0;
+        for (int l = 0, r = 0; r < nums.size(); ++r) {
+            product *= nums[r];
+            while (product >= k) product /= nums[l++];
+            count += r - l + 1;
+        }
+
+        return count;
+    }
+};
