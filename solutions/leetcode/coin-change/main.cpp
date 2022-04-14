6a7ff56cae210f35fcae000509c710cd369f92ed
TH Huang <tzhsn.huang@gmail.com>
2022-04-14 16:18:20 +0800

    [leetcode] coin change

---
+++ solutions/leetcode/coin-change/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        for (int i = 0; i < amount; ++i) {
+            if (dp[i] == -1) continue;
+            for (int c : coins) {
+                if (i > amount - c) continue;
+                if (dp[i + c] == -1)
+                    dp[i + c] = dp[i] + 1;
+                else
+                    dp[i + c] = min(dp[i + c], dp[i] + 1);
+            }
+        }
+        return dp.back();
+    }
+};
