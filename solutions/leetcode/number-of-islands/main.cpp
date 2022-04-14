87c4ac4ed893f6f00ce7b9e9782752f8530cf562
TH Huang <tzhsn.huang@gmail.com>
2022-04-14 10:32:36 +0800

    [leetcode] number of islands: with vis

--- solutions/leetcode/number-of-islands/main.cpp
+++ solutions/leetcode/number-of-islands/main.cpp
@@ -32,3 +32,33 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid.front().size();
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        function<void(int, int)> dfs = [&](int i, int j) -> void {
+            if (i < 0 || i >= n || j < 0 || j >= m) return;
+            if (grid[i][j] != '1') return;
+            if (vis[i][j]) return;
+            vis[i][j] = true;
+            for (auto [di, dj] : directions) dfs(i + di, j + dj);
+        };
+
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] != '1' || vis[i][j]) continue;
+                dfs(i, j);
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
