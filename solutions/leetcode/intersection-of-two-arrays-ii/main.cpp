cb18dcebd36102ce7423501bb68f5d67a76464ca
TH Huang <tzhsn.huang@gmail.com>
2022-04-15 13:09:26 +0800

    [leetcode] intersection of two arrays ii: refactor

--- solutions/leetcode/intersection-of-two-arrays-ii/main.cpp
+++ solutions/leetcode/intersection-of-two-arrays-ii/main.cpp
@@ -2,12 +2,10 @@ class Solution {
    public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
-        unordered_map<int, int> counts;
-        for (int v : nums1) ++counts[v];
+        unordered_map<int, int> num_count;
+        for (int v : nums1) ++num_count[v];
         for (int v : nums2) {
-            if (!counts[v]) continue;
-            --counts[v];
-            result.push_back(v);
+            if (--num_count[v] >= 0) result.push_back(v);
         }
         return result;
     }
