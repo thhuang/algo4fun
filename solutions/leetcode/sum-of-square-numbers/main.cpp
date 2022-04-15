e63f3aa484391d921d32a1ab4d5b463c40496b13
TH Huang <tzhsn.huang@gmail.com>
2022-04-15 13:25:59 +0800

    [leetcode] sum of square numbers: sqrt

--- solutions/leetcode/sum-of-square-numbers/main.cpp
+++ solutions/leetcode/sum-of-square-numbers/main.cpp
@@ -17,3 +17,14 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+   public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; ++a) {
+            long long b = sqrt(c - a * a);
+            if (a * a + b * b == c) return true;
+        }
+        return false;
+    }
+};
