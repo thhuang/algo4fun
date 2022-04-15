979ecabdc8a74b983a7e53a9ffd7331447e9e56a
TH Huang <tzhsn.huang@gmail.com>
2022-04-15 12:36:55 +0800

    [leetcode] populating next right pointers in each node ii: optimize space

--- solutions/leetcode/populating-next-right-pointers-in-each-node-ii/main.cpp
+++ solutions/leetcode/populating-next-right-pointers-in-each-node-ii/main.cpp
@@ -33,3 +33,56 @@ class Solution {
         return root;
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+   public:
+    array<Node*, 2> process_child(Node* prev_child, Node* curr_child,
+                                  Node* left_most_node) {
+        if (!curr_child) return {prev_child, left_most_node};
+        if (prev_child) {
+            prev_child->next = curr_child;
+        } else {
+            left_most_node = curr_child;
+        }
+        return {curr_child, left_most_node};
+    }
+
+    Node* connect(Node* root) {
+        auto left_most_node = root;
+
+        while (left_most_node) {
+            Node* p = left_most_node;
+            Node* prev_child = nullptr;
+            left_most_node = nullptr;
+
+            while (p) {
+                array<Node*, 2> res;
+                res = process_child(prev_child, p->left, left_most_node);
+                prev_child = res[0], left_most_node = res[1];
+                res = process_child(prev_child, p->right, left_most_node);
+                prev_child = res[0], left_most_node = res[1];
+                p = p->next;
+            }
+        }
+
+        return root;
+    }
+};
