9d517ae0eec83135f294debf185d8c9368d98bac
TH Huang <tzhsn.huang@gmail.com>
2022-04-15 15:47:10 +0800

    [leetcode] word pattern: split

--- solutions/leetcode/word-pattern/main.cpp
+++ solutions/leetcode/word-pattern/main.cpp
@@ -22,3 +22,31 @@ class Solution {
         return i == pattern.size();
     }
 };
+
+class Solution {
+   public:
+    bool wordPattern(string pattern, string s) {
+        stringstream ss(s);
+        string word;
+        vector<string> words;
+        while (ss >> word) words.push_back(word);
+
+        if (words.size() != pattern.size()) return false;
+
+        unordered_map<char, string> c2str;
+        unordered_map<string, char> str2c;
+        for (int i = 0; i < words.size(); ++i) {
+            char c = pattern[i];
+            string str = words[i];
+            if (c2str.count(c) != str2c.count(str)) return false;
+            if (c2str.count(c) == 0) {
+                c2str[c] = str;
+                str2c[str] = c;
+                continue;
+            }
+            if (c2str[c] != str || str2c[str] != c) return false;
+        }
+
+        return true;
+    }
+};
