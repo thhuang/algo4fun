4d483c568f9d6804dfe815a83c7dd8fb3479d5e0
TH Huang <tzhsn.huang@gmail.com>
2022-04-16 15:34:24 +0800

    [leetcode] search in rotated sorted array: refactor

--- solutions/leetcode/search-in-rotated-sorted-array/main.cpp
+++ solutions/leetcode/search-in-rotated-sorted-array/main.cpp
@@ -4,13 +4,11 @@ class Solution {
         int l = 0, r = nums.size() - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (nums[l] == target) return l;
             if (nums[m] == target) return m;
-            if (nums[r] == target) return r;
-            if (nums[l] < nums[m] && (nums[l] < target && target < nums[m])) {
+            if (nums[l] < nums[m] && (nums[l] <= target && target < nums[m])) {
                 r = m - 1;
             } else if (nums[l] > nums[m] &&
-                       (nums[l] < target || target < nums[m])) {
+                       (nums[l] <= target || target < nums[m])) {
                 r = m - 1;
             } else {
                 l = m + 1;
