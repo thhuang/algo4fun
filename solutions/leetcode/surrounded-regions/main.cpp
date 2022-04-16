c84f3786d089d6fc63fca8010b37481657bcf61d
TH Huang <tzhsn.huang@gmail.com>
2022-04-16 11:59:59 +0800

    [leetcode] surruonded regions: optimize

--- solutions/leetcode/surrounded-regions/main.cpp
+++ solutions/leetcode/surrounded-regions/main.cpp
@@ -6,26 +6,22 @@ class Solution {
         int n = board.size();
         int m = board.front().size();
 
-        function<void(int, int, char)> dfs = [&](int i, int j,
-                                                 char new_val) -> void {
+        function<void(int, int)> dfs = [&](int i, int j) -> void {
             if (i < 0 || i >= n || j < 0 || j >= m) return;
             if (board[i][j] != 'O') return;
-            board[i][j] = new_val;
-            for (auto [di, dj] : directions) dfs(i + di, j + dj, new_val);
+            board[i][j] = 'V';
+            for (auto [di, dj] : directions) dfs(i + di, j + dj);
         };
 
-        for (int i = 0; i < n; ++i) dfs(i, 0, 'V'), dfs(i, m - 1, 'V');
-        for (int j = 0; j < m; ++j) dfs(0, j, 'V'), dfs(n - 1, j, 'V');
-
-        for (int i = 1; i < n - 1; ++i) {
-            for (int j = 1; j < m - 1; ++j) {
-                dfs(i, j, 'X');
-            }
-        }
+        for (int i = 0; i < n; ++i) dfs(i, 0), dfs(i, m - 1);
+        for (int j = 0; j < m; ++j) dfs(0, j), dfs(n - 1, j);
 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
-                if (board[i][j] == 'V') board[i][j] = 'O';
+                if (board[i][j] == 'V')
+                    board[i][j] = 'O';
+                else
+                    board[i][j] = 'X';
             }
         }
     }
