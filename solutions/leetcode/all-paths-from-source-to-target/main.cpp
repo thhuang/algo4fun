72bbb20382c1a38954a54608e2345f06e3c2e3cc
TH Huang <tzhsn.huang@gmail.com>
2022-04-16 12:11:00 +0800

    [leetcode] all paths from source to target: refactor

--- solutions/leetcode/all-paths-from-source-to-target/main.cpp
+++ solutions/leetcode/all-paths-from-source-to-target/main.cpp
@@ -1,27 +1,24 @@
 class Solution {
    public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int n = size(graph);
-        if (n == 0) return {};
+        int n = graph.size();
+        vector<vector<int>> result;
 
-        vector<vector<int>> paths;
-        vector<int> curr_path = {0};
-
+        vector<int> path = {0};
         function<void(int)> dfs = [&](int u) -> void {
             if (u == n - 1) {
-                paths.push_back(curr_path);
+                result.push_back(path);
                 return;
             }
-
             for (int v : graph[u]) {
-                curr_path.push_back(v);
+                path.push_back(v);
                 dfs(v);
-                curr_path.pop_back();
+                path.pop_back();
             }
         };
 
         dfs(0);
 
-        return paths;
+        return result;
     }
 };
