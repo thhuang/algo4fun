fc3724847bb91fc0274067e2cbfbf497ee6dd73e
TH Huang <tzhsn.huang@gmail.com>
2022-04-16 14:36:29 +0800

    [leetcode] maximum distance between a pair of values

---
+++ solutions/leetcode/maximum-distance-between-a-pair-of-values/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int result = 0;
+        int i = 0, j = 0;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] <= nums2[j]) {
+                result = max(result, j - i);
+                ++j;
+            } else {
+                if (i == j) {
+                    ++i, ++j;
+                    continue;
+                }
+                ++i;
+            }
+        }
+        return result;
+    }
+};
