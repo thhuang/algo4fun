a07f8248d5a950e43f7104d5b16be93454ad499f
TH Huang <tzhsn.huang@gmail.com>
2022-04-17 17:53:10 +0800

    [leetcode] repeated dna sequences: rabin-karp

--- solutions/leetcode/repeated-dna-sequences/main.cpp
+++ solutions/leetcode/repeated-dna-sequences/main.cpp
@@ -20,3 +20,32 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    unordered_map<char, int> score = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+    int base = 4;
+
+   public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        if (s.size() < 10) return {};
+
+        int curr = 0;
+        int base9 = 1;
+        for (int i = 0; i < 10; ++i) {
+            if (i > 0) base9 *= base;
+            curr = curr * base + score[s[i]];
+        }
+
+        unordered_map<int, int> count;
+        ++count[curr];
+
+        vector<string> result;
+        for (int i = 10; i < s.size(); ++i) {
+            curr -= score[s[i - 10]] * base9;
+            curr = curr * base + score[s[i]];
+            if (++count[curr] == 2) result.push_back(s.substr(i - 9, 10));
+        }
+
+        return result;
+    }
+};
