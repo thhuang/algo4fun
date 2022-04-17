a5bc36604cecdfa88795b0a0ada8b30e9e1e7660
TH Huang <tzhsn.huang@gmail.com>
2022-04-17 20:44:36 +0800

    [leetcode] add two numbers

---
+++ solutions/leetcode/add-two-numbers/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+   public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy;
+        ListNode* p = &dummy;
+        int carry = 0;
+
+        while (l1 || l2) {
+            if (l1) carry += l1->val, l1 = l1->next;
+            if (l2) carry += l2->val, l2 = l2->next;
+            p->next = new ListNode(carry % 10);
+            p = p->next;
+            carry /= 10;
+        }
+        if (carry) p->next = new ListNode(carry);
+
+        return dummy.next;
+    }
+};
