b1785f7c54e4fa084cf3e044342c8d141d53219d
TH Huang <tzhsn.huang@gmail.com>
2022-04-19 20:51:06 +0800

    [leetcode] palindrome number

---
+++ solutions/leetcode/palindrome-number/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int v = x;
+        int left_pos = 1;
+        int count = 1;
+        while (v /= 10) left_pos *= 10, ++count;
+
+        int right_pos = 1;
+        for (int i = 0; i < count / 2; ++i, left_pos /= 10, right_pos *= 10) {
+            int left = (x / left_pos) % 10;
+            int right = (x / right_pos) % 10;
+            if (left != right) return false;
+        }
+
+        return true;
+    }
+};
