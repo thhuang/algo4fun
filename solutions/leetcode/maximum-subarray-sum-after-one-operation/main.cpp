f09c75ed02e7dd567d6ad5300b0348a6d8b6f9da
TH Huang <tzhsn.huang@gmail.com>
2022-04-21 12:22:48 +0800

    [leetcode] maximum subarray sum after one operation

---
+++ solutions/leetcode/maximum-subarray-sum-after-one-operation/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    int maxSumAfterOperation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> left_sum = nums, right_sum = nums;
+        for (int i = 1; i < n; ++i) {
+            left_sum[i] += max(0, left_sum[i - 1]);
+            right_sum[n - 1 - i] += max(0, right_sum[n - i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            int v = nums[i] * nums[i];
+            if (i - 1 >= 0) v += max(0, left_sum[i - 1]);
+            if (i + 1 <= n - 1) v += max(0, right_sum[i + 1]);
+
+            result = max(result, v);
+        }
+
+        return result;
+    }
+};
