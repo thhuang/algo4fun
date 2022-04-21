66d115cba5606d8437fc315fe872a339a12d602c
TH Huang <tzhsn.huang@gmail.com>
2022-04-21 19:14:35 +0800

    [leetcode] find the winner of the circular game: josephus

--- solutions/leetcode/find-the-winner-of-the-circular-game/main.cpp
+++ solutions/leetcode/find-the-winner-of-the-circular-game/main.cpp
@@ -17,3 +17,14 @@ class Solution {
         return friends[0];
     }
 };
+
+// Josephus problem
+class Solution {
+    int solve(int n, int k) {
+        if (n == 1) return 0;
+        return (solve(n - 1, k) + k) % n;
+    };
+
+   public:
+    int findTheWinner(int n, int k) { return solve(n, k) + 1; }
+};
