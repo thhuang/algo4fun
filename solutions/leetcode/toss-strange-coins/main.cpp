db5b379a6db794b41566df379a8e4654958197e9
TH Huang <tzhsn.huang@gmail.com>
2022-04-21 11:14:43 +0800

    [leetcode] toss strange coins

---
+++ solutions/leetcode/toss-strange-coins/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    double probabilityOfHeads(vector<double>& prob, int target) {
+        int n = prob.size();
+        vector<vector<double>> dp(n + 1, vector<double>(target + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int t = 0; t <= target && t <= i; ++t) {
+                dp[i][t] += dp[i - 1][t] * (1 - prob[i - 1]);
+                if (t - 1 >= 0) dp[i][t] += dp[i - 1][t - 1] * prob[i - 1];
+            }
+        }
+        return dp.back().back();
+    }
+};
