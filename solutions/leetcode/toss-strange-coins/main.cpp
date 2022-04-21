aadac3a21f2da4020ab253bd3daf35072c3ba491
TH Huang <tzhsn.huang@gmail.com>
2022-04-21 11:18:10 +0800

    [leetcode] toss strange coins: optimize

--- solutions/leetcode/toss-strange-coins/main.cpp
+++ solutions/leetcode/toss-strange-coins/main.cpp
@@ -13,3 +13,21 @@ class Solution {
         return dp.back().back();
     }
 };
+
+class Solution {
+   public:
+    double probabilityOfHeads(vector<double>& prob, int target) {
+        int n = prob.size();
+        vector<double> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            vector<double> new_dp(target + 1, 0);
+            for (int t = 0; t <= target && t <= i; ++t) {
+                new_dp[t] += dp[t] * (1 - prob[i - 1]);
+                if (t - 1 >= 0) new_dp[t] += dp[t - 1] * prob[i - 1];
+            }
+            dp = move(new_dp);
+        }
+        return dp.back();
+    }
+};
