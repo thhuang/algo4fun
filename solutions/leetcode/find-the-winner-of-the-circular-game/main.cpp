a8f2e2f0b79c86316fcb1ed733b62876cdc8ab0c
TH Huang <tzhsn.huang@gmail.com>
2022-04-21 19:16:31 +0800

    [leetcode] find the winner of the circular game: optimize

--- solutions/leetcode/find-the-winner-of-the-circular-game/main.cpp
+++ solutions/leetcode/find-the-winner-of-the-circular-game/main.cpp
@@ -28,3 +28,14 @@ class Solution {
    public:
     int findTheWinner(int n, int k) { return solve(n, k) + 1; }
 };
+
+class Solution {
+   public:
+    int findTheWinner(int n, int k) {
+        int result = 0;
+        for (int i = 1; i <= n; ++i) {
+            result = (result + k) % i;
+        }
+        return result + 1;
+    }
+};
