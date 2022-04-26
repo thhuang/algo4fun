3e82833d7cbea34efb992d3636c6fcfd8b66f048
TH Huang <tzhsn.huang@gmail.com>
2022-04-26 13:11:42 +0800

    [leetcode] edit distance: refactor

--- solutions/leetcode/edit-distance/main.cpp
+++ solutions/leetcode/edit-distance/main.cpp
@@ -1,18 +1,20 @@
 class Solution {
    public:
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
+        vector<vector<int>> dp(word1.size() + 1,
+                               vector<int>(word2.size() + 1, 0));
+        for (int i = 0; i <= word1.size(); ++i) dp[i][0] = i;
+        for (int j = 0; j <= word2.size(); ++j) dp[0][j] = j;
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int i = 1; i <= n; ++i) dp[i][0] = i;
-        for (int j = 1; j <= m; ++j) dp[0][j] = j;
-
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                dp[i][j] = min(
-                    {dp[i - 1][j - 1] + (word1[i - 1] == word2[j - 1] ? 0 : 1),
-                     dp[i][j - 1] + 1, dp[i - 1][j] + 1});
+        for (int i = 1; i <= word1.size(); ++i) {
+            for (int j = 1; j <= word2.size(); ++j) {
+                char c1 = word1[i - 1], c2 = word2[j - 1];
+                if (c1 == c2) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] =
+                        1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+                }
             }
         }
 
