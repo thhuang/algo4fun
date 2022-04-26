989ab5ae660bfd066ebcfe5e9c34b3c425e70906
TH Huang <tzhsn.huang@gmail.com>
2022-04-26 14:16:02 +0800

    [leetcode] coin change: backward search

--- solutions/leetcode/coin-change/main.cpp
+++ solutions/leetcode/coin-change/main.cpp
@@ -16,3 +16,25 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; ++i) {
+            for (int c : coins) {
+                if (i < c) continue;
+                if (dp[i - c] == -1) continue;
+                if (dp[i] == -1) {
+                    dp[i] = dp[i - c] + 1;
+                } else {
+                    dp[i] = min(dp[i], dp[i - c] + 1);
+                }
+            }
+        }
+
+        return dp.back();
+    }
+};
