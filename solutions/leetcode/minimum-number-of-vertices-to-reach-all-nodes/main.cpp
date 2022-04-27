9f34a413bace4bd7af820cdb352178fafae4d976
TH Huang <tzhsn.huang@gmail.com>
2022-04-27 14:15:29 +0800

    [leetcode] minimum number of vertices to reach all nodes: optimize

--- solutions/leetcode/minimum-number-of-vertices-to-reach-all-nodes/main.cpp
+++ solutions/leetcode/minimum-number-of-vertices-to-reach-all-nodes/main.cpp
@@ -13,3 +13,16 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<bool> is_root(n, true);
+        for (auto e : edges) is_root[e[1]] = false;
+        vector<int> result;
+        for (int i = 0; i < n; ++i) {
+            if (is_root[i]) result.push_back(i);
+        }
+        return result;
+    }
+};
