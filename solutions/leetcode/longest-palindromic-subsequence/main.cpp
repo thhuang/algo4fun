9c486b95bc22ff012f3e88927dba9d016f9c552b
TH Huang <tzhsn.huang@gmail.com>
2022-04-27 15:32:13 +0800

    [leetcode] longest palindromic subsequence: refactor

--- solutions/leetcode/longest-palindromic-subsequence/main.cpp
+++ solutions/leetcode/longest-palindromic-subsequence/main.cpp
@@ -25,3 +25,27 @@ class Solution {
         return dp[0][n - 1];
     }
 };
+
+class Solution {
+   public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.size();
+        if (n <= 1) return n;
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i) dp[i][i] = 1;
+        for (int i = 0; i + 1 < n; ++i) dp[i][i + 1] = s[i] == s[i + 1] ? 2 : 1;
+        for (int d = 3; d <= n; ++d) {
+            for (int l = 0; l + d - 1 < n; ++l) {
+                int r = l + d - 1;
+                if (s[l] == s[r]) {
+                    dp[l][r] = dp[l + 1][r - 1] + 2;
+                } else {
+                    dp[l][r] = max(dp[l][r - 1], dp[l + 1][r]);
+                }
+            }
+        }
+
+        return dp.front().back();
+    }
+};
