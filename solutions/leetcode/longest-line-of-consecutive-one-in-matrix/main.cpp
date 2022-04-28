4b701fb707df44516ef1375274ef10fd7fd36aa9
TH Huang <tzhsn.huang@gmail.com>
2022-04-28 14:17:17 +0800

    [leetcode] longest line of consecutive one in matrix: optimize

--- solutions/leetcode/longest-line-of-consecutive-one-in-matrix/main.cpp
+++ solutions/leetcode/longest-line-of-consecutive-one-in-matrix/main.cpp
@@ -48,3 +48,55 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int longestLine(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat.front().size();
+
+        int result = 0;
+
+        for (int i = 0, length; i < n; ++i) {
+            length = 0;
+            for (int j = 0; j < m; ++j) {
+                if (mat[i][j] == 0) {
+                    length = 0;
+                } else {
+                    result = max(result, ++length);
+                }
+            }
+        }
+
+        for (int j = 0, length; j < m; ++j) {
+            length = 0;
+            for (int i = 0; i < n; ++i) {
+                if (mat[i][j] == 0) {
+                    length = 0;
+                } else {
+                    result = max(result, ++length);
+                }
+            }
+        }
+
+        vector<int> diag(m, 0), adiag(m, 0);
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_diag(m, 0), new_adiag(m, 0);
+            for (int j = 0; j < m; ++j) {
+                if (mat[i][j] == 0) continue;
+                new_diag[j] = 1;
+                if (j - 1 >= 0) new_diag[j] += diag[j - 1];
+                result = max(result, new_diag[j]);
+            }
+            for (int j = m - 1; ~j; --j) {
+                if (mat[i][j] == 0) continue;
+                new_adiag[j] = 1;
+                if (j + 1 < m) new_adiag[j] += adiag[j + 1];
+                result = max(result, new_adiag[j]);
+            }
+            diag = move(new_diag), adiag = move(new_adiag);
+        }
+
+        return result;
+    }
+};
