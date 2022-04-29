2402585c47c23f5f00b87566fd9127d9f8cc9b82
TH Huang <tzhsn.huang@gmail.com>
2022-04-29 18:12:43 +0800

    [leetcode] max points on a line: optimize

--- solutions/leetcode/max-points-on-a-line/main.cpp
+++ solutions/leetcode/max-points-on-a-line/main.cpp
@@ -21,3 +21,45 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    int gcd(int a, int b) {
+        if (a < b) swap(a, b);
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+   public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        sort(points.begin(), points.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[0] == b[0]) return a[1] < b[1];
+                 return a[0] < b[0];
+             });
+
+        // (y1 - y0) * x - (x1 - x0) * y + c = 0
+        int result = 1;
+        unordered_map<
+            int, unordered_map<int, unordered_map<int, unordered_set<int>>>>
+            line_points;
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int x0 = points[i][0], y0 = points[i][1];
+                int x1 = points[j][0], y1 = points[j][1];
+                int dx = x1 - x0, dy = y1 - y0;
+
+                int v = gcd(abs(dx), abs(dy));
+                int a = v ? dy / v : dy;
+                int b = v ? dx / v : dx;
+                int c = a * points[i][0] - b * points[i][1];
+                line_points[a][b][c].insert(i);
+                line_points[a][b][c].insert(j);
+                result = max(result, (int)line_points[a][b][c].size());
+            }
+        }
+
+        return result;
+    }
+};
