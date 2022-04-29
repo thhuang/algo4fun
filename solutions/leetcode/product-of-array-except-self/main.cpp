8942bb0aa5f71a9dd08abed9ce5f81ecf4b204c4
TH Huang <tzhsn.huang@gmail.com>
2022-04-29 19:06:15 +0800

    [leetcode] product of array except self: refactor

--- solutions/leetcode/product-of-array-except-self/main.cpp
+++ solutions/leetcode/product-of-array-except-self/main.cpp
@@ -19,3 +19,22 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> products(nums.size());
+        for (int i = nums.size() - 1, p = 1; ~i; --i) {
+            products[i] = p *= nums[i];
+        }
+
+        vector<int> result(nums.size());
+        for (int i = 0, p = 1; i < nums.size(); ++i) {
+            result[i] = p;
+            if (i + 1 < nums.size()) result[i] *= products[i + 1];
+            p *= nums[i];
+        }
+
+        return result;
+    }
+};
