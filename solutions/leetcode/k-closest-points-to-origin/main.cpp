5fd586647fb2ebab80305ef25bc775b3ce10113a
TH Huang <tzhsn.huang@gmail.com>
2022-04-29 15:02:01 +0800

    [leetcode] k closest points to origin: optimize

--- solutions/leetcode/k-closest-points-to-origin/main.cpp
+++ solutions/leetcode/k-closest-points-to-origin/main.cpp
@@ -9,3 +9,51 @@ class Solution {
         return {result.begin(), result.begin() + k};
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        if (k == points.size()) return points;
+
+        auto result = points;
+        auto partition = [&result](int l, int r, int pivot_index) -> int {
+            auto pivot_point = result[pivot_index];
+            int pivot_value = pivot_point[0] * pivot_point[0] +
+                              pivot_point[1] * pivot_point[1];
+
+            swap(result[pivot_index], result[r]);
+
+            int p = l, pl = l, pr = r - 1;
+            while (pl <= pr) {
+                int dist =
+                    result[p][0] * result[p][0] + result[p][1] * result[p][1];
+                if (dist < pivot_value) {
+                    swap(result[p++], result[pl++]);
+                } else {  // dist >= pivot_value
+                    swap(result[p], result[pr--]);
+                }
+            }
+
+            swap(result[pl], result[r]);
+
+            return pl;
+        };
+
+        random_device rd;
+        default_random_engine gen(rd());
+        int l = 0, r = points.size() - 1;
+        while (l < r) {
+            int pivot_index = uniform_int_distribution<int>(l, r)(gen);
+            pivot_index = partition(l, r, pivot_index);
+
+            if (pivot_index == k) break;
+            if (pivot_index < k) {
+                l = pivot_index + 1;
+            } else {  // pivot_index > k
+                r = pivot_index - 1;
+            }
+        }
+
+        return {result.begin(), result.begin() + k};
+    }
+};
