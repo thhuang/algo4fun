b389bb9a1cc2b237bbaaa8a67c24baf2a09f1c4f
TH Huang <tzhsn.huang@gmail.com>
2022-04-29 11:30:05 +0800

    [leetcode] is graph bipartite: refactor

--- solutions/leetcode/is-graph-bipartite/main.cpp
+++ solutions/leetcode/is-graph-bipartite/main.cpp
@@ -18,3 +18,32 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> group(n, 0);
+
+        function<bool(int, int)> dfs = [&](int u, int expected_group) -> bool {
+            if (group[u]) return group[u] == expected_group;
+            group[u] = expected_group;
+
+            int opposite_group = 1;
+            if (expected_group == 1) opposite_group = 2;
+
+            for (int v : graph[u]) {
+                if (!dfs(v, opposite_group)) return false;
+            }
+
+            return true;
+        };
+
+        for (int i = 0; i < n; ++i) {
+            if (group[i]) continue;
+            if (!dfs(i, 1)) return false;
+        }
+
+        return true;
+    }
+};
