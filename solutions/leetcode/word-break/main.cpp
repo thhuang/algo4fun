ce35e47b995f5a66c9532f54d7265770f5bb0060
TH Huang <tzhsn.huang@gmail.com>
2022-04-30 12:19:28 +0800

    [leetcode] evaluate division: while loop

--- solutions/leetcode/word-break/main.cpp
+++ solutions/leetcode/word-break/main.cpp
@@ -23,3 +23,30 @@ class Solution {
         return valid.back();
     }
 };
+
+class Solution {
+   public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> words;
+        for (string w : wordDict) words.insert(w);
+
+        vector<bool> valid(s.size(), false);
+        for (int r = 0; r < s.size(); ++r) {
+            int l = 0;
+            if (words.count(s.substr(l, r - l + 1))) {
+                valid[r] = true;
+                continue;
+            }
+
+            while (++l <= r) {
+                if (!valid[l - 1]) continue;
+                if (words.count(s.substr(l, r - l + 1))) {
+                    valid[r] = true;
+                    break;
+                }
+            }
+        }
+
+        return valid.back();
+    }
+};
