55a2d33c137b99105eba24fcd96b327b4f54d0ec
TH Huang <tzhsn.huang@gmail.com>
2022-04-30 14:10:16 +0800

    [leetcode] factor combinations: optimize

--- solutions/leetcode/factor-combinations/main.cpp
+++ solutions/leetcode/factor-combinations/main.cpp
@@ -23,3 +23,28 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int>> result;
+        vector<int> factors;
+
+        function<void(int, int)> search = [&](int l, int r) -> void {
+            for (int i = l; i * i <= r; ++i) {
+                if (r % i != 0) continue;
+                factors.push_back(i);
+
+                result.push_back(factors);
+                result.back().push_back(r / i);
+
+                search(i, r / i);
+                factors.pop_back();
+            }
+        };
+
+        search(2, n);
+
+        return result;
+    }
+};
