715cd35d48ef4fa313c82f251a339876e3a52be7
TH Huang <tzhsn.huang@gmail.com>
2022-05-01 12:34:58 +0800

    [leetcode] count prefixes of a given string: refactor

--- solutions/leetcode/count-prefixes-of-a-given-string/main.cpp
+++ solutions/leetcode/count-prefixes-of-a-given-string/main.cpp
@@ -14,3 +14,30 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int countPrefixes(vector<string>& words, string s) {
+        unordered_map<string, int> valid;
+        for (string w : words) ++valid[w];
+
+        int count = 0;
+        string prefix;
+        for (int i = 0; i < s.size(); ++i) {
+            count += valid[prefix += s[i]];
+        }
+
+        return count;
+    }
+};
+
+class Solution {
+   public:
+    int countPrefixes(vector<string>& words, string s) {
+        int count = 0;
+        for (string w : words) {
+            if (s.substr(0, w.size()) == w) ++count;
+        }
+        return count;
+    }
+};
