59dedca958f0a2302f5e10a175fc420a923009db
TH Huang <tzhsn.huang@gmail.com>
2022-05-02 12:03:58 +0800

    [leetcode] 2 keys keyboard: optimize

--- solutions/leetcode/2-keys-keyboard/main.cpp
+++ solutions/leetcode/2-keys-keyboard/main.cpp
@@ -12,3 +12,17 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int minSteps(int n) {
+        int result = 0;
+        for (int i = 2; i <= n; ++i) {
+            while (n % i == 0) {
+                result += i;
+                n /= i;
+            }
+        }
+        return result;
+    }
+};
