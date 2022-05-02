5da0de0308eca7e5b4c027423cd74e293183efea
TH Huang <tzhsn.huang@gmail.com>
2022-05-02 11:43:42 +0800

    [leetcode] out of boundary paths: optimize

--- solutions/leetcode/out-of-boundary-paths/main.cpp
+++ solutions/leetcode/out-of-boundary-paths/main.cpp
@@ -25,3 +25,32 @@ class Solution {
         return dp[maxMove][startRow][startColumn];
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    const int mod = 1e9 + 7;
+
+   public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int mv = 1; mv <= maxMove; ++mv) {
+            vector<vector<int>> new_dp(m, vector<int>(n, 0));
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    for (auto [di, dj] : directions) {
+                        int ii = i + di;
+                        int jj = j + dj;
+                        if (ii < 0 || ii >= m || jj < 0 || jj >= n) {
+                            ++new_dp[i][j];
+                        } else {
+                            new_dp[i][j] += dp[ii][jj];
+                        }
+                        new_dp[i][j] %= mod;
+                    }
+                }
+            }
+            dp = move(new_dp);
+        }
+        return dp[startRow][startColumn];
+    }
+};
