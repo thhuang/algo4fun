bfa07916e00c46cefb83a9ba21d9710bf90fb9f5
TH Huang <tzhsn.huang@gmail.com>
2022-05-02 11:56:17 +0800

    [leetcode] 2 keys keyboard

---
+++ solutions/leetcode/2-keys-keyboard/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    int minSteps(int n) {
+        vector<int> dp(n + 1);
+        iota(dp.begin(), dp.end(), 0);
+        dp[1] = 0;
+        for (int i = 2; i < n / 2; ++i) {
+            for (int j = i + i, steps = dp[i] + 2; j <= n; j += i, ++steps) {
+                dp[j] = min(dp[j], steps);
+            }
+        }
+        return dp.back();
+    }
+};
