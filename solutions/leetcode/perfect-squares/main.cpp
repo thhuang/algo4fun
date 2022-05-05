2ef496e7ab964b22d9c0e1c4185c4711a193cd3a
TH Huang <tzhsn.huang@gmail.com>
2022-05-05 12:50:07 +0800

    [leetcode] perfect squares: alternative

--- solutions/leetcode/perfect-squares/main.cpp
+++ solutions/leetcode/perfect-squares/main.cpp
@@ -12,3 +12,17 @@ class Solution {
         return dp[n];
     }
 };
+
+class Solution {
+   public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, numeric_limits<int>::max());
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; i - j * j >= 0; ++j) {
+                dp[i] = min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp.back();
+    }
+};
