e0588a38a35f0d9a56ce25addc94288ff06ada6d
TH Huang <tzhsn.huang@gmail.com>
2022-05-05 13:03:43 +0800

    [leetcode] perfect squares: optimize

--- solutions/leetcode/perfect-squares/main.cpp
+++ solutions/leetcode/perfect-squares/main.cpp
@@ -26,3 +26,25 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int numSquares(int n) {
+        unordered_set<int> nums;
+        for (int i = 1; i * i <= n; ++i) nums.insert(i * i);
+
+        function<bool(int, int)> search = [&](int target, int count) -> bool {
+            if (count == 1) return nums.count(target);
+            for (int v : nums) {
+                if (v > target) continue;
+                if (search(target - v, count - 1)) return true;
+            }
+            return false;
+        };
+
+        for (int i = 1; i < n; ++i) {
+            if (search(n, i)) return i;
+        }
+        return n;
+    }
+};
