d2d10df42f7a7135ea0ea6daab62b629cff2ea77
TH Huang <tzhsn.huang@gmail.com>
2022-05-06 12:42:13 +0800

    [leetcode] partition equal subset sum: optimize

--- solutions/leetcode/partition-equal-subset-sum/main.cpp
+++ solutions/leetcode/partition-equal-subset-sum/main.cpp
@@ -20,3 +20,27 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    bool canPartition(vector<int>& nums) {
+        int total = 0;
+        for (int v : nums) total += v;
+
+        if (total & 1) return false;
+
+        int target = total / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int v : nums) {
+            if (target - v >= 0 && dp[target - v]) return true;
+            for (int i = dp.size() - 1; ~i; --i) {
+                if (i - v < 0) continue;
+                dp[i] = dp[i] || dp[i - v];
+            }
+        }
+
+        return false;
+    }
+};
