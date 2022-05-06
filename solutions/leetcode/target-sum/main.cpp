208140ea14a9293da9a9120048bdb43456ec9ad2
TH Huang <tzhsn.huang@gmail.com>
2022-05-06 13:04:24 +0800

    [leetcode] target sum: refactor

--- solutions/leetcode/target-sum/main.cpp
+++ solutions/leetcode/target-sum/main.cpp
@@ -6,17 +6,17 @@ class Solution {
 
         if (target < -total || total < target) return 0;
 
-        unordered_map<int, int> sum_ways = {{0, 1}};
+        unordered_map<int, int> dp = {{0, 1}};
         for (int v : nums) {
-            unordered_map<int, int> new_sum_ways;
-            for (auto [s, w] : sum_ways) {
-                new_sum_ways[s + v] += sum_ways[s];
-                new_sum_ways[s - v] += sum_ways[s];
+            unordered_map<int, int> new_dp;
+            for (auto [sum, count] : dp) {
+                new_dp[sum + v] += count;
+                new_dp[sum - v] += count;
             }
-            sum_ways = move(new_sum_ways);
+            dp = move(new_dp);
         }
 
-        return sum_ways[target];
+        return dp[target];
     }
 };
 
