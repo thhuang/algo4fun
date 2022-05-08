4ca0ebe15df73dec262a9b9ce8df4a4b78b981bc
TH Huang <tzhsn.huang@gmail.com>
2022-05-08 16:59:38 +0800

    [leetcode] shortest distance to target color: alternative

--- solutions/leetcode/shortest-distance-to-target-color/main.cpp
+++ solutions/leetcode/shortest-distance-to-target-color/main.cpp
@@ -26,3 +26,47 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> shortestDistanceColor(vector<int>& colors,
+                                      vector<vector<int>>& queries) {
+        int n = colors.size();
+        vector<vector<int>> color_dist(n, vector<int>(3, -1));
+
+        color_dist.front()[colors.front() - 1] = 0;
+        for (int i = 1; i < n; ++i) {
+            int ci = colors[i] - 1;
+            color_dist[i][ci] = 0;
+            for (int j = 0; j < 3; ++j) {
+                if (color_dist[i - 1][j] == -1) continue;
+                if (color_dist[i][j] == -1)
+                    color_dist[i][j] = color_dist[i - 1][j] + 1;
+                else
+                    color_dist[i][j] =
+                        min(color_dist[i][j], color_dist[i - 1][j] + 1);
+            }
+        }
+
+        color_dist.back()[colors.back() - 1] = 0;
+        for (int i = n - 2; ~i; --i) {
+            int ci = colors[i] - 1;
+            color_dist[i][ci] = 0;
+            for (int j = 0; j < 3; ++j) {
+                if (color_dist[i + 1][j] == -1) continue;
+                if (color_dist[i][j] == -1)
+                    color_dist[i][j] = color_dist[i + 1][j] + 1;
+                else
+                    color_dist[i][j] =
+                        min(color_dist[i][j], color_dist[i + 1][j] + 1);
+            }
+        }
+
+        vector<int> result(queries.size());
+        for (int i = 0; i < queries.size(); ++i) {
+            result[i] = color_dist[queries[i][0]][queries[i][1] - 1];
+        }
+
+        return result;
+    }
+};
