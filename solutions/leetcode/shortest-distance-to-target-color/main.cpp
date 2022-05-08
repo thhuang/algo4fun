d7f6dbb585093a57a881cbf8fb03e80950c83959
TH Huang <tzhsn.huang@gmail.com>
2022-05-08 17:08:27 +0800

    [leetcode] shortest distance to target color: refactor

--- solutions/leetcode/shortest-distance-to-target-color/main.cpp
+++ solutions/leetcode/shortest-distance-to-target-color/main.cpp
@@ -3,24 +3,23 @@ class Solution {
     vector<int> shortestDistanceColor(vector<int>& colors,
                                       vector<vector<int>>& queries) {
         int n = colors.size();
-        vector<vector<int>> color_dist(3, vector<int>(n));
+        vector<vector<int>> color_dist(3, vector<int>(n, n));
+
         for (int c = 0; c < 3; ++c) {
-            int color = c + 1;
             for (int i = 0, d = n; i < n; ++i) {
-                color_dist[c][i] = ++d;
-                if (colors[i] == color) color_dist[c][i] = d = 0;
+                color_dist[c][i] = min(color_dist[c][i], ++d);
+                if (c == colors[i] - 1) color_dist[c][i] = d = 0;
             }
             for (int i = n - 1, d = n; ~i; --i) {
                 color_dist[c][i] = min(color_dist[c][i], ++d);
-                if (colors[i] == color) color_dist[c][i] = d = 0;
+                if (c == colors[i] - 1) color_dist[c][i] = d = 0;
             }
         }
 
-        vector<int> result;
-        for (const auto& q : queries) {
-            int i = q[0], c = q[1] - 1;
-            result.emplace_back(color_dist[c][i]);
-            if (result.back() >= n) result.back() = -1;
+        vector<int> result(queries.size());
+        for (int i = 0; i < queries.size(); ++i) {
+            result[i] = color_dist[queries[i][1] - 1][queries[i][0]];
+            if (result[i] >= n) result[i] = -1;
         }
 
         return result;
