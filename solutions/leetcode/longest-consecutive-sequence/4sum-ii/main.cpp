01ba8ada6d151a5b079d8a314a6f02ad56e86b11
TH Huang <tzhsn.huang@gmail.com>
2022-05-09 12:44:29 +0800

    [leetcode] 4 sum ii: optimize

--- solutions/leetcode/longest-consecutive-sequence/4sum-ii/main.cpp
+++ solutions/leetcode/longest-consecutive-sequence/4sum-ii/main.cpp
@@ -24,3 +24,25 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3,
+                     vector<int>& nums4) {
+        unordered_map<int, int> sum_count;
+        for (int a : nums1) {
+            for (int b : nums2) {
+                ++sum_count[a + b];
+            }
+        }
+
+        int count = 0;
+        for (int a : nums3) {
+            for (int b : nums4) {
+                count += sum_count[-(a + b)];
+            }
+        }
+
+        return count;
+    }
+};
