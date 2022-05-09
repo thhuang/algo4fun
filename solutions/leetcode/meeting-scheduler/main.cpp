2475223653e755eebd134148d46e619dfc05f283
TH Huang <tzhsn.huang@gmail.com>
2022-05-09 13:22:16 +0800

    [leetcode] meeting scheduler: optimize

--- solutions/leetcode/meeting-scheduler/main.cpp
+++ solutions/leetcode/meeting-scheduler/main.cpp
@@ -3,15 +3,18 @@ class Solution {
     vector<int> minAvailableDuration(vector<vector<int>>& slots1,
                                      vector<vector<int>>& slots2,
                                      int duration) {
-        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> q;
-        for (const auto& s : slots1) q.push({s[0], 1}), q.push({s[1], -1});
-        for (const auto& s : slots2) q.push({s[0], 1}), q.push({s[1], -1});
+        vector<array<int, 2>> q;
+        for (const auto& s : slots1)
+            q.push_back({s[0], 1}), q.push_back({s[1], -1});
+        for (const auto& s : slots2)
+            q.push_back({s[0], 1}), q.push_back({s[1], -1});
+        sort(q.begin(), q.end(), greater<>());
 
         int available_count = 0;
         int start = numeric_limits<int>::max();
         while (!q.empty()) {
-            auto [t, c] = q.top();
-            q.pop();
+            auto [t, c] = q.back();
+            q.pop_back();
 
             if (available_count == 2 && t - start >= duration) {
                 return {start, start + duration};
