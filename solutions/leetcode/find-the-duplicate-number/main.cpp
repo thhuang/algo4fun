bc9e9c3ea7abe4a90fc7d3f0cc392f7fb290445f
TH Huang <tzhsn.huang@gmail.com>
2022-05-09 15:27:04 +0800

    [leetcode] find the duplicate number: refactor

--- solutions/leetcode/find-the-duplicate-number/main.cpp
+++ solutions/leetcode/find-the-duplicate-number/main.cpp
@@ -1,20 +1,6 @@
 class Solution {
    public:
     int findDuplicate(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            while (i + 1 != nums[i]) {
-                int j = nums[i] - 1;
-                if (nums[i] == nums[j]) return nums[i];
-                swap(nums[i], nums[j]);
-            }
-        }
-        return 0;
-    }
-};
-
-class Solution {
-   public:
-    int findDuplicate(vector<int>& nums) {
         while (nums[0] != nums[nums[0]]) {
             swap(nums[0], nums[nums[0]]);
         }
