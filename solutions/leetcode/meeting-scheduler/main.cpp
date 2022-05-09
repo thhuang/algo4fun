d26f85789743287152ee04973e5d2b508f1c94d5
TH Huang <tzhsn.huang@gmail.com>
2022-05-09 13:41:07 +0800

    [leetcode] meeting scheduler: optimize

--- solutions/leetcode/meeting-scheduler/main.cpp
+++ solutions/leetcode/meeting-scheduler/main.cpp
@@ -27,3 +27,32 @@ class Solution {
         return {};
     }
 };
+
+class Solution {
+   public:
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1,
+                                     vector<vector<int>>& slots2,
+                                     int duration) {
+        sort(slots1.begin(), slots1.end());
+        sort(slots2.begin(), slots2.end());
+
+        int p1 = 0, p2 = 0;
+        while (p1 < slots1.size() && p2 < slots2.size()) {
+            auto& s1 = slots1[p1];
+            auto& s2 = slots2[p2];
+
+            if (s1[1] < s2[0]) {
+                ++p1;
+            } else if (s2[1] < s1[0]) {
+                ++p2;
+            } else {
+                int l = max(s1[0], s2[0]);
+                int r = min(s1[1], s2[1]);
+                if (r - l >= duration) return {l, l + duration};
+                s1[1] < s2[1] ? ++p1 : ++p2;
+            }
+        }
+
+        return {};
+    }
+};
