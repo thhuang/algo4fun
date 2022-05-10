04963b9e6f491f0a735528d260cd299622691b4f
TH Huang <tzhsn.huang@gmail.com>
2022-05-10 15:13:39 +0800

    [leetcode] product of two run length encoded arrays: optimize

--- solutions/leetcode/product-of-two-run-length-encoded-arrays/main.cpp
+++ solutions/leetcode/product-of-two-run-length-encoded-arrays/main.cpp
@@ -25,3 +25,32 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> findRLEArray(vector<vector<int>>& encoded1,
+                                     vector<vector<int>>& encoded2) {
+        vector<vector<int>> result;
+
+        int i1 = 0, c1 = 0;
+        int i2 = 0, c2 = 0;
+        while (i1 < encoded1.size() && i2 < encoded2.size()) {
+            c1 = c1 ? c1 : encoded1[i1][1];
+            c2 = c2 ? c2 : encoded2[i2][1];
+
+            int value = encoded1[i1][0] * encoded2[i2][0];
+            int count = min(c1, c2);
+
+            if (result.empty() || result.back()[0] != value) {
+                result.push_back({value, count});
+            } else {
+                result.back()[1] += count;
+            }
+
+            if ((c1 -= count) == 0) ++i1;
+            if ((c2 -= count) == 0) ++i2;
+        }
+
+        return result;
+    }
+};
