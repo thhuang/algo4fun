894a357e8a054bea60aa2bea3684affb8c05f346
TH Huang <tzhsn.huang@gmail.com>
2022-05-11 15:39:54 +0800

    [leetcode] longest substring with at most two distinct characters: two pointers

--- solutions/leetcode/longest-substring-with-at-most-two-distinct-characters/main.cpp
+++ solutions/leetcode/longest-substring-with-at-most-two-distinct-characters/main.cpp
@@ -20,3 +20,24 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        unordered_map<char, int> char_count;
+        int result = 0;
+
+        for (int l = 0, r = 0; r < s.size(); ++r) {
+            ++char_count[s[r]];
+
+            while (char_count.size() > 2) {
+                if (--char_count[s[l]] == 0) char_count.erase(s[l]);
+                ++l;
+            }
+
+            result = max(result, r - l + 1);
+        }
+
+        return result;
+    }
+};
