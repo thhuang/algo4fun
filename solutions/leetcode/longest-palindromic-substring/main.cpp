9155923758d72ede287e25d220304671937e8c97
TH Huang <tzhsn.huang@gmail.com>
2022-05-11 14:51:12 +0800

    [leetcode] longest palindromic substring: refactor

--- solutions/leetcode/longest-palindromic-substring/main.cpp
+++ solutions/leetcode/longest-palindromic-substring/main.cpp
@@ -35,3 +35,22 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    string longestPalindrome(string s) {
+        string result;
+
+        auto search = [&](int l, int r) {
+            while (0 <= l && r < s.size() && s[l] == s[r]) --l, ++r;
+            if (r - l - 1 > result.size()) result = s.substr(l + 1, r - l - 1);
+        };
+
+        for (int i = 0; i < s.size(); ++i) {
+            search(i, i);
+            search(i, i + 1);
+        }
+
+        return result;
+    }
+};
