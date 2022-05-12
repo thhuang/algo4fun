71e429222047508b2e8be561a999e37736a0115e
TH Huang <tzhsn.huang@gmail.com>
2022-05-12 14:45:55 +0800

    [leetcode] shortest palindrome: refactor

--- solutions/leetcode/shortest-palindrome/main.cpp
+++ solutions/leetcode/shortest-palindrome/main.cpp
@@ -1,28 +1,21 @@
 class Solution {
    public:
     string shortestPalindrome(string s) {
-        if (s.size() < 2) return s;
-
-        int n = s.size();
-
-        auto check = [&](int l, int r) -> bool {
-            while (0 <= l && s[l] == s[r]) --l, ++r;
-            return l < 0;
+        auto check = [&](int r) -> bool {
+            for (int i = 0; i < r / 2 + 1; ++i) {
+                if (s[i] != s[r - i]) return false;
+            }
+            return true;
         };
 
-        int last = 0;
-        for (int i = (n - 1) / 2; ~i; --i) {
-            if (check(i, i)) last = max(last, i * 2);
-
-            int l = i, r = i + 1;
-            if (n & 1) --l, --r;
-            if (check(l, r)) last = max(last, l * 2 + 1);
+        for (int i = s.size() - 1; ~i; --i) {
+            if (check(i)) {
+                string result = s.substr(i + 1);
+                reverse(result.begin(), result.end());
+                return result + s;
+            }
         }
 
-        string result = s.substr(last + 1);
-        reverse(result.begin(), result.end());
-        result += s;
-
-        return result;
+        return "";
     }
 };
