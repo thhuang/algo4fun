006a6a9abccbbad091abaf4d9ceddcd66b364bf0
TH Huang <tzhsn.huang@gmail.com>
2022-05-12 17:07:34 +0800

    [leetcode] sliding window maximum: monotonic queue

--- solutions/leetcode/sliding-window-maximum/main.cpp
+++ solutions/leetcode/sliding-window-maximum/main.cpp
@@ -14,3 +14,30 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+
+        auto push = [&](int v) -> int {
+            while (!q.empty() && q.back() < v) q.pop_back();
+            q.push_back(v);
+            return q.front();
+        };
+
+        auto pop = [&](int v) -> void {
+            if (v == q.front()) q.pop_front();
+        };
+
+        for (int i = 0; i < k - 1; ++i) push(nums[i]);
+
+        vector<int> result;
+        for (int i = k - 1; i < nums.size(); ++i) {
+            result.push_back(push(nums[i]));
+            pop(nums[i - k + 1]);
+        }
+
+        return result;
+    }
+};
