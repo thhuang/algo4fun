6e52ad452c9aa8584296e1721cb139a536c31b32
TH Huang <tzhsn.huang@gmail.com>
2022-05-13 20:37:05 +0800

    [leetcode] pacific atlantic water flow: alternative

--- solutions/leetcode/pacific-atlantic-water-flow/main.cpp
+++ solutions/leetcode/pacific-atlantic-water-flow/main.cpp
@@ -36,3 +36,40 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+    vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    const char unknown = ' ';
+
+   public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights.front().size();
+
+        vector<vector<char>> vis(m, vector<char>(n, unknown));
+
+        vector<vector<int>> result;
+        function<void(int, int, char)> dfs = [&](int i, int j, char ocean) {
+            if (vis[i][j] == ocean) return;
+            if (vis[i][j] != unknown) result.push_back({i, j});
+
+            vis[i][j] = ocean;
+
+            for (auto [di, dj] : directions) {
+                int ii = i + di;
+                int jj = j + dj;
+                if (ii < 0 || ii >= m || jj < 0 || jj >= n) continue;
+                if (heights[ii][jj] < heights[i][j]) continue;
+
+                dfs(ii, jj, ocean);
+            }
+        };
+
+        for (int j = 0; j < n; ++j) dfs(0, j, 'p');
+        for (int i = 0; i < m; ++i) dfs(i, 0, 'p');
+        for (int j = 0; j < n; ++j) dfs(m - 1, j, 'a');
+        for (int i = 0; i < m; ++i) dfs(i, n - 1, 'a');
+
+        return result;
+    }
+};
