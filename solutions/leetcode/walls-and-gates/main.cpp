a5ffd6a218c1238e6ae261879cbe1047bdbd0d63
TH Huang <tzhsn.huang@gmail.com>
2022-05-13 20:06:31 +0800

    [leetcode] walls and gates

---
+++ solutions/leetcode/walls-and-gates/main.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+        int m = rooms.size();
+        int n = rooms.front().size();
+
+        vector<array<int, 2>> q;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (rooms[i][j] == 0) q.push_back({i, j});
+            }
+        }
+
+        int dist = 0;
+        while (!q.empty()) {
+            vector<array<int, 2>> new_q;
+
+            for (auto [i, j] : q) {
+                if (rooms[i][j] < dist) continue;
+                rooms[i][j] = dist;
+
+                for (auto [di, dj] : directions) {
+                    int ii = i + di;
+                    int jj = j + dj;
+                    if (ii < 0 || ii >= m || jj < 0 || jj >= n) continue;
+                    new_q.push_back({ii, jj});
+                }
+            }
+
+            q = move(new_q);
+            ++dist;
+        }
+    }
+};
