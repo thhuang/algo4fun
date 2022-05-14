eee800bd96ed24925c2da1f5fea6916aeba7406e
TH Huang <tzhsn.huang@gmail.com>
2022-05-14 16:07:54 +0800

    [leetcode] network delay time: little optimize

--- solutions/leetcode/network-delay-time/main.cpp
+++ solutions/leetcode/network-delay-time/main.cpp
@@ -24,3 +24,30 @@ class Solution {
         return t;
     }
 };
+
+class Solution {
+   public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<array<int, 2>>> graph(n + 1);
+        for (auto uvw : times) graph[uvw[0]].push_back({uvw[2], uvw[1]});
+
+        unordered_set<int> vis;
+        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<>> q;
+
+        int t = 0;
+        q.push({0, k});
+        while (!q.empty()) {
+            auto [wu, u] = q.top();
+            q.pop();
+
+            if (vis.count(u)) continue;
+            vis.insert(u);
+
+            t = wu;
+            for (auto [wv, v] : graph[u]) q.push({wv + wu, v});
+        }
+
+        if (vis.size() < n) return -1;
+        return t;
+    }
+};
