1bb6b10fbb289abf548380706bf2b128c588c3ad
TH Huang <tzhsn.huang@gmail.com>
2022-05-14 18:49:30 +0800

    [leetcode] flatten a multilevel doubly linked list

---
+++ solutions/leetcode/flatten-a-multilevel-doubly-linked-list/main.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+    Node* process(Node* head) {
+        Node* p = head;
+        Node* prev = nullptr;
+
+        while (p) {
+            if (!p->child) {
+                prev = p;
+                p = p->next;
+                continue;
+            }
+
+            Node* p1 = p->next;
+            Node* q0 = p->child;
+            Node* q1 = process(q0);
+
+            p->child = nullptr;
+            p->next = q0;
+            q0->prev = p;
+            q1->next = p1;
+            if (p1) p1->prev = q1;
+
+            prev = q1;
+            p = p1;
+        }
+
+        return prev;
+    }
+
+   public:
+    Node* flatten(Node* head) {
+        process(head);
+        return head;
+    }
+};
