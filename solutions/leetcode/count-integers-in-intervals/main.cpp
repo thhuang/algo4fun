0b29ce90b97067001d985b9109dc4965bd80f14f
TH Huang <tzhsn.huang@gmail.com>
2022-05-15 13:29:40 +0800

    [leetcode] count integers in intervals: refactor

--- solutions/leetcode/count-integers-in-intervals/main.cpp
+++ solutions/leetcode/count-integers-in-intervals/main.cpp
@@ -4,46 +4,27 @@ class CountIntervals {
 
    public:
     void add(int left, int right) {
-        if (intervals_[left]) {
-            count_ -= intervals_[left] - left + 1;
+        auto it = intervals_.upper_bound(left);
+
+        if (it != intervals_.begin() && left <= prev(it)->second) {
+            auto prev_it = prev(it);
+
+            left = prev_it->first;
+            right = max(right, prev_it->second);
+
+            count_ -= prev_it->second - prev_it->first + 1;
+            intervals_.erase(prev_it);
         }
 
-        intervals_[left] = max(intervals_[left], right);
-        count_ += right - left + 1;
+        while (it != intervals_.end() && it->first <= right) {
+            right = max(right, it->second);
 
-        auto it = intervals_.find(left);
-        if (it != intervals_.begin()) {
-            auto prev = it;
-            --prev;
-            if (prev->second >= it->first) {
-                count_ -= prev->second - prev->first + 1;
-                count_ -= it->second - it->first + 1;
-
-                prev->second = max(prev->second, it->second);
-
-                count_ += prev->second - prev->first + 1;
-
-                intervals_.erase(it);
-                it = prev;
-            }
-        }
-        ++it;
-
-        for (; it != intervals_.end(); ++it) {
-            auto prev = it;
-            --prev;
-
-            if (prev->second < it->first) break;
-
-            count_ -= prev->second - prev->first + 1;
             count_ -= it->second - it->first + 1;
-
-            prev->second = max(prev->second, it->second);
-
-            count_ += prev->second - prev->first + 1;
-
-            it = --intervals_.erase(it);
+            it = intervals_.erase(it);
         }
+
+        count_ += right - left + 1;
+        intervals_[left] = right;
     }
 
     int count() { return count_; }
