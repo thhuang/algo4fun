ff9cd1f096c094300b24ca8c4698461150e53fb2
TH Huang <tzhsn.huang@gmail.com>
2022-05-15 19:08:04 +0800

    [leetcode] open the lock: alternative

--- solutions/leetcode/open-the-lock/main.cpp
+++ solutions/leetcode/open-the-lock/main.cpp
@@ -32,3 +32,40 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+    const string start = "0000";
+    const array<int, 2> directions = {1, 9};
+
+   public:
+    int openLock(vector<string>& deadends, string target) {
+        unordered_map<string, int> vis;
+        for (string s : deadends) vis[s] = -1;
+
+        if (vis.count(start) || vis.count(target)) return -1;
+
+        deque<string> q = {start};
+        while (!q.empty()) {
+            string s = q.front();
+            q.pop_front();
+
+            int turns = vis[s];
+            if (s == target) return turns;
+
+            for (int i = 0; i < s.size(); ++i) {
+                char c = s[i];
+
+                for (int d : directions) {
+                    s[i] = '0' + (c - '0' + d) % 10;
+                    if (vis.count(s)) continue;
+                    vis[s] = turns + 1;
+                    q.push_back(s);
+                }
+
+                s[i] = c;
+            }
+        }
+
+        return -1;
+    }
+};
