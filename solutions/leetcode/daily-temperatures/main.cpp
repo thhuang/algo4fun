459ffe46b64970a866448552408e3278d2c75622
TH Huang <tzhsn.huang@gmail.com>
2022-05-16 20:47:29 +0800

    [leetcode] daily temperatures: optimize

--- solutions/leetcode/daily-temperatures/main.cpp
+++ solutions/leetcode/daily-temperatures/main.cpp
@@ -7,11 +7,34 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             while (!temperature_index.empty() &&
                    temperature_index.back()[0] < temperatures[i]) {
-                auto [_, j] = temperature_index.back();
+                int j = temperature_index.back()[1];
                 temperature_index.pop_back();
                 result[j] = i - j;
             }
             temperature_index.push_back({temperatures[i], i});
+        }
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> result(n, 0);
+        int max_t = 0;
+        for (int i = n - 1; ~i; --i) {
+            int t = temperatures[i];
+            if (t >= max_t) {
+                max_t = t;
+                continue;
+            }
+
+            int d = 1;
+            while (temperatures[i] >= temperatures[i + d]) {
+                d += result[i + d];
+            }
+            result[i] = d;
         }
         return result;
     }
