60967f86658d4071c85a50da878382f8e8faae9a
TH Huang <tzhsn.huang@gmail.com>
2022-05-19 16:14:44 +0800

    [leetcode] longest increasing path in a matrix: refactor

--- solutions/leetcode/longest-increasing-path-in-a-matrix/main.cpp
+++ solutions/leetcode/longest-increasing-path-in-a-matrix/main.cpp
@@ -1,31 +1,29 @@
 class Solution {
-    vector<array<int, 2>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
    public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix.front().size();
+        int m = matrix.size();
+        int n = matrix.front().size();
 
-        vector<vector<int>> cache(n, vector<int>(m, 0));
+        vector<vector<int>> vis(m, vector<int>(n, 0));
         function<int(int, int)> dfs = [&](int i, int j) -> int {
-            if (cache[i][j]) return cache[i][j];
-
-            int d = 0;
+            if (vis[i][j]) return vis[i][j];
+            vis[i][j] = 1;
             for (auto [di, dj] : directions) {
                 int ii = i + di;
                 int jj = j + dj;
-                if (ii < 0 || ii >= n || jj < 0 || jj >= m) continue;
-                if (matrix[ii][jj] <= matrix[i][j]) continue;
-
-                d = max(d, dfs(ii, jj));
+                if (ii < 0 || ii >= m || jj < 0 || jj >= n) continue;
+                if (matrix[i][j] >= matrix[ii][jj]) continue;
+                vis[i][j] = max(vis[i][j], dfs(ii, jj) + 1);
             }
-
-            return cache[i][j] = d + 1;
+            return vis[i][j];
         };
 
         int result = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (vis[i][j]) continue;
                 result = max(result, dfs(i, j));
             }
         }
