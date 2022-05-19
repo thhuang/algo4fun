6950670e76ae1577ac77c56e818bf30ca0f2da72
TH Huang <tzhsn.huang@gmail.com>
2022-05-19 17:20:26 +0800

    [leetcode] regular expression matching

---
+++ solutions/leetcode/regular-expression-matching/main.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    bool isSimpleMatch(string& s, string& p) {
+        if (s.size() != p.size()) return false;
+        for (int i = 0; i < s.size(); ++i) {
+            if (p[i] == '.') continue;
+            if (s[i] != p[i]) return false;
+        }
+        return true;
+    }
+
+    bool isRegexMatch(string s, string p) {
+        while (!s.empty() && !p.empty()) {
+            auto it = find(p.rbegin(), p.rend(), '*');
+
+            int length = it - p.rbegin();
+            if (s.size() < length) return false;
+
+            if (length > 0) {
+                string sub_s, sub_p;
+                for (int i = 0; i < length; ++i) {
+                    sub_s += s.back(), s.pop_back();
+                    sub_p += p.back(), p.pop_back();
+                }
+                if (!isSimpleMatch(sub_s, sub_p)) return false;
+            }
+
+            if (s.empty() || p.empty()) break;
+
+            p.pop_back();  // pop '*'
+            char c = p.back();
+            p.pop_back();
+            while (!s.empty() && (c == '.' || s.back() == c)) {
+                if (isRegexMatch(s, p)) return true;
+                s.pop_back();
+            }
+        }
+
+        if (p.empty()) return s.empty();
+        while (!p.empty()) {
+            if (p.back() != '*') return false;
+            p.pop_back(), p.pop_back();
+        }
+        return true;
+    }
+
+   public:
+    bool isMatch(string s, string p) { return isRegexMatch(s, p); }
+};
