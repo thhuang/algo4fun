e05243b98bcac7567561aa9bdecb1c729613526a
TH Huang <tzhsn.huang@gmail.com>
2022-05-20 14:22:43 +0800

    [leetcode] house robber iii

---
+++ solutions/leetcode/house-robber-iii/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    int rob(TreeNode* root) {
+        unordered_map<TreeNode*, unordered_map<bool, int>> cache;
+        function<int(TreeNode*, bool)> dfs = [&](TreeNode* u,
+                                                 bool can_rob) -> int {
+            if (!u) return 0;
+            if (cache[u].count(can_rob)) return cache[u][can_rob];
+            int v = can_rob ? u->val : 0;
+            return cache[u][can_rob] =
+                       max(dfs(u->left, false) + dfs(u->right, false) + v,
+                           dfs(u->left, true) + dfs(u->right, true));
+        };
+        return dfs(root, true);
+    }
+};
