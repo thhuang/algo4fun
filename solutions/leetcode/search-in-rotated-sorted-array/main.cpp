72f1ee50b967f3740f5bb92c05c46d02139d57ea
TH Huang <tzhsn.huang@gmail.com>
2022-05-21 22:39:47 +0800

    [leetcode] search in rotated sorted array: alternative

--- solutions/leetcode/search-in-rotated-sorted-array/main.cpp
+++ solutions/leetcode/search-in-rotated-sorted-array/main.cpp
@@ -17,3 +17,23 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[l] <= nums[m] && (nums[m] < target || target < nums[l])) {
+                l = m + 1;
+            } else if (nums[l] > nums[m] &&
+                       (nums[m] < target && target < nums[l])) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return -1;
+    }
+};
