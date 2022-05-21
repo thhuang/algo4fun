e62a7cb4d15cc11050fac02295400b479878277c
TH Huang <tzhsn.huang@gmail.com>
2022-05-21 13:32:11 +0800

    [leetcode] delete leaves with a given value

---
+++ solutions/leetcode/delete-leaves-with-a-given-value/main.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        function<TreeNode*(TreeNode*)> dfs = [&](TreeNode* u) -> TreeNode* {
+            if (!u) return nullptr;
+            u->left = dfs(u->left);
+            u->right = dfs(u->right);
+            return u->val != target || u->left || u->right ? u : nullptr;
+        };
+        return dfs(root);
+    }
+};
