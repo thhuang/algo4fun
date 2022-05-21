eec4188419c4a8c7f42ba6df48bc976230c4f69e
TH Huang <tzhsn.huang@gmail.com>
2022-05-21 13:33:36 +0800

    [leetcode] delete leaves with a given value: alternative

--- solutions/leetcode/delete-leaves-with-a-given-value/main.cpp
+++ solutions/leetcode/delete-leaves-with-a-given-value/main.cpp
@@ -22,3 +22,14 @@ class Solution {
         return dfs(root);
     }
 };
+
+class Solution {
+   public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root) return nullptr;
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+        return root->val != target || root->left || root->right ? root
+                                                                : nullptr;
+    }
+};
