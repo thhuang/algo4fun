0515596aa1e6aac96449125d254f694c0a89429e
TH Huang <tzhsn.huang@gmail.com>
2022-05-22 16:58:03 +0800

    [leetcode] best time to buy and sell stock with transaction fee: refactor

--- solutions/leetcode/best-time-to-buy-and-sell-stock-with-transaction-fee/main.cpp
+++ solutions/leetcode/best-time-to-buy-and-sell-stock-with-transaction-fee/main.cpp
@@ -34,3 +34,20 @@ class Solution {
         return dp.sold;
     }
 };
+
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices, int fee) {
+        struct DP {
+            int bought, sold;
+        };
+
+        DP dp = {-prices.front(), 0};
+        for (int v : prices) {
+            dp = {max(dp.bought, dp.sold - v),
+                  max(dp.sold, dp.bought + v - fee)};
+        }
+
+        return dp.sold;
+    }
+};
