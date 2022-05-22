4b490ea3ec996065908e214123a847a12d286921
TH Huang <tzhsn.huang@gmail.com>
2022-05-22 14:25:15 +0800

    [leetcode] palindromic substrings: refactor

--- solutions/leetcode/palindromic-substrings/main.cpp
+++ solutions/leetcode/palindromic-substrings/main.cpp
@@ -16,3 +16,20 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        auto search = [&](int l, int r) -> void {
+            while (0 <= l && r < n && s[l] == s[r]) ++count, --l, ++r;
+        };
+
+        for (int i = 0; i < n; ++i) search(i, i);
+        for (int i = 0; i < n - 1; ++i) search(i, i + 1);
+
+        return count;
+    }
+};
