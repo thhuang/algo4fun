dc5cbfd111cabbb463a878aae1c92e0a1817444c
TH Huang <tzhsn.huang@gmail.com>
2022-05-23 12:32:49 +0800

    [leetcode] ones and zeroes: optimize

--- solutions/leetcode/ones-and-zeroes/main.cpp
+++ solutions/leetcode/ones-and-zeroes/main.cpp
@@ -21,3 +21,27 @@ class Solution {
         return dp.back().back().back();
     }
 };
+
+class Solution {
+   public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < strs.size(); ++i) {
+            int one = 0;
+            for (char c : strs[i]) one += c == '1';
+            int zero = int(strs[i].size()) - one;
+
+            vector<vector<int>> new_dp(m + 1, vector<int>(n + 1, 0));
+            for (int z = 0; z <= m; ++z) {
+                for (int o = 0; o <= n; ++o) {
+                    new_dp[z][o] = dp[z][o];
+                    if (z - zero < 0 || o - one < 0) continue;
+                    new_dp[z][o] = max(new_dp[z][o], 1 + dp[z - zero][o - one]);
+                }
+            }
+            dp = move(new_dp);
+        }
+
+        return dp.back().back();
+    }
+};
