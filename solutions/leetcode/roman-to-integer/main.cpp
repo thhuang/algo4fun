c3fb5f8837a22ad383fe74a0d39e73f48cde361a
TH Huang <tzhsn.huang@gmail.com>
2022-05-23 19:45:13 +0800

    [leetcode] roman to integer

---
+++ solutions/leetcode/roman-to-integer/main.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    const unordered_map<char, int> char2num = {
+        {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+        {'C', 100}, {'D', 500}, {'M', 1000}};
+
+    bool shouldMinus(char curr, char next) {
+        switch (curr) {
+            case 'I':
+                return next == 'V' || next == 'X';
+            case 'X':
+                return next == 'L' || next == 'C';
+            case 'C':
+                return next == 'D' || next == 'M';
+        }
+        return false;
+    }
+
+   public:
+    int romanToInt(string s) {
+        int result = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            int v = char2num.at(s[i]);
+            if (i + 1 < s.size() && shouldMinus(s[i], s[i + 1])) v = -v;
+            result += v;
+        }
+        return result;
+    }
+};
