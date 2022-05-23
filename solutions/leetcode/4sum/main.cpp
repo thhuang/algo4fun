bcf40bdb1a962710b5633cd753b74c1e3f2f88f9
TH Huang <tzhsn.huang@gmail.com>
2022-05-23 20:24:17 +0800

    [leetcode] 4 sum: fix silly mistakes

--- solutions/leetcode/4sum/main.cpp
+++ solutions/leetcode/4sum/main.cpp
@@ -6,19 +6,17 @@ class Solution {
         vector<vector<int>> result;
         for (int i = 0; i < nums.size(); ++i) {
             for (int j = i + 1; j < nums.size(); ++j) {
-                for (int l = j + 1; l < nums.size(); ++l) {
-                    int r = nums.size() - 1;
-                    while (l < r) {
-                        long s = (long)nums[i] + nums[j] + nums[l] + nums[r];
-                        if (s < target) {
-                            ++l;
-                        } else if (s > target) {
-                            --r;
-                        } else {  // s == target
-                            result.push_back({nums[i], nums[j], nums[l], nums[r]});
-                            while (l + 1 < nums.size() && nums[l] == nums[l + 1]) ++l;
-                            ++l;
-                        }
+                int l = j + 1, r = nums.size() - 1;
+                while (l < r) {
+                    long s = (long)nums[i] + nums[j] + nums[l] + nums[r];
+                    if (s < target) {
+                        ++l;
+                    } else if (s > target) {
+                        --r;
+                    } else {  // s == target
+                        result.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        while (l + 1 < nums.size() && nums[l] == nums[l + 1]) ++l;
+                        ++l;
                     }
                 }
                 while (j + 1 < nums.size() && nums[j] == nums[j + 1]) ++j;
