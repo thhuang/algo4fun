9bbc762049d9af5d941c05521543ac01f9013169
TH Huang <tzhsn.huang@gmail.com>
2022-05-24 15:53:35 +0800

    [leetcode] number of provinces: union find

--- solutions/leetcode/number-of-provinces/main.cpp
+++ solutions/leetcode/number-of-provinces/main.cpp
@@ -21,3 +21,36 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> group(n);
+        iota(group.begin(), group.end(), 0);
+
+        function<int(int)> find = [&group](int u) -> int {
+            while (group[u] != u) {
+                u = group[u] = group[group[u]];
+            }
+            return u;
+        };
+
+        function<void(int, int)> unite = [&group, &find](int a, int b) -> void {
+            int pa = find(a);
+            int pb = find(b);
+            group[pa] = pb;
+        };
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isConnected[i][j]) unite(i, j);
+            }
+        }
+
+        unordered_set<int> result;
+        for (int i = 0; i < n; ++i) result.insert(find(i));
+
+        return result.size();
+    }
+};
