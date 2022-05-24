26763fa5e7a466f4ad08fb1b13416f5422865c88
TH Huang <tzhsn.huang@gmail.com>
2022-05-24 17:03:00 +0800

    [leetcode] maximal rectangle: dp

--- solutions/leetcode/maximal-rectangle/main.cpp
+++ solutions/leetcode/maximal-rectangle/main.cpp
@@ -35,3 +35,50 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        int result = 0;
+
+        vector<int> left(n, 0), right(n, n - 1), height(n, 0);
+        for (int i = 0; i < m; ++i) {
+            int curr_left = 0;
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '0') {
+                    left[j] = 0;
+                    curr_left = j + 1;
+                } else if (j - 1 >= 0) {
+                    left[j] = max(left[j], curr_left);
+                }
+            }
+
+            int curr_right = n - 1;
+            for (int j = n - 1; j >= 0; --j) {
+                if (matrix[i][j] == '0') {
+                    right[j] = n - 1;
+                    curr_right = j - 1;
+                } else if (j + 1 < n) {
+                    right[j] = min(right[j], curr_right);
+                }
+            }
+
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '0') {
+                    height[j] = 0;
+                } else {
+                    ++height[j];
+                }
+            }
+
+            for (int j = 0; j < n; ++j) {
+                result = max(result, (right[j] - left[j] + 1) * height[j]);
+            }
+        }
+
+        return result;
+    }
+};
