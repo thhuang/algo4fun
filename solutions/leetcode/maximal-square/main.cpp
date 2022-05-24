560a0240ca9bbf928e6debe966ed5b252691588a
TH Huang <tzhsn.huang@gmail.com>
2022-05-24 16:07:03 +0800

    [leetcode] maximal square: alternative

--- solutions/leetcode/maximal-square/main.cpp
+++ solutions/leetcode/maximal-square/main.cpp
@@ -20,3 +20,26 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        int result = 0;
+        vector<int> dp(n, 0);
+        for (int i = 0; i < m; ++i) {
+            vector<int> new_dp(n, 0);
+            new_dp[0] = matrix[i][0] == '1';
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == '0') continue;
+                new_dp[j] = 1 + min(new_dp[j - 1], min(dp[j - 1], dp[j]));
+            }
+            dp = move(new_dp);
+            result = max(result, *max_element(dp.begin(), dp.end()));
+        }
+
+        return result * result;
+    }
+};
