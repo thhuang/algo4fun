5f42203aea0b08dca8d6c0fd506699ee6779156b
TH Huang <tzhsn.huang@gmail.com>
2022-05-25 16:43:29 +0800

    [leetcode] number of operations to make network connected: union find

--- solutions/leetcode/number-of-operations-to-make-network-connected/main.cpp
+++ solutions/leetcode/number-of-operations-to-make-network-connected/main.cpp
@@ -24,3 +24,31 @@ class Solution {
         return count - 1;
     }
 };
+
+class Solution {
+   public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if (connections.size() < n - 1) return -1;
+
+        vector<int> group(n);
+        iota(group.begin(), group.end(), 0);
+
+        function<int(int)> find = [&](int u) -> int {
+            if (u == group[u]) return u;
+            return group[u] = find(group[u]);
+        };
+
+        function<void(int, int)> unite = [&](int u, int v) -> void {
+            int pu = find(u);
+            int pv = find(v);
+            group[pu] = pv;
+        };
+
+        for (const auto& c : connections) unite(c[0], c[1]);
+
+        unordered_set<int> networks;
+        for (int g : group) networks.insert(find(g));
+
+        return networks.size() - 1;
+    }
+};
