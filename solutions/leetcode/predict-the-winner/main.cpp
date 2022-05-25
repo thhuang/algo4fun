2ed07560ab858791f5b14bf1074d3d7240ac295b
TH Huang <tzhsn.huang@gmail.com>
2022-05-25 22:12:26 +0800

    [leetcode] predict the winner: optimize

--- solutions/leetcode/predict-the-winner/main.cpp
+++ solutions/leetcode/predict-the-winner/main.cpp
@@ -41,3 +41,22 @@ class Solution {
         return dp[0][n - 1] >= 0;
     }
 };
+
+class Solution {
+   public:
+    bool PredictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        for (int i = 0; i < n; ++i) dp[i] = nums[i];
+
+        for (int k = 1; k < n; ++k) {
+            vector<int> new_dp(n);
+            for (int i = 0; i + k < n; ++i) {
+                new_dp[i] = max(nums[i] - dp[i + 1], nums[i + k] - dp[i]);
+            }
+            dp = move(new_dp);
+        }
+
+        return dp[0] >= 0;
+    }
+};
