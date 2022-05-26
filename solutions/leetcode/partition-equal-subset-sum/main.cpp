ee7763be2e9dbe8730a87c19517728844850af17
TH Huang <tzhsn.huang@gmail.com>
2022-05-26 16:05:27 +0800

    [leetcode] partition equal subset sum: refactor

--- solutions/leetcode/partition-equal-subset-sum/main.cpp
+++ solutions/leetcode/partition-equal-subset-sum/main.cpp
@@ -1,44 +1,22 @@
 class Solution {
    public:
     bool canPartition(vector<int>& nums) {
-        int total_sum = 0;
-        for (int v : nums) total_sum += v;
+        int n = nums.size();
 
-        if (total_sum & 1) return false;
+        int sum = 0;
+        for (int v : nums) sum += v;
+        if (sum & 1) return false;
 
-        int target_sum = total_sum / 2;
-        vector<bool> dp(target_sum + 1, false);
-        dp[0] = true;
-
-        for (int v : nums) {
-            for (int i = dp.size() - 1; ~i; --i) {
-                if (i - v < 0) continue;
-                dp[i] = dp[i] || dp[i - v];
-            }
-        }
-
-        return dp.back();
-    }
-};
-
-class Solution {
-   public:
-    bool canPartition(vector<int>& nums) {
-        int total = 0;
-        for (int v : nums) total += v;
-
-        if (total & 1) return false;
-
-        int target = total / 2;
+        int target = sum / 2;
         vector<bool> dp(target + 1, false);
         dp[0] = true;
 
-        for (int v : nums) {
-            if (target - v >= 0 && dp[target - v]) return true;
-            for (int i = dp.size() - 1; ~i; --i) {
-                if (i - v < 0) continue;
-                dp[i] = dp[i] || dp[i - v];
+        for (int i = 0; i < n; ++i) {
+            int v = nums[i];
+            for (int j = target; j - v >= 0; --j) {
+                dp[j] = dp[j] || dp[j - v];
             }
+            if (dp.back()) return true;
         }
 
         return false;
