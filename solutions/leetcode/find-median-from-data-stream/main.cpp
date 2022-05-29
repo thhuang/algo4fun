7849afd232f9b5f415d3464f0ff2092a70994be9
TH Huang <tzhsn.huang@gmail.com>
2022-05-29 17:13:03 +0800

    [leetcode] find median from data stream: heap

--- solutions/leetcode/find-median-from-data-stream/main.cpp
+++ solutions/leetcode/find-median-from-data-stream/main.cpp
@@ -23,6 +23,32 @@ class MedianFinder {
     }
 };
 
+class MedianFinder {
+    priority_queue<int> left;
+    priority_queue<int, vector<int>, greater<>> right;
+
+   public:
+    void addNum(int num) {
+        left.push(num);
+
+        right.push(left.top());
+        left.pop();
+
+        if (left.size() >= right.size()) return;
+
+        left.push(right.top());
+        right.pop();
+    }
+
+    double findMedian() {
+        double result = left.top();
+        if (left.size() == right.size()) {
+            result = 0.5 * (result + right.top());
+        }
+        return result;
+    }
+};
+
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder* obj = new MedianFinder();
