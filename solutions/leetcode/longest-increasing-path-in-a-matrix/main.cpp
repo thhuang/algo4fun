14c6a8e01d8f752a7bc450293730d1309d709c2f
TH Huang <tzhsn.huang@gmail.com>
2022-05-29 20:28:02 +0800

    [leetcode] longest increasing path in a matrix: alternative

--- solutions/leetcode/longest-increasing-path-in-a-matrix/main.cpp
+++ solutions/leetcode/longest-increasing-path-in-a-matrix/main.cpp
@@ -31,3 +31,40 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        vector<vector<int>> dist(m, vector<int>(n, -1));
+        function<int(int, int)> dfs = [&](int i, int j) -> int {
+            int d = 0;
+            for (auto [di, dj] : directions) {
+                int ii = i + di;
+                int jj = j + dj;
+                if (ii < 0 || ii >= m || jj < 0 || jj >= n) continue;
+                if (matrix[ii][jj] <= matrix[i][j]) continue;
+                if (dist[ii][jj] != -1) {
+                    d = max(d, dist[ii][jj]);
+                    continue;
+                }
+                d = max(d, dfs(ii, jj));
+            }
+            return dist[i][j] = d + 1;
+        };
+
+        int result = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dist[i][j] != -1) continue;
+                result = max(result, dfs(i, j));
+            }
+        }
+
+        return result;
+    }
+};
