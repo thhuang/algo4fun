2e34804f08c07dbcbb4469ce1d70fb4ddbecb8d1
TH Huang <tzhsn.huang@gmail.com>
2022-05-29 16:48:56 +0800

    [leetcode] kth smallest element in a sorted matrix: binary search

--- solutions/leetcode/kth-smallest-element-in-a-sorted-matrix/main.cpp
+++ solutions/leetcode/kth-smallest-element-in-a-sorted-matrix/main.cpp
@@ -21,3 +21,34 @@ class Solution {
         return *q.top().it;
     }
 };
+
+class Solution {
+   public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int l = matrix.front().front();
+        int r = matrix.back().back();
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            int count = 0;
+            for (const auto& vec : matrix) {
+                count += upper_bound(vec.begin(), vec.end(), m) - vec.begin();
+            }
+
+            if (count <= k - 1) {
+                l = m + 1;
+            } else if (count > k - 1) {
+                r = m - 1;
+            }
+        }
+
+        int result = matrix.back().back();
+        for (const auto& vec : matrix) {
+            const auto it = upper_bound(vec.begin(), vec.end(), r);
+            if (it != vec.end()) result = min(result, *it);
+        }
+
+        return result;
+    }
+};
