32dddd34f0dfad7b6e40040c42095072732efd9c
TH Huang <tzhsn.huang@gmail.com>
2022-05-30 11:33:12 +0800

    [leetcode] parallel courses: refactor

--- solutions/leetcode/parallel-courses/main.cpp
+++ solutions/leetcode/parallel-courses/main.cpp
@@ -2,13 +2,10 @@ class Solution {
    public:
     int minimumSemesters(int n, vector<vector<int>>& relations) {
         vector<vector<int>> adj(n);
-        for (const auto& r : relations) {
-            adj[r[0] - 1].push_back(r[1] - 1);
-        }
+        for (const auto& r : relations) adj[r[0] - 1].push_back(r[1] - 1);
 
-        vector<int> depth(n);
+        vector<int> depth(n, -1);
         vector<int> vis(n, 0);
-
         function<int(int)> dfs = [&](int u) -> int {
             vis[u] = 1;
 
@@ -17,25 +14,23 @@ class Solution {
                 if (vis[v] == 1) return -1;
                 if (vis[v] == 2) {
                     d = max(d, depth[v]);
-                    continue;
+                } else {  // vis[v] == 0
+                    int res = dfs(v);
+                    if (res == -1) return -1;
+                    d = max(d, res);
                 }
-
-                int res = dfs(v);
-                if (res == -1) return -1;
-                d = max(d, res);
             }
 
             vis[u] = 2;
+
             return depth[u] = d + 1;
         };
 
-        int result = 0;
         for (int i = 0; i < n; ++i) {
             if (vis[i]) continue;
             if (dfs(i) == -1) return -1;
-            result = max(result, dfs(i));
         }
 
-        return result;
+        return *max_element(depth.begin(), depth.end());
     }
 };
