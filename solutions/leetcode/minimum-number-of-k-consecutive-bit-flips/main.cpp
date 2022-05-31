aec7efd76dd6266384137dbd17e0ab39050d2b24
TH Huang <tzhsn.huang@gmail.com>
2022-05-31 20:45:26 +0800

    [leetcode] minimum number of k consecutive bit flips: optimize

--- solutions/leetcode/minimum-number-of-k-consecutive-bit-flips/main.cpp
+++ solutions/leetcode/minimum-number-of-k-consecutive-bit-flips/main.cpp
@@ -18,3 +18,30 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        bool flipped = false;
+        deque<bool> flip;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i >= k) {
+                flipped ^= flip.front();
+                flip.pop_front();
+            }
+            flip.push_back(false);
+
+            if (flipped != nums[i]) continue;
+            if (i + k > n) return -1;
+
+            flip.back() = true;
+            flipped = !flipped;
+            ++count;
+        }
+
+        return count;
+    }
+};
