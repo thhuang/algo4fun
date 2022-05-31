e5f4cc5627dd2a0c5be5859ed57fe951b84ffea1
TH Huang <tzhsn.huang@gmail.com>
2022-05-31 14:09:15 +0800

    [leetcode] check if a string contains all binary codes of size k: alternative

--- solutions/leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.cpp
+++ solutions/leetcode/check-if-a-string-contains-all-binary-codes-of-size-k/main.cpp
@@ -1,6 +1,24 @@
 class Solution {
    public:
     bool hasAllCodes(string s, int k) {
+        if (s.size() < k) return false;
+
+        unordered_set<int> us;
+        unsigned v = 0;
+        for (int i = 0; i < k - 1; ++i) v = (v << 1) + (s[i] == '1');
+        for (int i = k - 1; i < s.size(); ++i) {
+            v = (v << 1) + (s[i] == '1');
+            us.insert(v);
+            v -= (s[i - k + 1] == '1') << (k - 1);
+        }
+
+        return us.size() == (1 << k);
+    }
+};
+
+class Solution {
+   public:
+    bool hasAllCodes(string s, int k) {
         if (k > s.size()) return false;
 
         vector<bool> vis(1 << k, false);
