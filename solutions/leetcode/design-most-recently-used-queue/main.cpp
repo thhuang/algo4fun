80a7ba6c496e29e92619dea6e22a94873817f0c2
TH Huang <tzhsn.huang@gmail.com>
2022-06-02 22:24:50 +0800

    [leetcode] design most recently used queue: skip list

--- solutions/leetcode/design-most-recently-used-queue/main.cpp
+++ solutions/leetcode/design-most-recently-used-queue/main.cpp
@@ -12,6 +12,35 @@ class MRUQueue {
     }
 };
 
+class MRUQueue {
+    int m;
+    list<int> data;
+    vector<list<int>::iterator> iters;
+
+   public:
+    MRUQueue(int n) : m(round(sqrt(n))), data(n), iters((n - 1) / m + 1) {
+        for (int i = 0; i < n; ++i) {
+            auto it = data.insert(data.end(), i + 1);
+            if (i % m == 0) iters[i / m] = it;
+        }
+    }
+
+    int fetch(int k) {
+        --k;
+
+        int i = k / m;
+        auto it = iters[i];
+        for (int j = k % m; j; --j) ++it;
+
+        data.push_back(*it);
+        if (k % m == 0) ++iters[i];
+        for (++i; i < iters.size(); ++i) ++iters[i];
+        it = data.erase(it);
+
+        return data.back();
+    }
+};
+
 /**
  * Your MRUQueue object will be instantiated and called as such:
  * MRUQueue* obj = new MRUQueue(n);
