1e758022070b53773015584483eb3061a90f6a8c
TH Huang <tzhsn.huang@gmail.com>
2022-06-05 14:33:11 +0800

    [leetcode] egg drop with 2 eggs and n floors: alternative

--- solutions/leetcode/egg-drop-with-2-eggs-and-n-floors/main.cpp
+++ solutions/leetcode/egg-drop-with-2-eggs-and-n-floors/main.cpp
@@ -16,3 +16,18 @@ class Solution {
         return cache[n] = result;
     }
 };
+
+class Solution {
+   public:
+    int twoEggDrop(int n) {
+        vector<int> dp(n + 1, numeric_limits<int>::max());
+        dp[0] = 0, dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                int v = max(j, 1 + dp[i - j]);
+                dp[i] = min(dp[i], v);
+            }
+        }
+        return dp.back();
+    }
+};
