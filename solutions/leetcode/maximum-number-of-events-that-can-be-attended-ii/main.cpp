4f653366040da98e6cfd8e3a4fe727b924ca8996
TH Huang <tzhsn.huang@gmail.com>
2022-06-07 17:02:17 +0800

    [leetcode] maximum number of events that can be attended ii: refactor

--- solutions/leetcode/maximum-number-of-events-that-can-be-attended-ii/main.cpp
+++ solutions/leetcode/maximum-number-of-events-that-can-be-attended-ii/main.cpp
@@ -2,21 +2,19 @@ class Solution {
    public:
     int maxValue(vector<vector<int>>& events, int k) {
         sort(events.begin(), events.end());
-
-        vector<vector<int>> dp(events.size(), vector(k + 1, -1));
-
-        function<int(int, int)> dfs = [&](int ei, int ki) -> int {
-            if (ei >= events.size() || ki == 0) return 0;
-            if (~dp[ei][ki]) return dp[ei][ki];
-            int next_ei =
-                upper_bound(events.begin(), events.end(), events[ei][1],
-                            [](int t, const vector<int>& v) -> bool {
-                                return t < v[0];
-                            }) - events.begin();
-            return dp[ei][ki] = max(dfs(ei + 1, ki),
-                                    dfs(next_ei, ki - 1) + events[ei][2]);
+        
+        vector<vector<int>> dp(events.size(), vector<int>(k + 1, -1));
+        
+        function<int(int, int)> dfs = [&events, &dp, &dfs](int i, int k) -> int {
+            if (i >= events.size() || k == 0) return 0;
+            if (~dp[i][k]) return dp[i][k];
+            int j = upper_bound(events.begin(), events.end(), events[i][1],
+                                [](int t, const vector<int>& v) -> bool {
+                                    return t < v[0];
+                                }) - events.begin();
+            return dp[i][k] = max(dfs(i + 1, k), dfs(j, k - 1) + events[i][2]);
         };
-
+        
         return dfs(0, k);
     }
 };
