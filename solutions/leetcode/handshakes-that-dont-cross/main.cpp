3ccae66699cb19e6968a8a63fe8a1f730bbe39ef
TH Huang <tzhsn.huang@gmail.com>
2022-06-08 11:25:36 +0800

    [leetcode] handshakes that don't cross

---
+++ solutions/leetcode/handshakes-that-dont-cross/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int numberOfWays(int numPeople) {
+        vector<long> dp(numPeople + 1, 0);
+        dp[0] = dp[2] = 1;
+        for (int i = 4; i <= numPeople; i += 2) {
+            long v = 0;
+            for (int j = 1; j < i; j += 2) {
+                v = (v + dp[j - 1] * dp[i - j - 1] % mod) % mod;
+            }
+            dp[i] = v;
+        }
+        return dp.back();
+    }
+};
