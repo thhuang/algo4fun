2c0729ca57529a590392cd4fa84781eb5d9f3712
TH Huang <tzhsn.huang@gmail.com>
2022-06-11 18:16:13 +0800

    [leetcode] longest repeating substring: optimize

--- solutions/leetcode/longest-repeating-substring/main.cpp
+++ solutions/leetcode/longest-repeating-substring/main.cpp
@@ -30,3 +30,53 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+    const int base = 26;
+
+   public:
+    int longestRepeatingSubstring(string s) {
+        auto check = [&](int length) -> bool {
+            if (length == 0) return true;
+
+            long h = 0;
+            long b = 1;
+            for (int i = 0; i < length - 1; ++i) {
+                long v = s[i] - 'a';
+
+                h = h * base % mod;
+                h = (h + v) % mod;
+
+                b = b * base % mod;
+            }
+
+            unordered_set<int> seen;
+            for (int i = length - 1; i < s.size(); ++i) {
+                long v = s[i] - 'a';
+
+                h = h * base % mod;
+                h = (h + v) % mod;
+
+                if (seen.count(h)) return true;
+                seen.insert(h);
+
+                long d = (s[i - length + 1] - 'a') * b % mod;
+                h = (h - d + mod) % mod;
+            }
+
+            return false;
+        };
+
+        int l = 0, r = s.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (check(m)) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l - 1;
+    }
+};
