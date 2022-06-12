0dfb605dfd7b7e60ea2497f813b17c4a65eae7bf
TH Huang <tzhsn.huang@gmail.com>
2022-06-12 19:30:23 +0800

    [leetcode] maximum erasure value: alternative

--- solutions/leetcode/maximum-erasure-value/main.cpp
+++ solutions/leetcode/maximum-erasure-value/main.cpp
@@ -23,3 +23,27 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        vector<int> prefix_sum(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); ++i) {
+            prefix_sum[i + 1] = prefix_sum[i] + nums[i];
+        }
+
+        int result = 0;
+        unordered_map<int, int> last_index;
+        int l = -1;
+        for (int i = 0; i < nums.size(); ++i) {
+            int v = nums[i];
+            if (auto it = last_index.find(v); it != last_index.end()) {
+                l = max(l, it->second);
+            }
+            result = max(result, prefix_sum[i + 1] - prefix_sum[l + 1]);
+            last_index[v] = i;
+        }
+
+        return result;
+    }
+};
