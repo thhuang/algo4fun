b93f99e964e81786eb34f4d799d936f30ce4ac51
TH Huang <tzhsn.huang@gmail.com>
2022-06-13 14:57:15 +0800

    [leetcode] valid palindrome iii: optimize

--- solutions/leetcode/valid-palindrome-iii/main.cpp
+++ solutions/leetcode/valid-palindrome-iii/main.cpp
@@ -16,3 +16,24 @@ class Solution {
         return dp.back().back() + k >= n;
     }
 };
+
+class Solution {
+   public:
+    bool isValidPalindrome(string s, int k) {
+        int n = s.size();
+        string r(s.rbegin(), s.rend());
+        vector<int> dp(n + 1, 0);
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_dp(n + 1, 0);
+            for (int j = 0; j < n; ++j) {
+                if (s[i] == r[j]) {
+                    new_dp[j + 1] = dp[j] + 1;
+                } else {
+                    new_dp[j + 1] = max(dp[j + 1], new_dp[j]);
+                }
+            }
+            dp = move(new_dp);
+        }
+        return dp.back() + k >= n;
+    }
+};
