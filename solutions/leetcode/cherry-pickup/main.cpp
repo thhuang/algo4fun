091bcaa623033ee086871fe637f1d803c7a279bd
TH Huang <tzhsn.huang@gmail.com>
2022-06-14 17:43:16 +0800

    [leetcode] cherry pickup: optimize

--- solutions/leetcode/cherry-pickup/main.cpp
+++ solutions/leetcode/cherry-pickup/main.cpp
@@ -4,8 +4,7 @@ class Solution {
         int m = grid.size();
         int n = grid.front().size();
 
-        vector<vector<vector<int>>> dp(
-            m + n - 1, vector<vector<int>>(m, vector<int>(m, -1)));
+        vector<vector<vector<int>>> dp(m + n - 1, vector<vector<int>>(m, vector<int>(m, -1)));
         dp[0][0][0] = grid[0][0];
         for (int s = 1; s < m + n - 1; ++s) {
             for (int r1 = max(0, s - n + 1); r1 < m && r1 <= s; ++r1) {
@@ -30,6 +29,43 @@ class Solution {
         }
 
         int result = dp.back().back().back();
+        return ~result ? result : 0;
+    }
+};
+
+class Solution {
+   public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid.front().size();
+
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+        dp[0][0] = grid[0][0];
+        for (int s = 1; s < m + n - 1; ++s) {
+            vector<vector<int>> new_dp(m, vector<int>(m, -1));
+            for (int r1 = max(0, s - n + 1); r1 < m && r1 <= s; ++r1) {
+                for (int r2 = r1; r2 < m && r2 <= s; ++r2) {
+                    int c1 = s - r1;
+                    int c2 = s - r2;
+                    if (grid[r1][c1] == -1 || grid[r2][c2] == -1) continue;
+
+                    int v = -1;
+                    if (r1 > 0 && r2 > 0) v = max(v, dp[r1 - 1][r2 - 1]);
+                    if (r1 > 0 && s - r2 > 0) v = max(v, dp[r1 - 1][r2]);
+                    if (s - r1 > 0 && r2 > 0) v = max(v, dp[r1][r2 - 1]);
+                    if (s - r1 > 0 && s - r2 > 0) v = max(v, dp[r1][r2]);
+                    if (v == -1) continue;
+
+                    v += grid[r1][c1];
+                    if (r1 != r2 || c1 != c2) v += grid[r2][c2];
+
+                    new_dp[r1][r2] = v;
+                }
+            }
+            dp = move(new_dp);
+        }
+
+        int result = dp.back().back();
         return ~result ? result : 0;
     }
 };
