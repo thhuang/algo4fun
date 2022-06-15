880bc8ae85205bb5502fb84e25a737fe76b9b839
TH Huang <tzhsn.huang@gmail.com>
2022-06-15 15:10:34 +0800

    [leetcode] word break: refactor

--- solutions/leetcode/word-break/main.cpp
+++ solutions/leetcode/word-break/main.cpp
@@ -27,26 +27,20 @@ class Solution {
 class Solution {
    public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words;
-        for (string w : wordDict) words.insert(w);
+        unordered_set<string> valid(wordDict.begin(), wordDict.end());
 
-        vector<bool> valid(s.size(), false);
-        for (int r = 0; r < s.size(); ++r) {
-            int l = 0;
-            if (words.count(s.substr(l, r - l + 1))) {
-                valid[r] = true;
-                continue;
-            }
-
-            while (++l <= r) {
-                if (!valid[l - 1]) continue;
-                if (words.count(s.substr(l, r - l + 1))) {
-                    valid[r] = true;
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int r = 1; r <= s.size(); ++r) {
+            for (int l = 0; l < r; ++l) {
+                if (!dp[l]) continue;
+                if (valid.count(s.substr(l, r - l))) {
+                    dp[r] = true;
                     break;
                 }
             }
         }
 
-        return valid.back();
+        return dp.back();
     }
 };
