0b4aaa428675971db749c47319cd5941c30e0560
TH Huang <tzhsn.huang@gmail.com>
2022-06-18 15:16:20 +0800

    [leetcode] count ways to distribute candies

---
+++ count-ways-to-distribute-candies/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int waysToDistribute(int n, int k) {
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int i = 1; i <= n; ++i) {
+            for (int j = min(i, k); j > 0; --j) {
+                if (i == j) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = ((long long)j * dp[i - 1][j] % mod + dp[i - 1][j - 1]) % mod;
+                }
+            }
+        }
+        return dp[n][k];
+    }
+};
