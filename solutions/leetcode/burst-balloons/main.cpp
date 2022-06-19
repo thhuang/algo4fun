c85ecd1de35a987019d36e2c07faef7b62ad8959
TH Huang <tzhsn.huang@gmail.com>
2022-06-19 17:48:15 +0800

    [leetcode] burst balloons

---
+++ solutions/leetcode/burst-balloons/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+        for (int l = n; l >= 1; --l) {
+            for (int r = l; r <= n; ++r) {
+                for (int i = l; i <= r; ++i) {
+                    dp[l][r] = max(dp[l][r],
+                                   dp[l][i - 1] + dp[i + 1][r] + nums[l - 1] * nums[i] * nums[r + 1]);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+};
