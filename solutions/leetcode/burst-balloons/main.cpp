56b64fc764cdf65781b9206054ba999c3f831a36
TH Huang <tzhsn.huang@gmail.com>
2022-06-19 17:51:49 +0800

    [leetcode] burst balloons: refactor

--- solutions/leetcode/burst-balloons/main.cpp
+++ solutions/leetcode/burst-balloons/main.cpp
@@ -19,3 +19,26 @@ class Solution {
         return dp[1][n];
     }
 };
+
+class Solution {
+   public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2));
+        for (int d = 1; d <= n; ++d) {
+            for (int l = 1; l + d - 1 <= n; ++l) {
+                int r = l + d - 1;
+                for (int m = l; m <= r; ++m) {
+                    dp[l][r] = max(dp[l][r],
+                                   dp[l][m - 1] + dp[m + 1][r] + nums[l - 1] * nums[m] * nums[r + 1]);
+                }
+            }
+        }
+        
+        return dp[1][n];
+    }
+};
