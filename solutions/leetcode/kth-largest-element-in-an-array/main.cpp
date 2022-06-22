51a70646d29eeac6d167fcfc02d4d81f378fa48e
TH Huang <tzhsn.huang@gmail.com>
2022-06-22 09:51:48 +0800

    [leetcode] kth largest element in an array: refactor

--- solutions/leetcode/kth-largest-element-in-an-array/main.cpp
+++ solutions/leetcode/kth-largest-element-in-an-array/main.cpp
@@ -1,40 +1,32 @@
 class Solution {
    public:
     int findKthLargest(vector<int>& nums, int k) {
-        int target_index = nums.size() - k;
+        int n = nums.size();
+        int t = n - k;
 
-        auto partition = [&nums](int l, int r, int p) -> int {
-            int pivot = nums[p];
-            swap(nums[p], nums[r]);
-            int pl = l, pr = r - 1;
-            int i = l;
-            while (pl <= pr) {
-                if (nums[i] < pivot) {
-                    swap(nums[i++], nums[pl++]);
-                } else {  // nums[i] >= pivot
-                    swap(nums[i], nums[pr--]);
+        auto partition = [&](int l, int r, int i) -> int {
+            swap(nums[i], nums[r]);
+            int p = l;
+            for (int j = l; j < r; ++j) {
+                if (nums[j] < nums[r]) {
+                    swap(nums[j], nums[p++]);
                 }
             }
-            swap(nums[pl], nums[r]);
-            return pl;
+            swap(nums[p], nums[r]);
+            return p;
         };
 
-        int l = 0, r = nums.size() - 1;
-        while (true) {
-            if (l == r) return nums[l];
-
-            int p = l + rand() % (r - l + 1);
-            p = partition(l, r, p);
-
-            if (p == target_index) {
-                return nums[p];
-            } else if (p < target_index) {
-                l = p + 1;
-            } else {  // p > target_index
-                r = p - 1;
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            m = partition(l, r, m);
+            if (m <= t) {
+                l = m + 1;
+            } else {  // m > t
+                r = m - 1;
             }
         }
 
-        return 0;
+        return nums[r];
     }
 };
