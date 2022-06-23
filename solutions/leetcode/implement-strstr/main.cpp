2c6e25d3d292ac833c848388b46c6460dbe07d27
TH Huang <tzhsn.huang@gmail.com>
2022-06-23 23:40:24 +0800

    [leetcode] implement strStr(): refactor

--- solutions/leetcode/implement-strstr/main.cpp
+++ solutions/leetcode/implement-strstr/main.cpp
@@ -9,15 +9,14 @@ class Solution {
             prefix[i] = d;
         }
 
-        int i = 0;  // haystack
-        int j = 0;  // needle
+        int i = 0, j = 0;
         while (i < haystack.size()) {
             if (haystack[i] == needle[j]) {
                 ++i, ++j;
                 if (j == needle.size()) return i - needle.size();
                 continue;
             }
-            j != 0 ? j = prefix[j - 1] : ++i;
+            j == 0 ? ++i : j = prefix[j - 1];
         }
 
         return -1;
