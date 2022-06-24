6b824bfa4ad40120db761957090509292c82a013
TH Huang <tzhsn.huang@gmail.com>
2022-06-24 15:21:29 +0800

    [leetcode] partition to k equal sum subsets: refactor

--- solutions/leetcode/partition-to-k-equal-sum-subsets/main.cpp
+++ solutions/leetcode/partition-to-k-equal-sum-subsets/main.cpp
@@ -1,37 +1,28 @@
 class Solution {
    public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int mx = 0;
-        int total = 0;
-        for (int v : nums) {
-            mx = max(mx, v);
-            total += v;
-        }
+        int mx = 0, total = 0;
+        for (int v : nums) max(mx, v), total += v;
         if (total % k != 0 || mx > total / k) return false;
-
-        sort(nums.begin(), nums.end(), greater<>());
+        int target = total / k;
 
         unordered_map<int, bool> cache;
         function<bool(int, int, int, int)> search =
-            [&](int i, int target, int count, int taken) -> bool {
-            if (count == k - 1) return true;
-            if (target < 0) return false;
-
-            if (cache.count(taken)) return cache[taken];
-            if (target == 0)
-                return cache[taken] = search(0, total / k, count + 1, taken);
-
+            [&](int i, int sum, int count, int vis) -> bool {
+            if (count == k) return true;
+            if (sum > target) return false;
+            if (auto it = cache.find(vis); it != cache.end()) return it->second;
+            if (sum == target) return cache[vis] = search(0, 0, count + 1, vis);
             if (i == nums.size()) return false;
 
             int mask = 1 << i;
-            if ((taken & mask) == 0 &&
-                search(i + 1, target - nums[i], count, taken | mask))
+            if ((vis & mask) == 0 &&
+                search(i + 1, sum + nums[i], count, vis | mask))
                 return true;
-            if (search(i + 1, target, count, taken)) return true;
-
-            return cache[taken] = false;
+            if (search(i + 1, sum, count, vis)) return true;
+            return cache[vis] = false;
         };
 
-        return search(0, total / k, 0, 0);
+        return search(0, 0, 0, 0);
     }
 };
