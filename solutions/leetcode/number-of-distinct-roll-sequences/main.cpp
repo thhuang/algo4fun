5d4572f6f83aabb1603d5450368d7ae10ccb2b81
TH Huang <tzhsn.huang@gmail.com>
2022-06-26 10:28:23 +0800

    [leetcode] number of distinct roll sequences: optimize

--- solutions/leetcode/number-of-distinct-roll-sequences/main.cpp
+++ solutions/leetcode/number-of-distinct-roll-sequences/main.cpp
@@ -33,3 +33,40 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int distinctSequences(int n) {
+        vector<vector<int>> valid = {
+            {},        {2, 3, 4, 5, 6}, {1, 3, 5}, {1, 2, 4, 5},
+            {1, 3, 5}, {1, 2, 3, 4, 6}, {1, 5}};
+
+        vector<vector<int>> dp(7, vector<int>(7, 0));
+        for (int i = 1; i <= 6; ++i) dp[i][i] = 1;
+        for (int i = 1; i < n; ++i) {
+            vector<vector<int>> new_dp(7, vector<int>(7, 0));
+            for (int p0 = 1; p0 <= 6; ++p0) {
+                for (int p1 : valid[p0]) {
+                    long long v = 0;
+                    for (int p2 = 1; p2 <= 6; ++p2) {
+                        if (p2 == p0) continue;
+                        v = (v + dp[p1][p2]) % mod;
+                    }
+                    new_dp[p0][p1] = v;
+                }
+            }
+            dp = move(new_dp);
+        }
+
+        long long result = 0;
+        for (int i = 1; i <= 6; ++i) {
+            for (int j = 1; j <= 6; ++j) {
+                result = (result + dp[i][j]) % mod;
+            }
+        }
+
+        return result;
+    }
+};
