52422df586c7b79bc491427dd68a6320b7d3981b
TH Huang <tzhsn.huang@gmail.com>
2022-06-26 17:29:36 +0800

    [leetcode] largest bst subtree

---
+++ solutions/leetcode/largest-bst-subtree/main.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+    struct Res {
+        int mn, mx, sz = 0;
+    };
+
+   public:
+    int largestBSTSubtree(TreeNode* root) {
+        int result = 0;
+
+        function<Res(TreeNode*)> dfs = [&](TreeNode* u) -> Res {
+            if (u == nullptr) return Res();
+
+            Res res = {u->val, u->val, 1};
+            if (u->left) {
+                auto l = dfs(u->left);
+                if (l.sz && l.mx < u->val) {
+                    res.mn = l.mn;
+                    res.sz += l.sz;
+                } else {
+                    res.sz = 0;
+                }
+            }
+            if (u->right) {
+                auto r = dfs(u->right);
+                if (res.sz && r.sz && u->val < r.mn) {
+                    res.mx = r.mx;
+                    res.sz += r.sz;
+                } else {
+                    res.sz = 0;
+                }
+            }
+
+            result = max(result, res.sz);
+
+            return res;
+        };
+
+        dfs(root);
+
+        return result;
+    }
+};
