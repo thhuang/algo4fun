618cf797a39f67ebf06a62b59764422ef9247c46
TH Huang <tzhsn.huang@gmail.com>
2022-06-29 16:12:09 +0800

    [leetcode] two sum: alternative

--- solutions/leetcode/two-sum/main.cpp
+++ solutions/leetcode/two-sum/main.cpp
@@ -9,3 +9,16 @@ class Solution {
         return {};
     }
 };
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (auto it = seen.find(nums[i]); it != seen.end())
+                return {it->second, i};
+            seen[target - nums[i]] = i;
+        }
+        return {};
+    }
+};
