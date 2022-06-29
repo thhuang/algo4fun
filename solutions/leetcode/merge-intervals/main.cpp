fb5a2225b4a95ece39046606b070e4cd194cb69a
TH Huang <tzhsn.huang@gmail.com>
2022-06-29 17:06:33 +0800

    [leetcode] merge intervals: refactor

--- solutions/leetcode/merge-intervals/main.cpp
+++ solutions/leetcode/merge-intervals/main.cpp
@@ -2,16 +2,14 @@ class Solution {
    public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-
         vector<vector<int>> result;
-        for (const vector<int>& interval : intervals) {
-            if (result.size() == 0 || result.back()[1] < interval[0]) {
-                result.push_back(interval);
-                continue;
+        for (const vector<int>& in : intervals) {
+            if (result.empty() || result.back()[1] < in[0]) {
+                result.push_back(in);
+            } else {
+                result.back()[1] = max(result.back()[1], in[1]);
             }
-            result.back()[1] = max(result.back()[1], interval[1]);
         }
-
         return result;
     }
 };
