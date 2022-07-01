ef2e7b9325b351d7cec2447a3503794ca579a51f
TH Huang <tzhsn.huang@gmail.com>
2022-07-01 12:19:52 +0800

    [leetcode] paint house: alternative

--- solutions/leetcode/paint-house/main.cpp
+++ solutions/leetcode/paint-house/main.cpp
@@ -10,3 +10,24 @@ class Solution {
         return *min_element(dp.begin(), dp.end());
     }
 };
+
+class Solution {
+    const int m = 3;
+
+   public:
+    int minCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        vector<int> dp(m, 0);
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_dp(m, numeric_limits<int>::max());
+            for (int prev = 0; prev < m; ++prev) {
+                for (int curr = 0; curr < m; ++curr) {
+                    if (prev == curr) continue;
+                    new_dp[curr] = min(new_dp[curr], dp[prev] + costs[i][curr]);
+                }
+            }
+            dp = move(new_dp);
+        }
+        return *min_element(dp.begin(), dp.end());
+    }
+};
