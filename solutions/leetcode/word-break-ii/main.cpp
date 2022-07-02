613da4b4ece8b3201c2ce4acc98c3d582026f172
TH Huang <tzhsn.huang@gmail.com>
2022-07-02 19:14:17 +0800

    [leetcode] word break ii: refactor

--- solutions/leetcode/word-break-ii/main.cpp
+++ solutions/leetcode/word-break-ii/main.cpp
@@ -1,39 +1,38 @@
 class Solution {
    public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        unordered_set<string_view> valid(wordDict.begin(), wordDict.end());
+        unordered_set<string_view> valid;
+        for (const string& w : wordDict) valid.emplace(w);
 
+        int n = s.size();
         vector<vector<int>> dp(n);
         for (int r = 0; r < n; ++r) {
             string sub = s.substr(0, r + 1);
-            string_view w(sub);
-            if (valid.count(w)) dp[r].push_back(0);
-            for (int l = 1; l <= r; ++l) {
-                w.remove_prefix(1);
-                if (dp[l - 1].size() && valid.count(w)) dp[r].push_back(l);
+            string_view sv(sub);
+            for (int l = 0; l <= r; ++l, sv.remove_prefix(1)) {
+                if (l != 0 && dp[l - 1].empty()) continue;
+                if (valid.count(sv) == 0) continue;
+                dp[r].push_back(l);
             }
         }
 
         vector<string> result;
-        deque<int> breakpoints;
-        function<void(int)> dfs = [&](int r) -> void {
-            if (r < 0) {
-                string sentence;
-                for (int i = 0; i < breakpoints.size() - 1; ++i) {
-                    int l = breakpoints[i];
-                    int r = breakpoints[i + 1];
-                    sentence += s.substr(l, r - l) + " ";
+        vector<int> starts;
+        function<void(int)> dfs = [&](int i) -> void {
+            if (i == -1) {
+                string sentance;
+                for (auto it = starts.rbegin(); it != prev(starts.rend()); ++it) {
+                    sentance += s.substr(*it, *next(it) - *it) + " ";
                 }
-                sentence += s.substr(breakpoints.back());
-                result.push_back(sentence);
+                sentance += s.substr(*starts.begin());
+                result.push_back(sentance);
                 return;
             }
 
-            for (int l : dp[r]) {
-                breakpoints.push_front(l);
-                dfs(l - 1);
-                breakpoints.pop_front();
+            for (int v : dp[i]) {
+                starts.push_back(v);
+                dfs(v - 1);
+                starts.pop_back();
             }
         };
 
