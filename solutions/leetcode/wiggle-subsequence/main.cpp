6c515c0e3cb368df8aeba8820a4a7b1440aa5a99
TH Huang <tzhsn.huang@gmail.com>
2022-07-03 11:57:22 +0800

    [leetcode] wiggle subsequence: alternative

--- solutions/leetcode/wiggle-subsequence/main.cpp
+++ solutions/leetcode/wiggle-subsequence/main.cpp
@@ -17,3 +17,21 @@ class Solution {
         return max(dp.pos, dp.neg);
     }
 };
+
+class Solution {
+   public:
+    int wiggleMaxLength(vector<int>& nums) {
+        auto process = [&](bool pos) -> int {
+            int c = 1;
+            for (int i = 1, v = nums.front(); i < nums.size(); ++i) {
+                if ((pos && nums[i] > v) || (!pos && nums[i] < v)) {
+                    pos = !pos;
+                    ++c;
+                }
+                v = nums[i];
+            }
+            return c;
+        };
+        return max(process(true), process(false));
+    }
+};
