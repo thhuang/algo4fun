2a03203d7362c62d16bdc582ace503c5c6fb9393
TH Huang <tzhsn.huang@gmail.com>
2022-07-05 21:09:01 +0800

    [leetcode] longest consecutive sequence: refactor

--- solutions/leetcode/longest-consecutive-sequence/main.cpp
+++ solutions/leetcode/longest-consecutive-sequence/main.cpp
@@ -15,3 +15,31 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> lr;
+        unordered_map<int, int> rl;
+
+        for (int v : nums) {
+            int r = v;
+            if (auto it = lr.find(v + 1); it != lr.end()) {
+                r = max(r, it->second);
+            }
+
+            int l = v;
+            if (auto it = rl.find(v - 1); it != rl.end()) {
+                l = min(l, it->second);
+            }
+
+            lr[l] = lr.count(l) ? max(lr[l], r) : r;
+            rl[r] = rl.count(r) ? min(rl[r], l) : l;
+        }
+
+        int result = 0;
+        for (const auto [l, r] : lr) result = max(result, r - l + 1);
+
+        return result;
+    }
+};
