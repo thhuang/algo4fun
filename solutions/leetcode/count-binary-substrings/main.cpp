95da70821ab3d46d3539b1673152354ef3fb20e3
TH Huang <tzhsn.huang@gmail.com>
2022-07-08 17:27:17 +0800

    [leetcode] count binary substrings: alternative

--- solutions/leetcode/count-binary-substrings/main.cpp
+++ solutions/leetcode/count-binary-substrings/main.cpp
@@ -12,3 +12,21 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int countBinarySubstrings(string s) {
+        int result = 0;
+        array<int, 2> counts = {0, 1};
+        for (int i = 1; i < s.size(); ++i) {
+            if (s[i - 1] == s[i]) {
+                ++counts[1];
+            } else {
+                result += min(counts[0], counts[1]);
+                counts = {counts[1], 1};
+            }
+        }
+        result += min(counts[0], counts[1]);
+        return result;
+    }
+};
