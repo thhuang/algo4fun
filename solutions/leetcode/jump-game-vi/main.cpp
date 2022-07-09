1f72c660080e9015ada73094d0b0225ebfdf94fe
TH Huang <tzhsn.huang@gmail.com>
2022-07-09 10:56:58 +0800

    [leetcode] jump game vi: refactor

--- solutions/leetcode/jump-game-vi/main.cpp
+++ solutions/leetcode/jump-game-vi/main.cpp
@@ -3,18 +3,18 @@ class Solution {
     int maxResult(vector<int>& nums, int k) {
         int n = nums.size();
 
-        vector<int> dp(n, -1e9);
+        vector<int> score(n, -1e9);
         multiset<int> window;
 
-        dp.front() = nums.front();
-        window.emplace(dp.front());
+        score.front() = nums.front();
+        window.emplace(score.front());
 
         for (int i = 1; i < n; ++i) {
-            dp[i] = *window.rbegin() + nums[i];
-            window.emplace(dp[i]);
-            if (window.size() > k) window.erase(window.find(dp[i - k]));
+            score[i] = *window.rbegin() + nums[i];
+            window.emplace(score[i]);
+            if (window.size() > k) window.erase(window.find(score[i - k]));
         }
 
-        return dp.back();
+        return score.back();
     }
 };
