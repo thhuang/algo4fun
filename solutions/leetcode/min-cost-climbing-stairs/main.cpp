1182ccd615f006e288e950245a6fe1c45f5a8dd6
TH Huang <tzhsn.huang@gmail.com>
2022-07-10 12:00:31 +0800

    [leetcode] min cost climbing stairs: alternative

--- solutions/leetcode/min-cost-climbing-stairs/main.cpp
+++ solutions/leetcode/min-cost-climbing-stairs/main.cpp
@@ -8,3 +8,14 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        array<int, 2> dp = {cost[0], cost[1]};
+        for (int i = 2; i < cost.size(); ++i) {
+            dp = {dp[1], min(dp[0], dp[1]) + cost[i]};
+        }
+        return min(dp[0], dp[1]);
+    }
+};
