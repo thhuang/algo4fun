91b8f684c8122a141b01f047e3fa9f1ceeb2f2a5
TH Huang <tzhsn.huang@gmail.com>
2022-07-11 23:51:26 +0800

    [leetcode] count the number of ideal arrays: optimize

--- solutions/leetcode/count-the-number-of-ideal-arrays/main.cpp
+++ solutions/leetcode/count-the-number-of-ideal-arrays/main.cpp
@@ -51,3 +51,53 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+    long long dp[10001][14];
+
+   public:
+    int idealArrays(int n, int maxValue) {
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= 13; ++j) {
+                for (int t = 0; t <= j; ++t) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - t]) % mod;
+                }
+            }
+        }
+
+        vector<int> primes = move(generatePrimes(maxValue));
+
+        long long result = 0;
+        for (int i = 1; i <= maxValue; ++i) {
+            long long v = 1;
+            for (int p : primes) {
+                int x = i, count = 0;
+                while (x % p == 0) {
+                    x /= p;
+                    ++count;
+                }
+                v = v * dp[n][count] % mod;
+            }
+            result = (result + v) % mod;
+        }
+
+        return result;
+    }
+
+    vector<int> generatePrimes(int n) {
+        vector<bool> primes(n + 1, true);
+        primes[0] = primes[1] = false;
+
+        vector<int> result;
+        for (int i = 2; i <= n; ++i) {
+            if (!primes[i]) continue;
+            result.push_back(i);
+            for (int j = i + i; j <= n; j += i) primes[j] = false;
+        }
+
+        return result;
+    }
+};
