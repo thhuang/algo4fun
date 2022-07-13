c086f30f4b35af0da00428c61b00881059d50576
TH Huang <tzhsn.huang@gmail.com>
2022-07-13 11:16:35 +0200

    [leetcode] reorganize string: optimize

--- solutions/leetcode/reorganize-string/main.cpp
+++ solutions/leetcode/reorganize-string/main.cpp
@@ -33,3 +33,38 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    string reorganizeString(string s) {
+        array<int, 26> counts;
+        fill(counts.begin(), counts.end(), 0);
+        for (char c : s) ++counts[c - 'a'];
+
+        priority_queue<pair<int, char>> pq;
+        for (int i = 0; i < 26; ++i) {
+            if (counts[i]) pq.push({counts[i], 'a' + i});
+        }
+
+        if (pq.top().first > (s.size() + 1) / 2) return "";
+
+        deque<pair<int, char>> buf;
+
+        string result;
+        while (!pq.empty()) {
+            auto [cnt, ch] = pq.top();
+            pq.pop();
+
+            result += ch;
+
+            while (!buf.empty()) {
+                pq.push(buf.front());
+                buf.pop_front();
+            }
+
+            if (cnt > 1) buf.push_back({cnt - 1, ch});
+        }
+
+        return result;
+    }
+};
