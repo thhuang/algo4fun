ed641670579a56fd843c3fc179d89cd8f0512c65
TH Huang <tzhsn.huang@gmail.com>
2022-07-15 20:13:34 +0200

    [leetcode] shortest distance to target color: refactor

--- solutions/leetcode/shortest-distance-to-target-color/main.cpp
+++ solutions/leetcode/shortest-distance-to-target-color/main.cpp
@@ -1,27 +1,33 @@
 class Solution {
+    const int INF = numeric_limits<int>::max();
+
    public:
     vector<int> shortestDistanceColor(vector<int>& colors,
                                       vector<vector<int>>& queries) {
         int n = colors.size();
-        vector<vector<int>> color_dist(3, vector<int>(n, n));
-
-        for (int c = 0; c < 3; ++c) {
-            for (int i = 0, d = n; i < n; ++i) {
-                color_dist[c][i] = min(color_dist[c][i], ++d);
-                if (c == colors[i] - 1) color_dist[c][i] = d = 0;
+        vector<array<int, 4>> dist(n, {INF, INF, INF});
+        for (int i = 0; i < n; ++i) {
+            dist[i][colors[i]] = 0;
+            if (i == 0) continue;
+            for (int c = 1; c <= 3; ++c) {
+                if (dist[i - 1][c] == INF || c == colors[i]) continue;
+                dist[i][c] = dist[i - 1][c] + 1;
             }
-            for (int i = n - 1, d = n; ~i; --i) {
-                color_dist[c][i] = min(color_dist[c][i], ++d);
-                if (c == colors[i] - 1) color_dist[c][i] = d = 0;
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            dist[i][colors[i]] = 0;
+            if (i == n - 1) continue;
+            for (int c = 1; c <= 3; ++c) {
+                if (dist[i + 1][c] == INF || c == colors[i]) continue;
+                dist[i][c] = min(dist[i][c], dist[i + 1][c] + 1);
             }
         }
 
-        vector<int> result(queries.size());
-        for (int i = 0; i < queries.size(); ++i) {
-            result[i] = color_dist[queries[i][1] - 1][queries[i][0]];
-            if (result[i] >= n) result[i] = -1;
+        vector<int> result;
+        for (const vector<int>& q : queries) {
+            int v = dist[q[0]][q[1]];
+            result.push_back(v == INF ? -1 : v);
         }
-
         return result;
     }
 };
@@ -31,39 +37,23 @@ class Solution {
     vector<int> shortestDistanceColor(vector<int>& colors,
                                       vector<vector<int>>& queries) {
         int n = colors.size();
-        vector<vector<int>> color_dist(n, vector<int>(3, -1));
+        vector<array<int, 3>> dist(n, {n, n, n});
 
-        color_dist.front()[colors.front() - 1] = 0;
-        for (int i = 1; i < n; ++i) {
-            int ci = colors[i] - 1;
-            color_dist[i][ci] = 0;
-            for (int j = 0; j < 3; ++j) {
-                if (color_dist[i - 1][j] == -1) continue;
-                if (color_dist[i][j] == -1)
-                    color_dist[i][j] = color_dist[i - 1][j] + 1;
-                else
-                    color_dist[i][j] =
-                        min(color_dist[i][j], color_dist[i - 1][j] + 1);
+        for (int c = 0; c < 3; ++c) {
+            for (int i = 0, d = n; i < n; ++i) {
+                dist[i][c] = ++d;
+                if (c + 1 == colors[i]) dist[i][c] = d = 0;
+            }
+            for (int i = n - 1, d = n; i >= 0; --i) {
+                dist[i][c] = min(dist[i][c], ++d);
+                if (c + 1 == colors[i]) dist[i][c] = d = 0;
             }
         }
 
-        color_dist.back()[colors.back() - 1] = 0;
-        for (int i = n - 2; ~i; --i) {
-            int ci = colors[i] - 1;
-            color_dist[i][ci] = 0;
-            for (int j = 0; j < 3; ++j) {
-                if (color_dist[i + 1][j] == -1) continue;
-                if (color_dist[i][j] == -1)
-                    color_dist[i][j] = color_dist[i + 1][j] + 1;
-                else
-                    color_dist[i][j] =
-                        min(color_dist[i][j], color_dist[i + 1][j] + 1);
-            }
-        }
-
-        vector<int> result(queries.size());
-        for (int i = 0; i < queries.size(); ++i) {
-            result[i] = color_dist[queries[i][0]][queries[i][1] - 1];
+        vector<int> result;
+        for (const vector<int>& q : queries) {
+            int v = dist[q[0]][q[1] - 1];
+            result.push_back(v >= n ? -1 : v);
         }
 
         return result;
