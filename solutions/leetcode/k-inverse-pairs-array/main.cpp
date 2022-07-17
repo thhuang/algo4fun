ae6f78a28ab9254d89ed31bf8df389f68c4d34a7
TH Huang <tzhsn.huang@gmail.com>
2022-07-17 19:18:19 +0200

    [leetcode] k inverse pairs array

---
+++ solutions/leetcode/k-inverse-pairs-array/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int i = 0; i <= n; ++i) dp[i][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                for (int r = 0; r <= min(j, i - 1); ++r) {
+                    dp[i][j] = ((long long)dp[i][j] + dp[i - 1][j - r]) % mod;
+                }
+            }
+        }
+        return dp.back().back();
+    }
+};
