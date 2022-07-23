7f112426db4c49a754d4ac5227235d9ddb3a0bb2
TH Huang <tzhsn.huang@gmail.com>
2022-07-24 00:39:30 +0200

    [leetcode] count of smaller numbers after self: segment tree

--- solutions/leetcode/count-of-smaller-numbers-after-self/main.cpp
+++ solutions/leetcode/count-of-smaller-numbers-after-self/main.cpp
@@ -35,3 +35,43 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> countSmaller(vector<int>& nums) {
+        auto p = minmax_element(nums.begin(), nums.end());
+        int mn = *p.first, mx = *p.second;
+
+        int n = 1;
+        while (n < mx - mn + 1) n *= 2;
+        vector<int> tree(n * 2, 0);
+
+        auto sum = [&](int a, int b) -> int {
+            a += n, b += n;
+            int s = 0;
+            while (a <= b) {
+                if (a % 2 == 1) s += tree[a++];
+                if (b % 2 == 0) s += tree[b--];
+                a /= 2, b /= 2;
+            }
+            return s;
+        };
+
+        auto add = [&](int k, int x) -> void {
+            k += n;
+            tree[k] += x;
+            for (k /= 2; k >= 1; k /= 2) {
+                tree[k] = tree[k * 2] + tree[k * 2 + 1];
+            }
+        };
+
+        vector<int> result(nums.size());
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int k = nums[i] - mn;
+            add(k, 1);
+            result[i] = sum(0, k - 1);
+        }
+
+        return result;
+    }
+};
