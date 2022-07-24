b65e876b0a865d8bb5d458c645d697b907142cc4
TH Huang <tzhsn.huang@gmail.com>
2022-07-24 10:11:44 +0200

    [leetcode] search a 2d matrix ii: refactor

--- solutions/leetcode/search-a-2d-matrix-ii/main.cpp
+++ solutions/leetcode/search-a-2d-matrix-ii/main.cpp
@@ -1,16 +1,15 @@
 class Solution {
    public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix.front().size();
-        int i = 0, j = m - 1;
-        while (i < n && j >= 0) {
-            if (target == matrix[i][j]) return true;
-            if (target < matrix[i][j]) {
-                --j;
-            } else {  // target > matrix[i][j]
+        int m = matrix.size(), n = matrix.front().size();
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            const int& v = matrix[i][j];
+            if (v == target) return true;
+            if (v < target)
                 ++i;
-            }
+            else  // v > target
+                --j;
         }
         return false;
     }
