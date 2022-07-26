2a102b69da14b6dab5bea2fb9f0f4e9567a5b24c
TH Huang <tzhsn.huang@gmail.com>
2022-07-27 00:00:56 +0200

    [leetcode] lowest common ancestor of a binary tree: refactor

--- solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/main.cpp
+++ solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/main.cpp
@@ -11,27 +11,11 @@ class Solution {
    public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (!root) return nullptr;
-        auto left = lowestCommonAncestor(root->left, p, q);
-        auto right = lowestCommonAncestor(root->right, p, q);
-        if (root == p || root == q) return root;
-        if (left && right) return root;
-        if (!left && !right) return nullptr;
-        return left ? left : right;
-    }
-};
-
-class Solution {
-   public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root) return nullptr;
         auto l = lowestCommonAncestor(root->left, p, q);
         auto r = lowestCommonAncestor(root->right, p, q);
-        if (l && r) return root;
-        if (!l && !r) {
-            if (root == p || root == q) return root;
-            return nullptr;
-        }
         if (root == p || root == q) return root;
+        if (l && r) return root;
+        if (!l && !r) nullptr;
         return l ? l : r;
     }
 };
