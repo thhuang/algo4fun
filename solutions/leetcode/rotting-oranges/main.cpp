dd9a0e8fe60a2ad0ffb1489516a7f536edb4ff7a
TH Huang <tzhsn.huang@gmail.com>
2022-08-02 22:24:12 +0200

    [leetcode] rotting oranges: alternative

--- solutions/leetcode/rotting-oranges/main.cpp
+++ solutions/leetcode/rotting-oranges/main.cpp
@@ -38,3 +38,46 @@ class Solution {
         return ans;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+   public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid.front().size();
+
+        queue<array<int, 3>> q;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 2) q.push({i, j, 0});
+            }
+        }
+
+        vector<vector<int>> time(m, vector<int>(n, -1));
+        while (!q.empty()) {
+            auto [i, j, t] = q.front();
+            q.pop();
+
+            if (time[i][j] != -1) continue;
+            time[i][j] = t;
+
+            for (auto [di, dj] : directions) {
+                int ii = i + di, jj = j + dj;
+                if (ii < 0 || ii >= m || jj < 0 || jj >= n) continue;
+                if (grid[ii][jj] == 0 || time[ii][jj] != -1) continue;
+                q.push({ii, jj, t + 1});
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (time[i][j] == -1 && grid[i][j] == 1) return -1;
+                result = max(result, time[i][j]);
+            }
+        }
+
+        return result;
+    }
+};
