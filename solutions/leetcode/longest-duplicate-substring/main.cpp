f776152743259f2040c4cfe8f8aaf754ab6b5b04
TH Huang <tzhsn.huang@gmail.com>
2022-08-03 13:31:53 +0200

    [leetcode] longest duplicate substring: refactor

--- solutions/leetcode/longest-duplicate-substring/main.cpp
+++ solutions/leetcode/longest-duplicate-substring/main.cpp
@@ -53,3 +53,60 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    typedef long long ll;
+
+    const int base = 26;
+    const int mod = 1e9 + 7;
+
+   public:
+    string longestDupSubstring(string s) {
+        string result;
+
+        vector<ll> powers(s.size(), 1);
+        for (ll i = 1; i < s.size(); ++i) {
+            powers[i] = powers[i - 1] * base % mod;
+        }
+
+        auto check = [&](int n) -> bool {
+            unordered_map<ll, vector<int>> seen;
+
+            ll v = 0;
+            for (int i = n - 2, j = 1; i >= 0; --i, ++j) {
+                v = (v + (s[i] - 'a') * powers[j] % mod) % mod;
+            }
+
+            for (int i = n - 1; i < s.size(); ++i) {
+                int l = i - n + 1;
+                v = (v + s[i] - 'a') % mod;
+
+                if (auto it = seen.find(v); it != seen.end()) {
+                    for (int j : it->second) {
+                        if (s.compare(l, n, s, j, n) != 0) continue;
+                        if (n > result.size()) result = s.substr(i - n + 1, n);
+                        return true;
+                    }
+                }
+                seen[v].push_back(i - n + 1);
+
+                v = (v - (s[l] - 'a') * powers[n - 1] % mod + mod) % mod;
+                v = v * base % mod;
+            }
+
+            return false;
+        };
+
+        int l = 0, r = s.size();
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (check(m)) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return result;
+    }
+};
