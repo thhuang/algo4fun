0b8a64862a95a0fff0210f36775d7daec7843088
TH Huang <tzhsn.huang@gmail.com>
2022-08-03 14:54:58 +0200

    [leetcode] minimum swaps to group all 1s together: alternative

--- solutions/leetcode/minimum-swaps-to-group-all-1s-together/main.cpp
+++ solutions/leetcode/minimum-swaps-to-group-all-1s-together/main.cpp
@@ -15,3 +15,24 @@ class Solution {
         return total - max_in_window;
     }
 };
+
+class Solution {
+   public:
+    int minSwaps(vector<int>& data) {
+        int total = 0;
+        for (int v : data) total += v;
+        if (total <= 1) return 0;
+
+        int window = 0;
+        for (int i = 0; i < total - 1; ++i) window += data[i];
+
+        int result = data.size();
+        for (int i = total - 1; i < data.size(); ++i) {
+            window += data[i];
+            result = min(result, total - window);
+            window -= data[i - total + 1];
+        }
+
+        return result;
+    }
+};
