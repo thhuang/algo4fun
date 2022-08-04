1ec0a33f27c64970017df96ade40b2434f1b40c8
TH Huang <tzhsn.huang@gmail.com>
2022-08-04 17:33:30 +0200

    [leetcode] maximum length of subarray with positive product

--- solutions/leetcode/maximum-length-of-subarray-with-positive-product/main.cpp
+++ solutions/leetcode/maximum-length-of-subarray-with-positive-product/main.cpp
@@ -20,3 +20,26 @@ class Solution {
         return max_pos;
     }
 };
+
+class Solution {
+    int search(const vector<int>& nums, int l, int r) {
+        vector<int> negs;
+        for (int i = l; i < r; ++i) {
+            if (nums[i] < 0) negs.push_back(i);
+        }
+        if (negs.size() % 2 == 0) return r - l;
+        return r - l - min(negs.front() - l + 1, r - negs.back());
+    }
+
+   public:
+    int getMaxLen(vector<int>& nums) {
+        int result = 0;
+        int l = 0;
+        for (int r = 0; r < nums.size(); ++r) {
+            if (nums[r] != 0) continue;
+            result = max(result, search(nums, l, r));
+            l = r + 1;
+        }
+        return max(result, search(nums, l, nums.size()));
+    }
+};
