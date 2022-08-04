0c212312dc5124de807509ba418caa11bf1002e9
TH Huang <tzhsn.huang@gmail.com>
2022-08-04 21:35:22 +0200

    [leetcode] design parking system: alternative

--- solutions/leetcode/design-parking-system/main.cpp
+++ solutions/leetcode/design-parking-system/main.cpp
@@ -9,6 +9,21 @@ class ParkingSystem {
     bool addCar(int carType) { return --spots[carType] >= 0; }
 };
 
+class ParkingSystem {
+    array<int, 3> quota;
+
+   public:
+    ParkingSystem(int big, int medium, int small)
+        : quota({big, medium, small}) {}
+
+    bool addCar(int carType) {
+        int& q = quota[carType - 1];
+        if (q == 0) return false;
+        --q;
+        return true;
+    }
+};
+
 /**
  * Your ParkingSystem object will be instantiated and called as such:
  * ParkingSystem* obj = new ParkingSystem(big, medium, small);
