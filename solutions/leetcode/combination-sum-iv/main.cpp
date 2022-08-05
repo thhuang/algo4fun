6d9aa3e539fdbacdf0a3b3dce81b089f573f4b22
TH Huang <tzhsn.huang@gmail.com>
2022-08-05 08:53:58 +0200

    [leetcode] combination sum iv: alternative

--- solutions/leetcode/combination-sum-iv/main.cpp
+++ solutions/leetcode/combination-sum-iv/main.cpp
@@ -29,3 +29,19 @@ class Solution {
         return calculate(target);
     }
 };
+
+class Solution {
+   public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned long long> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 0; i <= target; ++i) {
+            if (dp[i] == 0) continue;
+            for (int v : nums) {
+                if (target - i < v) continue;
+                dp[i + v] += dp[i];
+            }
+        }
+        return dp.back();
+    }
+};
