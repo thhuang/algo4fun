bd57972ce5d5fbe5d18fcd81655d2b42fecc2a3b
TH Huang <tzhsn.huang@gmail.com>
2022-08-07 12:58:01 +0200

    [leetcode] total appeal of a string: alternative

--- solutions/leetcode/total-appeal-of-a-string/main.cpp
+++ solutions/leetcode/total-appeal-of-a-string/main.cpp
@@ -22,3 +22,26 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    long long appealSum(string s) {
+        int n = s.size();
+        array<vector<int>, 26> char_indices;
+
+        for (int i = 0; i < n; ++i) {
+            char_indices[s[i] - 'a'].push_back(i);
+        }
+
+        long long result = 0;
+        for (int i = 0; i < 26; ++i) {
+            char_indices[i].push_back(n);
+            for (int j = 0; j < char_indices[i].size() - 1; ++j) {
+                result += (1LL + char_indices[i][j]) *
+                          (char_indices[i][j + 1] - char_indices[i][j]);
+            }
+        }
+
+        return result;
+    }
+};
