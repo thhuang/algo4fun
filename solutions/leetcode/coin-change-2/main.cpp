442689061cc2b9f0db7f00331efe4428512f3d06
TH Huang <tzhsn.huang@gmail.com>
2022-08-09 22:06:17 -0500

    [leetcode] coin change ii: alternative

--- solutions/leetcode/coin-change-2/main.cpp
+++ solutions/leetcode/coin-change-2/main.cpp
@@ -1,6 +1,28 @@
 class Solution {
    public:
     int change(int amount, vector<int>& coins) {
+        vector<vector<int>> cache(coins.size(), vector<int>(amount + 1, -1));
+
+        function<int(int, int)> dfs = [&](int i, int k) -> int {
+            if (k < 0) return 0;
+            if (k == 0) return 1;
+            if (cache[i][k] != -1) return cache[i][k];
+
+            int s = 0;
+            for (int j = i; j < coins.size(); ++j) {
+                s += dfs(j, k - coins[j]);
+            }
+
+            return cache[i][k] = s;
+        };
+
+        return dfs(0, amount);
+    }
+};
+
+class Solution {
+   public:
+    int change(int amount, vector<int>& coins) {
         vector<int> dp(amount + 1, 0);
         dp[0] = 1;
         for (int c : coins) {
