87256715e885ba3c4735f33516390045989e3103
TH Huang <tzhsn.huang@gmail.com>
2022-08-15 11:02:20 -0500

    [leetcode] valid palindrome

---
+++ solutions/leetcode/valid-palindrome/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {
+                ++l;
+            } else if (!isalnum(s[r])) {
+                --r;
+            } else if (tolower(s[l]) != tolower(s[r])) {
+                return false;
+            } else {
+                ++l, --r;
+            }
+        }
+        return true;
+    }
+};
