52b41dc490f769d7ef62f4c50a778e3614b8ca2e
TH Huang <tzhsn.huang@gmail.com>
2022-08-19 15:06:00 -0500

    [leetcode] split array into consecutive subsequences: optimize

--- solutions/leetcode/split-array-into-consecutive-subsequences/main.cpp
+++ solutions/leetcode/split-array-into-consecutive-subsequences/main.cpp
@@ -23,3 +23,48 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+    struct Entry {
+        int val, one, two, three;
+    };
+
+   public:
+    bool isPossible(vector<int>& nums) {
+        vector<Entry> dp = {{nums.front() - 2, 0, 0, 0},
+                            {nums.front() - 1, 0, 0, 0}};
+
+        function<void()> update = [&]() -> void {
+            if (dp[0].one) {
+                --dp[0].one;
+                ++dp[1].two;
+            } else if (dp[0].two) {
+                --dp[0].two;
+                ++dp[1].three;
+            } else if (dp[0].three) {
+                --dp[0].three;
+                ++dp[1].three;
+            } else {
+                ++dp[1].one;
+            }
+        };
+
+        for (int v : nums) {
+            int p = v - 1;
+            if (dp[0].val != p && dp[1].val != p) {
+                if (dp[0].one || dp[0].two || dp[1].one || dp[1].two)
+                    return false;
+                dp = {{p, 0, 0, 0}, {v, 1, 0, 0}};
+            } else if (dp[0].val == p) {
+                update();
+            } else {  // dp[1].val == p
+                if (dp[0].one || dp[0].two) return false;
+                dp = {dp[1], {v, 0, 0, 0}};
+                update();
+            }
+        }
+
+        return dp[0].one == 0 && dp[0].two == 0 && dp[1].one == 0 &&
+               dp[1].two == 0;
+    }
+};
