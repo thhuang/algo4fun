70703b03bcf170674a6e52b7a9bc44a6f1a5f5db
TH Huang <tzhsn.huang@gmail.com>
2022-08-20 16:44:43 -0500

    [leetcode] minimum number of refueling stops: optimize

--- solutions/leetcode/minimum-number-of-refueling-stops/main.cpp
+++ solutions/leetcode/minimum-number-of-refueling-stops/main.cpp
@@ -28,3 +28,39 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+   public:
+    int minRefuelStops(int target, int startFuel,
+                       vector<vector<int>>& stations) {
+        int n = stations.size();
+
+        vector<long long> dp(n + 1, 0);
+        dp[0] = startFuel;
+
+        for (int i = 1; i <= n; ++i) {
+            vector<long long> new_dp(n + 1, 0);
+            new_dp[0] = startFuel;
+
+            int position = stations[i - 1][0];
+            int fuel = stations[i - 1][1];
+
+            for (int j = 1; j <= i; ++j) {
+                if (dp[j - 1] >= position) {
+                    new_dp[j] = dp[j - 1] + fuel;
+                }
+                if (dp[j] >= position) {
+                    new_dp[j] = max(new_dp[j], dp[j]);
+                }
+            }
+
+            dp = move(new_dp);
+        }
+
+        for (int i = 0; i <= n; ++i) {
+            if (dp[i] >= target) return i;
+        }
+
+        return -1;
+    }
+};
