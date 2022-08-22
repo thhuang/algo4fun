b9a8c8f44a272d7e211ffc81d2adfa53d3ca876c
TH Huang <tzhsn.huang@gmail.com>
2022-08-22 11:39:57 -0500

    [leetcode] count primes: optimize

--- solutions/leetcode/count-primes/main.cpp
+++ solutions/leetcode/count-primes/main.cpp
@@ -11,3 +11,18 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int countPrimes(int n) {
+        int result = 0;
+        vector<bool> valid(n, true);
+        for (int i = 2; i < n; ++i) {
+            if (!valid[i]) continue;
+            ++result;
+            if (i > n / i) continue;
+            for (int j = i * i; j < n; j += i) valid[j] = false;
+        }
+        return result;
+    }
+};
