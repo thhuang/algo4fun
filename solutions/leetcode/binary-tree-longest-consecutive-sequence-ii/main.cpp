dd8087e188ab039266acfb089100791e804a39c8
TH Huang <tzhsn.huang@gmail.com>
2022-08-22 10:24:53 -0500

    [leetcode] binary tree longest consecutive sequence ii: alternative

--- solutions/leetcode/binary-tree-longest-consecutive-sequence-ii/main.cpp
+++ solutions/leetcode/binary-tree-longest-consecutive-sequence-ii/main.cpp
@@ -44,3 +44,44 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    struct Res {
+        int inc, dec;
+
+        int max() const { return ::max(inc, dec); }
+    };
+
+   public:
+    int longestConsecutive(TreeNode* root) {
+        int result = 0;
+
+        function<Res(TreeNode*)> dfs = [&](TreeNode* u) -> Res {
+            if (!u) return Res{0, 0};
+
+            auto l = dfs(u->left);
+            auto r = dfs(u->right);
+
+            bool linc = u->left && u->left->val + 1 == u->val;
+            bool ldec = u->left && u->left->val - 1 == u->val;
+            bool rinc = u->right && u->right->val + 1 == u->val;
+            bool rdec = u->right && u->right->val - 1 == u->val;
+
+            auto res = Res{1, 1};
+            if (linc) res.inc = max(res.inc, l.inc + 1);
+            if (ldec) res.dec = max(res.dec, l.dec + 1);
+            if (rinc) res.inc = max(res.inc, r.inc + 1);
+            if (rdec) res.dec = max(res.dec, r.dec + 1);
+            result = max(result, res.max());
+
+            if (linc && rdec) result = max(result, l.inc + r.dec + 1);
+            if (ldec && rinc) result = max(result, l.dec + r.inc + 1);
+
+            return res;
+        };
+
+        dfs(root);
+
+        return result;
+    }
+};
