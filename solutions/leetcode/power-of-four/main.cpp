61e5a192a780150c0c24e8f5f3131a3850cb86ad
TH Huang <tzhsn.huang@gmail.com>
2022-08-21 19:33:06 -0500

    [leetcode] power of four: optimize

--- solutions/leetcode/power-of-four/main.cpp
+++ solutions/leetcode/power-of-four/main.cpp
@@ -9,3 +9,31 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool isPowerOfFour(int n) {
+        long long v = 1;
+        while (v < n) v *= 4;
+        return v == n;
+    }
+};
+
+class Solution {
+   public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        double v = log(n) / log(4);
+        return v == (int)v;
+    }
+};
+
+class Solution {
+   public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        if (__builtin_popcount(n) != 1) return false;
+        if (__builtin_ctz(n) % 2) return false;
+        return true;
+    }
+};
