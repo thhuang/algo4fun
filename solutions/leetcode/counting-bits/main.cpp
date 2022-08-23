31d06dc63f1b96e5957d4ae286cad5ae96b34e97
TH Huang <tzhsn.huang@gmail.com>
2022-08-23 11:56:46 -0500

    [leetcode] counting bits: alternative

--- solutions/leetcode/counting-bits/main.cpp
+++ solutions/leetcode/counting-bits/main.cpp
@@ -13,3 +13,14 @@ class Solution {
         return new_counts;
     }
 };
+
+class Solution {
+   public:
+    vector<int> countBits(int n) {
+        vector<int> result(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            result[i] = result[i & (i - 1)] + 1;
+        }
+        return result;
+    }
+};
