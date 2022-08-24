5945abac152c9d7c1d65a2bc777dbab88920ab48
TH Huang <tzhsn.huang@gmail.com>
2022-08-24 11:45:26 -0500

    [leetcode] longest repeating character replacement: optimize

--- solutions/leetcode/longest-repeating-character-replacement/main.cpp
+++ solutions/leetcode/longest-repeating-character-replacement/main.cpp
@@ -15,3 +15,19 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int characterReplacement(string s, int k) {
+        array<int, 26> count;
+        count.fill(0);
+        int max_count = 0;
+        int result = 0;
+        for (int l = 0, r = 0; r < s.size(); ++r) {
+            max_count = max(max_count, ++count[s[r] - 'A']);
+            while (r - l + 1 - max_count > k) --count[s[l++] - 'A'];
+            result = max(result, r - l + 1);
+        }
+        return result;
+    }
+};
