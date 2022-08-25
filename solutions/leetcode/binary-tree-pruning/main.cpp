04b99353091eb9cbfeef16ed552a40a7bffb646c
TH Huang <tzhsn.huang@gmail.com>
2022-08-24 20:25:21 -0500

    [leetcode] binary tree pruning

---
+++ solutions/leetcode/binary-tree-pruning/main.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    TreeNode* pruneTree(TreeNode* root) {
+        function<bool(TreeNode*)> dfs = [&](TreeNode* u) -> bool {
+            if (u == nullptr) return false;
+            bool l = dfs(u->left);
+            bool r = dfs(u->right);
+            if (!l) {
+                delete u->left;
+                u->left = nullptr;
+            }
+            if (!r) {
+                delete u->right;
+                u->right = nullptr;
+            }
+            return l || r || u->val == 1;
+        };
+
+        if (!dfs(root)) {
+            delete root;
+            return nullptr;
+        }
+        return root;
+    }
+};
