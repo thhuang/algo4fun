1bc3f4d6fa406720d052681e3c3d4d21f37f41e4
TH Huang <tzhsn.huang@gmail.com>
2022-08-24 20:45:12 -0500

    [leetcode] ransom note: refactor

--- solutions/leetcode/ransom-note/main.cpp
+++ solutions/leetcode/ransom-note/main.cpp
@@ -2,7 +2,7 @@ class Solution {
    public:
     bool canConstruct(string ransomNote, string magazine) {
         array<int, 26> chars;
-        fill(begin(chars), end(chars), 0);
+        chars.fill(0);
         for (char c : magazine) ++chars[c - 'a'];
         for (char c : ransomNote) {
             if (--chars[c - 'a'] < 0) return false;
