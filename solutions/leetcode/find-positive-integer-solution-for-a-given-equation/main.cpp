162b53677a19a958c39241256cd32893545da30e
TH Huang <tzhsn.huang@gmail.com>
2022-08-26 12:07:22 -0500

    [leetcode] find positive integer solution for a given equation: optimize

--- solutions/leetcode/find-positive-integer-solution-for-a-given-equation/main.cpp
+++ solutions/leetcode/find-positive-integer-solution-for-a-given-equation/main.cpp
@@ -24,3 +24,22 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> result;
+        int x = 1, y = 1000;
+        while (x <= 1000 && y >= 1) {
+            int v = customfunction.f(x, y);
+            if (v == z) {
+                result.push_back({x++, y--});
+            } else if (v < z) {
+                ++x;
+            } else {  // v > z
+                --y;
+            }
+        }
+        return result;
+    }
+};
