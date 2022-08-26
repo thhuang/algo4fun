26cf5320de1b54753a590f47e3717d8254570037
TH Huang <tzhsn.huang@gmail.com>
2022-08-26 12:59:10 -0500

    [leetcode] count number of nice subarrays: refactor

--- solutions/leetcode/count-number-of-nice-subarrays/main.cpp
+++ solutions/leetcode/count-number-of-nice-subarrays/main.cpp
@@ -23,3 +23,23 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> odd = {-1};
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] & 1) odd.push_back(i);
+        }
+        odd.push_back(n);
+
+        int result = 0;
+        for (int l = 1; l + k - 1 < odd.size() - 1; ++l) {
+            int r = l + k - 1;
+            result += (odd[l] - odd[l - 1]) * (odd[r + 1] - odd[r]);
+        }
+
+        return result;
+    }
+};
