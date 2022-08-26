c6ddcc52bc594487c172978130b73d82018e3a18
TH Huang <tzhsn.huang@gmail.com>
2022-08-25 21:24:28 -0500

    [leetcode] reordered power of 2: refactor

--- solutions/leetcode/reordered-power-of-2/main.cpp
+++ solutions/leetcode/reordered-power-of-2/main.cpp
@@ -1,40 +1,18 @@
-array<vector<array<int, 10>>, 11> mp;
-bool init = false;
+unordered_set<string> valid;
 
 class Solution {
    public:
     bool reorderedPowerOf2(int n) {
-        if (!init) {
+        if (valid.empty()) {
             for (int v = 1; v <= 1e9; v *= 2) {
                 string s = to_string(v);
-
-                array<int, 10> cnt;
-                cnt.fill(0);
-
-                for (char c : s) ++cnt[c - '0'];
-
-                mp[s.size()].push_back(cnt);
+                sort(s.begin(), s.end());
+                valid.insert(s);
             }
-            init = true;
         }
 
         string s = to_string(n);
-
-        array<int, 10> cnt;
-        cnt.fill(0);
-
-        for (char c : s) ++cnt[c - '0'];
-
-        for (const auto& arr : mp[s.size()]) {
-            int found = true;
-            for (int i = 0; found && i < 10; ++i) {
-                if (arr[i] != cnt[i]) {
-                    found = false;
-                }
-            }
-            if (found) return true;
-        }
-
-        return false;
+        sort(s.begin(), s.end());
+        return valid.find(s) != valid.end();
     }
 };
