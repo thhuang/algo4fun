6d069eb15298db88e51cebfc2fb2eb6658fcca4a
TH Huang <tzhsn.huang@gmail.com>
2022-08-28 11:57:31 -0500

    [leetcode] sell diminishing-valued colored balls: refactor

--- solutions/leetcode/sell-diminishing-valued-colored-balls/main.cpp
+++ solutions/leetcode/sell-diminishing-valued-colored-balls/main.cpp
@@ -38,3 +38,35 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    typedef long long ll;
+
+    const int mod = 1e9 + 7;
+
+   public:
+    int maxProfit(vector<int>& inventory, int orders) {
+        inventory.push_back(0);
+        sort(inventory.begin(), inventory.end());
+
+        ll result = 0;
+        for (int i = inventory.size() - 1, colors = 1; i > 0; --i, ++colors) {
+            int hi = inventory[i], lo = inventory[i - 1];
+            if (hi == lo) continue;
+            int rounds = min(orders / colors, hi - lo);
+
+            orders -= rounds * colors;
+
+            result += ((ll)hi + hi - rounds + 1) * rounds * colors / 2 % mod;
+            result %= mod;
+
+            if (rounds < hi - lo) {
+                result += ((ll)hi - rounds) * orders % mod;
+                result %= mod;
+                break;
+            }
+        }
+
+        return result;
+    }
+};
