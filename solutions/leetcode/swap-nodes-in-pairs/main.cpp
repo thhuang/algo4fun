211a6c023678b4f4f3c729a5a225743781e88a7c
TH Huang <tzhsn.huang@gmail.com>
2022-09-02 12:15:12 -0500

    [leetcode] swap nodes in pairs: refactor

--- solutions/leetcode/swap-nodes-in-pairs/main.cpp
+++ solutions/leetcode/swap-nodes-in-pairs/main.cpp
@@ -12,15 +12,15 @@ class Solution {
    public:
     ListNode* swapPairs(ListNode* head) {
         ListNode dummy(0, head);
-        ListNode* p = &dummy;
+        auto p = &dummy;
         while (p->next && p->next->next) {
-            ListNode* p0 = p->next;
-            ListNode* p1 = p->next->next;
-            ListNode* p2 = p->next->next->next;
-            p1->next = p0;
-            p0->next = p2;
-            p->next = p1;
-            p = p0;
+            auto a = p->next;
+            auto b = p->next->next;
+            auto c = p->next->next->next;
+            p->next = b;
+            p->next->next = a;
+            p->next->next->next = c;
+            p = a;
         }
         return dummy.next;
     }
