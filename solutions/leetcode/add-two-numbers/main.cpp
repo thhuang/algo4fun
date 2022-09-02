e484d8e75387ea661ef5d8aa1aafe40d0ba1789e
TH Huang <tzhsn.huang@gmail.com>
2022-09-02 12:06:59 -0500

    [leetcode] add two numbers: refactor

--- solutions/leetcode/add-two-numbers/main.cpp
+++ solutions/leetcode/add-two-numbers/main.cpp
@@ -11,19 +11,16 @@
 class Solution {
    public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
         ListNode dummy;
         ListNode* p = &dummy;
-        int carry = 0;
-
-        while (l1 || l2) {
+        while (l1 || l2 || carry) {
             if (l1) carry += l1->val, l1 = l1->next;
             if (l2) carry += l2->val, l2 = l2->next;
             p->next = new ListNode(carry % 10);
             p = p->next;
             carry /= 10;
         }
-        if (carry) p->next = new ListNode(carry);
-
         return dummy.next;
     }
 };
