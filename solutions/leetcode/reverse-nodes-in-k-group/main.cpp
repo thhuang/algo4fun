66760720e1cf0e09ad21b756fb07c172f7fe571e
TH Huang <tzhsn.huang@gmail.com>
2022-09-03 11:10:33 -0500

    [leetcode] reverse nodes in k group: alternative

--- solutions/leetcode/reverse-nodes-in-k-group/main.cpp
+++ solutions/leetcode/reverse-nodes-in-k-group/main.cpp
@@ -41,3 +41,37 @@ class Solution {
         return dummy.next;
     }
 };
+
+class Solution {
+    ListNode* forward(ListNode* u, int k) {
+        for (int i = 0; u && i < k; ++i) u = u->next;
+        return u;
+    }
+
+    pair<ListNode*, ListNode*> reverse(ListNode* head) {
+        ListNode *p0 = nullptr, *p1 = head;
+        while (p1) {
+            ListNode* p2 = p1->next;
+            p1->next = p0;
+            p0 = p1;
+            p1 = p2;
+        }
+        return {p0, head};
+    }
+
+   public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode dummy(0, head);
+
+        for (ListNode *p = &dummy, *q = forward(p, k); q != nullptr;
+             p = q, q = forward(p, k)) {
+            ListNode* c = q->next;
+            q->next = nullptr;
+
+            tie(p->next, q) = reverse(p->next);
+            q->next = c;
+        }
+
+        return dummy.next;
+    }
+};
