b674c713f54eff7ffa23865d405255fe6fe68883
TH Huang <tzhsn.huang@gmail.com>
2022-09-05 16:11:39 -0500

    [leetcode] minimum height trees: refactor

--- solutions/leetcode/minimum-height-trees/main.cpp
+++ solutions/leetcode/minimum-height-trees/main.cpp
@@ -7,52 +7,6 @@ class Solution {
             adj[e[1]].push_back(e[0]);
         }
 
-        int a = 0;
-        int max_dist = 0;
-        function<void(int, int, int)> dfs0 = [&](int u, int p, int d) -> void {
-            if (d > max_dist) a = u, max_dist = d;
-            for (int v : adj[u]) {
-                if (v == p) continue;
-                dfs0(v, u, d + 1);
-            }
-        };
-        dfs0(0, -1, 1);
-
-        vector<int> path;
-        function<bool(int, int, int)> dfs1 = [&](int u, int p, int d) -> bool {
-            if (d >= max_dist && adj[u].size() < 2) {
-                path = {u};
-                max_dist = d;
-                return true;
-            }
-
-            bool res = false;
-            for (int v : adj[u]) {
-                if (v == p) continue;
-                if (dfs1(v, u, d + 1)) {
-                    res = true;
-                    path.push_back(u);
-                }
-            }
-            return res;
-        };
-        dfs1(a, -1, 1);
-
-        int i = path.size() / 2;
-        if (path.size() % 2 == 0) return {path[i - 1], path[i]};
-        return {path[i]};
-    }
-};
-
-class Solution {
-   public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for (const auto& e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-
         deque<array<int, 2>> q = {{0, -1}};
         int a;
         while (!q.empty()) {
@@ -91,5 +45,47 @@ class Solution {
         int l2 = path.size() / 2;
         if (path.size() & 1) return {path[l2]};
         return {path[l2 - 1], path[l2]};
+    }
+};
+
+class Solution {
+   public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (const vector<int>& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector<int> longest_path;
+        vector<int> curr;
+
+        function<void(int, int)> dfs = [&](int u, int p) -> void {
+            curr.push_back(u);
+
+            if ((adj[u].empty() || (adj[u].size() == 1 && adj[u][0] == p)) &&
+                curr.size() > longest_path.size())
+                longest_path = curr;
+            for (int v : adj[u]) {
+                if (v != p) dfs(v, u);
+            }
+
+            curr.pop_back();
+        };
+
+        auto clear = [&]() -> void {
+            longest_path.clear();
+            curr.clear();
+        };
+
+        dfs(0, -1);
+        int a = longest_path.back();
+        clear();
+
+        dfs(a, -1);
+
+        int m = longest_path.size() / 2;
+        if (longest_path.size() % 2 == 1) return {longest_path[m]};
+        return {longest_path[m - 1], longest_path[m]};
     }
 };
