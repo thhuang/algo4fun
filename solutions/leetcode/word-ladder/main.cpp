33e9f24262dd89b796b5985f0d1119eb7c5d6e7b
TH Huang <tzhsn.huang@gmail.com>
2022-09-07 15:55:58 -0500

    [leetcode] word ladder: refactor

--- solutions/leetcode/word-ladder/main.cpp
+++ solutions/leetcode/word-ladder/main.cpp
@@ -2,66 +2,31 @@ class Solution {
    public:
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        int n = beginWord.size();
-
-        unordered_set<string> valid;
-        for (string s : wordList) valid.insert(s);
+        unordered_set<string> valid(wordList.begin(), wordList.end());
+        valid.erase(beginWord);
 
         if (valid.count(endWord) == 0) return 0;
 
-        int steps = 1;
-        vector<string> q = {beginWord};
-        while (!q.empty()) {
-            vector<string> new_q;
-            for (string u : q) {
-                if (u == endWord) return steps;
-
-                for (int i = 0; i < n; ++i) {
-                    string v = u;
-                    for (int j = 0; j < 26; ++j) {
-                        char c = 'a' + j;
-                        v[i] = c;
-                        if (valid.count(v)) new_q.push_back(v);
-                        valid.erase(v);
-                    }
-                }
-            }
-
-            ++steps;
-            q = new_q;
-        }
-
-        return 0;
-    }
-};
-
-class Solution {
-   public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        unordered_set<string> valid;
-        for (const string& s : wordList) valid.insert(s);
-
-        if (valid.count(endWord) == 0) return 0;
-
-        queue<pair<string, int>> q;
+        queue<tuple<string, int>> q;
         q.push({beginWord, 1});
+
         while (!q.empty()) {
-            auto [s, d] = q.front();
+            auto [u, s] = q.front();
             q.pop();
 
-            if (s == endWord) return d;
+            if (u == endWord) return s;
 
-            for (int i = 0; i < s.size(); ++i) {
-                char u = s[i];
-                for (char v = 'a'; v <= 'z'; ++v) {
-                    if (v == u) continue;
-                    s[i] = v;
-                    if (valid.count(s) == 0) continue;
-                    q.push({s, d + 1});
-                    valid.erase(s);
+            ++s;
+            for (int i = 0; i < u.size(); ++i) {
+                char o = u[i];
+                for (char c = 'a'; c <= 'z'; ++c) {
+                    if (c == o) continue;
+                    u[i] = c;
+                    if (valid.count(u) == 0) continue;
+                    valid.erase(u);
+                    q.push({u, s});
                 }
-                s[i] = u;
+                u[i] = o;
             }
         }
 
