1d17677559526b17ee2cca0f90c610e4f272aaea
TH Huang <tzhsn.huang@gmail.com>
2022-09-07 14:48:26 -0500

    [leetcode] cheapest flights within k stops: alternative

--- solutions/leetcode/cheapest-flights-within-k-stops/main.cpp
+++ solutions/leetcode/cheapest-flights-within-k-stops/main.cpp
@@ -32,3 +32,32 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+   public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
+                          int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (const vector<int>& f : flights) adj[f[0]].push_back({f[1], f[2]});
+
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>>
+            pq;  // {cost, stop, city}
+        pq.push({0, -1, src});
+
+        vector<int> vis(n, -1);
+
+        while (!pq.empty()) {
+            auto [c, s, u] = pq.top();
+            pq.pop();
+
+            if (u == dst) return c;
+            if (s == k) continue;
+            if (vis[u] != -1 && vis[u] <= s) continue;
+            vis[u] = s;
+
+            for (auto [v, cc] : adj[u]) pq.push({c + cc, s + 1, v});
+        }
+
+        return -1;
+    }
+};
