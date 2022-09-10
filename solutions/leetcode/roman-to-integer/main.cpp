06a08831a85d500924f79291855041f3e4a74327
TH Huang <tzhsn.huang@gmail.com>
2022-09-10 15:10:52 -0500

    [leetcode] roman to integer: refactor

--- solutions/leetcode/roman-to-integer/main.cpp
+++ solutions/leetcode/roman-to-integer/main.cpp
@@ -1,44 +1,15 @@
 class Solution {
-    const unordered_map<char, int> char2num = {
-        {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
-        {'C', 100}, {'D', 500}, {'M', 1000}};
-
-    bool shouldMinus(char curr, char next) {
-        switch (curr) {
-            case 'I':
-                return next == 'V' || next == 'X';
-            case 'X':
-                return next == 'L' || next == 'C';
-            case 'C':
-                return next == 'D' || next == 'M';
-        }
-        return false;
-    }
+    unordered_map<char, int> mp = {{'I', 1},   {'V', 5},   {'X', 10},
+                                   {'L', 50},  {'C', 100}, {'D', 500},
+                                   {'M', 1000}};
 
    public:
     int romanToInt(string s) {
-        int result = 0;
+        int result;
         for (int i = 0; i < s.size(); ++i) {
-            int v = char2num.at(s[i]);
-            if (i + 1 < s.size() && shouldMinus(s[i], s[i + 1])) v = -v;
-            result += v;
-        }
-        return result;
-    }
-};
-
-class Solution {
-    const unordered_map<char, int> char2num = {
-        {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
-        {'C', 100}, {'D', 500}, {'M', 1000}};
-
-   public:
-    int romanToInt(string s) {
-        int result = 0;
-        for (int i = 0; i < s.size(); ++i) {
-            int v = char2num.at(s[i]);
-            if (i + 1 < s.size() && v < char2num.at(s[i + 1])) v = -v;
-            result += v;
+            int sign = 1;
+            if (i < s.size() - 1 && mp[s[i]] < mp[s[i + 1]]) sign = -1;
+            result += mp[s[i]] * sign;
         }
         return result;
     }
