c9e838e01dfc39ca2037c586458c9329c22e0b71
TH Huang <tzhsn.huang@gmail.com>
2022-09-10 12:56:30 -0500

    [leetcode] lfu cache: alternative

--- solutions/leetcode/lfu-cache/main.cpp
+++ solutions/leetcode/lfu-cache/main.cpp
@@ -9,7 +9,8 @@ class LFUCache {
     int n;
 
     void addFreq(
-        unordered_map<int, pair<int, list<pair<int, int>>::iterator>>::iterator it) {
+        unordered_map<int, pair<int, list<pair<int, int>>::iterator>>::iterator
+            it) {
         int f = it->second.first;
         auto [k, v] = *it->second.second;
 
@@ -58,6 +59,59 @@ class LFUCache {
 
         freq[0].push_front({key, value});
         mp[key] = {0, freq[0].begin()};
+    }
+};
+
+class LFUCache {
+    int n;
+    vector<list<pair<int, int>>> freq2kv;  // {k, v}
+    unordered_map<int, pair<int, list<pair<int, int>>::iterator>> mp;  // {key, {freq, iterator}}
+
+   public:
+    LFUCache(int capacity) : n(capacity) {}
+
+    int get(int key) {
+        auto it = mp.find(key);
+        if (it == mp.end()) return -1;
+
+        int f = it->second.first;
+        auto p = it->second.second;
+        int v = p->second;
+
+        mp.erase(key);
+        freq2kv[f++].erase(p);
+
+        while (freq2kv.size() <= f) freq2kv.push_back({});
+        freq2kv[f].push_front({key, v});
+        mp[key] = {f, freq2kv[f].begin()};
+
+        return v;
+    }
+
+    void put(int key, int value) {
+        if (n == 0) return;
+
+        int f = 1;
+        if (auto it = mp.find(key); it != mp.end()) {
+            f = it->second.first;
+            auto p = it->second.second;
+            mp.erase(key);
+            freq2kv[f++].erase(p);
+        }
+
+        if (mp.size() == n) {
+            for (int i = 0; i < freq2kv.size(); ++i) {
+                if (freq2kv[i].empty()) continue;
+                auto [k, _] = freq2kv[i].back();
+                freq2kv[i].pop_back();
+                mp.erase(k);
+                break;
+            }
+        }
+
+        while (freq2kv.size() <= f) freq2kv.push_back({});
+        freq2kv[f].push_front({key, value});
+        mp[key] = {f, freq2kv[f].begin()};
     }
 };
 
