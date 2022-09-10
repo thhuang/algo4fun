9fa853cfe020260061f0eb8de5f6b534292b1584
TH Huang <tzhsn.huang@gmail.com>
2022-09-09 20:22:35 -0500

    [leetcode] best time to buy and sell stock iv: alternative

--- solutions/leetcode/best-time-to-buy-and-sell-stock-iv/main.cpp
+++ solutions/leetcode/best-time-to-buy-and-sell-stock-iv/main.cpp
@@ -19,3 +19,26 @@ class Solution {
         return dp.back().sold;
     }
 };
+
+class Solution {
+   public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.size() == 0 || k == 0) return 0;
+
+        vector<int> dp(k * 2, 0);
+        for (int i = 0; i < dp.size(); i += 2) dp[i] = -prices[0];
+
+        for (int i = 1; i < prices.size(); ++i) {
+            for (int j = dp.size() - 1; j >= 1; --j) {
+                if (j % 2 == 0) {
+                    dp[j] = max(dp[j], dp[j - 1] - prices[i]);
+                } else {
+                    dp[j] = max(dp[j], dp[j - 1] + prices[i]);
+                }
+            }
+            dp[0] = max(dp[0], -prices[i]);
+        }
+
+        return dp.back();
+    }
+};
