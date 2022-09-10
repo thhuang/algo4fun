09307d48df5ac6be2b51c47afce24a71b5a2d32b
TH Huang <tzhsn.huang@gmail.com>
2022-09-10 14:47:07 -0500

    [leetcode] sliding window maximum: refactor

--- solutions/leetcode/sliding-window-maximum/main.cpp
+++ solutions/leetcode/sliding-window-maximum/main.cpp
@@ -41,3 +41,23 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> desc;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (!desc.empty() && desc.back() < nums[i]) desc.pop_back();
+            desc.push_back(nums[i]);
+
+            if (i < k - 1) continue;
+
+            result.push_back(desc.front());
+            if (nums[i - k + 1] == desc.front()) desc.pop_front();
+        }
+
+        return result;
+    }
+};
