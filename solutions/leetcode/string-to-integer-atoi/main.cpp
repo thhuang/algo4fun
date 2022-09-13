c8002dd5fa990dc49019f583fd1c3c5a70297c60
TH Huang <tzhsn.huang@gmail.com>
2022-09-13 18:56:49 -0500

    [leetcode] string to integer (atoi): alternative

--- solutions/leetcode/string-to-integer-atoi/main.cpp
+++ solutions/leetcode/string-to-integer-atoi/main.cpp
@@ -46,3 +46,48 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int myAtoi(string s) {
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != ' ') {
+                s = s.substr(i);
+                break;
+            }
+        }
+        if (s.empty()) return 0;
+
+        int sign = 1;
+        if (s[0] == '-') sign = -1;
+
+        string t;
+        for (int i = s[0] == '-' || s[0] == '+' ? 1 : 0;
+             i < s.size() && isdigit(s[i]); ++i)
+            t += s[i];
+
+        for (int i = 0; i < t.size(); ++i) {
+            if (t[i] != '0') {
+                t = t.substr(i);
+                break;
+            }
+            if (i == t.size() - 1) return 0;
+        }
+
+        int n = 10;
+        if (t.size() > n)
+            return sign > 0 ? numeric_limits<int>::max()
+                            : numeric_limits<int>::min();
+
+        t = string(n - t.size(), '0') + t;
+        if (sign > 0 && t > to_string(numeric_limits<int>::max()))
+            return numeric_limits<int>::max();
+        if (sign < 0 && t > to_string(numeric_limits<int>::min()).substr(1))
+            return numeric_limits<int>::min();
+
+        int v = 0;
+        for (char c : t) v = v * 10 + sign * (c - '0');
+
+        return v;
+    }
+};
