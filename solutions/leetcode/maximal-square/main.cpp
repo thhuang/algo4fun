a148bc219d5e7a292c88519987421da549d0c9c0
TH Huang <tzhsn.huang@gmail.com>
2022-09-14 11:37:21 -0500

    [leetcode] maximal square: refactor

--- solutions/leetcode/maximal-square/main.cpp
+++ solutions/leetcode/maximal-square/main.cpp
@@ -1,45 +1,25 @@
 class Solution {
    public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix.front().size();
-
-        vector<int> prev(m, 0);
-        int result = 0;
-        for (int i = 0; i < n; ++i) {
-            vector<int> curr(m, 0);
-            for (int j = 0; j < m; ++j) {
-                if (matrix[i][j] == '0') continue;
-                curr[j] = 1;
-                if (j > 0) curr[j] += min({prev[j], prev[j - 1], curr[j - 1]});
-                result = max(result, curr[j] * curr[j]);
-            }
-            prev = move(curr);
-        }
-
-        return result;
-    }
-};
-
-class Solution {
-   public:
-    int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size();
         int n = matrix.front().size();
 
-        int result = 0;
-        vector<int> dp(n, 0);
+        vector<int> dp(n + 1, 0);
+
+        int mx_len = 0;
         for (int i = 0; i < m; ++i) {
-            vector<int> new_dp(n, 0);
-            new_dp[0] = matrix[i][0] == '1';
-            for (int j = 1; j < n; ++j) {
-                if (matrix[i][j] == '0') continue;
-                new_dp[j] = 1 + min(new_dp[j - 1], min(dp[j - 1], dp[j]));
+            vector<int> new_dp(n + 1, 0);
+
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == '1') {
+                    new_dp[j + 1] = min(dp[j], min(new_dp[j], dp[j + 1])) + 1;
+                }
             }
+
             dp = move(new_dp);
-            result = max(result, *max_element(dp.begin(), dp.end()));
+            mx_len = max(mx_len, *max_element(dp.begin(), dp.end()));
         }
 
-        return result * result;
+        return mx_len * mx_len;
     }
 };
