daf7814c39c0c62243f58cc0d42c938e008dad1d
TH Huang <tzhsn.huang@gmail.com>
2022-09-14 11:47:59 -0500

    [leetcode] find median from data stream: refactor

--- solutions/leetcode/find-median-from-data-stream/main.cpp
+++ solutions/leetcode/find-median-from-data-stream/main.cpp
@@ -1,29 +1,4 @@
 class MedianFinder {
-    multiset<int> left, right;
-
-   public:
-    void addNum(int num) {
-        left.insert(num);
-
-        right.insert(*left.rbegin());
-        left.erase(--left.end());
-
-        if (left.size() >= right.size()) return;
-
-        left.insert(*right.begin());
-        right.erase(right.begin());
-    }
-
-    double findMedian() {
-        double result = *left.rbegin();
-        if (left.size() == right.size()) {
-            result = 0.5 * (result + *right.begin());
-        }
-        return result;
-    }
-};
-
-class MedianFinder {
     priority_queue<int> left;
     priority_queue<int, vector<int>, greater<>> right;
 
@@ -34,18 +9,15 @@ class MedianFinder {
         right.push(left.top());
         left.pop();
 
-        if (left.size() >= right.size()) return;
+        if (left.size() == right.size()) return;
 
         left.push(right.top());
         right.pop();
     }
 
     double findMedian() {
-        double result = left.top();
-        if (left.size() == right.size()) {
-            result = 0.5 * (result + right.top());
-        }
-        return result;
+        if (left.size() > right.size()) return left.top();
+        return 0.5 * (left.top() + right.top());
     }
 };
 
