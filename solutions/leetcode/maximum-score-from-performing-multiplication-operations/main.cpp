961884b8a291d2988e8f42e69ad9b559397cba66
TH Huang <tzhsn.huang@gmail.com>
2022-09-16 16:15:16 -0500

    [leetcode] maximum score from performing multiplication operations: optimize

--- solutions/leetcode/maximum-score-from-performing-multiplication-operations/main.cpp
+++ solutions/leetcode/maximum-score-from-performing-multiplication-operations/main.cpp
@@ -16,3 +16,24 @@ class Solution {
         return dp[0][0];
     }
 };
+
+class Solution {
+   public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int m = multipliers.size();
+        int n = nums.size();
+
+        vector<int> dp(m + 1, 0);
+        for (int i = m - 1; i >= 0; --i) {
+            vector<int> new_dp(m + 1, 0);
+            for (int l = 0; l <= i; ++l) {
+                int k = multipliers[i];
+                new_dp[l] = max(k * nums[l] + dp[l + 1],
+                                k * nums[n - i + l - 1] + dp[l]);
+            }
+            dp = move(new_dp);
+        }
+
+        return dp[0];
+    }
+};
