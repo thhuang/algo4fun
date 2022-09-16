30a962d68db80cfa8fc710e4a215d8e5145c6bdc
TH Huang <tzhsn.huang@gmail.com>
2022-09-16 16:15:59 -0500

    [leetcode] maximum score from performing multiplication operations: top-down

--- solutions/leetcode/maximum-score-from-performing-multiplication-operations/main.cpp
+++ solutions/leetcode/maximum-score-from-performing-multiplication-operations/main.cpp
@@ -4,6 +4,27 @@ class Solution {
         int m = multipliers.size();
         int n = nums.size();
 
+        vector<vector<int>> dp(m, vector<int>(m, -1));
+
+        function<int(int, int)> dfs = [&](int i, int l) -> int {
+            if (i == m) return 0;
+            if (dp[i][l] != -1) return dp[i][l];
+
+            int k = multipliers[i];
+            return dp[i][l] = max(k * nums[l] + dfs(i + 1, l + 1),
+                                  k * nums[n - i + l - 1] + dfs(i + 1, l));
+        };
+
+        return dfs(0, 0);
+    }
+};
+
+class Solution {
+   public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int m = multipliers.size();
+        int n = nums.size();
+
         vector<vector<int>> dp(m + 1, vector<int>(m + 1, 0));
         for (int i = m - 1; i >= 0; --i) {
             for (int l = 0; l <= i; ++l) {
