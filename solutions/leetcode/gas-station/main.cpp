0ce28e779edbee945c55bcb5fd61a0384b1462b4
TH Huang <tzhsn.huang@gmail.com>
2022-09-18 13:46:15 -0500

    [leetcode] gas station: optimize

--- solutions/leetcode/gas-station/main.cpp
+++ solutions/leetcode/gas-station/main.cpp
@@ -10,3 +10,20 @@ class Solution {
         return min_element(delta.begin(), delta.end()) - delta.begin();
     }
 };
+
+class Solution {
+   public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int result = 0, mn = 0, curr = 0;
+        for (int i = 1; i < n; ++i) {
+            curr += gas[i - 1] - cost[i - 1];
+            if (curr < mn) {
+                mn = curr;
+                result = i;
+            }
+        }
+        if (curr + gas.back() - cost.back() < 0) return -1;
+        return result;
+    }
+};
