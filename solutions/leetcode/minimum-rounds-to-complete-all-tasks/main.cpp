1fa5b61d09f6f07c48061a87619bd1c4d41259e6
TH Huang <tzhsn.huang@gmail.com>
2022-09-18 13:52:19 -0500

    [leetcode] minimum rounds to complete all tasks: refactor

--- solutions/leetcode/minimum-rounds-to-complete-all-tasks/main.cpp
+++ solutions/leetcode/minimum-rounds-to-complete-all-tasks/main.cpp
@@ -1,14 +1,16 @@
 class Solution {
    public:
     int minimumRounds(vector<int>& tasks) {
-        unordered_map<int, int> level_count;
-        for (int t : tasks) ++level_count[t];
-        int count = 0;
-        for (auto it = level_count.begin(); it != level_count.end(); ++it) {
-            if (it->second == 1) return -1;
-            count += it->second / 3;
-            if (it->second % 3 != 0) ++count;
+        unordered_map<int, int> task_count;
+        for (int v : tasks) ++task_count[v];
+
+        int result = 0;
+        for (auto [_, c] : task_count) {
+            if (c == 1) return -1;
+            result += c / 3;
+            if (c % 3 > 0) ++result;
         }
-        return count;
+
+        return result;
     }
 };
