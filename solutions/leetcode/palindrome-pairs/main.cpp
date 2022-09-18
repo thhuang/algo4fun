2e402fa906aab7d98560bef4c9a46ae08fd45d82
TH Huang <tzhsn.huang@gmail.com>
2022-09-17 20:33:55 -0500

    [leetcode] palindrome pairs: optimize

--- solutions/leetcode/palindrome-pairs/main.cpp
+++ solutions/leetcode/palindrome-pairs/main.cpp
@@ -1,4 +1,3 @@
-
 class Solution {
     bool is_palindrome(const string& s) {
         int n = s.size();
@@ -43,6 +42,67 @@ class Solution {
                 if (auto it = mp.find(r);
                     it != mp.end() && is_palindrome(l) && it->second != i)
                     result.push_back({it->second, i});
+            }
+        }
+
+        return result;
+    }
+};
+
+class Solution {
+    bool is_palindrome(const string& s) {
+        int n = s.size();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s[i] != s[n - 1 - i]) return false;
+        }
+        return true;
+    }
+
+   public:
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        unordered_map<string, int> mp;
+        vector<bool> sz;
+
+        for (int i = 0; i < words.size(); ++i) {
+            string s(words[i].rbegin(), words[i].rend());
+            mp.insert({s, i});
+            while (s.size() >= sz.size()) sz.push_back(false);
+            sz[s.size()] = true;
+        }
+
+        vector<vector<int>> result;
+
+        if (auto it = mp.find(""); it != mp.end()) {
+            for (int i = 0; i < words.size(); ++i) {
+                if (words[i] == "" || !is_palindrome(words[i])) continue;
+                result.push_back({i, it->second});
+                result.push_back({it->second, i});
+            }
+        }
+
+        for (int i = 0; i < words.size(); ++i) {
+            const string& s = words[i];
+
+            if (sz[s.size()] && mp.count(s) && mp[s] != i)
+                result.push_back({i, mp[s]});
+
+            string l;
+            string r{s.rbegin(), s.rend()};
+            for (int d = 1; d < s.size(); ++d) {
+                l += r.back();
+                r.pop_back();
+
+                reverse(r.begin(), r.end());
+
+                if (sz[l.size()] && mp.count(l) && is_palindrome(r) &&
+                    mp[l] != i)
+                    result.push_back({i, mp[l]});
+
+                if (sz[r.size()] && mp.count(r) && is_palindrome(l) &&
+                    mp[r] != i)
+                    result.push_back({mp[r], i});
+
+                reverse(r.begin(), r.end());
             }
         }
 
