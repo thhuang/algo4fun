c631b27e2a5899a43b8a1356e2d1551f827d6cfd
TH Huang <tzhsn.huang@gmail.com>
2022-09-18 17:08:00 -0500

    [leetcode] jump game: alternative

--- solutions/leetcode/jump-game/main.cpp
+++ solutions/leetcode/jump-game/main.cpp
@@ -9,3 +9,14 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool canJump(vector<int>& nums) {
+        for (int i = 0, r = 0; i <= r; ++i) {
+            r = max(r, i + nums[i]);
+            if (r >= nums.size() - 1) return true;
+        }
+        return false;
+    }
+};
