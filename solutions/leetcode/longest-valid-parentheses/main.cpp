e2ffda5b5ce2c5c5b32ce0d041221c67cf61011f
TH Huang <tzhsn.huang@gmail.com>
2022-09-18 21:35:38 -0500

    [leetcode] longest valid parentheses: refactor

--- solutions/leetcode/longest-valid-parentheses/main.cpp
+++ solutions/leetcode/longest-valid-parentheses/main.cpp
@@ -31,3 +31,29 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        int result = 0;
+
+        function<void(char)> search = [&](char open) {
+            for (int l = 0, r = 0, c = 0; r < n; ++r) {
+                c += s[r] == open ? 1 : -1;
+                if (c < 0) {
+                    c = 0;
+                    l = r + 1;
+                } else if (c == 0) {
+                    result = max(result, r - l + 1);
+                }
+            }
+        };
+
+        search('(');
+        reverse(s.begin(), s.end());
+        search(')');
+
+        return result;
+    }
+};
