1f6422668db2b869c6744e346b79f1e059db6b75
TH Huang <tzhsn.huang@gmail.com>
2022-09-19 21:21:39 -0500

    [leetcode] maximum length of repeated subarray: optimize

--- solutions/leetcode/maximum-length-of-repeated-subarray/main.cpp
+++ solutions/leetcode/maximum-length-of-repeated-subarray/main.cpp
@@ -17,3 +17,27 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() < nums2.size()) swap(nums1, nums2);
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        vector<int> dp(n + 1, 0);
+
+        int result = 0;
+        for (int i = 1; i <= m; ++i) {
+            vector<int> new_dp(n + 1, 0);
+            for (int j = 1; j <= n; ++j) {
+                if (nums1[i - 1] == nums2[j - 1])
+                    result = max(result, new_dp[j] = dp[j - 1] + 1);
+            }
+            dp = move(new_dp);
+        }
+
+        return result;
+    }
+};
