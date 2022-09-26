22343abd89e78405a6dc69a7ce089ea1eacdd84a
TH Huang <tzhsn.huang@gmail.com>
2022-09-26 16:11:26 -0500

    [leetcode] satisfiability of equality equations: with rank

--- solutions/leetcode/satisfiability-of-equality-equations/main.cpp
+++ solutions/leetcode/satisfiability-of-equality-equations/main.cpp
@@ -30,3 +30,43 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool equationsPossible(vector<string>& equations) {
+        array<int, 26> group;
+        iota(group.begin(), group.end(), 0);
+
+        array<int, 26> sz;
+        fill(sz.begin(), sz.end(), 1);
+
+        function<int(int)> find = [&](int u) -> int {
+            if (u == group[u]) return u;
+            return group[u] = find(group[u]);
+        };
+
+        function<void(int, int)> unite = [&](int a, int b) -> void {
+            a = find(a);
+            b = find(b);
+            if (a == b) return;
+            if (sz[a] > sz[b]) {
+                group[b] = a;
+                sz[a] += sz[b];
+            } else {
+                group[a] = b;
+                sz[b] += sz[a];
+            }
+        };
+
+        for (const string& eq : equations) {
+            if (eq[1] == '=') unite(eq[0] - 'a', eq[3] - 'a');
+        }
+
+        for (const string& eq : equations) {
+            if (eq[1] == '!' && find(eq[0] - 'a') == find(eq[3] - 'a'))
+                return false;
+        }
+
+        return true;
+    }
+};
