aeeaec0b2d7588e0e6ca0f7cfc7c18a8104b1dcf
TH Huang <tzhsn.huang@gmail.com>
2022-09-27 10:52:26 -0500

    [leetcode] push dominoes: alternative

--- solutions/leetcode/push-dominoes/main.cpp
+++ solutions/leetcode/push-dominoes/main.cpp
@@ -35,3 +35,46 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+
+        vector<int> right(n, 0);
+        for (int i = 0, f = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') {
+                f = n;
+            } else if (dominoes[i] == 'L') {
+                f = 0;
+            }
+            right[i] = f;
+            f = max(0, f - 1);
+        }
+
+        vector<int> left(n, 0);
+        for (int i = n - 1, f = 0; i >= 0; --i) {
+            if (dominoes[i] == 'L') {
+                f = -n;
+            } else if (dominoes[i] == 'R') {
+                f = 0;
+            }
+            left[i] = f;
+            f = min(0, f + 1);
+        }
+
+        string result;
+        for (int i = 0; i < n; ++i) {
+            int f = right[i] + left[i];
+            if (f < 0) {
+                result += 'L';
+            } else if (f > 0) {
+                result += 'R';
+            } else {
+                result += '.';
+            }
+        }
+
+        return result;
+    }
+};
