00bf856006c8e7884e7b418114e186c3d7a38acc
TH Huang <tzhsn.huang@gmail.com>
2022-09-27 19:27:35 -0500

    [leetcode] remove nth node from end of list: refactor

--- solutions/leetcode/remove-nth-node-from-end-of-list/main.cpp
+++ solutions/leetcode/remove-nth-node-from-end-of-list/main.cpp
@@ -11,12 +11,11 @@
 class Solution {
    public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        auto dummy = new ListNode(0, head);
-        auto r = head;
-        auto l = dummy;
+        ListNode dummy(0, head);
+        ListNode *l = &dummy, *r = head;
         for (int i = 0; i < n; ++i) r = r->next;
         while (r) l = l->next, r = r->next;
         l->next = l->next->next;
-        return dummy->next;
+        return dummy.next;
     }
 };
