918914476b977426fc0d05cdb0cd2e0af8f0c351
TH Huang <tzhsn.huang@gmail.com>
2022-09-28 21:22:18 -0500

    [leetcode] find k closest elements: refactor

--- solutions/leetcode/find-k-closest-elements/main.cpp
+++ solutions/leetcode/find-k-closest-elements/main.cpp
@@ -4,25 +4,21 @@ class Solution {
         int n = arr.size();
 
         int i = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
-        if (i == n) --i;
-        if (i > 0 && abs(arr[i - 1] - x) <= abs(arr[i] - x)) --i;
+        if (i == n || (i > 0 && abs(arr[i - 1] - x) < abs(arr[i] - x))) --i;
 
         int l = i, r = i;
-        while (r - l + 1 < k) {
-            if (r == n - 1) {
-                --l;
-            } else if (l == 0) {
+        while (r - l - 1 < k) {
+            if (l < 0) {
                 ++r;
-            } else if (abs(arr[l - 1] - x) <= abs(arr[r + 1] - x)) {
+            } else if (r == n) {
+                --l;
+            } else if (abs(arr[l] - x) <= abs(arr[r] - x)) {
                 --l;
             } else {
                 ++r;
             }
         }
 
-        vector<int> result;
-        for (int i = l; i <= r; ++i) result.push_back(arr[i]);
-
-        return result;
+        return {arr.begin() + l + 1, arr.begin() + r};
     }
 };
