9e26763896e5924411701461d4bd69d407b5783d
TH Huang <tzhsn.huang@gmail.com>
2022-09-30 23:09:52 -0500

    [leetcode] decode ways

--- solutions/leetcode/decode-ways/main.cpp
+++ solutions/leetcode/decode-ways/main.cpp
@@ -17,3 +17,22 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int numDecodings(string s) {
+        if (s.front() == '0') return 0;
+
+        int n = s.size();
+        array<int, 2> dp = {1, 1};
+        for (int i = 2; i <= n; ++i) {
+            int v1 = 0, v2 = 0;
+            if (s[i - 1] - '0' > 0) v1 = dp[1];
+            if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] - '0' <= 6))
+                v2 = dp[0];
+            dp = {dp[1], v1 + v2};
+        }
+
+        return dp.back();
+    }
+};
