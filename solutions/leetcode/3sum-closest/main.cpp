9e0f62fcbe0137b3bff7ec9253280be0a02dcdae
TH Huang <tzhsn.huang@gmail.com>
2022-10-08 10:21:22 -0500

    [leetcode] 3 sum closest: refactor

--- solutions/leetcode/3sum-closest/main.cpp
+++ solutions/leetcode/3sum-closest/main.cpp
@@ -2,26 +2,24 @@ class Solution {
    public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-        int diff = numeric_limits<int>::max();
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int l = i + 1, r = nums.size() - 1;
+        int result = 1e8;
+        for (int i = 0; i < n; ++i) {
+            int l = i + 1, r = n - 1;
             while (l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if (abs(sum - target) < abs(diff)) {
-                    diff = sum - target;
+                int v = nums[i] + nums[l] + nums[r];
+                if (abs(target - v) < abs(target - result)) {
+                    result = v;
                 }
-
-                if (sum == target) return target;
-                if (sum < target) {
-                    ++l;
-                } else {  // sum > target
+                if (v > target) {
                     --r;
+                } else {
+                    ++l;
                 }
             }
         }
 
-        return target + diff;
+        return result;
     }
 };
