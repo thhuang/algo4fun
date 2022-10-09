e9a7fce4a7b9a624862cc7e75543b00dc05ea48f
TH Huang <tzhsn.huang@gmail.com>
2022-10-08 23:08:48 -0500

    [leetcode] the employee that worked on the longest task: refactor

--- solutions/leetcode/the-employee-that-worked-on-the-longest-task/main.cpp
+++ solutions/leetcode/the-employee-that-worked-on-the-longest-task/main.cpp
@@ -1,24 +1,16 @@
 class Solution {
    public:
     int hardestWorker(int n, vector<vector<int>>& logs) {
-        unordered_map<int, int> mp;
-        for (int i = 0, t = 0; i < logs.size(); ++i) {
-            int id = logs[i][0];
-            int time = logs[i][1];
-            mp[id] = max(mp[id], time - t);
-            t = time;
-        }
-
         int result = 0, time = 0;
-        for (auto [id, t] : mp) {
-            if (t > time) {
-                result = id;
-                time = t;
-            } else if (t == time && id < result) {
-                result = id;
+        for (int i = 0, p = 0; i < logs.size(); ++i) {
+            int id = logs[i][0];
+            int t = logs[i][1];
+            if (int v = t - p; v >= time) {
+                result = v == time ? min(result, id) : id;
+                time = t - p;
             }
+            p = t;
         }
-
         return result;
     }
 };
