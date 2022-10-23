53dfe6b8a8cd69fe32599d345ba8abf64a557bb8
TH Huang <tzhsn.huang@gmail.com>
2022-10-22 19:39:39 -0500

    [leetcode] minimum window substring: refactor

--- solutions/leetcode/minimum-window-substring/main.cpp
+++ solutions/leetcode/minimum-window-substring/main.cpp
@@ -1,26 +1,53 @@
 class Solution {
    public:
     string minWindow(string s, string t) {
-        int count = t.size();
-        unordered_map<char, int> char_count;
-        for (char c : t) ++char_count[c];
+        unordered_map<char, int> target;
+        for (char c : t) ++target[c];
 
-        int start = 0;
-        int length = numeric_limits<int>::max();
-        for (int l = 0, r = 0; r < s.size(); ++r) {
-            if (char_count.count(s[r]) && --char_count[s[r]] >= 0) --count;
-            while (count == 0) {
-                if (length > r - l + 1) {
-                    length = r - l + 1;
-                    start = l;
+        unordered_map<char, int> mp;
+        auto check = [&]() -> bool {
+            for (auto [c, v] : target) {
+                if (mp[c] < v) return false;
+            }
+            return true;
+        };
+
+        pair<int, int> result = {-1, numeric_limits<int>::max()};
+        for (int r = 0, l = 0; r < s.size(); ++r) {
+            ++mp[s[r]];
+            while (check()) {
+                if (r - l + 1 < result.second) {
+                    result = {l, r - l + 1};
                 }
-
-                if (char_count.count(s[l]) && ++char_count[s[l]] > 0) ++count;
-                ++l;
+                --mp[s[l++]];
             }
         }
 
-        if (length > s.size()) return "";
-        return s.substr(start, length);
+        if (result.first == -1) return "";
+        return s.substr(result.first, result.second);
+    }
+};
+
+class Solution {
+   public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> target;
+        for (char c : t) ++target[c];
+        int count = t.size();
+
+        pair<int, int> result = {-1, numeric_limits<int>::max()};
+        unordered_map<char, int> mp;
+        for (int r = 0, l = 0; r < s.size(); ++r) {
+            if (++mp[s[r]] <= target[s[r]]) --count;
+            for (; count == 0; ++l) {
+                if (r - l + 1 < result.second) {
+                    result = {l, r - l + 1};
+                }
+                if (mp[s[l]]-- <= target[s[l]]) ++count;
+            }
+        }
+
+        if (result.first == -1) return "";
+        return s.substr(result.first, result.second);
     }
 };
