9eec8845aaed36e7b50c39db119c9b850a586ed4
TH Huang <tzhsn.huang@gmail.com>
2022-12-03 22:50:20 -0600

    [utils] add cp template

---
+++ utils/cpp/template/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+inline void solve() {
+    // TODO: implementation
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+
+    return 0;
+}
