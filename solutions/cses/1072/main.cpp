ea021471b8feb4205c13def4cbea262166ab89e4
TH Huang <tzhsn.huang@gmail.com>
2022-12-07 10:32:13 -0600

    [cses] 1072

--- solutions/cses/1072/main.cpp
+++ solutions/cses/1072/main.cpp
@@ -18,11 +18,20 @@ inline void solve() {
     }
 }
 
+inline void solve1() {
+    int n;
+    cin >> n;
+
+    for (ll i = 1; i <= n; ++i) {
+        cout << i * i * (i * i - 1) / 2 - 4 * (i - 1) * (i - 2) << "\n";
+    }
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    solve();
+    solve1();
 
     return 0;
 }
