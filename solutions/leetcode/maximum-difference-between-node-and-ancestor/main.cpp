95cfb3852e18cd7d1b492da92a824f3a2afdda38
TH Huang <tzhsn.huang@gmail.com>
2022-12-08 23:20:05 -0600

    [leetcode] maximum difference between node and ancestor

---
+++ solutions/leetcode/maximum-difference-between-node-and-ancestor/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+   public:
+    int maxAncestorDiff(TreeNode* root) {
+        if (root == nullptr) return 0;
+
+        int result = 0;
+        function<void(TreeNode*, int, int)> dfs = [&](TreeNode* u, int mn,
+                                                      int mx) -> void {
+            if (u == nullptr) {
+                result = max(result, abs(mx - mn));
+                return;
+            }
+            mn = min(mn, u->val);
+            mx = max(mx, u->val);
+            dfs(u->left, mn, mx);
+            dfs(u->right, mn, mx);
+        };
+        dfs(root, root->val, root->val);
+
+        return result;
+    }
+};
