bb7547eddcbd259246d23237c1a5d4c590bffc55
TH Huang <tzhsn.huang@gmail.com>
2023-02-01 10:47:46 -0600

    [leetcode] greatest common divisor of strings

--- solutions/leetcode/greatest-common-divisor-of-strings/main.cpp
+++ solutions/leetcode/greatest-common-divisor-of-strings/main.cpp
@@ -20,3 +20,12 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        int n = gcd(str1.size(), str2.size());
+        return str1.substr(0, n);
+    }
+};
