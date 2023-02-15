7a65a8234b787932d602ce2ef67c10085b0caec9
TH Huang <tzhsn.huang@gmail.com>
2023-02-15 11:30:39 -0600

    [leetcode] add to array form of integer

---
+++ solutions/leetcode/add-to-array-form-of-integer/main.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        reverse(num.begin(), num.end());
+
+        vector<int> rresult;
+        for (int i = 0, carry = 0; i < num.size() || k || carry; ++i) {
+            int a = 0;
+            if (i < num.size()) a = num[i];
+
+            int b = k % 10;
+            k /= 10;
+
+            carry += a + b;
+            rresult.push_back(carry % 10);
+            carry /= 10;
+        }
+
+        return {rresult.rbegin(), rresult.rend()};
+    }
+};
