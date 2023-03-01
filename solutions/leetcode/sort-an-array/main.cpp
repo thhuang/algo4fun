0171d42940bbbf27ca02d7eb69b5231fe1a08874
TH Huang <tzhsn.huang@gmail.com>
2023-02-28 19:36:05 -0600

    [leetcode] sort an array

---
+++ solutions/leetcode/sort-an-array/main.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    vector<int> sortArray(vector<int>& nums) {
+        function<void(int, int)> mergeSort = [&](int l, int r) -> void {
+            if (l >= r) return;
+            int m = l + (r - l) / 2;
+
+            int la = l, ra = m;
+            mergeSort(la, ra);
+
+            int lb = m + 1, rb = r;
+            mergeSort(lb, rb);
+
+            vector<int> result;
+            while (la <= ra && lb <= rb) {
+                if (nums[la] < nums[lb]) {
+                    result.push_back(nums[la++]);
+                } else {
+                    result.push_back(nums[lb++]);
+                }
+            }
+            while (la <= ra) result.push_back(nums[la++]);
+            while (lb <= rb) result.push_back(nums[lb++]);
+
+            for (int i = 0; i < result.size(); ++i) nums[l + i] = result[i];
+        };
+
+        mergeSort(0, nums.size() - 1);
+
+        return nums;
+    }
+};
