ee40c1d84e2ebcae6cbed0ca2790f7c6a0902e32
TH Huang <tzhsn.huang@gmail.com>
2023-03-12 21:37:35 +0800

    [leetcode] valid number: refactor

--- solutions/leetcode/valid-number/main.cpp
+++ solutions/leetcode/valid-number/main.cpp
@@ -6,3 +6,15 @@ class Solution {
         return regex_match(s, rule);
     }
 };
+
+class Solution {
+    string integer = "([+-]?\\d+)";
+    string decimal = "([+-]?(\\d*\\.\\d+|\\d+\\.\\d*))";
+    string integerOrDecimal = "(" + integer + "|" + decimal + ")";
+    string e = "([eE]" + integer + ")";
+    string number = "(" + integerOrDecimal + e + "?" + ")";
+    regex rule = regex(number);
+
+   public:
+    bool isNumber(string s) { return regex_match(s, rule); }
+};
