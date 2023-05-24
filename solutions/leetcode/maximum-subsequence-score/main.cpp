e15a9bd401257e16b02734a387dff5aeb0b6fe41
TH Huang <tzhsn.huang@gmail.com>
2023-05-24 18:11:09 -0500

    [leetcode] maximum subsequence score: alternative

--- solutions/leetcode/maximum-subsequence-score/main.cpp
+++ solutions/leetcode/maximum-subsequence-score/main.cpp
@@ -32,3 +32,38 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+
+        vector<array<int, 2>> nums(n);
+        for (int i = 0; i < n; ++i) nums[i] = {nums2[i], nums1[i]};
+
+        sort(nums.begin(), nums.end(), greater<>());
+
+        long long result = 0;
+        long long curr = 0;
+        priority_queue<int, vector<int>, greater<>> pq;
+
+        for (int i = 0; i < n; ++i) {
+            int v1 = nums[i][1];
+            int v2 = nums[i][0];
+
+            pq.push(v1);
+            curr += v1;
+
+            if (pq.size() > k) {
+                curr -= pq.top();
+                pq.pop();
+            }
+
+            if (pq.size() == k) {
+                result = max(result, curr * v2);
+            }
+        }
+
+        return result;
+    }
+};
