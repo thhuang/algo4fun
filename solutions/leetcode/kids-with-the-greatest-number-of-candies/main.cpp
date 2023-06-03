582120f9ab25499be6c38ab0cdaa293061e4e8d4
TH Huang <tzhsn.huang@gmail.com>
2023-06-03 15:44:25 -0500

    [leetcode] kids with the greatest number of candies: refactor

--- solutions/leetcode/kids-with-the-greatest-number-of-candies/main.cpp
+++ solutions/leetcode/kids-with-the-greatest-number-of-candies/main.cpp
@@ -1,9 +1,10 @@
 class Solution {
    public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = candies.size();
         int mx = *max_element(candies.begin(), candies.end());
-        vector<bool> result;
-        for (int v : candies) result.push_back(v + extraCandies >= mx);
+        vector<bool> result(n);
+        for (int i = 0; i < n; ++i) result[i] = candies[i] + extraCandies >= mx;
         return result;
     }
 };
