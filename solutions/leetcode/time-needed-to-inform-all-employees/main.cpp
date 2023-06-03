654bf1be0d70a2de71aadcee495151ca4ae86af0
TH Huang <tzhsn.huang@gmail.com>
2023-06-03 10:53:51 -0500

    [leetcode] time needed to inform all employees: refactor

--- solutions/leetcode/time-needed-to-inform-all-employees/main.cpp
+++ solutions/leetcode/time-needed-to-inform-all-employees/main.cpp
@@ -2,23 +2,21 @@ class Solution {
    public:
     int numOfMinutes(int n, int headID, vector<int>& manager,
                      vector<int>& informTime) {
-        vector<vector<int>> edges(manager.size());
-        for (int i = 0; i < manager.size(); ++i) {
+        vector<vector<int>> subordinates(n);
+        for (int i = 0; i < n; ++i) {
             if (i == headID) continue;
-            edges[manager[i]].push_back(i);
+            subordinates[manager[i]].push_back(i);
         }
 
-        int ans = 0;
+        int result = 0;
         function<void(int, int)> dfs = [&](int u, int t) -> void {
-            if (edges[u].empty()) {
-                ans = max(ans, t);
-                return;
+            result = max(result, t);
+            for (int v : subordinates[u]) {
+                dfs(v, t + informTime[u]);
             }
-            for (int v : edges[u]) dfs(v, t + informTime[u]);
         };
-
         dfs(headID, 0);
 
-        return ans;
+        return result;
     }
 };
