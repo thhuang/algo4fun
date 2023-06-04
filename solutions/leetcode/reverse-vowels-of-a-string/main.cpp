01a6a53182908ac8d85cbe62e71870f0530fe820
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 12:43:05 -0500

    [leetcode] reverse vowels of a string: alternative

--- solutions/leetcode/reverse-vowels-of-a-string/main.cpp
+++ solutions/leetcode/reverse-vowels-of-a-string/main.cpp
@@ -16,3 +16,19 @@ class Solution {
         return s;
     }
 };
+
+class Solution {
+    const unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u',
+                                        'A', 'E', 'I', 'O', 'U'};
+
+   public:
+    string reverseVowels(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < r && vowels.count(s[l]) == 0) ++l;
+            while (l < r && vowels.count(s[r]) == 0) --r;
+            swap(s[l++], s[r--]);
+        }
+        return s;
+    }
+};
