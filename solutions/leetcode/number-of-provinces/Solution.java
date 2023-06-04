0ea794cc2ef12e1a0a9f86831acaa6f0e7b8f06b
TH Huang <tzhsn.huang@gmail.com>
2023-06-03 22:49:37 -0500

    [leetcode] number of provinces: refactor

--- solutions/leetcode/number-of-provinces/Solution.java
+++ solutions/leetcode/number-of-provinces/Solution.java
@@ -1,57 +1,61 @@
 class Solution {
-    private ArrayList<Integer> rank;
-    private ArrayList<Integer> group;
+    private class UnionFind {
+        int[] rank;
+        int[] group;
 
-    int find(int k) {
-        if (group.get(k) == k)
-            return k;
-        int v = find(group.get(k));
-        group.set(k, v);
-        return v;
-    }
-
-    void unite(int a, int b) {
-        int ga = find(a);
-        int gb = find(b);
-        if (ga == gb)
-            return;
-
-        if (rank.get(ga) > rank.get(gb)) {
-            group.set(gb, ga);
-        } else if (rank.get(gb) > rank.get(ga)) {
-            group.set(ga, gb);
-        } else {
-            rank.set(ga, rank.get(ga) + 1);
-            group.set(gb, ga);
+        UnionFind(int n) {
+            rank = new int[n];
+            group = new int[n];
+            for (int i = 0; i < n; ++i) {
+                group[i] = i;
+            }
         }
-    }
+
+        int find(int k) {
+            if (group[k] == k) {
+                return k;
+            }
+            return group[k] = find(group[k]);
+        }
+
+        boolean unite(int a, int b) {
+            int ga = find(a);
+            int gb = find(b);
+            if (ga == gb) {
+                return false;
+            }
+
+            if (rank[ga] < rank[gb]) {
+                group[ga] = gb;
+            } else if (rank[gb] < rank[ga]) {
+                group[gb] = ga;
+            } else {
+                group[ga] = gb;
+                ++rank[gb];
+            }
+            return true;
+        }
+    };
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
 
-        rank = new ArrayList<Integer>(n);
-        for (int i = 0; i < n; ++i) {
-            rank.add(0);
-        }
-
-        group = new ArrayList<Integer>(n);
-        for (int i = 0; i < n; ++i) {
-            group.add(i);
-        }
-
+        int result = n;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (isConnected[i][j] == 1) {
-                    unite(i, j);
+                if (isConnected[i][j] == 0) {
+                    continue;
+                }
+                if (uf.unite(i, j)) {
+                    --result;
                 }
             }
         }
 
-        HashSet<Integer> provinces = new HashSet<Integer>();
-        for (int i = 0; i < n; ++i) {
-            provinces.add(find(i));
-        }
-
-        return provinces.size();
+        return result;
     }
+
 }
+
+    
