ba10c3996a69e06bd543b7c0055c92a82424a6d7
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 11:28:39 -0500

    [leetcode] can place flowers: alternative

--- solutions/leetcode/can-place-flowers/main.cpp
+++ solutions/leetcode/can-place-flowers/main.cpp
@@ -44,3 +44,23 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+   public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (n == 0) return true;
+
+        int m = flowerbed.size();
+        for (int i = 0; i < m; ++i) {
+            if (i - 1 >= 0 && flowerbed[i - 1] == 1) continue;
+            if (flowerbed[i] == 1) continue;
+            if (i + 1 < m && flowerbed[i + 1] == 1) continue;
+
+            flowerbed[i] = 1;
+            --n;
+            if (n == 0) return true;
+        }
+
+        return false;
+    }
+};
