0368510a654739cecf0e98257d032e5767e9cd02
TH Huang <tzhsn.huang@gmail.com>
2023-06-05 13:43:52 -0500

    [leetcode] string compression

--- solutions/leetcode/string-compression/main.cpp
+++ solutions/leetcode/string-compression/main.cpp
@@ -18,3 +18,23 @@ class Solution {
         return p;
     }
 };
+
+class Solution {
+   public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int p = 0;
+        for (int l = 0; l < n;) {
+            int r = l + 1;
+            while (r < n && chars[l] == chars[r]) ++r;
+
+            int d = r - l;
+            chars[p++] = chars[l];
+            l = r;
+
+            if (d == 1) continue;
+            for (char c : to_string(d)) chars[p++] = c;
+        }
+        return p;
+    }
+};
