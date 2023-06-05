e2133ea0e1222a8dcdbebd7a126beec740cc173f
TH Huang <tzhsn.huang@gmail.com>
2023-06-04 23:27:34 -0500

    [leetcode] check if it is a straight line

--- solutions/leetcode/check-if-it-is-a-straight-line/main.cpp
+++ solutions/leetcode/check-if-it-is-a-straight-line/main.cpp
@@ -1,13 +1,11 @@
 class Solution {
    public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int x = coordinates[0][0];
-        int y = coordinates[0][1];
-        int dx = x - coordinates[1][0];
-        int dy = y - coordinates[1][1];
-        for (int i = 0; i < coordinates.size(); ++i) {
-            if (dx * (y - coordinates[i][1]) != dy * (x - coordinates[i][0]))
-                return false;
+        int x = coordinates[0][0], y = coordinates[0][1];
+        int dx = coordinates[1][0] - x, dy = coordinates[1][1] - y;
+        for (vector<int>& v : coordinates) {
+            int di = x - v[0], dj = y - v[1];
+            if (dx * dj != dy * di) return false;
         }
         return true;
     }
