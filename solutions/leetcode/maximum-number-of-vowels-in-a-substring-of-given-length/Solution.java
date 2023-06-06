89ac24adef2b34fa11026efdf0db7d79d3b77ad1
TH Huang <tzhsn.huang@gmail.com>
2023-06-06 14:56:59 -0500

    [leetcode] maximum number of vowels in a substring of given length

--- solutions/leetcode/maximum-number-of-vowels-in-a-substring-of-given-length/Solution.java
+++ solutions/leetcode/maximum-number-of-vowels-in-a-substring-of-given-length/Solution.java
@@ -24,3 +24,31 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    private static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public int maxVowels(String s, int k) {
+        int curr = 0;
+        for (int i = 0; i < k - 1; ++i) {
+            if (isVowel(s.charAt(i))) {
+                ++curr;
+            }
+        }
+
+        int result = 0;
+        for (int i = k - 1; i < s.length(); ++i) {
+            if (isVowel(s.charAt(i))) {
+                ++curr;
+            }
+            result = Math.max(result, curr);
+            if (isVowel(s.charAt(i - k + 1))) {
+                --curr;
+            }
+        }
+
+        return result;
+    }
+}
