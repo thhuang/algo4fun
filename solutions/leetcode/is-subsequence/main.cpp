e1f3764dd54ffb6b56f16908a4295afecc0a726a
TH Huang <tzhsn.huang@gmail.com>
2023-06-06 14:27:16 -0500

    [leetcode] is subsequence

--- solutions/leetcode/is-subsequence/main.cpp
+++ solutions/leetcode/is-subsequence/main.cpp
@@ -9,3 +9,20 @@ class Solution {
         return i == s.size();
     }
 };
+
+class Solution {
+   public:
+    bool isSubsequence(string s, string t) {
+        if (s.size() == 0) return true;
+
+        int m = s.size(), n = t.size();
+        for (int i = 0, j = 0; j < n; ++j) {
+            if (s[i] == t[j]) {
+                ++i;
+                if (i == m) return true;
+            }
+        }
+
+        return false;
+    }
+};
