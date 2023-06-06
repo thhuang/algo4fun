eced503b2ea1e8f6e92729add7eaf6df3b73816d
TH Huang <tzhsn.huang@gmail.com>
2023-06-05 21:40:20 -0500

    [leetcode] can make arithmetic progression from sequence: optimize

--- solutions/leetcode/can-make-arithmetic-progression-from-sequence/Solution.java
+++ solutions/leetcode/can-make-arithmetic-progression-from-sequence/Solution.java
@@ -10,3 +10,43 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int mx = Integer.MIN_VALUE;
+        int mn = Integer.MAX_VALUE;
+        for (int v : arr) {
+            mx = Math.max(mx, v);
+            mn = Math.min(mn, v);
+        }
+
+        int n = arr.length;
+        if ((mx - mn) % (n - 1) != 0) {
+            return false;
+        }
+        int d = (mx - mn) / (n - 1);
+        if (d == 0) {
+            return true;
+        }
+
+        boolean[] vis = new boolean[n];
+        for (int v : arr) {
+            int dv = v - mn;
+            if (dv % d != 0) {
+                return false;
+            }
+
+            int i = dv / d;
+            if (vis[i]) {
+                return false;
+            }
+            vis[i] = true;
+        }
+
+        return true;
+    }
+}
+
+
+
+    
