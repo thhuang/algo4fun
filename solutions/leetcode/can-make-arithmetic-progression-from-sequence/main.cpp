2373231e65e337511e54c6ff77d898edbf4145db
TH Huang <tzhsn.huang@gmail.com>
2023-06-05 20:39:49 -0500

    [leetcode] can make arithmetic progression from sequence: refactor

--- solutions/leetcode/can-make-arithmetic-progression-from-sequence/main.cpp
+++ solutions/leetcode/can-make-arithmetic-progression-from-sequence/main.cpp
@@ -1,12 +1,10 @@
 class Solution {
    public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        if (size(arr) < 3) return true;
-
-        sort(begin(arr), end(arr));
+        sort(arr.begin(), arr.end());
         int d = arr[1] - arr[0];
-        for (int i = 0; i < size(arr) - 1; ++i) {
-            if (arr[i + 1] - arr[i] != d) return false;
+        for (int i = 2; i < arr.size(); ++i) {
+            if (arr[i] - arr[i - 1] != d) return false;
         }
         return true;
     }
