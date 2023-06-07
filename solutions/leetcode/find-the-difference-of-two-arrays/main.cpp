355a1d66317e20d58f604b9c90588aeafb3a3ded
TH Huang <tzhsn.huang@gmail.com>
2023-06-07 17:12:23 -0500

    [leetcode] find the difference of two arrays

--- solutions/leetcode/find-the-difference-of-two-arrays/main.cpp
+++ solutions/leetcode/find-the-difference-of-two-arrays/main.cpp
@@ -1,26 +1,18 @@
 class Solution {
    public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1, set2;
-        for (int v : nums1) set1.insert(v);
-        for (int v : nums2) set2.insert(v);
-
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
         unordered_set<int> ans1, ans2;
         for (int v : nums1) {
-            if (set2.count(v)) continue;
-            ans1.insert(v);
+            if (s2.count(v) == 0) ans1.insert(v);
         }
         for (int v : nums2) {
-            if (set1.count(v)) continue;
-            ans2.insert(v);
+            if (s1.count(v) == 0) ans2.insert(v);
         }
-
         vector<vector<int>> result(2);
-        for (auto it = ans1.begin(); it != ans1.end(); ++it)
-            result[0].push_back(*it);
-        for (auto it = ans2.begin(); it != ans2.end(); ++it)
-            result[1].push_back(*it);
-
+        result[0] = {ans1.begin(), ans1.end()};
+        result[1] = {ans2.begin(), ans2.end()};
         return result;
     }
 };
