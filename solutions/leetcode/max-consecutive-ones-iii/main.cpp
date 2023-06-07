57cfb7b0d885402e4fd9d9dc26f576295349b7e1
TH Huang <tzhsn.huang@gmail.com>
2023-06-07 16:48:37 -0500

    [leetcode] max consecutive ones iii

--- solutions/leetcode/max-consecutive-ones-iii/main.cpp
+++ solutions/leetcode/max-consecutive-ones-iii/main.cpp
@@ -12,3 +12,19 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int longestOnes(vector<int>& nums, int k) {
+        int result = 0;
+        for (int l = 0, r = 0; r < nums.size(); ++r) {
+            if (nums[r] == 0) --k;
+            if (k < 0) {
+                while (nums[l++] == 1);
+                ++k;
+            }
+            result = max(result, r - l + 1);
+        }
+        return result;
+    }
+};
