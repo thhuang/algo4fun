0870d68f6c04367184e1de221d1b146b9463c741
TH Huang <tzhsn.huang@gmail.com>
2023-06-08 20:57:04 -0500

    [leetcode] determine if two strings are close

--- solutions/leetcode/determine-if-two-strings-are-close/main.cpp
+++ solutions/leetcode/determine-if-two-strings-are-close/main.cpp
@@ -21,3 +21,26 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+
+        unordered_set<char> char1, char2;
+        unordered_map<char, int> charCount1, charCount2;
+        for (char c : word1) ++charCount1[c], char1.insert(c);
+        for (char c : word2) ++charCount2[c], char2.insert(c);
+
+        if (char1 != char2) return false;
+
+        unordered_map<int, int> countChar1, countChar2;
+        for (auto [_, cnt] : charCount1) ++countChar1[cnt];
+        for (auto [_, cnt] : charCount2) ++countChar2[cnt];
+
+        for (auto [cnt, chs] : countChar1) {
+            if (countChar2[cnt] != chs) return false;
+        }
+        return true;
+    }
+};
