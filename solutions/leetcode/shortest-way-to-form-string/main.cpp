8c97620f0199512c68bbe303b8e2ad2b36af6d75
TH Huang <tzhsn.huang@gmail.com>
2023-06-13 15:21:07 -0500

    [leetcode] shortest way to form string

--- solutions/leetcode/shortest-way-to-form-string/main.cpp
+++ solutions/leetcode/shortest-way-to-form-string/main.cpp
@@ -15,3 +15,26 @@ class Solution {
         return count;
     }
 };
+
+class Solution {
+   public:
+    int shortestWay(string source, string target) {
+        unordered_set<int> chars(source.begin(), source.end());
+        for (char c : target) {
+            if (chars.count(c) == 0) return -1;
+        }
+
+        int result = 1;
+        int i = 0;
+        for (char c : target) {
+            if (i == source.size()) i = 0, ++result;
+            while (source[i] != c) {
+                ++i;
+                if (i == source.size()) i = 0, ++result;
+            }
+            ++i;
+        }
+
+        return result;
+    }
+};
