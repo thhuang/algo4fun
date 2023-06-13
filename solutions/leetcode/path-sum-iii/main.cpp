a814d029cf7c7e8a427ffc04c1a45b55b959b4b5
TH Huang <tzhsn.huang@gmail.com>
2023-06-12 20:12:27 -0500

    [leetcode] path sum iii

--- solutions/leetcode/path-sum-iii/main.cpp
+++ solutions/leetcode/path-sum-iii/main.cpp
@@ -6,51 +6,21 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
    public:
     int pathSum(TreeNode* root, int targetSum) {
-        unordered_map<long, int> sum_count;
-        ++sum_count[0];
-
         int result = 0;
-        long v = 0;
-        function<void(TreeNode*)> dfs = [&](TreeNode* u) -> void {
-            if (!u) return;
+        
+        unordered_map<long long, int> prefix;
+        prefix[0] = 1;
+
+        function<void(TreeNode*, long long)> dfs = [&](TreeNode* u, long long v) -> void {
+            if (u == nullptr) return;    
 
             v += u->val;
-            result += sum_count[v - targetSum];
-            ++sum_count[v];
-
-            dfs(u->left);
-            dfs(u->right);
-
-            --sum_count[v];
-            v -= u->val;
-        };
-
-        dfs(root);
-
-        return result;
-    }
-};
-
-class Solution {
-    typedef long long ll;
-
-   public:
-    int pathSum(TreeNode* root, int targetSum) {
-        unordered_map<ll, int> prefix;
-        prefix.insert({0, 1});
-        int result = 0;
-
-        function<void(TreeNode*, ll)> dfs = [&](TreeNode* u, ll s) -> void {
-            if (!u) return;
-
-            ll v = s + u->val;
             result += prefix[v - targetSum];
 
             ++prefix[v];
