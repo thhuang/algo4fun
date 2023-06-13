af6e66d1562cdef5cca959bcaab19136fbf8c667
TH Huang <tzhsn.huang@gmail.com>
2023-06-12 21:00:24 -0500

    [leetcode] lowest common ancestor of a binary search tree

--- solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/main.cpp
+++ solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/main.cpp
@@ -19,3 +19,16 @@ class Solution {
         return l ? l : r;
     }
 };
+
+class Solution {
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == nullptr) return nullptr;
+        if (root == p || root == q) return root;
+        auto l = lowestCommonAncestor(root->left, p, q);
+        auto r = lowestCommonAncestor(root->right, p, q);
+        if (l == nullptr) return r;
+        if (r == nullptr) return l;
+        return root;
+    }
+};
