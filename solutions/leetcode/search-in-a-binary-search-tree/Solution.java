6b200df2604cb1e9992fe4a39bf0b250c987c00b
TH Huang <tzhsn.huang@gmail.com>
2023-06-13 12:36:36 -0500

    [leetcode] search in a binary search tree

---
+++ solutions/leetcode/search-in-a-binary-search-tree/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val)
+            return root;
+        return (val < root.val) ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
