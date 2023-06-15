5283534b4b4b4c8166ab054a126b2cd93525daba
TH Huang <tzhsn.huang@gmail.com>
2023-06-15 18:06:53 -0500

    [leetcode] koko eating bananas

--- solutions/leetcode/koko-eating-bananas/main.cpp
+++ solutions/leetcode/koko-eating-bananas/main.cpp
@@ -1,15 +1,16 @@
 class Solution {
    public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end());
+        auto check = [&](int m) -> bool {
+            long long sum = 0;
+            for (int v : piles) sum += (v - 1) / m + 1;
+            return sum <= h;
+        };
 
+        int l = 1, r = *max_element(piles.begin(), piles.end());
         while (l <= r) {
             int m = l + (r - l) / 2;
-
-            long long v = 0;
-            for (int p : piles) v += (p - 1) / m + 1;
-
-            if (v <= h) {
+            if (check(m)) {
                 r = m - 1;
             } else {
                 l = m + 1;
