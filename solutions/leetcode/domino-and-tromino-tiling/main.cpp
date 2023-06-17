5d9691758d3ba9bdeb469a0db7e9723fbc538df1
TH Huang <tzhsn.huang@gmail.com>
2023-06-17 15:59:45 -0500

    [leetcode] domino and tromino tiling

--- solutions/leetcode/domino-and-tromino-tiling/main.cpp
+++ solutions/leetcode/domino-and-tromino-tiling/main.cpp
@@ -37,3 +37,20 @@ class Solution {
         return dp[1][0];
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int numTilings(int n) {
+        array<array<int, 2>, 2> dp;
+        dp[0] = {1, 0};
+        dp[1] = {1, 0};
+        for (int i = 2; i <= n; ++i) {
+            dp = {dp[1],
+                  {(int)(((long long)dp[0][0] + dp[1][0] + dp[1][1] * 2) % mod),
+                   (int)(((long long)dp[0][0] + dp[1][1]) % mod)}};
+        }
+        return dp[1][0];
+    }
+};
