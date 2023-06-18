7892167337e8d5157f3e6136d16c10422bd50cbf
TH Huang <tzhsn.huang@gmail.com>
2023-06-18 18:17:20 -0500

    [leetcode] number of increasing paths in a grid

--- solutions/leetcode/number-of-increasing-paths-in-a-grid/main.cpp
+++ solutions/leetcode/number-of-increasing-paths-in-a-grid/main.cpp
@@ -9,21 +9,22 @@ class Solution {
 
         vector<vector<int>> dp(m, vector<int>(n, -1));
         function<int(int, int, int)> dfs = [&](int i, int j, int p) -> int {
-            if (i < 0 || i >= m || j < 0 || j >= n) return 0;
-            if (grid[i][j] <= p) return 0;
+            if (i < 0 || m <= i || j < 0 || n <= j) return -1;
+            if (p >= grid[i][j]) return -1;
             if (dp[i][j] != -1) return dp[i][j];
 
-            long long v = 1;
+            long long result = 0;
             for (auto [di, dj] : directions) {
-                v = (v + dfs(i + di, j + dj, grid[i][j])) % mod;
+                (result += dfs(i + di, j + dj, grid[i][j]) + 1) %= mod;
             }
-            return dp[i][j] = v;
+
+            return dp[i][j] = result;
         };
 
-        long long result = 0;
+        long long result = m * n;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                result = (result + dfs(i, j, 0)) % mod;
+                (result += dfs(i, j, 0)) %= mod;
             }
         }
 
@@ -42,19 +43,19 @@ class Solution {
 
         vector<vector<int>> dp(m, vector<int>(n, -1));
         function<int(int, int, int)> dfs = [&](int i, int j, int p) -> int {
-            if (i < 0 || m <= i || j < 0 || n <= j) return -1;
-            if (p >= grid[i][j]) return -1;
+            if (i < 0 || m <= i || j < 0 || n <= j) return 0;
+            if (p >= grid[i][j]) return 0;
             if (dp[i][j] != -1) return dp[i][j];
 
-            long long result = 0;
+            long long result = 1;
             for (auto [di, dj] : directions) {
-                (result += dfs(i + di, j + dj, grid[i][j]) + 1) %= mod;
+                (result += dfs(i + di, j + dj, grid[i][j])) %= mod;
             }
 
             return dp[i][j] = result;
         };
 
-        long long result = m * n;
+        long long result = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 (result += dfs(i, j, 0)) %= mod;
