5d70b02fb46c37e3e29ff3a514b3516f914eb2de
TH Huang <tzhsn.huang@gmail.com>
2023-06-18 19:06:01 -0500

    [leetcode] minimum flips to make a or b equal to c

--- solutions/leetcode/minimum-flips-to-make-a-or-b-equal-to-c/main.cpp
+++ solutions/leetcode/minimum-flips-to-make-a-or-b-equal-to-c/main.cpp
@@ -17,3 +17,21 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int minFlips(int a, int b, int c) {
+        int result = 0;
+        for (int i = 0; i < 32; ++i) {
+            int mask = 1 << i;
+
+            bool aBit = a & mask;
+            bool bBit = b & mask;
+            bool cBit = c & mask;
+
+            if ((aBit || bBit) == cBit) continue;
+            result += cBit ? 1 : aBit + bBit;
+        }
+        return result;
+    }
+};
