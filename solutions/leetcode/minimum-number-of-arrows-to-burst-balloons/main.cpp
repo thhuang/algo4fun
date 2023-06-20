78190fa064187387427061420050da7579ddbb46
TH Huang <tzhsn.huang@gmail.com>
2023-06-20 13:14:52 -0500

    [leetcode] minimum number of arrows to burst balloons

--- solutions/leetcode/minimum-number-of-arrows-to-burst-balloons/main.cpp
+++ solutions/leetcode/minimum-number-of-arrows-to-burst-balloons/main.cpp
@@ -1,28 +1,6 @@
 class Solution {
    public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 if (a[1] == b[1]) return a[0] < b[0];
-                 return a[1] < b[1];
-             });
-
-        int count = 0;
-        int i = 0;
-        while (i < points.size()) {
-            ++count;
-
-            int x = points[i][1];
-            while (i < points.size() && points[i][0] <= x) ++i;
-        }
-
-        return count;
-    }
-};
-
-class Solution {
-   public:
-    int findMinArrowShots(vector<vector<int>>& points) {
         priority_queue<int, vector<int>, greater<>> pq;
         vector<int> shots;
         for (auto& p : points) shots.push_back(p[1]);
@@ -54,17 +32,42 @@ class Solution {
 class Solution {
    public:
     int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 if (a[1] == b[1]) return a[0] < b[0];
+                 return a[1] < b[1];
+             });
+
+        int count = 0;
+        int i = 0;
+        while (i < points.size()) {
+            ++count;
+
+            int x = points[i][1];
+            while (i < points.size() && points[i][0] <= x) ++i;
+        }
+
+        return count;
+    }
+};
+
+class Solution {
+   public:
+    int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end());
-        int count = 1;
-        int last_arrow = points[0][1];
+
+        int result = 1;
+        int lastArrow = points[0][1];
+
         for (const auto& p : points) {
-            if (last_arrow < p[0]) {
-                ++count;
-                last_arrow = p[1];
+            if (lastArrow < p[0]) {
+                ++result;
+                lastArrow = p[1];
             } else {
-                last_arrow = min(last_arrow, p[1]);
+                lastArrow = min(lastArrow, p[1]);
             }
         }
-        return count;
+
+        return result;
     }
 };
