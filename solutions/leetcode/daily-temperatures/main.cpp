034a1b97eface5c62752d699f99bfdcd97ce2189
TH Huang <tzhsn.huang@gmail.com>
2023-06-20 13:44:17 -0500

    [leetcode] daily temperatures

--- solutions/leetcode/daily-temperatures/main.cpp
+++ solutions/leetcode/daily-temperatures/main.cpp
@@ -4,14 +4,14 @@ class Solution {
         int n = temperatures.size();
         vector<int> result(n, 0);
 
-        vector<int> desc;
-        for (int i = 0; i < temperatures.size(); ++i) {
-            while (!desc.empty() &&
-                   temperatures[desc.back()] < temperatures[i]) {
-                result[desc.back()] = i - desc.back();
-                desc.pop_back();
+        for (int i = n - 1, mx = temperatures.back(); i >= 0; --i) {
+            if (temperatures[i] >= mx) {
+                mx = temperatures[i];
+                continue;
             }
-            desc.push_back(i);
+            int d = 1;
+            while (temperatures[i] >= temperatures[i + d]) d += result[i + d];
+            result[i] = d;
         }
 
         return result;
@@ -21,17 +21,36 @@ class Solution {
 class Solution {
    public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> result(n, 0);
+        vector<int> result(temperatures.size(), 0);
 
-        for (int i = n - 1, mx = temperatures.back(); i >= 0; --i) {
-            if (temperatures[i] >= mx) {
-                mx = temperatures[i];
-                continue;
+        vector<array<int, 2>> tempDays;  // {temp, day}
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!tempDays.empty() && tempDays.back()[0] < temperatures[i]) {
+                auto [_, d] = tempDays.back();
+                tempDays.pop_back();
+                result[d] = i - d;
             }
-            int d = 1;
-            while (temperatures[i] >= temperatures[i + d]) d += result[i + d];
-            result[i] = d;
+            tempDays.push_back({temperatures[i], i});
+        }
+
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+
+        vector<int> days;  // {temp, day}
+        for (int i = 0; i < temperatures.size(); ++i) {
+            while (!days.empty() &&
+                   temperatures[days.back()] < temperatures[i]) {
+                int d = days.back();
+                days.pop_back();
+                result[d] = i - d;
+            }
+            days.push_back(i);
         }
 
         return result;
