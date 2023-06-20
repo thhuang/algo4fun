b220091a8e5e4e5fd2747347b013e5b07823ed7c
TH Huang <tzhsn.huang@gmail.com>
2023-06-20 13:21:09 -0500

    [leetcode] minimum number of arrows to burst balloons

--- solutions/leetcode/minimum-number-of-arrows-to-burst-balloons/main.cpp
+++ solutions/leetcode/minimum-number-of-arrows-to-burst-balloons/main.cpp
@@ -33,21 +33,20 @@ class Solution {
    public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 if (a[1] == b[1]) return a[0] < b[0];
-                 return a[1] < b[1];
+             [](const auto& a, const auto& b) -> bool {
+                 if (a[1] != b[1]) return a[1] < b[1];
+                 return a[0] < b[0];
              });
 
-        int count = 0;
-        int i = 0;
-        while (i < points.size()) {
-            ++count;
+        int result = 0;
 
+        for (int i = 0; i < points.size();) {
+            ++result;
             int x = points[i][1];
             while (i < points.size() && points[i][0] <= x) ++i;
         }
 
-        return count;
+        return result;
     }
 };
 
