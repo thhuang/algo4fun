1d26a46f75d138460f14fe2bfc6dfeb29472adfb
TH Huang <tzhsn.huang@gmail.com>
2023-06-21 16:18:37 -0500

    [leetcode] majority element

--- solutions/leetcode/majority-element/main.cpp
+++ solutions/leetcode/majority-element/main.cpp
@@ -1,16 +1,13 @@
 class Solution {
    public:
     int majorityElement(vector<int>& nums) {
-        int result = nums.front();
-        int count = 1;
+        int cnt = 1;
+        int result = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
             if (nums[i] == result) {
-                ++count;
-                continue;
-            }
-            if (--count == 0) {
-                result = nums[i];
-                count = 1;
+                ++cnt;
+            } else {
+                if (--cnt == -1) cnt = 1, result = nums[i];
             }
         }
         return result;
