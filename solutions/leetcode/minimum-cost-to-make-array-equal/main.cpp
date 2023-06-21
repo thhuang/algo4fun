11f81f3bc0aa9fd73b6674863d77a70a32c5c1da
TH Huang <tzhsn.huang@gmail.com>
2023-06-20 20:43:46 -0500

    [leetcode] minimum cost to make array equal

--- solutions/leetcode/minimum-cost-to-make-array-equal/main.cpp
+++ solutions/leetcode/minimum-cost-to-make-array-equal/main.cpp
@@ -1,29 +1,27 @@
 class Solution {
    public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        int n = nums.size();
-        int l = *min_element(nums.begin(), nums.end());
-        int r = *max_element(nums.begin(), nums.end());
-
-        auto cal = [&](int t) -> long long {
+        function<long long(int)> calc = [&](int t) -> long long {
             long long result = 0;
-            for (int i = 0; i < n; ++i) {
-                result += abs((long long)(nums[i] - t) * cost[i]);
+            for (int i = 0; i < nums.size(); ++i) {
+                result += (long long)abs(nums[i] - t) * cost[i];
             }
             return result;
         };
 
-        while (l < r) {
+        int l = *min_element(nums.begin(), nums.end());
+        int r = *max_element(nums.begin(), nums.end());
+        while (l <= r) {
             int m = l + (r - l) / 2;
-            long long c1 = cal(m);
-            long long c2 = cal(m + 1);
-            if (c1 < c2) {
-                r = m;
-            } else {
+            long long a = calc(m - 1);
+            long long b = calc(m);
+            if (a > b) {
                 l = m + 1;
+            } else {
+                r = m - 1;
             }
         }
 
-        return min(cal(l), cal(r));
+        return calc(r);
     }
 };
