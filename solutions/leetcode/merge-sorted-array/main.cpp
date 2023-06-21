318a1641b62b8910351c666078a0407b4b0df080
TH Huang <tzhsn.huang@gmail.com>
2023-06-21 16:02:45 -0500

    [leetcode] merge sorted array

--- solutions/leetcode/merge-sorted-array/main.cpp
+++ solutions/leetcode/merge-sorted-array/main.cpp
@@ -32,3 +32,22 @@ class Solution {
         }
     }
 };
+
+class Solution {
+   public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int p = m + n - 1;
+        while (p >= 0) {
+            if (i < 0)
+                nums1[p--] = nums2[j--];
+            else if (j < 0)
+                nums1[p--] = nums1[i--];
+            else if (nums1[i] < nums2[j])
+                nums1[p--] = nums2[j--];
+            else
+                nums1[p--] = nums1[i--];
+        }
+    }
+};
