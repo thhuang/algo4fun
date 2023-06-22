6fca928451b222e1f301bbb53aa328ab51e8b12a
TH Huang <tzhsn.huang@gmail.com>
2023-06-22 17:27:45 -0500

    [leetcode] evaluate division

--- solutions/leetcode/evaluate-division/Solution.java
+++ solutions/leetcode/evaluate-division/Solution.java
@@ -1,26 +1,21 @@
 class Solution {
     HashMap<String, HashMap<String, Double>> adj;
     HashSet<String> vis;
+    
 
     private double dfs(String a, String t) {
-        if (a == t)
-            return 0.0;
-        if (!adj.containsKey(a))
-            return -1.0;
-        if (adj.get(a).containsKey(t))
-            return adj.get(a).get(t);
-        if (vis.contains(a))
-            return -1.0;
+        if (a == t) return 0.0;
+        if (!adj.containsKey(a)) return -1.0;
+        if (adj.get(a).containsKey(t)) return adj.get(a).get(t);
+        if (vis.contains(a)) return -1.0;
         vis.add(a);
         for (var bv : adj.get(a).entrySet()) {
             var b = bv.getKey();
             var v = bv.getValue();
-            if (v < 0)
-                continue;
+            if (v < 0) continue;
 
             var got = dfs(b, t);
-            if (got < 0)
-                continue;
+            if (got < 0) continue;
 
             var res = v * got;
             adj.get(a).put(t, res);
@@ -29,8 +24,7 @@ class Solution {
         return -1.0;
     }
 
-    public double[] calcEquation(
-        List<List<String>> equations, double[] values, List<List<String>> queries) {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         adj = new HashMap<>();
 
         for (int i = 0; i < equations.size(); ++i) {
@@ -38,14 +32,11 @@ class Solution {
             var b = equations.get(i).get(1);
             var v = values[i];
 
-            if (!adj.containsKey(a))
-                adj.put(a, new HashMap<String, Double>());
-            if (!adj.containsKey(b))
-                adj.put(b, new HashMap<String, Double>());
-
+            if (!adj.containsKey(a)) adj.put(a, new HashMap<String, Double>());
+            if (!adj.containsKey(b)) adj.put(b, new HashMap<String, Double>());
+            
             adj.get(a).put(b, v);
-            if (v != 0)
-                adj.get(b).put(a, 1.0 / v);
+            if (v != 0) adj.get(b).put(a, 1.0 / v);
         }
 
         var result = new double[queries.size()];
