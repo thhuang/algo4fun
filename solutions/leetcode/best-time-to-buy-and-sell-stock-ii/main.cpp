cd161681f2c9d74031bb4685307ddbeca8f4606d
TH Huang <tzhsn.huang@gmail.com>
2023-06-22 23:06:05 -0500

    [leetcode] best time to buy and sell stock ii

--- solutions/leetcode/best-time-to-buy-and-sell-stock-ii/main.cpp
+++ solutions/leetcode/best-time-to-buy-and-sell-stock-ii/main.cpp
@@ -1,10 +1,10 @@
 class Solution {
    public:
     int maxProfit(vector<int>& prices) {
-        int ans = 0;
+        int result = 0;
         for (int i = 1; i < prices.size(); ++i) {
-            ans += max(0, prices[i] - prices[i - 1]);
+            result += max(0, prices[i] - prices[i - 1]);
         }
-        return ans;
+        return result;
     }
 };
