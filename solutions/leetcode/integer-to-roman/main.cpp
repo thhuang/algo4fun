e87767e8f7f26d2baaba662aa09debc23a8ca83e
TH Huang <tzhsn.huang@gmail.com>
2023-06-24 16:18:06 -0500

    [leetcode] integer to roman

--- solutions/leetcode/integer-to-roman/main.cpp
+++ solutions/leetcode/integer-to-roman/main.cpp
@@ -13,3 +13,33 @@ class Solution {
                ten[num % 100 / 10] + one[num % 10];
     }
 };
+
+class Solution {
+    string helper(int v, char ten, char five, char one) {
+        string result;
+        if (v == 9) {
+            result += one;
+            result += ten;
+        } else if (v == 4) {
+            result += one;
+            result += five;
+        } else {
+            if (v >= 5) result += five;
+            result += string(v % 5, one);
+        }
+        return result;
+    }
+
+   public:
+    string intToRoman(int num) {
+        string result;
+        result += string(num / 1000, 'M');
+        num %= 1000;
+        result += helper(num / 100, 'M', 'D', 'C');
+        num %= 100;
+        result += helper(num / 10, 'C', 'L', 'X');
+        num %= 10;
+        result += helper(num, 'X', 'V', 'I');
+        return result;
+    }
+};
