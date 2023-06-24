5b29438c06eacf9d5bcce322bc25dbfc869b1f1c
TH Huang <tzhsn.huang@gmail.com>
2023-06-24 14:36:13 -0500

    [leetcode] h-index

--- solutions/leetcode/h-index/main.cpp
+++ solutions/leetcode/h-index/main.cpp
@@ -20,3 +20,15 @@ class Solution {
         return r;
     }
 };
+
+class Solution {
+   public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        sort(citations.begin(), citations.end());
+        for (int i = n - 1; i >= 0; --i) {
+            if (citations[i] < n - i) return n - i - 1;
+        }
+        return citations.size();
+    }
+};
