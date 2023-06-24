e1bf99677d7b560e5b937e8fefcff6b811661fb1
TH Huang <tzhsn.huang@gmail.com>
2023-06-24 16:00:16 -0500

    [leetcode] roman to integer

--- solutions/leetcode/roman-to-integer/main.cpp
+++ solutions/leetcode/roman-to-integer/main.cpp
@@ -14,3 +14,30 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    int mp(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+
+   public:
+    int romanToInt(string s) {
+        int n = s.size();
+        int result = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            int v = mp(s[i]);
+            if (i + 1 < n && v < mp(s[i + 1])) v = -v;
+            result += v;
+        }
+        return result;
+    }
+};
