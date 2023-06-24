cdae2effad63d7700a5f3eb6b7dd9b5d1d46d127
TH Huang <tzhsn.huang@gmail.com>
2023-06-24 15:35:46 -0500

    [leetcode] candy

--- solutions/leetcode/candy/main.cpp
+++ solutions/leetcode/candy/main.cpp
@@ -18,3 +18,31 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+
+        vector<int> ones;
+        for (int i = 0; i < n; ++i) {
+            int l = (i - 1 < 0) ? numeric_limits<int>::max() : ratings[i - 1];
+            int r = (i + 1 == n) ? numeric_limits<int>::max() : ratings[i + 1];
+            if (l >= ratings[i] && ratings[i] <= r) ones.push_back(i);
+        }
+
+        vector<int> candies(n, 1);
+        for (int m : ones) {
+            for (int i = 1; m + i < n && ratings[m + i] > ratings[m + i - 1];
+                 ++i) {
+                candies[m + i] = max(candies[m + i], 1 + i);
+            }
+            for (int i = 1; m - i >= 0 && ratings[m - i] > ratings[m - i + 1];
+                 ++i) {
+                candies[m - i] = max(candies[m - i], 1 + i);
+            }
+        }
+
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
