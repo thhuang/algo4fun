85d726012417b0ca303f1c9f6ff0ebae6b564a58
TH Huang <tzhsn.huang@gmail.com>
2023-06-25 13:27:55 -0700

    [leetcode] text justification

--- solutions/leetcode/text-justification/main.cpp
+++ solutions/leetcode/text-justification/main.cpp
@@ -41,3 +41,51 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        auto generate = [&](int l, int r) -> string {
+            int spaces = maxWidth;
+            for (int i = l; i < r; ++i) spaces -= words[i].size();
+
+            int cnt = r - l - 1;
+            if (cnt == 0) return words[l] + string(spaces, ' ');
+
+            int pad = spaces / cnt;
+            int rem = spaces % cnt;
+
+            string result;
+            for (int i = l; i < r - 1; ++i) {
+                result += words[i] + string(pad + (rem-- > 0), ' ');
+            }
+            result += words[r - 1];
+
+            return result;
+        };
+
+        vector<string> result;
+        int n = words.size();
+        for (int l = 0; l < n;) {
+            int r = l + 1, cnt = words[l].size();
+            while (r < n) {
+                cnt += 1 + words[r].size();
+                if (cnt > maxWidth) break;
+                ++r;
+            }
+
+            if (r < n) {
+                result.push_back(generate(l, r));
+            } else {
+                string s = words[l];
+                for (int i = l + 1; i < r; ++i) s += " " + words[i];
+                s += string(maxWidth - s.size(), ' ');
+                result.push_back(s);
+            }
+
+            l = r;
+        }
+
+        return result;
+    }
+};
