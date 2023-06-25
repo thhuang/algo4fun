8619c3e94b9b58fee3ae999adb68f392012eed82
TH Huang <tzhsn.huang@gmail.com>
2023-06-25 10:49:36 -0700

    [leetcode] zigzag conversion

--- solutions/leetcode/zigzag-conversion/main.cpp
+++ solutions/leetcode/zigzag-conversion/main.cpp
@@ -2,16 +2,14 @@ class Solution {
    public:
     string convert(string s, int numRows) {
         if (numRows == 1) return s;
+
         vector<string> rows(numRows);
-        int i = 0;
-        int dir = 1;
+        int r = 0, dir = 1;
         for (char c : s) {
-            rows[i] += c;
-            if (i + dir < 0 || i + dir == numRows) dir = -dir;
-            i += dir;
+            rows[r] += c;
+            if (r + dir < 0 || numRows == r + dir) dir = -dir;
+            r += dir;
         }
-        string result;
-        for (string r : rows) result += r;
-        return result;
+        return accumulate(rows.begin(), rows.end(), string());
     }
 };
