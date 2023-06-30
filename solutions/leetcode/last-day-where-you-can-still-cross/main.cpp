152d4f5163ff3aed4378efe683304360910ca36f
TH Huang <tzhsn.huang@gmail.com>
2023-06-29 19:40:54 -0700

    [leetcode] last day where you can still cross

--- solutions/leetcode/last-day-where-you-can-still-cross/main.cpp
+++ solutions/leetcode/last-day-where-you-can-still-cross/main.cpp
@@ -41,3 +41,64 @@ class Solution {
         return l;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {
+        {0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+   public:
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        int n = row * col;
+
+        vector<int> rank(n + 2, 0);
+        vector<int> group(n + 2);
+        iota(group.begin(), group.end(), 0);
+
+        function<int(int)> find = [&](int k) -> int {
+            if (k == group[k]) return k;
+            return group[k] = find(group[k]);
+        };
+
+        function<void(int, int)> unite = [&](int a, int b) -> void {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa == pb) return;
+            if (rank[pa] < rank[pb]) {
+                group[pa] = pb;
+            } else if (rank[pb] < rank[pa]) {
+                group[pb] = pa;
+            } else {
+                group[pb] = pa;
+                ++rank[pa];
+            }
+        };
+
+        vector<bool> water(n + 2, false);
+        for (int t = 0; t < n; ++t) {
+            int i = cells[t][0] - 1, j = cells[t][1] - 1;
+            int u = i * col + j;
+
+            water[u] = true;
+
+            for (auto [di, dj] : directions) {
+                int ii = i + di, jj = j + dj;
+                if (ii < 0 || row <= ii || jj < 0 || col <= jj) continue;
+
+                int v = ii * col + jj;
+                if (!water[v]) continue;
+
+                unite(u, v);
+            }
+
+            if (j == 0) {
+                unite(u, n);
+            } else if (j == col - 1) {
+                unite(u, n + 1);
+            }
+
+            if (find(n) == find(n + 1)) return t;
+        }
+
+        return -1;
+    }
+};
