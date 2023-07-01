a7d97b5418f29a324709857fb4f90dbe4d388e9d
TH Huang <tzhsn.huang@gmail.com>
2023-06-30 18:14:52 -0700

    [leetcode] valid palindrome

--- solutions/leetcode/valid-palindrome/main.cpp
+++ solutions/leetcode/valid-palindrome/main.cpp
@@ -16,3 +16,17 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) ++l;
+            while (l < r && !isalnum(s[r])) --r;
+            if (l >= r) break;
+            if (tolower(s[l++]) != tolower(s[r--])) return false;
+        }
+        return true;
+    }
+};
