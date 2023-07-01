41466c71f9bc60108dca765d867b4504d7035773
TH Huang <tzhsn.huang@gmail.com>
2023-06-30 18:07:04 -0700

    [leetcode] fair distribution of cookies

--- solutions/leetcode/fair-distribution-of-cookies/main.cpp
+++ solutions/leetcode/fair-distribution-of-cookies/main.cpp
@@ -22,3 +22,27 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        int result = accumulate(cookies.begin(), cookies.end(), 0);
+        vector<int> got(k, 0);
+        function<void(int)> dfs = [&](int i) -> void {
+            if (i == cookies.size()) {
+                result = min(result, *max_element(got.begin(), got.end()));
+                return;
+            }
+
+            for (int j = 0; j < k; ++j) {
+                got[j] += cookies[i];
+                if (got[j] < result) dfs(i + 1);
+                got[j] -= cookies[i];
+            }
+        };
+
+        dfs(0);
+
+        return result;
+    }
+};
