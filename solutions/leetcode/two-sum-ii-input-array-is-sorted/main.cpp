a46ec50934006adb0b2c9c31b9682425a58bbe5f
TH Huang <tzhsn.huang@gmail.com>
2023-07-01 18:49:30 -0700

    [leetcode] two sum ii input array is sorted

--- solutions/leetcode/two-sum-ii-input-array-is-sorted/main.cpp
+++ solutions/leetcode/two-sum-ii-input-array-is-sorted/main.cpp
@@ -14,3 +14,21 @@ class Solution {
         return {};
     }
 };
+
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum < target) {
+                ++l;
+            } else if (sum > target) {
+                --r;
+            } else {
+                return {l + 1, r + 1};
+            }
+        }
+        return {};
+    }
+};
