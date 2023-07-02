7b3df415493a0a62f22af39cdaf791653b6bdfe2
TH Huang <tzhsn.huang@gmail.com>
2023-07-01 18:45:23 -0700

    [leetcode] 3sum

--- solutions/leetcode/3sum/main.cpp
+++ solutions/leetcode/3sum/main.cpp
@@ -73,3 +73,35 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            int l = i + 1, r = nums.size() - 1;
+
+            while (l < r) {
+                if (l > i + 1 && nums[l - 1] == nums[l]) {
+                    ++l;
+                    continue;
+                }
+
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < 0) {
+                    ++l;
+                } else if (sum > 0) {
+                    --r;
+                } else {
+                    result.push_back({nums[i], nums[l], nums[r]});
+                    ++l, --r;
+                }
+            }
+        }
+
+        return result;
+    }
+};
