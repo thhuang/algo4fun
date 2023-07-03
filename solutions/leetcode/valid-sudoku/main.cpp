d7684e586e2b3719ada744b37996107d415b5d7e
TH Huang <tzhsn.huang@gmail.com>
2023-07-02 18:43:38 -0700

    [leetcode] valid sudoku

--- solutions/leetcode/valid-sudoku/main.cpp
+++ solutions/leetcode/valid-sudoku/main.cpp
@@ -40,18 +40,19 @@ class Solution {
 };
 
 class Solution {
+    array<bool, 9> seen;
+
    public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        function<bool(int, int, int, int)> check = [&](int x, int y, int dx,
-                                                       int dy) -> bool {
-            vector<bool> seen(9, false);
-            for (int i = x; i < x + dx; ++i) {
-                for (int j = y; j < y + dy; ++j) {
-                    if (board[i][j] == '.') continue;
-
-                    int digit = board[i][j] - '1';
-                    if (seen[digit]) return false;
-                    seen[digit] = true;
+        auto check = [&](int x, int y, int dx, int dy) -> bool {
+            seen.fill(false);
+            for (int i = 0; i < dx; ++i) {
+                for (int j = 0; j < dy; ++j) {
+                    char c = board[x + i][y + j];
+                    if (c == '.') continue;
+                    int v = c - '1';
+                    if (seen[v]) return false;
+                    seen[v] = true;
                 }
             }
             return true;
@@ -60,8 +61,9 @@ class Solution {
         for (int i = 0; i < 9; ++i) {
             if (!check(i, 0, 1, 9)) return false;
             if (!check(0, i, 9, 1)) return false;
-            if (!check((i / 3) * 3, (i % 3) * 3, 3, 3)) return false;
+            if (!check(i / 3 * 3, i % 3 * 3, 3, 3)) return false;
         }
+
         return true;
     }
 };
