990b55eaf03b7ff1a57dc40828169103bd64ff55
TH Huang <tzhsn.huang@gmail.com>
2023-07-02 19:47:32 -0700

    [leetcode] set matrix zeroes

--- solutions/leetcode/set-matrix-zeroes/main.cpp
+++ solutions/leetcode/set-matrix-zeroes/main.cpp
@@ -48,3 +48,30 @@ class Solution {
         }
     }
 };
+
+class Solution {
+   public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        bool row0 = false, col0 = false;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] != 0) continue;
+                matrix[0][j] = matrix[i][0] = 0;
+                if (i == 0) row0 = true;
+                if (j == 0) col0 = true;
+            }
+        }
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (matrix[i][0] != 0 && matrix[0][j] != 0) continue;
+                if (i == 0 && !row0) continue;
+                if (j == 0 && !col0) continue;
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
