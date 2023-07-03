18089a7d339606554074a597d1b659ae510838d3
TH Huang <tzhsn.huang@gmail.com>
2023-07-02 18:54:43 -0700

    [leetcode] spiral matrix

--- solutions/leetcode/spiral-matrix/main.cpp
+++ solutions/leetcode/spiral-matrix/main.cpp
@@ -28,3 +28,34 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+   public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        vector<int> result(m * n);
+        for (int k = 0, i = 0, j = 0, d = 0; k < m * n; ++k) {
+            result[k] = matrix[i][j];
+            matrix[i][j] += 1000;
+
+            int ii = i + directions[d][0];
+            int jj = j + directions[d][1];
+
+            if (ii < 0 || m <= ii || jj < 0 || n <= jj ||
+                matrix[ii][jj] > 100) {
+                d = (d + 1) % 4;
+                ii = i + directions[d][0];
+                jj = j + directions[d][1];
+            }
+
+            i = ii;
+            j = jj;
+        }
+
+        return result;
+    }
+};
