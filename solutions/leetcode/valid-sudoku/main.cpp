2b39ede73e2d6d34b30bdd2f186a362d9717b64e
TH Huang <tzhsn.huang@gmail.com>
2023-07-02 18:30:23 -0700

    [leetcode] valid sudoku

--- solutions/leetcode/valid-sudoku/main.cpp
+++ solutions/leetcode/valid-sudoku/main.cpp
@@ -1,4 +1,45 @@
 class Solution {
+    array<bool, 9> seen;
+
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; ++i) {
+            seen.fill(false);
+            for (int j = 0; j < 9; ++j) {
+                char c = board[i][j];
+                if (c == '.') continue;
+                int v = c - '1';
+                if (seen[v]) return false;
+                seen[v] = true;
+            }
+
+            seen.fill(false);
+            for (int j = 0; j < 9; ++j) {
+                char c = board[j][i];
+                if (c == '.') continue;
+                int v = c - '1';
+                if (seen[v]) return false;
+                seen[v] = true;
+            }
+
+            int r = i / 3 * 3;
+            int c = i % 3 * 3;
+            seen.fill(false);
+            for (int x = r; x < r + 3; ++x) {
+                for (int y = c; y < c + 3; ++y) {
+                    char c = board[x][y];
+                    if (c == '.') continue;
+                    int v = c - '1';
+                    if (seen[v]) return false;
+                    seen[v] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+class Solution {
    public:
     bool isValidSudoku(vector<vector<char>>& board) {
         function<bool(int, int, int, int)> check = [&](int x, int y, int dx,
