a765bdbcf2e1864fb8d2755abb64e79040165dd1
TH Huang <tzhsn.huang@gmail.com>
2023-07-02 17:42:26 -0700

    [leetcode] substring with concatenation of all words

--- solutions/leetcode/substring-with-concatenation-of-all-words/main.cpp
+++ solutions/leetcode/substring-with-concatenation-of-all-words/main.cpp
@@ -24,3 +24,38 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        int m = words[0].size();
+        int n = words.size();
+
+        unordered_map<string, int> valid;
+        for (string& w : words) ++valid[w];
+
+        vector<int> result;
+        for (int i = 0; i < m; ++i) {
+            unordered_map<string, int> seen;
+            for (int l = i, r = i; r + m <= s.size(); r += m) {
+                string w = s.substr(r, m);
+
+                if (valid.count(w) == 0) {
+                    seen.clear();
+                    l = r + m;
+                    continue;
+                }
+
+                while (seen[w] >= valid[w]) {
+                    --seen[s.substr(l, m)];
+                    l += m;
+                }
+                ++seen[w];
+
+                if ((r - l) / m + 1 == n) result.push_back(l);
+            }
+        }
+
+        return result;
+    }
+};
