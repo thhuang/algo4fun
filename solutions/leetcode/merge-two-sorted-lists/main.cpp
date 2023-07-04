62191f3c602227afd3289b85fb585de80049a4d9
TH Huang <tzhsn.huang@gmail.com>
2023-07-04 13:42:31 -0700

    [leetcode] merge two sorted lists

--- solutions/leetcode/merge-two-sorted-lists/main.cpp
+++ solutions/leetcode/merge-two-sorted-lists/main.cpp
@@ -11,6 +11,38 @@
 class Solution {
    public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* p = &dummy;
+
+        auto select1 = [&]() -> void {
+            p = p->next = list1;
+            list1 = list1->next;
+        };
+
+        auto select2 = [&]() -> void {
+            p = p->next = list2;
+            list2 = list2->next;
+        };
+
+        while (list1 || list2) {
+            if (!list1) {
+                select2();
+            } else if (!list2) {
+                select1();
+            } else if (list1->val < list2->val) {
+                select1();
+            } else {
+                select2();
+            }
+        }
+
+        return dummy.next;
+    }
+};
+
+class Solution {
+   public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         auto dummy = new ListNode();
         auto p = dummy;
 
