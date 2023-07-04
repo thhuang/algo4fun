b834483dde8f872d7fb0ef6d616b326702bc7d3f
TH Huang <tzhsn.huang@gmail.com>
2023-07-03 21:17:31 -0700

    [leetcode] summary ranges

--- solutions/leetcode/summary-ranges/main.cpp
+++ solutions/leetcode/summary-ranges/main.cpp
@@ -10,12 +10,31 @@ class Solution {
             if (r - l == 1) {
                 result.push_back(to_string(nums[l]));
             } else {
-                result.push_back(to_string(nums[l]) + "->" + to_string(nums[r - 1]));
+                result.push_back(to_string(nums[l]) + "->" +
+                                 to_string(nums[r - 1]));
             }
 
             l = r;
         }
 
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        for (int l = 0, r; l < nums.size(); l = r) {
+            r = l + 1;
+            while (r < nums.size() && nums[r - 1] + 1 == nums[r]) ++r;
+            if (l + 1 == r) {
+                result.push_back(to_string(nums[l]));
+            } else {
+                result.push_back(to_string(nums[l]) + "->" +
+                                 to_string(nums[r - 1]));
+            }
+        }
         return result;
     }
 };
