c075ecd6402d8759c47c62431f4fbe9f88f3fc9f
TH Huang <tzhsn.huang@gmail.com>
2023-07-03 19:45:14 -0700

    [leetcode] valid anagram

--- solutions/leetcode/valid-anagram/main.cpp
+++ solutions/leetcode/valid-anagram/main.cpp
@@ -3,11 +3,10 @@ class Solution {
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
         array<int, 26> cnt;
-        fill(cnt.begin(), cnt.end(), 0);
+        cnt.fill(0);
         for (char c : s) ++cnt[c - 'a'];
-        for (char c : t) {
+        for (char c : t)
             if (--cnt[c - 'a'] < 0) return false;
-        }
         return true;
     }
 };
@@ -15,12 +14,12 @@ class Solution {
 class Solution {
    public:
     bool isAnagram(string s, string t) {
-        if (size(s) != size(t)) return false;
-        unordered_map<char, int> count;
-        for (auto c : s) ++count[c];
-        for (auto c : t) {
-            if (--count[c] < 0) return false;
-        }
+        array<int, 26> cnt;
+        cnt.fill(0);
+        for (char c : s) ++cnt[c - 'a'];
+        for (char c : t) --cnt[c - 'a'];
+        for (int v : cnt)
+            if (v != 0) return false;
         return true;
     }
 };
