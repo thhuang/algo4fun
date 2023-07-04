d4605ca40976f5acf780e9ecd59a7367719e34ff
TH Huang <tzhsn.huang@gmail.com>
2023-07-04 13:26:37 -0700

    [leetcode] basic calculator

--- solutions/leetcode/basic-calculator/main.cpp
+++ solutions/leetcode/basic-calculator/main.cpp
@@ -30,3 +30,34 @@ class Solution {
         return sum += v * local_sign * sign.back();
     }
 };
+
+class Solution {
+    int eval(string& s, int& i) {
+        vector<int> nums;
+        int sign = 1;
+        for (; i < s.size() && s[i] != ')'; ++i) {
+            if (s[i] == ' ' || s[i] == '+') {
+                continue;
+            } else if (s[i] == '(') {
+                nums.push_back(sign * eval(s, ++i));
+                sign = 1;
+            } else if (s[i] == '-') {
+                sign *= -1;
+            } else {  // digit
+                int v = 0;
+                for (; i < s.size() && isdigit(s[i]); ++i)
+                    v = v * 10 + (s[i] - '0');
+                --i;
+                nums.push_back(sign * v);
+                sign = 1;
+            }
+        }
+        return accumulate(nums.begin(), nums.end(), 0);
+    }
+
+   public:
+    int calculate(string s) {
+        int i = 0;
+        return eval(s, i);
+    }
+};
