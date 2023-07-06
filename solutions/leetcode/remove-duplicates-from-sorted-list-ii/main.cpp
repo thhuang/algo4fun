4213cf58ce72cdd0ac3d09f9ba6fa0a120cd5059
TH Huang <tzhsn.huang@gmail.com>
2023-07-05 21:11:38 -0700

    [leetcode] remove duplicates from sorted list ii

--- solutions/leetcode/remove-duplicates-from-sorted-list-ii/main.cpp
+++ solutions/leetcode/remove-duplicates-from-sorted-list-ii/main.cpp
@@ -12,6 +12,29 @@ class Solution {
    public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode dummy(0, head);
+        ListNode* p = &dummy;
+
+        while (p && p->next) {
+            int v = p->next->val;
+
+            int cnt = 0;
+            ListNode* q = p;
+            while (q->next && q->next->val == v) q = q->next, ++cnt;
+            if (cnt == 1) {
+                p = q;
+            } else {
+                p->next = q->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
+
+class Solution {
+   public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0, head);
         auto p = &dummy;
         while (p && p->next && p->next->next) {
             if (p->next->val != p->next->next->val) {
