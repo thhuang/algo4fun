5cdfbf741ff2f1714b8f7762a2b25f65dbc41ff2
TH Huang <tzhsn.huang@gmail.com>
2023-07-07 18:11:35 -0700

    [leetcode] construct binary tree from preorder and inorder traversal

--- solutions/leetcode/construct-binary-tree-from-preorder-and-inorder-traversal/main.cpp
+++ solutions/leetcode/construct-binary-tree-from-preorder-and-inorder-traversal/main.cpp
@@ -46,10 +46,32 @@ class Solution {
             if (j < l || r < j) return nullptr;
 
             ++i;
-            return new TreeNode(v, construct(i, l, j - 1), construct(i, j + 1, r));
+            return new TreeNode(v, construct(i, l, j - 1),
+                                construct(i, j + 1, r));
         };
 
         int i = 0;
         return construct(i, 0, inorder.size() - 1);
+    }
+};
+
+class Solution {
+   public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> inorderToIndex;
+        for (int i = 0; i < inorder.size(); ++i) {
+            inorderToIndex[inorder[i]] = i;
+        }
+
+        function<TreeNode*(int&, int, int)> build = [&](int& p, int il,
+                                                        int ir) -> TreeNode* {
+            if (il > ir) return nullptr;
+            int v = preorder[p++];
+            int i = inorderToIndex[v];
+            return new TreeNode(v, build(p, il, i - 1), build(p, i + 1, ir));
+        };
+
+        int i = 0;
+        return build(i, 0, inorder.size() - 1);
     }
 };
