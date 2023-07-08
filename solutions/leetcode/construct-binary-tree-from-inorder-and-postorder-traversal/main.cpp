260d4e0e1f3669a135c0a513fb24c51090706cfd
TH Huang <tzhsn.huang@gmail.com>
2023-07-07 18:20:12 -0700

    [leetcode] construct binary tree from inorder and postorder traversal

--- solutions/leetcode/construct-binary-tree-from-inorder-and-postorder-traversal/main.cpp
+++ solutions/leetcode/construct-binary-tree-from-inorder-and-postorder-traversal/main.cpp
@@ -15,32 +15,22 @@ class Solution {
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = inorder.size();
 
-        unordered_map<int, int> inorderValueToIndex;
+        unordered_map<int, int> inorderToIndex;
         for (int i = 0; i < n; ++i) {
-            inorderValueToIndex[inorder[i]] = i;
+            inorderToIndex[inorder[i]] = i;
         }
 
-        function<TreeNode*(pair<int, int>, pair<int, int>)> build =
-            [&](pair<int, int> inorderRange,
-                pair<int, int> postorderRange) -> TreeNode* {
-            auto [il, ir] = inorderRange;
-            auto [pl, pr] = postorderRange;
-
+        function<TreeNode*(int&, int, int)> build = [&](int& p, int il,
+                                                        int ir) -> TreeNode* {
             if (il > ir) return nullptr;
-
-            int p = pr;
-            int v = postorder[p];
-            int i = inorderValueToIndex[v];
-
-            int l = i - il;
-            int r = ir - i;
-
-            auto node = new TreeNode(v, build({il, i - 1}, {pl, pl + l - 1}),
-                                     build({i + 1, ir}, {pl + l, p - 1}));
-
-            return node;
+            int v = postorder[p--];
+            int i = inorderToIndex[v];
+            auto r = build(p, i + 1, ir);
+            auto l = build(p, il, i - 1);
+            return new TreeNode(v, l, r);
         };
 
-        return build({0, n - 1}, {0, n - 1});
+        int i = n - 1;
+        return build(i, 0, n - 1);
     }
 };
