f5df2304e13789b5756dc6086ee4da98a73978f9
TH Huang <tzhsn.huang@gmail.com>
2023-07-10 21:48:50 -0700

    [leetcode] surrounded regions

--- solutions/leetcode/surrounded-regions/main.cpp
+++ solutions/leetcode/surrounded-regions/main.cpp
@@ -1,27 +1,27 @@
 class Solution {
-    vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
    public:
     void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board.front().size();
+        int m = board.size();
+        int n = board.front().size();
 
         function<void(int, int)> dfs = [&](int i, int j) -> void {
-            if (i < 0 || i >= n || j < 0 || j >= m) return;
-            if (board[i][j] != 'O') return;
+            if (i < 0 || m <= i || j < 0 || n <= j || board[i][j] != 'O') return;
             board[i][j] = 'V';
             for (auto [di, dj] : directions) dfs(i + di, j + dj);
         };
 
-        for (int i = 0; i < n; ++i) dfs(i, 0), dfs(i, m - 1);
-        for (int j = 0; j < m; ++j) dfs(0, j), dfs(n - 1, j);
+        for (int i = 0; i < m; ++i) dfs(i, 0), dfs(i, n - 1);
+        for (int j = 0; j < n; ++j) dfs(0, j), dfs(m - 1, j);
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (board[i][j] == 'V')
-                    board[i][j] = 'O';
-                else
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                } else if (board[i][j] == 'V') {
+                    board[i][j] = 'O';
+                }
             }
         }
     }
