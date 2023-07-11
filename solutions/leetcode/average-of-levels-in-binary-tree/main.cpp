63c399bf07576b52e139a88a964eec9763ec3a11
TH Huang <tzhsn.huang@gmail.com>
2023-07-10 18:02:37 -0700

    [leetcode] average of levels in binary tree

--- solutions/leetcode/average-of-levels-in-binary-tree/main.cpp
+++ solutions/leetcode/average-of-levels-in-binary-tree/main.cpp
@@ -13,21 +13,28 @@
 class Solution {
    public:
     vector<double> averageOfLevels(TreeNode* root) {
-        vector<double> avg;
-        vector<int> cnt;
+        vector<double> result;
+        vector<int> amount;
+
         function<void(TreeNode*, int)> dfs = [&](TreeNode* u, int lv) -> void {
             if (!u) return;
-            if (avg.size() == lv) {
-                avg.push_back({});
-                cnt.push_back({});
+
+            if (result.size() == lv) {
+                result.push_back(0);
+                amount.push_back(0);
             }
-            avg[lv] += u->val;
-            ++cnt[lv];
+
+            result[lv] += u->val;
+            ++amount[lv];
+
             dfs(u->left, lv + 1);
             dfs(u->right, lv + 1);
         };
+
         dfs(root, 0);
-        for (int i = 0; i < avg.size(); ++i) avg[i] /= cnt[i];
-        return avg;
+
+        for (int i = 0; i < result.size(); ++i) result[i] /= amount[i];
+
+        return result;
     }
 };
