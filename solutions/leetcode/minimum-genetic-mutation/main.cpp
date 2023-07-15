e1f2385726b2a0824f1f48e97f6b8d08cf598e31
TH Huang <tzhsn.huang@gmail.com>
2023-07-15 13:15:25 -0700

    [leetcode] minimum genetic mutation

--- solutions/leetcode/minimum-genetic-mutation/main.cpp
+++ solutions/leetcode/minimum-genetic-mutation/main.cpp
@@ -31,3 +31,53 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+    bool isConnected(string& s, string& t) {
+        int cnt = 0;
+        for (int k = 0; k < s.size(); ++k) {
+            if (s[k] != t[k]) ++cnt;
+            if (cnt > 1) return false;
+        }
+        return cnt == 1;
+    }
+
+   public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        int n = bank.size();
+
+        int endIndex = -1;
+        vector<vector<int>> adj(bank.size() + 1);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (isConnected(bank[i], bank[j])) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+
+            if (isConnected(startGene, bank[i])) adj[n].push_back(i);
+
+            if (endIndex == -1 && bank[i] == endGene) endIndex = i;
+        }
+
+        queue<pair<int, int>> q;
+        q.push({n, 0});
+        vector<bool> vis(adj.size(), false);
+
+        while (!q.empty()) {
+            auto [u, d] = q.front();
+            q.pop();
+
+            if (u == endIndex) return d;
+
+            if (vis[u]) continue;
+            vis[u] = true;
+
+            for (int v : adj[u]) q.push({v, d + 1});
+        }
+
+        return -1;
+    }
+};
