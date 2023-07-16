ff91591f7360ef15ab6a1c14612a24addb0783a6
TH Huang <tzhsn.huang@gmail.com>
2023-07-16 12:04:14 -0700

    [leetcode] generate parentheses

--- solutions/leetcode/generate-parentheses/main.cpp
+++ solutions/leetcode/generate-parentheses/main.cpp
@@ -27,3 +27,36 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+
+        int open = 0;
+        string curr;
+
+        function<void(int)> dfs = [&](int i) -> void {
+            if (i == n * 2) {
+                result.push_back(curr);
+                return;
+            }
+
+            if (curr.size() + open + 2 <= n * 2) {
+                curr.push_back('('), ++open;
+                dfs(i + 1);
+                curr.pop_back(), --open;
+            }
+
+            if (open > 0) {
+                curr.push_back(')'), --open;
+                dfs(i + 1);
+                curr.pop_back(), ++open;
+            }
+        };
+
+        dfs(0);
+
+        return result;
+    }
+};
