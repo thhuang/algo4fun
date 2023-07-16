8dbc8c3384882f51b4ee948525a82da38e27a94f
TH Huang <tzhsn.huang@gmail.com>
2023-07-16 09:08:34 -0700

    [leetcode] n-queens ii

--- solutions/leetcode/n-queens-ii/main.cpp
+++ solutions/leetcode/n-queens-ii/main.cpp
@@ -1,26 +1,25 @@
 class Solution {
    public:
     int totalNQueens(int n) {
-        int count = 0;
-        vector<bool> col(n, false), diag0(n * 2 - 1, false), diag1(n * 2 - 1, false);
+        vector<bool> col(n), ldiag(n * 2 - 1), rdiag(n * 2 - 1);
 
-        function<void(int)> search = [&](int i) -> void {
+        int result = 0;
+        function<void(int)> dfs = [&](int i) -> void {
             if (i == n) {
-                ++count;
+                ++result;
                 return;
             }
 
             for (int j = 0; j < n; ++j) {
-                if (col[j] || diag0[i + j] || diag1[i - j + n - 1]) continue;
-
-                col[j] = diag0[i + j] = diag1[i - j + n - 1] = true;
-                search(i + 1);
-                col[j] = diag0[i + j] = diag1[i - j + n - 1] = false;
+                if (col[j] || ldiag[i + j] || rdiag[j - i + n - 1]) continue;
+                col[j] = ldiag[i + j] = rdiag[j - i + n - 1] = true;
+                dfs(i + 1);
+                col[j] = ldiag[i + j] = rdiag[j - i + n - 1] = false;
             }
         };
 
-        search(0);
+        dfs(0);
 
-        return count;
+        return result;
     }
 };
