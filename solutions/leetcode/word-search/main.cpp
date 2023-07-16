21b3234095c683969d6d3c557cc8663b4d159e63
TH Huang <tzhsn.huang@gmail.com>
2023-07-16 12:11:33 -0700

    [leetcode] word search

--- solutions/leetcode/word-search/main.cpp
+++ solutions/leetcode/word-search/main.cpp
@@ -75,3 +75,37 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board.front().size();
+
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        function<bool(int, int, int)> dfs = [&](int i, int j, int k) -> bool {
+            if (i < 0 || m <= i || j < 0 || n <= j || vis[i][j] || board[i][j] != word[k]) return false;
+            if (k + 1 == word.size()) return true;
+
+            vis[i][j] = true;
+
+            for (auto [di, dj] : directions) {
+                if (dfs(i + di, j + dj, k + 1)) return true;
+            }
+
+            vis[i][j] = false;
+
+            return false;
+        };
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dfs(i, j, 0)) return true;
+            }
+        }
+
+        return false;
+    }
+};
