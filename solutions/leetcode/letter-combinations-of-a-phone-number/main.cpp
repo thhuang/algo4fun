61582c977fee610c93ba2d1b2d4dd2a85f2f69d0
TH Huang <tzhsn.huang@gmail.com>
2023-07-15 22:11:13 -0700

    [leetcode] letter combinations of a phone number

--- solutions/leetcode/letter-combinations-of-a-phone-number/main.cpp
+++ solutions/leetcode/letter-combinations-of-a-phone-number/main.cpp
@@ -1,33 +1,29 @@
 class Solution {
-    vector<vector<char>> mp = {{},
-                               {},
-                               {'a', 'b', 'c'},
-                               {'d', 'e', 'f'},
-                               {'g', 'h', 'i'},
-                               {'j', 'k', 'l'},
-                               {'m', 'n', 'o'},
-                               {'p', 'q', 'r', 's'},
-                               {'t', 'u', 'v'},
-                               {'w', 'x', 'y', 'z'}};
+    vector<string> mp = {"",    "",    "abc",  "def", "ghi",
+                         "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
    public:
     vector<string> letterCombinations(string digits) {
         if (digits.empty()) return {};
 
         vector<string> result;
+
         string curr;
         function<void(int)> dfs = [&](int i) -> void {
             if (i == digits.size()) {
                 result.push_back(curr);
                 return;
             }
+
             for (char c : mp[digits[i] - '0']) {
                 curr.push_back(c);
                 dfs(i + 1);
                 curr.pop_back();
             }
         };
+
         dfs(0);
+
         return result;
     }
 };
