7fe5de85fa73d1eaf0b3c0d756e0196740924239
TH Huang <tzhsn.huang@gmail.com>
2023-07-15 20:15:56 -0700

    [leetcode] number of distinct substrings in a string

--- solutions/leetcode/number-of-distinct-substrings-in-a-string/main.cpp
+++ solutions/leetcode/number-of-distinct-substrings-in-a-string/main.cpp
@@ -1,3 +1,42 @@
+class Solution {
+    struct Node {
+        array<Node *, 26> children;
+
+        Node() { children.fill(nullptr); }
+
+        ~Node() {
+            for (auto &c : children) {
+                if (c) delete c;
+            }
+        }
+    };
+
+   public:
+    int countDistinct(string s) {
+        int n = s.size();
+
+        auto root = new Node();
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            auto curr = root;
+
+            for (int j = i; j < n; ++j) {
+                char k = s[j] - 'a';
+                if (curr->children[k] == nullptr) {
+                    curr->children[k] = new Node();
+                    ++result;
+                }
+                curr = curr->children[k];
+            }
+        }
+
+        delete root;
+
+        return result;
+    }
+};
+
 // https://leetcode.com/problems/number-of-distinct-substrings-in-a-string/solutions/985468/c-max-match-suffix-clean-code/
 class Solution {
    public:
