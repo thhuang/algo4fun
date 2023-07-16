a19856f59917ad3e85faa107d56f40005e6f24fc
TH Huang <tzhsn.huang@gmail.com>
2023-07-15 22:24:57 -0700

    [leetcode] permutations

--- solutions/leetcode/permutations/main.cpp
+++ solutions/leetcode/permutations/main.cpp
@@ -4,6 +4,38 @@ class Solution {
         int n = nums.size();
 
         vector<vector<int>> result;
+        vector<int> curr;
+
+        vector<bool> used(n, false);
+        function<void()> dfs = [&]() -> void {
+            if (curr.size() == nums.size()) {
+                result.push_back(curr);
+                return;
+            }
+
+            for (int i = 0; i < n; ++i) {
+                if (used[i]) continue;
+
+                used[i] = true;
+                curr.push_back(nums[i]);
+                dfs();
+                curr.pop_back();
+                used[i] = false;
+            }
+        };
+
+        dfs();
+
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>> result;
 
         function<void(int)> search = [&](int i) -> void {
             if (i == n) {
