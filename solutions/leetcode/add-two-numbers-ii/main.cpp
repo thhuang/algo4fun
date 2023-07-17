13f4788bc61eb3c93d5f0086407675c9890bc912
TH Huang <tzhsn.huang@gmail.com>
2023-07-16 23:20:51 -0700

    [leetcode] add two numbers ii

--- solutions/leetcode/add-two-numbers-ii/main.cpp
+++ solutions/leetcode/add-two-numbers-ii/main.cpp
@@ -48,3 +48,40 @@ class Solution {
         return reverse(dummy.next);
     }
 };
+
+class Solution {
+    ListNode* reverse(ListNode* u) {
+        ListNode* a = nullptr;
+        ListNode* b = u;
+        while (b) {
+            ListNode* c = b->next;
+            b->next = a;
+            a = b;
+            b = c;
+        }
+        return a;
+    };
+
+   public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        l1 = reverse(l1);
+        l2 = reverse(l2);
+
+        ListNode dummy;
+        ListNode* p = &dummy;
+        ListNode* p1 = l1;
+        ListNode* p2 = l2;
+        int carry = 0;
+        while (p1 || p2 || carry) {
+            if (p1) carry += p1->val, p1 = p1->next;
+            if (p2) carry += p2->val, p2 = p2->next;
+            p = p->next = new ListNode(carry % 10);
+            carry /= 10;
+        }
+
+        reverse(l1);
+        reverse(l2);
+
+        return reverse(dummy.next);
+    }
+};
