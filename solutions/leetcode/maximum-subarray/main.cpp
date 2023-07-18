9be4cb24d342be872132bf5831bc7cbe78a5b29a
TH Huang <tzhsn.huang@gmail.com>
2023-07-17 19:13:50 -0700

    [leetcode] maximum subarray

--- solutions/leetcode/maximum-subarray/main.cpp
+++ solutions/leetcode/maximum-subarray/main.cpp
@@ -1,6 +1,26 @@
 class Solution {
    public:
     int maxSubArray(vector<int>& nums) {
+        int result = *max_element(nums.begin(), nums.end());
+
+        for (int l = 0, r = 0, v = 0; r < nums.size(); ++r) {
+            v += nums[r];
+
+            if (v < 0) {
+                l = r + 1;
+                v = 0;
+            } else {
+                result = max(result, v);
+            }
+        }
+
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    int maxSubArray(vector<int>& nums) {
         int result = numeric_limits<int>::min();
         int curr = 0;
         for (int v : nums) {
