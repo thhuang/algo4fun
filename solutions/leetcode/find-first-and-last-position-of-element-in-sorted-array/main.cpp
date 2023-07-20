491ba5b67497f03cd76c6fd0a34ae15627e50a09
TH Huang <tzhsn.huang@gmail.com>
2023-07-19 20:20:33 -0700

    [leetcode] find first and last position of element in sorted array

--- solutions/leetcode/find-first-and-last-position-of-element-in-sorted-array/main.cpp
+++ solutions/leetcode/find-first-and-last-position-of-element-in-sorted-array/main.cpp
@@ -56,3 +56,23 @@ class Solution {
         return {l, r - 1};
     }
 };
+
+class Solution {
+   public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto search = [&](function<bool(int)> cmp) -> int {
+            int l = 0, r = nums.size() - 1;
+            while (l <= r) {
+                int m = l + (r - l) / 2;
+                cmp(nums[m]) ? l = m + 1 : r = m - 1;
+            }
+            return l;
+        };
+
+        int l = search([&](int v) -> bool { return v < target; });
+        int r = search([&](int v) -> bool { return v <= target; });
+
+        if (l == r) return {-1, -1};
+        return {l, r - 1};
+    }
+};
