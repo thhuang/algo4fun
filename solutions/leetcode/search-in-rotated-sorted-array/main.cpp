f7104b1d0c27be5798c376fde231fec0fde37d63
TH Huang <tzhsn.huang@gmail.com>
2023-07-19 20:07:57 -0700

    [leetcode] search in rotated sorted array

--- solutions/leetcode/search-in-rotated-sorted-array/main.cpp
+++ solutions/leetcode/search-in-rotated-sorted-array/main.cpp
@@ -7,8 +7,7 @@ class Solution {
             if (nums[m] == target) return m;
             if (nums[l] < nums[m] && (nums[l] <= target && target < nums[m])) {
                 r = m - 1;
-            } else if (nums[l] > nums[m] &&
-                       (nums[l] <= target || target < nums[m])) {
+            } else if (nums[l] > nums[m] && (nums[l] <= target || target < nums[m])) {
                 r = m - 1;
             } else {
                 l = m + 1;
@@ -27,13 +26,30 @@ class Solution {
             if (nums[m] == target) return m;
             if (nums[l] <= nums[m] && (nums[m] < target || target < nums[l])) {
                 l = m + 1;
-            } else if (nums[l] > nums[m] &&
-                       (nums[m] < target && target < nums[l])) {
+            } else if (nums[l] > nums[m] && (nums[m] < target && target < nums[l])) {
                 l = m + 1;
             } else {
                 r = m - 1;
             }
         }
         return -1;
+    }
+};
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[l] <= nums[m] && (target < nums[l] || nums[m] < target)) {
+                l = m + 1;
+            } else if (nums[l] > nums[m] && nums[m] < target && target < nums[l]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return (l < nums.size() && nums[l] == target) ? l : -1;
     }
 };
