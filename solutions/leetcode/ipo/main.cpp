d077d99f41064089b990b105dd84867d33f4ddb9
TH Huang <tzhsn.huang@gmail.com>
2023-07-22 11:51:54 -0700

    [leetcode] ipo

--- solutions/leetcode/ipo/main.cpp
+++ solutions/leetcode/ipo/main.cpp
@@ -27,3 +27,26 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital) {
+        int n = profits.size();
+
+        vector<array<int, 2>> projects(n);
+        for (int i = 0; i < n; ++i) projects[i] = {capital[i], profits[i]};
+        sort(projects.begin(), projects.end());
+
+        int result = w;
+        priority_queue<int> pq;
+        for (int i = 0, j = 0; i < k; ++i) {
+            while (j < n && projects[j][0] <= result) pq.push(projects[j++][1]);
+            if (pq.empty()) break;
+            result += pq.top();
+            pq.pop();
+        }
+
+        return result;
+    }
+};
