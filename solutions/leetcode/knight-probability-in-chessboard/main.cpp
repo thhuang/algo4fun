9b2c264de4520b82b7f32ea28d7682ec32f87b3a
TH Huang <tzhsn.huang@gmail.com>
2023-07-21 17:36:32 -0700

    [leetcode] knight probability in chessboard

--- solutions/leetcode/knight-probability-in-chessboard/main.cpp
+++ solutions/leetcode/knight-probability-in-chessboard/main.cpp
@@ -23,3 +23,30 @@ class Solution {
         return prob[row][column];
     }
 };
+
+class Solution {
+    vector<array<int, 2>> directions = {{1, 2}, {1, -2}, {-1, 2}, {-1, -2},
+                                        {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
+
+   public:
+    double knightProbability(int n, int k, int row, int column) {
+        vector<vector<vector<double>>> prob(
+            n, vector<vector<double>>(n, vector<double>(k + 1, -1)));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j) prob[i][j][0] = 1;
+
+        function<double(int, int, int)> dfs = [&](int i, int j,
+                                                  int mv) -> double {
+            if (i < 0 || n <= i || j < 0 || n <= j) return 0;
+            if (prob[i][j][mv] >= 0) return prob[i][j][mv];
+
+            double result = 0;
+            for (auto [di, dj] : directions)
+                result += dfs(i + di, j + dj, mv - 1);
+
+            return prob[i][j][mv] = result / directions.size();
+        };
+
+        return dfs(row, column, k);
+    }
+};
