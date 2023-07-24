49e2105e7d5d7303bf695c4691abd40dbb99784d
TH Huang <tzhsn.huang@gmail.com>
2023-07-23 18:03:10 -0700

    [leetcode] pow(x,n)

--- solutions/leetcode/powx-n/main.cpp
+++ solutions/leetcode/powx-n/main.cpp
@@ -18,3 +18,18 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    double myPow(double x, int n) {
+        bool isPositive = n >= 0;
+
+        double result = 1;
+        long long m = abs((long long)n);
+        for (double v = x; m; v *= v, m >>= 1) {
+            if (m & 1) result *= v;
+        }
+
+        return isPositive ? result : 1 / result;
+    }
+};
