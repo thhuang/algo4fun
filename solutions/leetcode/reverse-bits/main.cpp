7c06dd8f1c5782fe94f08725d1a54705b05a5900
TH Huang <tzhsn.huang@gmail.com>
2023-07-23 21:06:40 -0700

    [leetcode] reverse bits

--- solutions/leetcode/reverse-bits/main.cpp
+++ solutions/leetcode/reverse-bits/main.cpp
@@ -10,3 +10,14 @@ class Solution {
         return n;
     }
 };
+
+class Solution {
+   public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (uint32_t mask = 1 << 31; n; n >>= 1, mask >>= 1) {
+            if (n & 1) result |= mask;
+        }
+        return result;
+    }
+};
