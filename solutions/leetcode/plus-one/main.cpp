f55bb95959a7f615cf2729c7fc12a09e4083e66e
TH Huang <tzhsn.huang@gmail.com>
2023-07-24 19:23:37 -0700

    [leetcode] plus one

--- solutions/leetcode/plus-one/main.cpp
+++ solutions/leetcode/plus-one/main.cpp
@@ -17,3 +17,17 @@ class Solution {
         return digits;
     }
 };
+
+class Solution {
+   public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+        for (int i = digits.size() - 1; i >= 0 && carry == 1; --i) {
+            digits[i] += carry;
+            carry = digits[i] / 10;
+            digits[i] %= 10;
+        }
+        if (carry) digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
