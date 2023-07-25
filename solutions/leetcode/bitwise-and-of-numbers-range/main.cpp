e9d553623bf0b19432b02ec73f06017885a452cd
TH Huang <tzhsn.huang@gmail.com>
2023-07-24 19:08:03 -0700

    [leetcode] bitwise and of numbers range

--- solutions/leetcode/bitwise-and-of-numbers-range/main.cpp
+++ solutions/leetcode/bitwise-and-of-numbers-range/main.cpp
@@ -10,3 +10,15 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int rangeBitwiseAnd(int left, int right) {
+        int result = left & right;
+        for (int i = 0, diff = right - left; i < 31; ++i) {
+            int mask = 1 << i;
+            if ((diff >= mask) || (left & right & mask == 0)) result &= ~mask;
+        }
+        return result;
+    }
+};
