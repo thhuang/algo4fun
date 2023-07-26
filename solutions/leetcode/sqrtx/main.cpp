b67c42d1822a7bf527c751c03e947934e464c585
TH Huang <tzhsn.huang@gmail.com>
2023-07-25 18:25:43 -0700

    [leetcode] sqrt(x)

--- solutions/leetcode/sqrtx/main.cpp
+++ solutions/leetcode/sqrtx/main.cpp
@@ -1,10 +1,9 @@
 class Solution {
    public:
     int mySqrt(int x) {
-        long long l = 0, r = x / 2 + 1;
+        int l = 0, r = x;
         while (l <= r) {
             long long m = l + (r - l) / 2;
-            if (m * m == x) return m;
             if (m * m > x) {
                 r = m - 1;
             } else {
