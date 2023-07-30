486816f41b1b1f0ecb63fda9d321ece47aa1c481
TH Huang <tzhsn.huang@gmail.com>
2023-07-29 17:04:33 -0700

    [leetcode] word break

--- solutions/leetcode/word-break/main.cpp
+++ solutions/leetcode/word-break/main.cpp
@@ -65,3 +65,23 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+        for (int i = n - 1; i >= 0; --i) {
+            for (const string& w : wordDict) {
+                int m = w.size();
+                int j = i + m;
+                if (j > n || !dp[j]) continue;
+                if (s.substr(i, m) != w) continue;
+                dp[i] = true;
+                break;
+            }
+        }
+        return dp[0];
+    }
+};
