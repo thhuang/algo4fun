57828398074988c182113a690fc2d6192c6b6788
TH Huang <tzhsn.huang@gmail.com>
2023-07-30 17:32:47 -0700

    [leetcode] unique paths ii

--- solutions/leetcode/unique-paths-ii/main.cpp
+++ solutions/leetcode/unique-paths-ii/main.cpp
@@ -1,16 +1,17 @@
 class Solution {
    public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid.front().size();
+        int m = obstacleGrid.size();
+        int n = obstacleGrid.front().size();
 
-        vector<int> dp(m, 0);
-        dp[0] = 1;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
+        vector<int> dp(n, 0);
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; ++j) dp[j] = 1;
+        for (int i = 1; i < m; ++i) {
+            if (obstacleGrid[i][0] == 1) dp[0] = 0;
+            for (int j = 1; j < n; ++j) {
                 if (obstacleGrid[i][j] == 1) {
                     dp[j] = 0;
-                } else if (j > 0) {
+                } else {
                     dp[j] += dp[j - 1];
                 }
             }
@@ -27,13 +28,12 @@ class Solution {
         int n = obstacleGrid.front().size();
 
         vector<int> dp(n, 0);
-        for (int j = 0; j < n && obstacleGrid[0][j] == 0; ++j) dp[j] = 1;
-        for (int i = 1; i < m; ++i) {
-            if (obstacleGrid[i][0] == 1) dp[0] = 0;
-            for (int j = 1; j < n; ++j) {
+        dp[0] = 1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (obstacleGrid[i][j] == 1) {
                     dp[j] = 0;
-                } else {
+                } else if (j > 0) {
                     dp[j] += dp[j - 1];
                 }
             }
