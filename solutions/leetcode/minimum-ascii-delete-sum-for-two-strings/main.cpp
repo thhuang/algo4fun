46165bf192c1002da71c534eb95b66ce2143c00d
TH Huang <tzhsn.huang@gmail.com>
2023-07-30 22:19:42 -0700

    [leetcode] minimum ascii delete sum for two strings

--- solutions/leetcode/minimum-ascii-delete-sum-for-two-strings/main.cpp
+++ solutions/leetcode/minimum-ascii-delete-sum-for-two-strings/main.cpp
@@ -20,3 +20,29 @@ class Solution {
         return dp.back().back();
     }
 };
+
+class Solution {
+   public:
+    int minimumDeleteSum(string s1, string s2) {
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+
+        dp[n1][n2] = 0;
+        for (int i = n1 - 1, v = 0; i >= 0; --i) dp[i][n2] = v += s1[i];
+        for (int j = n2 - 1, v = 0; j >= 0; --j) dp[n1][j] = v += s2[j];
+
+        for (int i = n1 - 1; i >= 0; --i) {
+            for (int j = n2 - 1; j >= 0; --j) {
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = min(dp[i + 1][j] + s1[i], dp[i][j + 1] + s2[j]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
