a214ea37e116b800755e9eb2b6c5a4277d440421
TH Huang <tzhsn.huang@gmail.com>
2023-07-31 18:02:09 -0700

    [leetcode] interleaving string

--- solutions/leetcode/interleaving-string/main.cpp
+++ solutions/leetcode/interleaving-string/main.cpp
@@ -25,6 +25,30 @@ class Solution {
     bool isInterleave(string s1, string s2, string s3) {
         if (s1.size() + s2.size() != s3.size()) return false;
 
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        vector<vector<bool>> dp(n1 + 1, vector<bool>(n2 + 1, false));
+        dp[n1][n2] = true;
+
+        for (int i = n1; i >= 0; --i) {
+            for (int j = n2; j >= 0; --j) {
+                if (i < n1)
+                    dp[i][j] = dp[i][j] || dp[i + 1][j] && s1[i] == s3[i + j];
+                if (j < n2)
+                    dp[i][j] = dp[i][j] || dp[i][j + 1] && s2[j] == s3[i + j];
+            }
+        }
+
+        return dp.front().front();
+    }
+};
+
+class Solution {
+   public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) return false;
+
         int m = s1.size(), n = s2.size();
         vector<bool> dp(n + 1, false);
 
