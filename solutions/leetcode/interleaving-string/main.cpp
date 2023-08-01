4b27ae1f1cfe64aa54cc6b2b6f6c81258e075f82
TH Huang <tzhsn.huang@gmail.com>
2023-07-31 18:15:50 -0700

    [leetcode] interleaving string

--- solutions/leetcode/interleaving-string/main.cpp
+++ solutions/leetcode/interleaving-string/main.cpp
@@ -64,3 +64,29 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size()) return false;
+
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        vector<bool> dp(n2 + 1, false);
+
+        for (int i = n1; i >= 0; --i) {
+            vector<bool> dpNew(n2 + 1, false);
+
+            for (int j = n2; j >= 0; --j) {
+                bool b1 = i < n1 && dp[j] && s1[i] == s3[i + j];
+                bool b2 = j < n2 && dpNew[j + 1] && s2[j] == s3[i + j];
+                dpNew[j] = b1 || b2 || (i == n1 && j == n2);
+            }
+
+            dp = move(dpNew);
+        }
+
+        return dp[0];
+    }
+};
