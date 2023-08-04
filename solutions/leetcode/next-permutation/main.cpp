6139bdfa9eadf4fb506a24568ef33f53120cfcd0
TH Huang <tzhsn.huang@gmail.com>
2023-08-04 15:53:33 -0700

    [leetcode] next permutation

--- solutions/leetcode/next-permutation/main.cpp
+++ solutions/leetcode/next-permutation/main.cpp
@@ -23,3 +23,25 @@ class Solution {
         reverse(begin(nums) + i, end(nums));
     }
 };
+
+class Solution {
+   public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+
+        int l = n - 2;
+        while (l >= 0 && nums[l] >= nums[l + 1]) --l;
+
+        if (l < 0) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        int r = n - 1;
+        while (nums[l] >= nums[r]) --r;
+
+        swap(nums[l], nums[r]);
+
+        sort(nums.begin() + l + 1, nums.end());
+    }
+};
