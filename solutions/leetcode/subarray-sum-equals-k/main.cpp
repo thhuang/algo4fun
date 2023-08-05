ca8151801fd60cb826f9a3af71c766f935247cf6
TH Huang <tzhsn.huang@gmail.com>
2023-08-05 11:28:46 -0700

    [leetcode] subarray sum equals k

--- solutions/leetcode/subarray-sum-equals-k/main.cpp
+++ solutions/leetcode/subarray-sum-equals-k/main.cpp
@@ -1,18 +1,17 @@
 class Solution {
    public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> target_count;
-        target_count[k] = 1;
+        int result = 0;
 
-        int count = 0;
-        int accumulate = 0;
+        unordered_map<int, int> targetSum = {{k, 1}};
+        int sum = 0;
         for (int v : nums) {
-            accumulate += v;
-            count += target_count[accumulate];
-            ++target_count[accumulate + k];
+            sum += v;
+            result += targetSum[sum];
+            ++targetSum[sum + k];
         }
 
-        return count;
+        return result;
     }
 };
 
