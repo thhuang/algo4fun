98db5a96dfce27a15a74de27b46790b28d1854d5
TH Huang <tzhsn.huang@gmail.com>
2023-08-06 11:37:52 -0700

    [leetcode] number of music playlists

---
+++ solutions/leetcode/number-of-music-playlists/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    const int mod = 1e9 + 7;
+    typedef long long ll;
+
+   public:
+    int numMusicPlaylists(int n, int goal, int k) {
+        vector<vector<ll>> dp(goal + 1, vector<ll>(n + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 1; i <= goal; ++i) {
+            for (int j = 1; j <= min(i, n); ++j) {
+                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % mod;
+                if (j > k) {
+                    (dp[i][j] += dp[i - 1][j] * (j - k)) %= mod;
+                }
+            }
+        }
+        return dp[goal][n];
+    }
+};
