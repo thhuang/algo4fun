c174a2350d1e97a93687823d3433ea95808f1843
TH Huang <tzhsn.huang@gmail.com>
2023-08-09 00:18:17 -0700

    [leetcode] subsets

--- solutions/leetcode/subsets/main.cpp
+++ solutions/leetcode/subsets/main.cpp
@@ -2,18 +2,22 @@ class Solution {
    public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
+
         vector<int> subset;
         function<void(int)> search = [&](int i) -> void {
             if (i == nums.size()) {
                 result.push_back(subset);
                 return;
             }
+
             search(i + 1);
             subset.push_back(nums[i]);
             search(i + 1);
             subset.pop_back();
         };
+
         search(0);
+
         return result;
     }
 };
