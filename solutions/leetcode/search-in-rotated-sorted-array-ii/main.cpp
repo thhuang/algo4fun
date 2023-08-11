a8127e9291d2b607cd09b722ae6d432353b9f03c
TH Huang <tzhsn.huang@gmail.com>
2023-08-10 22:11:38 -0700

    [leetcode] search in rotated sorted array ii

--- solutions/leetcode/search-in-rotated-sorted-array-ii/main.cpp
+++ solutions/leetcode/search-in-rotated-sorted-array-ii/main.cpp
@@ -1,29 +1,24 @@
 class Solution {
    public:
     bool search(vector<int>& nums, int target) {
-        function<bool(int, int)> range_search = [&](int l, int r) -> bool {
+        function<bool(int, int)> f = [&](int l, int r) -> bool {
             while (l <= r) {
                 int m = l + (r - l) / 2;
-                if (target == nums[l] || target == nums[m] || target == nums[r])
-                    return true;
-                if (nums[l] == nums[m] || nums[m] == nums[r])
-                    return range_search(l, m - 1) || range_search(m + 1, r);
-
-                if (nums[l] < nums[m] &&
-                    (target < nums[l] || nums[m] < target)) {
-                    l = m + 1;
-                } else if (nums[m] < nums[r] &&
-                           (nums[m] < target && target < nums[r])) {
-                    l = m + 1;
-                } else {
+                if (nums[l] == target || nums[m] == target || nums[r] == target) return true;
+                if (nums[l] < nums[m] && (nums[l] < target && target < nums[m])) {
                     r = m - 1;
+                } else if (nums[l] > nums[m] && (target < nums[m] || nums[l] < target)) {
+                    r = m - 1;
+                } else if (nums[l] == nums[m]) {
+                    return f(l, m - 1) || f(m + 1, r);
+                } else {
+                    l = m + 1;
                 }
             }
-
             return false;
         };
 
-        return range_search(0, size(nums) - 1);
+        return f(0, nums.size() - 1);
     }
 };
 
