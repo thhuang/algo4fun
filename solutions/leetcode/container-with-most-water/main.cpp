8031d783fb2b7205ff5c466716c304f5ad1f3b16
TH Huang <tzhsn.huang@gmail.com>
2023-08-11 17:58:54 -0700

    [leetcode] container with most water

--- solutions/leetcode/container-with-most-water/main.cpp
+++ solutions/leetcode/container-with-most-water/main.cpp
@@ -11,3 +11,16 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int maxArea(vector<int>& height) {
+        int result = 0;
+        for (int l = 0, r = height.size() - 1; l < r;) {
+            int hl = height[l], hr = height[r];
+            result = max(result, min(hl, hr) * (r - l));
+            hl < hr ? ++l : --r;
+        }
+        return result;
+    }
+};
