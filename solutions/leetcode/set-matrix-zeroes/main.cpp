da779d99e45888535df0dec9033c682402cfe1fb
TH Huang <tzhsn.huang@gmail.com>
2023-08-13 17:41:32 -0700

    [leetcode] set matrix zeroes

--- solutions/leetcode/set-matrix-zeroes/main.cpp
+++ solutions/leetcode/set-matrix-zeroes/main.cpp
@@ -75,3 +75,54 @@ class Solution {
         }
     }
 };
+
+class Solution {
+   public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        bool row0 = false;
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+
+        bool col0 = false;
+        for (int i = 0; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] != 0) continue;
+            for (int i = 1; i < m; ++i) matrix[i][j] = 0;
+        }
+
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] != 0) continue;
+            for (int j = 1; j < n; ++j) matrix[i][j] = 0;
+        }
+
+        if (row0) {
+            for (int j = 0; j < n; ++j) matrix[0][j] = 0;
+        }
+
+        if (col0) {
+            for (int i = 0; i < m; ++i) matrix[i][0] = 0;
+        }
+    }
+};
