979545ead9218cf2754879082f39dc7fe91f5311
TH Huang <tzhsn.huang@gmail.com>
2023-08-14 23:21:40 -0700

    [leetcode] find peak element

--- solutions/leetcode/find-peak-element/main.cpp
+++ solutions/leetcode/find-peak-element/main.cpp
@@ -31,3 +31,19 @@ class Solution {
         return l;
     }
 };
+
+class Solution {
+   public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size() - 2;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < nums[m + 1]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l;
+    }
+};
