1a8ce4f30d079f3ca41853a6c427a5509d0e8098
TH Huang <tzhsn.huang@gmail.com>
2023-08-18 19:20:57 -0700

    [leetcode] longest continuous subarray with absolute diff less than or equal to limit

--- solutions/leetcode/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/main.cpp
+++ solutions/leetcode/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/main.cpp
@@ -21,3 +21,28 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int result = 0;
+        deque<int> inc, dec;
+
+        for (int l = 0, r = 0; r < nums.size(); ++r) {
+            while (!inc.empty() && inc.back() > nums[r]) inc.pop_back();
+            inc.push_back(nums[r]);
+            while (!dec.empty() && dec.back() < nums[r]) dec.pop_back();
+            dec.push_back(nums[r]);
+
+            while (dec.front() - inc.front() > limit) {
+                if (inc.front() == nums[l]) inc.pop_front();
+                if (dec.front() == nums[l]) dec.pop_front();
+                ++l;
+            }
+
+            result = max(result, r - l + 1);
+        }
+
+        return result;
+    }
+};
