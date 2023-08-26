64b283753725c8553e5e9ec5014d5285198eedd9
TH Huang <tzhsn.huang@gmail.com>
2023-08-25 17:52:36 -0700

    [leetcode] maximum length of pair chain

--- solutions/leetcode/maximum-length-of-pair-chain/main.cpp
+++ solutions/leetcode/maximum-length-of-pair-chain/main.cpp
@@ -2,18 +2,16 @@ class Solution {
    public:
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(), pairs.end(),
-             [](const vector<int>& a, const vector<int>& b) {
-                 return a[1] < b[1];
-             });
+             [](auto& a, auto& b) -> bool { return a[1] < b[1]; });
 
-        int count = 0;
-        int r = numeric_limits<int>::min();
-        for (const auto& p : pairs) {
-            if (p[0] <= r) continue;
-            r = p[1];
-            ++count;
+        int result = 0;
+        int v = numeric_limits<int>::min();
+        for (auto& p : pairs) {
+            if (p[0] <= v) continue;
+            ++result;
+            v = p[1];
         }
 
-        return count;
+        return result;
     }
 };
