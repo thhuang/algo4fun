d00327a5683b9e3841336e826dc1632aa73b4ed0
TH Huang <tzhsn.huang@gmail.com>
2023-08-28 22:32:24 -0700

    [leetcode] edit distance

--- solutions/leetcode/edit-distance/main.cpp
+++ solutions/leetcode/edit-distance/main.cpp
@@ -23,6 +23,32 @@ class Solution {
 class Solution {
    public:
     int minDistance(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1));
+        dp[n1][n2] = 0;
+        for (int i = 0; i < n1; ++i) dp[n1 - i - 1][n2] = i + 1;
+        for (int j = 0; j < n2; ++j) dp[n1][n2 - j - 1] = j + 1;
+
+        for (int i = n1 - 1; i >= 0; --i) {
+            for (int j = n2 - 1; j >= 0; --j) {
+                if (word1[i] == word2[j]) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = 1 + min(dp[i + 1][j + 1],
+                                       min(dp[i][j + 1], dp[i + 1][j]));
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+class Solution {
+   public:
+    int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
 
         for (int i = 0; i <= word1.size(); ++i) {
