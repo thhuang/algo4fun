c174623f6a7b11658d467f2ea87eabea906c0543
TH Huang <tzhsn.huang@gmail.com>
2023-08-28 19:12:14 -0700

    [leetcode] unique paths

--- solutions/leetcode/unique-paths/main.cpp
+++ solutions/leetcode/unique-paths/main.cpp
@@ -43,3 +43,16 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp.back();
+    }
+};
