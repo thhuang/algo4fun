68d4ae3ac4f9c41d05113d071c27a95ede52348e
TH Huang <tzhsn.huang@gmail.com>
2023-08-28 18:26:38 -0700

    [leetcode] maximum number of ones

--- solutions/leetcode/maximum-number-of-ones/main.cpp
+++ solutions/leetcode/maximum-number-of-ones/main.cpp
@@ -19,3 +19,19 @@ class Solution {
         return accumulate(cnt.begin(), cnt.begin() + maxOnes, 0);
     }
 };
+
+class Solution {
+   public:
+    int maximumNumberOfOnes(int width, int height, int sideLength, int maxOnes) {
+        vector<int> cnt(sideLength * sideLength, 0);
+        vector<vector<int>> mat(height, vector<int>(width));
+        for (int h = 0; h < height; h += sideLength)
+            for (int w = 0; w < width; w += sideLength)
+                for (int i = h, k = 0; i < h + sideLength; ++i)
+                    for (int j = w; j < w + sideLength; ++j, ++k)
+                        if (0 <= i && i < height && 0 <= j && j < width)
+                            ++cnt[k];
+        sort(cnt.begin(), cnt.end(), greater<>());
+        return accumulate(cnt.begin(), cnt.begin() + maxOnes, 0);
+    }
+};
