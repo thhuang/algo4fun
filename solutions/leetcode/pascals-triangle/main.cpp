9f1e095d09d46353576de4ea3c007492ce5451c9
TH Huang <tzhsn.huang@gmail.com>
2023-08-29 19:05:58 -0700

    [leetcode] pascals triangle

--- solutions/leetcode/pascals-triangle/main.cpp
+++ solutions/leetcode/pascals-triangle/main.cpp
@@ -29,3 +29,17 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result(numRows, {1});
+        for (int i = 1; i < numRows; ++i) {
+            for (int j = 1; j < i; ++j) {
+                result[i].push_back(result[i - 1][j] + result[i - 1][j - 1]);
+            }
+            result[i].push_back(1);
+        }
+        return result;
+    }
+};
