f36010243721851c4f65e63b7ff09a9a8c33ced4
TH Huang <tzhsn.huang@gmail.com>
2023-08-29 19:56:17 -0700

    [leetcode] perfect squares

--- solutions/leetcode/perfect-squares/main.cpp
+++ solutions/leetcode/perfect-squares/main.cpp
@@ -1,35 +1,6 @@
 class Solution {
    public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, numeric_limits<int>::max());
-        dp[0] = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int v = 1; i + v * v <= n; ++v) {
-                int j = i + v * v;
-                dp[j] = min(dp[j], dp[i] + 1);
-            }
-        }
-        return dp[n];
-    }
-};
-
-class Solution {
-   public:
-    int numSquares(int n) {
-        vector<int> dp(n + 1, numeric_limits<int>::max());
-        dp[0] = 0;
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; i - j * j >= 0; ++j) {
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
-            }
-        }
-        return dp.back();
-    }
-};
-
-class Solution {
-   public:
-    int numSquares(int n) {
         unordered_set<int> nums;
         for (int i = 1; i * i <= n; ++i) nums.insert(i * i);
 
@@ -46,5 +17,21 @@ class Solution {
             if (search(n, i)) return i;
         }
         return n;
+    }
+};
+
+class Solution {
+   public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, numeric_limits<int>::max());
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            for (int v = 1; v * v <= n; ++v) {
+                if (int j = i - v * v; j >= 0) {
+                    dp[i] = min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n];
     }
 };
