a4887388bf3965339925eff78c03b3b315d88b11
TH Huang <tzhsn.huang@gmail.com>
2023-08-29 20:57:52 -0700

    [leetcode] longest common subsequence

--- solutions/leetcode/longest-common-subsequence/main.cpp
+++ solutions/leetcode/longest-common-subsequence/main.cpp
@@ -39,3 +39,27 @@ class Solution {
         return dp.back();
     }
 };
+
+class Solution {
+   public:
+    int longestCommonSubsequence(string text1, string text2) {
+        if (text1.size() < text2.size()) swap(text1, text2);
+
+        int n1 = text1.size();
+        int n2 = text2.size();
+
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+        for (int i = n1 - 1; i >= 0; --i) {
+            for (int j = n2 - 1; j >= 0; --j) {
+                if (text1[i] == text2[j]) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+};
