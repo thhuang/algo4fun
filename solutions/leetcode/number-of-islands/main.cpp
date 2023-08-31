76ad9096cd6fadbaa1a3872f8df6bb033ef798b7
TH Huang <tzhsn.huang@gmail.com>
2023-08-30 19:30:18 -0700

    [leetcode] number of islands

--- solutions/leetcode/number-of-islands/main.cpp
+++ solutions/leetcode/number-of-islands/main.cpp
@@ -60,3 +60,41 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid.front().size();
+
+        function<bool(int, int)> search = [&](int x, int y) -> bool {
+            if (grid[x][y] != '1') return false;
+
+            vector<array<int, 2>> stk = {{x, y}};
+            while (!stk.empty()) {
+                auto [i, j] = stk.back();
+                stk.pop_back();
+
+                if (i < 0 || m <= i || j < 0 || n <= j || grid[i][j] != '1')
+                    continue;
+                grid[i][j] = '2';
+
+                for (auto [di, dj] : directions)
+                    stk.push_back({i + di, j + dj});
+            }
+
+            return true;
+        };
+
+        int result = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (search(i, j)) ++result;
+            }
+        }
+
+        return result;
+    }
+};
