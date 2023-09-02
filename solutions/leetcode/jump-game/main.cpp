47ecbc0fb764d29f5bc7b2477eacbd4dad220855
TH Huang <tzhsn.huang@gmail.com>
2023-09-02 16:28:42 -0700

    [leetcode] jump game

--- solutions/leetcode/jump-game/main.cpp
+++ solutions/leetcode/jump-game/main.cpp
@@ -1,22 +1,10 @@
 class Solution {
    public:
     bool canJump(vector<int>& nums) {
-        int max_dist = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > max_dist) return false;
-            max_dist = max(max_dist, i + nums[i]);
+        for (int i = 0, mx = 0; i < nums.size(); ++i) {
+            if (i > mx) return false;
+            mx = max(mx, i + nums[i]);
         }
         return true;
-    }
-};
-
-class Solution {
-   public:
-    bool canJump(vector<int>& nums) {
-        for (int i = 0, r = 0; i <= r; ++i) {
-            r = max(r, i + nums[i]);
-            if (r >= nums.size() - 1) return true;
-        }
-        return false;
     }
 };
