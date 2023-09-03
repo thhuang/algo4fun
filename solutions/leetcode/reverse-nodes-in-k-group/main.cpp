a5bb18c483b11f641194d8ecc78f8877dd7f6ee8
TH Huang <tzhsn.huang@gmail.com>
2023-09-03 16:36:21 -0700

    [leetcode] reverse nodes in k group

--- solutions/leetcode/reverse-nodes-in-k-group/main.cpp
+++ solutions/leetcode/reverse-nodes-in-k-group/main.cpp
@@ -9,74 +9,6 @@
  * };
  */
 class Solution {
-   public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode dummy(0, head);
-        ListNode* p0 = &dummy;
-
-        bool done = false;
-        while (true) {
-            ListNode* p3 = p0;
-            for (int i = 0; i <= k; ++i) {
-                if (p3) {
-                    p3 = p3->next;
-                } else {
-                    done = true;
-                }
-            }
-            if (done) break;
-
-            ListNode* p1 = p0->next;
-            for (int i = 0; i < k; ++i) {
-                ListNode* p2 = p1->next;
-                p1->next = p3;
-                p3 = p1;
-                p1 = p2;
-            }
-            p0->next = p3;
-
-            for (int i = 0; i < k; ++i) p0 = p0->next;
-        }
-
-        return dummy.next;
-    }
-};
-
-class Solution {
-    ListNode* forward(ListNode* u, int k) {
-        for (int i = 0; u && i < k; ++i) u = u->next;
-        return u;
-    }
-
-    pair<ListNode*, ListNode*> reverse(ListNode* head) {
-        ListNode *p0 = nullptr, *p1 = head;
-        while (p1) {
-            ListNode* p2 = p1->next;
-            p1->next = p0;
-            p0 = p1;
-            p1 = p2;
-        }
-        return {p0, head};
-    }
-
-   public:
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode dummy(0, head);
-
-        for (ListNode *p = &dummy, *q = forward(p, k); q != nullptr;
-             p = q, q = forward(p, k)) {
-            ListNode* c = q->next;
-            q->next = nullptr;
-
-            tie(p->next, q) = reverse(p->next);
-            q->next = c;
-        }
-
-        return dummy.next;
-    }
-};
-
-class Solution {
     void reverse(ListNode* head, int k) {
         ListNode* a = nullptr;
         ListNode* b = head;
@@ -106,6 +38,44 @@ class Solution {
             a->next = b;
             c->next = d;
 
+            a = c;
+        }
+
+        return dummy.next;
+    }
+};
+
+class Solution {
+   public:
+    void reverse(ListNode* head) {
+        ListNode* a = nullptr;
+        ListNode* b = head;
+        while (b) {
+            ListNode* c = b->next;
+            b->next = a;
+            a = b;
+            b = c;
+        }
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode dummy(0, head);
+        auto a = &dummy;
+
+        while (true) {
+            auto b = a;
+            for (int i = 0; i < k; ++i) {
+                if (!b->next) return dummy.next;
+                b = b->next;
+            }
+            auto c = a->next;
+            auto d = b->next;
+
+            b->next = nullptr;
+            reverse(c);
+
+            a->next = b;
+            c->next = d;
             a = c;
         }
 
