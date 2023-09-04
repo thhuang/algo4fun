4ad720329e3c2a12ad24bbd61a64095a7fc73054
TH Huang <tzhsn.huang@gmail.com>
2023-09-03 18:28:26 -0700

    [leetcode] spiral matrix

--- solutions/leetcode/spiral-matrix/main.cpp
+++ solutions/leetcode/spiral-matrix/main.cpp
@@ -1,36 +1,6 @@
-class Solution {
-    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    const int seen = numeric_limits<int>::max();
-
-   public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix.front().size();
-
-        int x = 0, y = 0, d = 0;
-        vector<int> result(m * n);
-        for (int i = 0; i < result.size(); ++i) {
-            result[i] = matrix[x][y];
-            matrix[x][y] = seen;
-
-            auto [dx, dy] = directions[d];
-            int xx = x + dx, yy = y + dy;
-            if (xx < 0 || xx >= m || yy < 0 || yy >= n ||
-                matrix[xx][yy] == seen) {
-                d = (d + 1) % 4;
-                tie(dx, dy) = directions[d];
-                xx = x + dx, yy = y + dy;
-            }
-
-            x = xx, y = yy;
-        }
-
-        return result;
-    }
-};
 
 class Solution {
-    const vector<array<int, 2>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    vector<array<int, 2>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
    public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
@@ -38,22 +8,30 @@ class Solution {
         int n = matrix.front().size();
 
         vector<int> result(m * n);
-        for (int k = 0, i = 0, j = 0, d = 0; k < m * n; ++k) {
+
+        for (int k = 0, d = 0, i = 0, j = 0; k < m * n; ++k) {
             result[k] = matrix[i][j];
             matrix[i][j] += 1000;
 
-            int ii = i + directions[d][0];
-            int jj = j + directions[d][1];
-
+            auto [di, dj] = directions[d];
+            int ii = i + di;
+            int jj = j + dj;
             if (ii < 0 || m <= ii || jj < 0 || n <= jj ||
                 matrix[ii][jj] > 100) {
-                d = (d + 1) % 4;
-                ii = i + directions[d][0];
-                jj = j + directions[d][1];
+                (d += 1) %= 4;
+                auto [di, dj] = directions[d];
+                ii = i + di;
+                jj = j + dj;
             }
 
             i = ii;
             j = jj;
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] -= 1000;
+            }
         }
 
         return result;
