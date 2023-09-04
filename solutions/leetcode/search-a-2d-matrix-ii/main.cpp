72e1c02a94f6f9c4373a7c72df5e253cbca2f274
TH Huang <tzhsn.huang@gmail.com>
2023-09-03 18:34:24 -0700

    [leetcode] search a 2d matrix ii

--- solutions/leetcode/search-a-2d-matrix-ii/main.cpp
+++ solutions/leetcode/search-a-2d-matrix-ii/main.cpp
@@ -1,16 +1,14 @@
 class Solution {
    public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix.front().size();
-        int i = 0, j = n - 1;
-        while (i < m && j >= 0) {
-            const int& v = matrix[i][j];
-            if (v == target) return true;
-            if (v < target)
-                ++i;
-            else  // v > target
-                --j;
+        int m = matrix.size();
+        int n = matrix.front().size();
+
+        for (int i = 0, j = n - 1; i < m && j >= 0;
+             (matrix[i][j] < target) ? ++i : --j) {
+            if (matrix[i][j] == target) return true;
         }
+
         return false;
     }
 };
