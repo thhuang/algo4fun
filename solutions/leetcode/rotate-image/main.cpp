bf54eee164c8d13dab1dcc00efe3a08f164719c7
TH Huang <tzhsn.huang@gmail.com>
2023-09-03 18:21:17 -0700

    [leetcode] rotate image

--- solutions/leetcode/rotate-image/main.cpp
+++ solutions/leetcode/rotate-image/main.cpp
@@ -24,3 +24,18 @@ class Solution {
             reverse(row.begin(), row.end());
     }
 };
+
+class Solution {
+   public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+            for (int l = 0, r = n - 1; l < r; ++l, --r) {
+                swap(matrix[i][l], matrix[i][r]);
+            }
+        }
+    }
+};
