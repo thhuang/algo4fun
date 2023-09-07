bef22827df77a9b7f75c94dc934754e199ba9df0
TH Huang <tzhsn.huang@gmail.com>
2023-09-06 20:50:18 -0700

    [leetcode] minimum time to build blocks

--- solutions/leetcode/minimum-time-to-build-blocks/main.cpp
+++ solutions/leetcode/minimum-time-to-build-blocks/main.cpp
@@ -21,3 +21,28 @@ class Solution {
         return build(n - 1, 1);
     }
 };
+
+class Solution {
+   public:
+    int minBuildTime(vector<int>& blocks, int split) {
+        sort(blocks.begin(), blocks.end(), greater<>());
+
+        int n = blocks.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, numeric_limits<int>::max()));
+        for (int w = 0; w <= n; ++w) dp[n][w] = 0;
+
+        for (int b = n - 1; b >= 0; --b) {
+            for (int w = n; w > 0; --w) {
+                if (b + w >= n) {
+                    dp[b][w] = blocks[b];
+                    continue;
+                }
+                int workNow = max(blocks[b], dp[b + 1][w - 1]);
+                int splitNow = split + dp[b][min(n, w * 2)];
+                dp[b][w] = min(workNow, splitNow);
+            }
+        }
+
+        return dp[0][1];
+    }
+};
