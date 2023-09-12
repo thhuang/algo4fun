846d4f835c36e66a1dd8f3cc9845789221c9d96e
TH Huang <tzhsn.huang@gmail.com>
2023-09-11 21:58:05 -0700

    [leetcode] minimum deletions to make character frequencies unique

--- solutions/leetcode/minimum-deletions-to-make-character-frequencies-unique/main.cpp
+++ solutions/leetcode/minimum-deletions-to-make-character-frequencies-unique/main.cpp
@@ -18,3 +18,28 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int minDeletions(string s) {
+        array<int, 26> charFreq;
+        charFreq.fill(0);
+        for (char c : s) ++charFreq[c - 'a'];
+
+        map<int, int> freqCount;
+        for (int i = 0; i < 26; ++i) ++freqCount[charFreq[i]];
+
+        int result = 0;
+        while (!freqCount.empty() && freqCount.rbegin()->first != 0) {
+            auto it = --freqCount.end();
+            auto [freq, cnt] = *it;
+            freqCount.erase(it);
+
+            result += --cnt;
+
+            if (cnt > 0) freqCount[freq - 1] += cnt;
+        }
+
+        return result;
+    }
+};
