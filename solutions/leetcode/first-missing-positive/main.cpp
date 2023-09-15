2a13bbfccaefa70d0109b3f1e6757c714d1b9570
TH Huang <tzhsn.huang@gmail.com>
2023-09-14 19:14:59 -0700

    [leetcode] first missing positive

--- solutions/leetcode/first-missing-positive/main.cpp
+++ solutions/leetcode/first-missing-positive/main.cpp
@@ -2,16 +2,19 @@ class Solution {
    public:
     int firstMissingPositive(vector<int>& nums) {
         nums.push_back(0);
-        for (int i = 0; i < nums.size(); ++i) {
-            int v = nums[i];
-            while (v > 0 && v != i && v < nums.size() && v != nums[v]) {
-                swap(nums[i], nums[v]);
-                v = nums[i];
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            while (0 <= nums[i] && nums[i] < n && nums[i] != nums[nums[i]]) {
+                swap(nums[i], nums[nums[i]]);
             }
         }
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] != i) return i;
+
+        for (int i = 1; i < n; ++i) {
+            if (i != nums[i]) return i;
         }
+
         return nums.size();
     }
 };
