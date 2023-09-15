691ad23cbaba7190eea96fb6ed463207097c75bc
TH Huang <tzhsn.huang@gmail.com>
2023-09-14 18:21:18 -0700

    [leetcode] move zeros

--- solutions/leetcode/move-zeroes/main.cpp
+++ solutions/leetcode/move-zeroes/main.cpp
@@ -1,25 +1,8 @@
 class Solution {
    public:
     void moveZeroes(vector<int>& nums) {
-        for (int l = 0, r = 0; r < size(nums); ++r) {
-            if (nums[r] == 0) continue;
-            swap(nums[l], nums[r]);
-            ++l;
-        }
-    }
-};
-
-class Solution {
-   public:
-    void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int p = 0;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == 0) continue;
-            nums[p++] = nums[i];
-        }
-        for (int i = p; i < n; ++i) {
-            nums[i] = 0;
+        for (int p = 0, i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) swap(nums[p++], nums[i]);
         }
     }
 };
