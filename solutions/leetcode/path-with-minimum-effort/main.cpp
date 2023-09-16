5a3b73d1f72bf9381290bb630d0c10ec0e36243b
TH Huang <tzhsn.huang@gmail.com>
2023-09-16 11:48:53 -0700

    [leetcode] path with maximum effort

--- solutions/leetcode/path-with-minimum-effort/main.cpp
+++ solutions/leetcode/path-with-minimum-effort/main.cpp
@@ -8,10 +8,12 @@ class Solution {
 
         vector<vector<bool>> vis(n, vector<bool>(m, false));
 
-        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>> q;  // [effort, row, col]
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>>
+            q;  // [effort, row, col]
 
-        // auto cmp = [](const array<int, 3>& a, const array<int, 3>& b) { return a[0] > b[0]; };
-        // priority_queue<array<int, 3>, vector<array<int, 3>>, decltype(cmp)> q(cmp);
+        // auto cmp = [](const array<int, 3>& a, const array<int, 3>& b) {
+        // return a[0] > b[0]; }; priority_queue<array<int, 3>,
+        // vector<array<int, 3>>, decltype(cmp)> q(cmp);
 
         q.push({0, 0, 0});
 
@@ -34,5 +36,40 @@ class Solution {
         }
 
         return -1;
+    }
+};
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+   public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights.front().size();
+
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>>
+            pq;  // {max effort, i, j}
+        pq.push({0, 0, 0});
+
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        while (!pq.empty()) {
+            auto [d, i, j] = pq.top();
+            pq.pop();
+
+            if (i == m - 1 && j == n - 1) return d;
+            if (vis[i][j]) continue;
+            vis[i][j] = true;
+
+            for (auto [di, dj] : directions) {
+                int ii = i + di;
+                int jj = j + dj;
+                if (ii < 0 || m <= ii || jj < 0 || n <= jj) continue;
+
+                int v = max(d, abs(heights[i][j] - heights[ii][jj]));
+                pq.push({v, ii, jj});
+            }
+        }
+
+        return numeric_limits<int>::max();
     }
 };
