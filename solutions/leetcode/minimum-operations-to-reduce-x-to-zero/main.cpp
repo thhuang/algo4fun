98f8d58c8105bed1358bcfb14dfc51e25930cc89
TH Huang <tzhsn.huang@gmail.com>
2023-09-20 12:18:24 -0500

    [leetcode] minimum operations to reduce x to zero

--- solutions/leetcode/minimum-operations-to-reduce-x-to-zero/main.cpp
+++ solutions/leetcode/minimum-operations-to-reduce-x-to-zero/main.cpp
@@ -45,3 +45,44 @@ class Solution {
         return result > n ? -1 : result;
     }
 };
+
+class Solution {
+   public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        int result = -1;
+
+        int target = accumulate(nums.begin(), nums.end(), 0) - x;
+        if (target < 0) return -1;
+
+        for (int l = 0, r = 0, window = 0; r < n; ++r) {
+            window += nums[r];
+            while (window > target) {
+                window -= nums[l++];
+            }
+            if (window == target && (result == -1 || n - r + l - 1 < result)) {
+                result = n - r + l - 1;
+            }
+        }
+
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        int result = n + 1;
+
+        for (int l = 0, r = 0, window = 0,
+                 target = accumulate(nums.begin(), nums.end(), 0) - x;
+             r < n; ++r) {
+            window += nums[r];
+            while (l <= r && window > target) window -= nums[l++];
+            if (window == target) result = min(result, n - r + l - 1);
+        }
+
+        return result > n ? -1 : result;
+    }
+};
