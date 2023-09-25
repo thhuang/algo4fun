c9ebe16e6b8753da992c977b3368b5f3560915af
TH Huang <tzhsn.huang@gmail.com>
2023-09-25 10:07:54 -0500

    [leetcode] find the difference

--- solutions/leetcode/find-the-difference/main.cpp
+++ solutions/leetcode/find-the-difference/main.cpp
@@ -1,9 +1,21 @@
 class Solution {
    public:
     char findTheDifference(string s, string t) {
-        char ans = 0;
-        for (char c : s) ans ^= c;
-        for (char c : t) ans ^= c;
-        return ans;
+        array<int, 26> freq = {};
+        for (char c : s) ++freq[c - 'a'];
+        for (char c : t) {
+            if (--freq[c - 'a'] < 0) return c;
+        }
+        return ' ';
+    }
+};
+
+class Solution {
+   public:
+    char findTheDifference(string s, string t) {
+        char result = 0;
+        for (char c : s) result ^= c;
+        for (char c : t) result ^= c;
+        return result;
     }
 };
