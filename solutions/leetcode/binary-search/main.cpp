777bb9e6631565658b6821851f4df0995c735f9b
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 17:02:43 -0500

    [leetcode] binary search

--- solutions/leetcode/binary-search/main.cpp
+++ solutions/leetcode/binary-search/main.cpp
@@ -14,3 +14,20 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (nums[m] <= target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        if (nums[l] == target) return l;
+        return -1;
+    }
+};
