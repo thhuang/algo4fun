bd53527df5c51406f80503e64cdd547e95e257b1
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 17:51:53 -0500

    [leetcode] find smallest letter greater than target

--- solutions/leetcode/find-smallest-letter-greater-than-target/main.cpp
+++ solutions/leetcode/find-smallest-letter-greater-than-target/main.cpp
@@ -6,3 +6,19 @@ class Solution {
         return *it;
     }
 };
+
+class Solution {
+   public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = -1, r = letters.size();
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (letters[m] <= target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return letters[r % letters.size()];
+    }
+};
