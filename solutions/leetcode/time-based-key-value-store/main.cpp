b7f9f3c132205a43e090190cbd0d3052bc07b352
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 18:32:57 -0500

    [leetcode] time based key value store

--- solutions/leetcode/time-based-key-value-store/main.cpp
+++ solutions/leetcode/time-based-key-value-store/main.cpp
@@ -18,6 +18,39 @@ class TimeMap {
     }
 };
 
+class TimeMap {
+    struct ValueTimestamp {
+        string value;
+        int timestamp;
+    };
+
+    unordered_map<string, vector<ValueTimestamp>> kvs;
+
+   public:
+    void set(string key, string value, int timestamp) {
+        kvs[key].push_back({value, timestamp});
+    }
+
+    string get(string key, int timestamp) {
+        auto it = kvs.find(key);
+        if (it == kvs.end()) return "";
+
+        const auto& vs = it->second;
+        int l = -1, r = vs.size();
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (vs[m].timestamp <= timestamp) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+
+        if (l == -1) return "";
+        return vs[l].value;
+    }
+};
+
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap* obj = new TimeMap();
