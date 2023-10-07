f1cdbc11e2149dc57648de42dc334681be57437f
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 17:10:55 -0500

    [leetcode] search insert position

--- solutions/leetcode/search-insert-position/main.cpp
+++ solutions/leetcode/search-insert-position/main.cpp
@@ -57,3 +57,19 @@ class Solution {
         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
     }
 };
+
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = -1, r = nums.size();
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (nums[m] < target) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        return r;
+    }
+};
