ca669f8d1f95491ec91a49b9a3b288b9ce48392e
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 18:46:07 -0500

    [leetcode] snapshot array

--- solutions/leetcode/snapshot-array/main.cpp
+++ solutions/leetcode/snapshot-array/main.cpp
@@ -15,11 +15,44 @@ class SnapshotArray {
     int snap() { return id++; }
 
     int get(int index, int snap_id) {
-        auto &records = updates[index];
+        auto& records = updates[index];
         auto it = --upper_bound(
             records.begin(), records.end(), snap_id,
             [](int t, pair<int, int> v) -> bool { return t < v.first; });
         return it->second;
+    }
+};
+
+class SnapshotArray {
+    struct History {
+        int value;
+        int id;
+    };
+
+    int length;
+    vector<vector<History>> data;
+    int id = 0;
+
+   public:
+    SnapshotArray(int length) : length(length), data(length, {{0, 0}}) {}
+
+    void set(int index, int val) {
+        vector<History>& history = data[index];
+        if (history.back().id == id) {
+            history.back().value = val;
+        } else {
+            history.push_back({val, id});
+        }
+    }
+
+    int snap() { return id++; }
+
+    int get(int index, int snap_id) {
+        const vector<History>& history = data[index];
+        auto it = upper_bound(
+            history.begin(), history.end(), snap_id,
+            [](int t, const History& e) -> bool { return t < e.id; });
+        return prev(it)->value;
     }
 };
 
