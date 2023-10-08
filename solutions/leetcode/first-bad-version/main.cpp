10b66d7a750c1ce9fea1cd8208900dbafc474763
TH Huang <tzhsn.huang@gmail.com>
2023-10-07 19:43:20 -0500

    [leetcode] first bad version

--- solutions/leetcode/first-bad-version/main.cpp
+++ solutions/leetcode/first-bad-version/main.cpp
@@ -16,3 +16,19 @@ class Solution {
         return l;
     }
 };
+
+class Solution {
+   public:
+    int firstBadVersion(int n) {
+        int l = 0, r = n;
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (isBadVersion(m)) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        return r;
+    }
+};
