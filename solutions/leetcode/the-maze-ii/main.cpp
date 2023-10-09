c5211f63d9f1da780a9e4fc6933c2fb4f7689d6b
TH Huang <tzhsn.huang@gmail.com>
2023-10-08 20:47:26 -0500

    [leetcode] the maze ii

--- solutions/leetcode/the-maze-ii/main.cpp
+++ solutions/leetcode/the-maze-ii/main.cpp
@@ -41,3 +41,59 @@ class Solution {
         return -1;
     }
 };
+
+class Solution {
+    const vector<array<int, 2>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    struct Status {
+        int i, j;
+        int d;
+        int dist;
+    };
+
+   public:
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start,
+                         vector<int>& destination) {
+        int m = maze.size();
+        int n = maze.front().size();
+
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        vis[start[0]][start[1]] = true;
+
+        queue<Status> q;
+        for (int d = 0; d < 4; ++d) {
+            q.push({start[0], start[1], d, 0});
+        }
+
+        while (!q.empty()) {
+            auto e = q.front();
+            q.pop();
+
+            int i = e.i, j = e.j, d = e.d, dist = e.dist;
+            auto [di, dj] = directions[d];
+
+            int ii = i + di, jj = j + dj;
+            if (0 <= ii && ii < m && 0 <= jj && jj < n && maze[ii][jj] != 1) {
+                q.push({ii, jj, d, dist + 1});
+                continue;
+            }
+
+            if (i == destination[0] && j == destination[1]) {
+                return dist;
+            }
+
+            if (vis[i][j]) continue;
+            vis[i][j] = true;
+
+            for (int k = 0; k < 4; ++k) {
+                auto [di, dj] = directions[k];
+                int ii = i + di, jj = j + dj;
+                if (ii < 0 || m <= ii || jj < 0 || n <= jj || maze[ii][jj] == 1)
+                    continue;
+                q.push({ii, jj, k, dist + 1});
+            }
+        }
+
+        return -1;
+    }
+};
