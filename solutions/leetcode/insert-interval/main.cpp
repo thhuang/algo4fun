c988b2fddee76dcfad52a6863a827e8121a62d1f
TH Huang <tzhsn.huang@gmail.com>
2023-10-13 15:04:45 -0500

    [leetcode] insert interval

--- solutions/leetcode/insert-interval/main.cpp
+++ solutions/leetcode/insert-interval/main.cpp
@@ -96,3 +96,31 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+        vector<vector<int>> result;
+
+        int i = 0;
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            result.push_back(intervals[i]);
+            ++i;
+        }
+
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            ++i;
+        }
+        result.push_back(newInterval);
+
+        while (i < intervals.size()) {
+            result.push_back(intervals[i]);
+            ++i;
+        }
+
+        return result;
+    }
+};
