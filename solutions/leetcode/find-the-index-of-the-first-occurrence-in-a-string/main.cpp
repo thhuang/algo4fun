e736f152fec7221a902fcd2b7e7816899b1f1fb7
TH Huang <tzhsn.huang@gmail.com>
2023-10-13 17:25:11 -0500

    [leetcode] find the index of the first occurrence in a string

--- solutions/leetcode/find-the-index-of-the-first-occurrence-in-a-string/main.cpp
+++ solutions/leetcode/find-the-index-of-the-first-occurrence-in-a-string/main.cpp
@@ -1,4 +1,38 @@
 class Solution {
+    typedef unsigned long long ull;
+    typedef long long ll;
+
+    const ull mod = numeric_limits<int>::max();
+
+   public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+
+        ull x = n + 1;
+        if (x % 2 == 0) ++x;
+
+        vector<ull> suffHash(n + 1, 0);
+        for (int i = n - 1; i >= 0; --i) {
+            suffHash[i] = (haystack[i] - 'a') + x * suffHash[i + 1] % mod;
+        }
+
+        ull t = 0, xm = 1;
+        for (int i = 0; i < m; ++i) {
+            (t += (needle[i] - 'a') * xm % mod) %= mod;
+            (xm *= x) %= mod;
+        }
+
+        for (int l = 0, r = l + m - 1; r < n; ++l, ++r) {
+            ull v = (suffHash[l] - xm * suffHash[r + 1] % mod + mod) % mod;
+            if (v == t) return l;
+        }
+
+        return -1;
+    }
+};
+
+class Solution {
    public:
     int strStr(string haystack, string needle) {
         vector<int> prefix(needle.size(), 0);
