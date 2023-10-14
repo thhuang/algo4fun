d1c7769d2b9fcd8dbd5c01e3f1823af1f5f1d666
TH Huang <tzhsn.huang@gmail.com>
2023-10-14 11:19:46 -0500

    [leetcode] wildcard matching

--- solutions/leetcode/wildcard-matching/main.cpp
+++ solutions/leetcode/wildcard-matching/main.cpp
@@ -1,26 +1,22 @@
 class Solution {
-    bool isSimpleMatch(string& s, string& p) {
+    bool isSimpleMatch(const string& s, const string& p) {
         for (int i = 0; i < s.size(); ++i) {
-            if (p[i] == '?') continue;
-            if (s[i] != p[i]) return false;
+            if (p[i] != '?' && s[i] != p[i]) return false;
         }
         return true;
     }
 
     bool isWildcardMatch(string& s, string& p) {
-        auto it = find(p.rbegin(), p.rend(), '*');
+        auto len = find(p.rbegin(), p.rend(), '*') - p.rbegin();
+        int n = s.size(), m = p.size();
 
-        int length = it - p.rbegin();
-        if (length > s.size()) return false;
+        if (n < len || !isSimpleMatch(s.substr(n - len), p.substr(m - len)))
+            return false;
+        if (m == len) return n == m;
 
-        string s_last = s.substr(s.size() - length, length);
-        string p_last = p.substr(p.size() - length, length);
+        s = s.substr(0, n - len), p = p.substr(0, m - len);
+        n -= len, m -= len;
 
-        if (!isSimpleMatch(s_last, p_last)) return false;
-        if (it == p.rend()) return s.size() == p.size();
-
-        s = s.substr(0, s.size() - length);
-        p = p.substr(0, p.size() - length);
         while (!p.empty() && p.back() == '*') p.pop_back();
 
         while (true) {
