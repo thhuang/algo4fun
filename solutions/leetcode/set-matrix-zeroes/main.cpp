2312b7fa9fcaa94396aea9472eb0e154ae0fd4a3
TH Huang <tzhsn.huang@gmail.com>
2023-10-14 12:18:05 -0500

    [leetcode] set matrix zeroes

--- solutions/leetcode/set-matrix-zeroes/main.cpp
+++ solutions/leetcode/set-matrix-zeroes/main.cpp
@@ -83,19 +83,13 @@ class Solution {
         int n = matrix.front().size();
 
         bool row0 = false;
-        for (int j = 0; j < n; ++j) {
-            if (matrix[0][j] == 0) {
-                row0 = true;
-                break;
-            }
-        }
-
         bool col0 = false;
+
         for (int i = 0; i < m; ++i) {
-            if (matrix[i][0] == 0) {
-                col0 = true;
-                break;
-            }
+            if (matrix[i][0] == 0) col0 = true;
+        }
+        for (int j = 0; j < n; ++j) {
+            if (matrix[0][j] == 0) row0 = true;
         }
 
         for (int i = 1; i < m; ++i) {
@@ -107,22 +101,30 @@ class Solution {
             }
         }
 
-        for (int j = 1; j < n; ++j) {
-            if (matrix[0][j] != 0) continue;
-            for (int i = 1; i < m; ++i) matrix[i][j] = 0;
-        }
-
         for (int i = 1; i < m; ++i) {
             if (matrix[i][0] != 0) continue;
-            for (int j = 1; j < n; ++j) matrix[i][j] = 0;
+            for (int j = 1; j < n; ++j) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] != 0) continue;
+            for (int i = 1; i < m; ++i) {
+                matrix[i][j] = 0;
+            }
         }
 
         if (row0) {
-            for (int j = 0; j < n; ++j) matrix[0][j] = 0;
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
         }
 
         if (col0) {
-            for (int i = 0; i < m; ++i) matrix[i][0] = 0;
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
         }
     }
 };
