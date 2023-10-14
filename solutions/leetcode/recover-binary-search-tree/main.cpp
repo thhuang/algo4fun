01ff911e99cccb4553b6edc0f09a65f049b124e6
TH Huang <tzhsn.huang@gmail.com>
2023-10-14 16:36:09 -0500

    [leetcode] recover binary search tree

--- solutions/leetcode/recover-binary-search-tree/main.cpp
+++ solutions/leetcode/recover-binary-search-tree/main.cpp
@@ -35,3 +35,40 @@ class Solution {
         swap(order[i]->val, order[j]->val);
     }
 };
+
+class Solution {
+   public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* p;
+
+        p = nullptr;
+        TreeNode* a = nullptr;
+        function<void(TreeNode*)> inorder = [&](TreeNode* u) -> void {
+            if (u == nullptr || a != nullptr) return;
+            inorder(u->left);
+            if (p != nullptr && p->val > u->val) {
+                a = p;
+                return;
+            }
+            p = u;
+            inorder(u->right);
+        };
+        inorder(root);
+
+        p = nullptr;
+        TreeNode* b = nullptr;
+        function<void(TreeNode*)> rinorder = [&](TreeNode* u) -> void {
+            if (u == nullptr || b != nullptr) return;
+            rinorder(u->right);
+            if (p != nullptr && p->val < u->val) {
+                b = p;
+                return;
+            }
+            p = u;
+            rinorder(u->left);
+        };
+        rinorder(root);
+
+        swap(a->val, b->val);
+    }
+};
