a2ae93c4bd0489ae2d2f98b974271313fd69913f
TH Huang <tzhsn.huang@gmail.com>
2023-10-15 16:43:02 -0500

    [leetcode] maximum number of books you can take

--- solutions/leetcode/maximum-number-of-books-you-can-take/main.cpp
+++ solutions/leetcode/maximum-number-of-books-you-can-take/main.cpp
@@ -37,3 +37,42 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    struct IndexTotal {
+        int i;
+        long long total;
+    };
+
+   public:
+    long long maximumBooks(vector<int>& books) {
+        auto calc = [&](int l, int r) -> long long {
+            long long d = min(r - l + 1, books[r]);
+            return (books[r] * 2 - d + 1) * d / 2;
+        };
+
+        long long result = 0;
+        stack<IndexTotal> indexTotal;
+
+        for (int i = 0; i < books.size(); ++i) {
+            while (!indexTotal.empty() &&
+                   books[indexTotal.top().i] + (i - indexTotal.top().i) >=
+                       books[i]) {
+                indexTotal.pop();
+            }
+
+            if (indexTotal.empty()) {
+                indexTotal.push({i, calc(0, i)});
+            } else {
+                indexTotal.push({i, indexTotal.top().total +
+                                        calc(indexTotal.top().i + 1, i)});
+            }
+
+            cout << indexTotal.top().total << ' ';
+
+            result = max(result, indexTotal.top().total);
+        }
+
+        return result;
+    }
+};
