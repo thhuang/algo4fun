1b29bc912ce82f1f729bbe8fc850696979028705
TH Huang <tzhsn.huang@gmail.com>
2023-10-15 15:31:46 -0500

    [leetcode] kth smallest element in a sorted matrix

--- solutions/leetcode/kth-smallest-element-in-a-sorted-matrix/main.cpp
+++ solutions/leetcode/kth-smallest-element-in-a-sorted-matrix/main.cpp
@@ -5,6 +5,29 @@ class Solution {
 
    public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+        auto cmp = [&](Row b, Row a) -> bool { return *a.it < *b.it; };
+
+        priority_queue<Row, vector<Row>, decltype(cmp)> pq(cmp);
+        for (auto& row : matrix) pq.push({row.begin(), row.end()});
+
+        while (--k) {
+            auto row = pq.top();
+            pq.pop();
+            row.it = next(row.it);
+            if (row.it != row.end) pq.push(row);
+        }
+
+        return *pq.top().it;
+    }
+};
+
+class Solution {
+    struct Row {
+        vector<int>::iterator it, end;
+    };
+
+   public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
         auto comp = [&](Row a, Row b) -> bool { return *a.it > *b.it; };
 
         priority_queue<Row, vector<Row>, decltype(comp)> q(comp);
@@ -74,24 +97,22 @@ class Solution {
 };
 
 class Solution {
-    struct Row {
-        vector<int>::iterator it, end;
-    };
-
    public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        auto cmp = [&](Row b, Row a) -> bool { return *a.it < *b.it; };
-
-        priority_queue<Row, vector<Row>, decltype(cmp)> pq(cmp);
-        for (auto& row : matrix) pq.push({row.begin(), row.end()});
-
-        while (--k) {
-            auto row = pq.top();
-            pq.pop();
-            row.it = next(row.it);
-            if (row.it != row.end) pq.push(row);
+        int l = matrix.front().front() - 1;
+        int r = matrix.back().back() + 1;
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            int cnt = 0;
+            for (const auto vec : matrix) {
+                cnt += upper_bound(vec.begin(), vec.end(), m) - vec.begin();
+            }
+            if (cnt < k) {
+                l = m;
+            } else {
+                r = m;
+            }
         }
-
-        return *pq.top().it;
+        return r;
     }
 };
