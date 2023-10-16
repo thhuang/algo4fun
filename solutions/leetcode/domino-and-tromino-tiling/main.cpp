f86bc12ea63c72a3894c89854ed74149079a0025
TH Huang <tzhsn.huang@gmail.com>
2023-10-15 22:09:08 -0500

    [leetcode] domino and tromino tiling

--- solutions/leetcode/domino-and-tromino-tiling/main.cpp
+++ solutions/leetcode/domino-and-tromino-tiling/main.cpp
@@ -1,26 +1,6 @@
 class Solution {
     const int mod = 1e9 + 7;
 
-    struct State {
-        long long partial, full;
-    };
-
-   public:
-    int numTilings(int n) {
-        if (n < 3) return n;
-        array<State, 2> dp = {State{0, 1}, {2, 2}};
-        for (int i = 3; i <= n; ++i) {
-            dp = {dp[1],
-                  {(dp[0].full * 2 + dp[1].partial) % mod,
-                   (dp[0].full + dp[1].partial + dp[1].full) % mod}};
-        }
-        return dp[1].full;
-    }
-};
-
-class Solution {
-    const int mod = 1e9 + 7;
-
    public:
     int numTilings(int n) {
         array<array<int, 3>, 2> dp;
@@ -41,17 +21,20 @@ class Solution {
 class Solution {
     const int mod = 1e9 + 7;
 
+    struct State {
+        long long full, half;
+    };
+
    public:
     int numTilings(int n) {
-        array<array<int, 2>, 2> dp;
-        dp[0] = {1, 0};
-        dp[1] = {1, 0};
-        for (int i = 2; i <= n; ++i) {
+        array<State, 2> dp = {State{1, 0}, State{2, 2}};
+        if (n <= 2) return dp[n - 1].full;
+        for (int i = 3; i <= n; ++i) {
             dp = {dp[1],
-                  {(int)(((long long)dp[0][0] + dp[1][0] + dp[1][1] * 2) % mod),
-                   (int)(((long long)dp[0][0] + dp[1][1]) % mod)}};
+                  {(dp[0].full + dp[1].full + dp[1].half) % mod,
+                   (dp[0].full * 2 + dp[1].half) % mod}};
         }
-        return dp[1][0];
+        return dp.back().full;
     }
 };
 
@@ -64,9 +47,8 @@ class Solution {
 
    public:
     int numTilings(int n) {
-        array<State, 2> dp = {State{1, 0}, State{2, 2}};
-        if (n <= 2) return dp[n - 1].full;
-        for (int i = 3; i <= n; ++i) {
+        array<State, 2> dp = {State{1, 0}, State{1, 0}};
+        for (int i = 2; i <= n; ++i) {
             dp = {dp[1],
                   {(dp[0].full + dp[1].full + dp[1].half) % mod,
                    (dp[0].full * 2 + dp[1].half) % mod}};
