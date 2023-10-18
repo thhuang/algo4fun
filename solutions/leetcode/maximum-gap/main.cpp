b198512015c89f918e1b20319bddc1f2178b7e3e
TH Huang <tzhsn.huang@gmail.com>
2023-10-18 15:07:36 -0500

    [leetcode] maximum gap

--- solutions/leetcode/maximum-gap/main.cpp
+++ solutions/leetcode/maximum-gap/main.cpp
@@ -3,27 +3,22 @@ class Solution {
 
    public:
     int maximumGap(vector<int>& nums) {
-        if (nums.size() < 2) return 0;
-
         int n = nums.size();
-        int mn = *max_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
 
-        for (int x = 1; x <= mn; x *= radix) {
+        for (int x = 1; x <= mx; x *= radix) {
             vector<int> numsNew(n);
             vector<int> cnt(radix, 0);
 
             for (int v : nums) {
                 ++cnt[v / x % radix];
             }
-
             for (int i = 1; i < radix; ++i) {
                 cnt[i] += cnt[i - 1];
             }
-
             for (auto it = nums.rbegin(); it != nums.rend(); ++it) {
                 numsNew[--cnt[*it / x % radix]] = *it;
             }
-
             nums = numsNew;
         }
 
@@ -31,7 +26,6 @@ class Solution {
         for (int i = 1; i < n; ++i) {
             result = max(result, nums[i] - nums[i - 1]);
         }
-
         return result;
     }
 };
