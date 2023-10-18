2287fe6e2db8108f9544f85df43cca4afb22de0c
TH Huang <tzhsn.huang@gmail.com>
2023-10-18 10:41:43 -0500

    [leetcode] one edit distance

--- solutions/leetcode/one-edit-distance/main.cpp
+++ solutions/leetcode/one-edit-distance/main.cpp
@@ -31,3 +31,29 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+   public:
+    bool isOneEditDistance(string s, string t) {
+        int lenDiff = abs((int)s.size() - (int)t.size());
+        if (lenDiff > 1) return false;
+
+        if (lenDiff == 0) {
+            int diff = 0;
+            for (int i = 0; i < s.size(); ++i) {
+                if (s[i] != t[i]) ++diff;
+            }
+            return diff == 1;
+        }
+
+        bool foundDiff = false;
+        for (int i = 0, j = 0; i < s.size() && j < t.size(); ++i, ++j) {
+            if (s[i] == t[j]) continue;
+            if (foundDiff) return false;
+            foundDiff = true;
+            s.size() < t.size() ? --i : --j;
+        }
+
+        return true;
+    }
+};
