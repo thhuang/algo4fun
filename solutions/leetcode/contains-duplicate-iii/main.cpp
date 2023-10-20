4059e15b7995dc06b16512bc970392e1f8d8ee13
TH Huang <tzhsn.huang@gmail.com>
2023-10-19 19:07:52 -0500

    [leetcode] contains duplicate iii

--- solutions/leetcode/contains-duplicate-iii/main.cpp
+++ solutions/leetcode/contains-duplicate-iii/main.cpp
@@ -26,3 +26,32 @@ class Solution {
         return false;
     }
 };
+
+class Solution {
+    int index(int v, int w) { return v >= 0 ? v / w : (v + 1) / w - 1; }
+
+   public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff,
+                                       int valueDiff) {
+        int w = valueDiff + 1;
+        unordered_map<int, int> buckets;
+
+        for (int r = 0, l = r - indexDiff; r < nums.size(); ++l, ++r) {
+            int i = index(nums[r], w);
+
+            if (buckets.count(i) > 0) return true;
+            if (buckets.count(i - 1) && abs(buckets[i - 1] - nums[r]) < w)
+                return true;
+            if (buckets.count(i + 1) && abs(buckets[i + 1] - nums[r]) < w)
+                return true;
+
+            buckets[i] = nums[r];
+
+            if (l < 0) continue;
+
+            buckets.erase(index(nums[l], w));
+        }
+
+        return false;
+    }
+};
