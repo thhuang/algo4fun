fee04fb0fdee8aae3c3402e3561c7f07233068e3
TH Huang <tzhsn.huang@gmail.com>
2023-10-20 22:15:30 -0500

    [leetcode] shortest word distance

--- solutions/leetcode/shortest-word-distance/main.cpp
+++ solutions/leetcode/shortest-word-distance/main.cpp
@@ -24,3 +24,26 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int shortestDistance(vector<string>& wordsDict, string word1,
+                         string word2) {
+        int n = wordsDict.size();
+
+        int result = n;
+
+        for (int i = 0, idx1 = -n, idx2 = -n; i < wordsDict.size(); ++i) {
+            if (wordsDict[i] == word1) {
+                idx1 = i;
+                result = min(result, idx1 - idx2);
+            }
+            if (wordsDict[i] == word2) {
+                idx2 = i;
+                result = min(result, idx2 - idx1);
+            }
+        }
+
+        return result;
+    }
+};
