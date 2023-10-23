5d51848f57db3b4b6a0f11527a768ab3b2cfe1b5
TH Huang <tzhsn.huang@gmail.com>
2023-10-23 18:50:12 -0500

    [leetcode] max number of k sum pairs

--- solutions/leetcode/max-number-of-k-sum-pairs/main.cpp
+++ solutions/leetcode/max-number-of-k-sum-pairs/main.cpp
@@ -2,13 +2,13 @@ class Solution {
    public:
     int maxOperations(vector<int>& nums, int k) {
         int result = 0;
-        unordered_map<int, int> cnt;
+        unordered_map<int, int> numCount;
         for (int v : nums) {
-            if (cnt[v] == 0) {
-                ++cnt[k - v];
-            } else {
-                --cnt[v];
+            if (numCount[v] > 0) {
+                --numCount[v];
                 ++result;
+            } else {
+                ++numCount[k - v];
             }
         }
         return result;
