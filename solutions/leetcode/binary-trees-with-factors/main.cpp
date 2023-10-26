e84e702f80bcd8e2dcee85dbe76629ad0c20a31d
TH Huang <tzhsn.huang@gmail.com>
2023-10-25 19:24:02 -0500

    [leetcode] binary trees with factors

--- solutions/leetcode/binary-trees-with-factors/main.cpp
+++ solutions/leetcode/binary-trees-with-factors/main.cpp
@@ -23,3 +23,31 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+    typedef long long ll;
+
+   public:
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int result = 0;
+        unordered_map<int, int> memo;
+
+        for (int v : arr) {
+            ll cnt = 1;
+            for (auto [a, aCnt] : memo) {
+                if (v % a != 0) continue;
+                int b = v / a;
+                if (memo.count(b) == 0) continue;
+                ll bCnt = memo[b];
+                (cnt += aCnt * bCnt % mod) %= mod;
+            }
+            memo[v] = cnt;
+            (result += cnt) %= mod;
+        }
+
+        return result;
+    }
+};
