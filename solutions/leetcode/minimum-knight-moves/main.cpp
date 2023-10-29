03928e60b742721dbf6d4e27f1263044de6e1ced
TH Huang <tzhsn.huang@gmail.com>
2023-10-29 11:38:29 -0500

    [leetcode] minimum knight moves

--- solutions/leetcode/minimum-knight-moves/main.cpp
+++ solutions/leetcode/minimum-knight-moves/main.cpp
@@ -1,38 +1,33 @@
 class Solution {
-    vector<array<int, 2>> directions = {{2, 1}, {-2, 1}, {2, -1}, {-2, -1},
-                                        {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
+    struct State {
+        int i, j, step;
+    };
+
+    const vector<array<int, 2>> directions = {
+        {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1}};
 
    public:
     int minKnightMoves(int x, int y) {
-        if (x == 0 && y == 0) return 0;
-
-        x = abs(x), y = abs(y);
         unordered_map<int, unordered_map<int, bool>> vis;
-
-        deque<array<int, 3>> q = {{0, 0, 0}};
-        vis[0][0] = true;
-
+        queue<State> q;
+        q.push({x, y, 0});
         while (!q.empty()) {
-            auto [i, j, d] = q.front();
-            q.pop_front();
+            auto [i, j, step] = q.front();
+            q.pop();
 
-            int dd = d + 1;
+            if (vis[i][j]) continue;
+            vis[i][j] = true;
+
+            if (i == 0 && j == 0) return step;
+
             for (auto [di, dj] : directions) {
-                int ii = i + di, jj = j + dj;
-                if (ii == x && jj == y) return dd;
-                if (ii < -1 || jj < -1) continue;
-
-                if (vis[ii][jj]) continue;
-                vis[ii][jj] = true;
-
-                q.push_back({ii, jj, dd});
+                q.push({i + di, j + dj, step + 1});
             }
         }
 
         return -1;
     }
 };
-
 class Solution {
    public:
     int minKnightMoves(int x, int y) {
