43749c5df46964a5500a98213cd12e8434201ea9
TH Huang <tzhsn.huang@gmail.com>
2023-11-03 22:27:25 -0500

    [leetcode] minimum size subarray sum

--- solutions/leetcode/minimum-size-subarray-sum/main.cpp
+++ solutions/leetcode/minimum-size-subarray-sum/main.cpp
@@ -29,3 +29,20 @@ class Solution {
         return result % (nums.size() + 1);
     }
 };
+
+class Solution {
+   public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int result = n + 1;
+        for (int l = 0, r = 0, sum = 0; r < n; ++r) {
+            sum += nums[r];
+            while (sum >= target) {
+                result = min(result, r - l + 1);
+                sum -= nums[l++];
+            }
+        }
+        if (result > n) return 0;
+        return result;
+    }
+};
