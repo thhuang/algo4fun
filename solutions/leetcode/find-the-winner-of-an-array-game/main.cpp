4fea784d5253ed2f1b3ae8a30ecdaee71f6920e2
TH Huang <tzhsn.huang@gmail.com>
2023-11-04 19:41:53 -0500

    [leetcode] find the winner of an array game

--- solutions/leetcode/find-the-winner-of-an-array-game/main.cpp
+++ solutions/leetcode/find-the-winner-of-an-array-game/main.cpp
@@ -26,3 +26,21 @@ class Solution {
         return winner;
     }
 };
+
+class Solution {
+   public:
+    int getWinner(vector<int>& arr, int k) {
+        int winner = arr[0];
+        int cnt = 0;
+        for (int i = 1; i < arr.size(); ++i) {
+            if (winner > arr[i]) {
+                ++cnt;
+            } else {
+                winner = arr[i];
+                cnt = 1;
+            }
+            if (cnt == k) break;
+        }
+        return winner;
+    }
+};
