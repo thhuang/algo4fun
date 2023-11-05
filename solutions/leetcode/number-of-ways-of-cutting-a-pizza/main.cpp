db34af772b5d98bdbc86bc5fc8e287f4fac3ff7f
TH Huang <tzhsn.huang@gmail.com>
2023-11-05 15:36:35 -0600

    [leetcode] number of ways of cutting a pizza

--- solutions/leetcode/number-of-ways-of-cutting-a-pizza/main.cpp
+++ solutions/leetcode/number-of-ways-of-cutting-a-pizza/main.cpp
@@ -43,3 +43,56 @@ class Solution {
         return dfs(0, 0, k);
     }
 };
+
+class Solution {
+    const int mod = 1e9 + 7;
+
+   public:
+    int ways(vector<string>& pizza, int k) {
+        int m = pizza.size();
+        int n = pizza.front().size();
+
+        vector<vector<int>> suffixSum(m + 1, vector<int>(n + 1, 0));
+        for (int i = m - 1; i >= 0; --i) {
+            int sum = 0;
+            for (int j = n - 1; j >= 0; --j) {
+                if (pizza[i][j] == 'A') ++sum;
+                suffixSum[i][j] = sum + suffixSum[i + 1][j];
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cout << suffixSum[i][j] << ' ';
+            }
+            cout << '\n';
+        }
+
+        vector<vector<vector<int>>> memo(
+            m, vector<vector<int>>(n, vector<int>(k + 1, -1)));
+
+        function<int(int, int, int)> dp = [&](int r, int c, int want) -> int {
+            if (want == 0 || suffixSum[r][c] == 0 || want > suffixSum[r][c])
+                return 0;
+            if (want == 1) return 1;
+            if (memo[r][c][want] != -1) return memo[r][c][want];
+
+            long long result = 0;
+
+            for (int i = r + 1; i < m; ++i) {
+                if (suffixSum[i][c] == 0 || suffixSum[r][c] == suffixSum[i][c])
+                    continue;
+                (result += dp(i, c, want - 1)) %= mod;
+            }
+            for (int j = c + 1; j < n; ++j) {
+                if (suffixSum[r][j] == 0 || suffixSum[r][c] == suffixSum[r][j])
+                    continue;
+                (result += dp(r, j, want - 1)) %= mod;
+            }
+
+            return memo[r][c][want] = result;
+        };
+
+        return dp(0, 0, k);
+    }
+};
