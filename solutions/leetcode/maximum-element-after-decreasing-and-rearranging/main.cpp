a3d2bde3526a5cdce51fd662e2bfe30491180f33
TH Huang <tzhsn.huang@gmail.com>
2023-11-15 10:00:48 -0600

    [leetcode] maximum element after decreasing and rearranging

--- solutions/leetcode/maximum-element-after-decreasing-and-rearranging/main.cpp
+++ solutions/leetcode/maximum-element-after-decreasing-and-rearranging/main.cpp
@@ -11,3 +11,21 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> freq(n + 1, 0);
+        for (int v : arr) {
+            ++freq[min(v, n)];
+        }
+
+        int result = 1;
+        for (int i = 2; i <= n; ++i) {
+            result = min(i, result + freq[i]);
+        }
+
+        return result;
+    }
+};
