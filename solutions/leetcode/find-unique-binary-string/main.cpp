197c2a62c939c2cad8d11341dc50f81071c247b5
TH Huang <tzhsn.huang@gmail.com>
2023-11-16 07:43:22 -0600

    [leetcode] find unique binary string

--- solutions/leetcode/find-unique-binary-string/main.cpp
+++ solutions/leetcode/find-unique-binary-string/main.cpp
@@ -15,3 +15,15 @@ class Solution {
         return string(m, '1');
     }
 };
+
+class Solution {
+   public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string result;
+        for (int i = 0; i < n; ++i) {
+            result += (nums[i][i] == '0' ? '1' : '0');
+        }
+        return result;
+    }
+};
