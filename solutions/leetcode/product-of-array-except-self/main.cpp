796227674e431858343df9cee8b7bd8df6240c32
TH Huang <tzhsn.huang@gmail.com>
2023-11-18 13:51:54 -0600

    [leetcode] product of array except self

--- solutions/leetcode/product-of-array-except-self/main.cpp
+++ solutions/leetcode/product-of-array-except-self/main.cpp
@@ -14,3 +14,20 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> suffix(n + 1, 1);
+        for (int i = n - 1; i >= 0; --i) {
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+        vector<int> result;
+        for (int i = 0, v = 1; i < n; ++i) {
+            result.push_back(v * suffix[i + 1]);
+            v *= nums[i];
+        }
+        return result;
+    }
+};
