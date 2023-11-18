e865a31b6ce598fbeecb372da9e0e529cda9a1e0
TH Huang <tzhsn.huang@gmail.com>
2023-11-18 12:52:59 -0600

    [leetcode] validate stack sequences

--- solutions/leetcode/validate-stack-sequences/main.cpp
+++ solutions/leetcode/validate-stack-sequences/main.cpp
@@ -13,3 +13,23 @@ class Solution {
         return true;
     }
 };
+
+class Solution {
+   public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int m = pushed.size(), n = popped.size();
+        int i = 0, j = 0;
+
+        stack<int> stk;
+        while (i < m || j < n) {
+            if (stk.empty() || stk.top() != popped[j]) {
+                if (i == m) return false;
+                stk.push(pushed[i++]);
+            } else {
+                ++j;
+                stk.pop();
+            }
+        }
+        return true;
+    }
+};
