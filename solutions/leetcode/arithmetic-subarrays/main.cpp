51e298710b60c3ad35cff7d0101e7779d2760094
TH Huang <tzhsn.huang@gmail.com>
2023-11-23 10:33:37 -0600

    [leetcode] arithmetic subarrays

---
+++ solutions/leetcode/arithmetic-subarrays/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+   public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l,
+                                          vector<int>& r) {
+        int n = nums.size();
+        int m = l.size();
+
+        vector<int> idx(n);
+        iota(idx.begin(), idx.end(), 0);
+        sort(idx.begin(), idx.end(),
+             [&](int i, int j) -> bool { return nums[i] < nums[j]; });
+
+        vector<bool> result(m, true);
+
+        for (int k = 0; k < m; ++k) {
+            int left = l[k], right = r[k];
+            vector<int> seq;
+            for (int i : idx) {
+                if (i < left || right < i) continue;
+                seq.push_back(nums[i]);
+            }
+            int d = seq[1] - seq[0];
+            for (int j = 2; j < seq.size(); ++j) {
+                if (seq[j] - seq[j - 1] != d) {
+                    result[k] = false;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
