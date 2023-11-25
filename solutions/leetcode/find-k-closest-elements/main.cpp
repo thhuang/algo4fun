34cd9cf9497d19a6f78ffa0975bd46b608d7cf91
TH Huang <tzhsn.huang@gmail.com>
2023-11-25 10:30:43 -0600

    [leetcode] maximum number of coins you can get

--- solutions/leetcode/find-k-closest-elements/main.cpp
+++ solutions/leetcode/find-k-closest-elements/main.cpp
@@ -22,3 +22,32 @@ class Solution {
         return {arr.begin() + l + 1, arr.begin() + r};
     }
 };
+
+class Solution {
+   public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+        if (n <= k) return arr;
+
+        auto p = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        if (p == n) --p;
+        if (p > 0 && x - arr[p - 1] <= arr[p] - x) --p;
+
+        int l = p, r = p;
+        while (r - l + 1 < k) {
+            int left = -1e5;
+            if (l - 1 >= 0) left = arr[l - 1];
+
+            int right = 1e5;
+            if (r + 1 < n) right = arr[r + 1];
+
+            if (x - left <= right - x) {
+                --l;
+            } else {
+                ++r;
+            }
+        }
+
+        return {arr.begin() + l, arr.begin() + r + 1};
+    }
+};
