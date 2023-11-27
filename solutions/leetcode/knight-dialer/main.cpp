96ade51c9bcab73cf2fa6c31b8a0d7a4e5361475
TH Huang <tzhsn.huang@gmail.com>
2023-11-27 08:47:54 -0600

    [leetcode] knight dialer

--- solutions/leetcode/knight-dialer/main.cpp
+++ solutions/leetcode/knight-dialer/main.cpp
@@ -35,3 +35,43 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+    typedef long long ll;
+    const int mod = 1e9 + 7;
+
+    vector<vector<int>> prevPos = {
+        {4, 6},     // 0
+        {6, 8},     // 1
+        {7, 9},     // 2
+        {4, 8},     // 3
+        {0, 3, 9},  // 4
+        {},         // 5
+        {0, 1, 7},  // 6
+        {2, 6},     // 7
+        {1, 3},     // 8
+        {2, 4},     // 9
+    };
+
+   public:
+    int knightDialer(int n) {
+        array<int, 10> dp;
+        fill(dp.begin(), dp.end(), 1);
+        for (int i = 1; i < n; ++i) {
+            array<int, 10> dpNew = {};
+            for (int to = 0; to < 10; ++to) {
+                for (int from : prevPos[to]) {
+                    (dpNew[to] += dp[from]) %= mod;
+                }
+            }
+            dp = dpNew;
+        }
+
+        int result = 0;
+        for (int v : dp) {
+            (result += v) %= mod;
+        }
+
+        return result;
+    }
+};
