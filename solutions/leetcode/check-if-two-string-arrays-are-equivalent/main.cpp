3a595c204293b0b0c618d244afd0a81009ee779b
TH Huang <tzhsn.huang@gmail.com>
2023-11-30 19:22:11 -0600

    [leetcode] check if two string arrays are equivalent

--- solutions/leetcode/check-if-two-string-arrays-are-equivalent/main.cpp
+++ solutions/leetcode/check-if-two-string-arrays-are-equivalent/main.cpp
@@ -1,6 +1,16 @@
 class Solution {
    public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1, s2;
+        for (const string& w : word1) s1 += w;
+        for (const string& w : word2) s2 += w;
+        return s1 == s2;
+    }
+};
+
+class Solution {
+   public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         int vi = 0, vj = 0, i = 0, j = 0;
         while (true) {
             if (i == word1[vi].size()) ++vi, i = 0;
@@ -9,15 +19,5 @@ class Solution {
             if (word1[vi][i++] != word2[vj][j++]) return false;
         }
         return vi == word1.size() && vj == word2.size();
-    }
-};
-
-class Solution {
-   public:
-    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1, s2;
-        for (const string& w : word1) s1 += w;
-        for (const string& w : word2) s2 += w;
-        return s1 == s2;
     }
 };
