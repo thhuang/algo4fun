0cae7c4423fc4e5274ec2f1dc6823baa37661dac
TH Huang <tzhsn.huang@gmail.com>
2023-11-30 19:21:58 -0600

    [leetcode] check if two string arrays are equivalent

--- solutions/leetcode/check-if-two-string-arrays-are-equivalent/main.cpp
+++ solutions/leetcode/check-if-two-string-arrays-are-equivalent/main.cpp
@@ -11,3 +11,13 @@ class Solution {
         return vi == word1.size() && vj == word2.size();
     }
 };
+
+class Solution {
+   public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string s1, s2;
+        for (const string& w : word1) s1 += w;
+        for (const string& w : word2) s2 += w;
+        return s1 == s2;
+    }
+};
