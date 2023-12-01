ca79d5cef35d5872ad3b6aa584d97838f13cd0c4
TH Huang <tzhsn.huang@gmail.com>
2023-11-30 19:19:31 -0600

    [leetcode] check if two string arrays are equivalent

---
+++ solutions/leetcode/check-if-two-string-arrays-are-equivalent/main.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        i1, i2 = 0, 0
+        j1, j2 = 0, 0
+        while True:
+            print(i1, j1, i2, j2)
+            if i1 == len(word1) and i2 == len(word2):
+                return True
+            if i1 == len(word1) or i2 == len(word2):
+                return False
+            if j1 < len(word1[i1]) and j2 < len(word2[i2]):
+                if word1[i1][j1] != word2[i2][j2]:
+                    return False
+                else:
+                    j1 += 1
+                    j2 += 1
+            else:
+                if j1 == len(word1[i1]):
+                    j1 = 0
+                    i1 += 1
+                if j2 == len(word2[i2]):
+                    j2 = 0
+                    i2 += 1
