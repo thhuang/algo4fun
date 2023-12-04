0e26618f31fa8c3b43ed0bf925dc4c0bc1dfca78
TH Huang <tzhsn.huang@gmail.com>
2023-12-03 22:18:36 -0600

    [leetcode] decode ways

---
+++ solutions/leetcode/decode-ways/main.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+        if len(s) == 1:
+            return 1
+
+        valid = {str(i + 1) for i in range(26)}
+
+        dp = [1, 0 if s[1] == "0" else 1]
+        if s[0:2] in valid:
+            dp[1] += 1
+
+        for i in range(2, len(s)):
+            dp = [
+                dp[1], (0 if s[i] == "0" else dp[1]) +
+                (dp[0] if s[i - 1:i + 1] in valid else 0)
+            ]
+
+        return dp[1]
