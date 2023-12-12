be7f819ccf294a7db143f51d9ee107343c21452a
TH Huang <tzhsn.huang@gmail.com>
2023-12-12 00:58:39 -0600

    [leetcode] element appearing more than 25 in sorted array

--- solutions/leetcode/element-appearing-more-than-25-in-sorted-array/main.cpp
+++ solutions/leetcode/element-appearing-more-than-25-in-sorted-array/main.cpp
@@ -32,3 +32,18 @@ class Solution {
         return p;
     }
 };
+
+class Solution {
+   public:
+    int findSpecialInteger(vector<int>& arr) {
+        int n = arr.size();
+        auto check = [&](int v) -> bool {
+            auto l = lower_bound(arr.begin(), arr.end(), v);
+            auto r = upper_bound(arr.begin(), arr.end(), v);
+            return (r - l) > arr.size() / 4;
+        };
+        if (int v = arr[n / 4]; check(v)) return v;
+        if (int v = arr[n / 2]; check(v)) return v;
+        return arr[n * 3 / 4];
+    }
+};
