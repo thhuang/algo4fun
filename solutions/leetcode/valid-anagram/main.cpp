baf7f45e14791fc5ad7f9514827cb11b9a9e3641
TH Huang <tzhsn.huang@gmail.com>
2023-12-16 00:48:06 -0500

    [leetcode] valid anagram

--- solutions/leetcode/valid-anagram/main.cpp
+++ solutions/leetcode/valid-anagram/main.cpp
@@ -2,24 +2,10 @@ class Solution {
    public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
-        array<int, 26> cnt;
-        cnt.fill(0);
-        for (char c : s) ++cnt[c - 'a'];
+        array<int, 26> charFreq = {};
+        for (char c : s) ++charFreq[c - 'a'];
         for (char c : t)
-            if (--cnt[c - 'a'] < 0) return false;
-        return true;
-    }
-};
-
-class Solution {
-   public:
-    bool isAnagram(string s, string t) {
-        array<int, 26> cnt;
-        cnt.fill(0);
-        for (char c : s) ++cnt[c - 'a'];
-        for (char c : t) --cnt[c - 'a'];
-        for (int v : cnt)
-            if (v != 0) return false;
+            if (--charFreq[c - 'a'] < 0) return false;
         return true;
     }
 };
