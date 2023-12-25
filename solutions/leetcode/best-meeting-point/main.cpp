1335d76dc2128a12e23462bd793a1177c3abef64
TH Huang <tzhsn.huang@gmail.com>
2023-12-24 20:21:52 -0600

    [leetcode] best meeting point

--- solutions/leetcode/best-meeting-point/main.cpp
+++ solutions/leetcode/best-meeting-point/main.cpp
@@ -4,6 +4,36 @@ class Solution {
         int m = grid.size();
         int n = grid.front().size();
 
+        vector<int> rows, cols;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    rows.push_back(i);
+                    cols.push_back(j);
+                }
+            }
+        }
+
+        sort(rows.begin(), rows.end());
+        sort(cols.begin(), cols.end());
+
+        int mr = rows[rows.size() / 2];
+        int mc = cols[cols.size() / 2];
+
+        int result = 0;
+        for (int r : rows) result += abs(r - mr);
+        for (int c : cols) result += abs(c - mc);
+
+        return result;
+    }
+};
+
+class Solution {
+   public:
+    int minTotalDistance(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid.front().size();
+
         vector<int> rowAccu(m, 0);
         for (int i = 0; i < m; ++i) {
             if (i != 0) {
@@ -55,36 +85,6 @@ class Solution {
                 result = min(result, dist[i][j]);
             }
         }
-
-        return result;
-    }
-};
-
-class Solution {
-   public:
-    int minTotalDistance(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid.front().size();
-
-        vector<int> rows, cols;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    rows.push_back(i);
-                    cols.push_back(j);
-                }
-            }
-        }
-
-        sort(rows.begin(), rows.end());
-        sort(cols.begin(), cols.end());
-
-        int mr = rows[rows.size() / 2];
-        int mc = cols[cols.size() / 2];
-
-        int result = 0;
-        for (int r : rows) result += abs(r - mr);
-        for (int c : cols) result += abs(c - mc);
 
         return result;
     }
