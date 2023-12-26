9f6f10bc5460eb4b20de87b03ec2e901a56c71f0
TH Huang <tzhsn.huang@gmail.com>
2023-12-25 23:56:20 -0600

    [leetcode] number of dice rolls with target sum

--- solutions/leetcode/number-of-dice-rolls-with-target-sum/main.cpp
+++ solutions/leetcode/number-of-dice-rolls-with-target-sum/main.cpp
@@ -5,19 +5,15 @@ class Solution {
     int numRollsToTarget(int n, int k, int target) {
         vector<int> dp(target + 1, 0);
         dp[0] = 1;
-
         for (int i = 0; i < n; ++i) {
-            vector<int> new_dp(target + 1, 0);
-            for (int j = target; j >= 1; --j) {
-                for (int v = 1; v <= k; ++v) {
-                    if (j - v < 0) continue;
-                    if (dp[j - v] == 0) continue;
-                    new_dp[j] = ((long long)new_dp[j] + dp[j - v]) % mod;
+            vector<int> dpNew(target + 1, 0);
+            for (int j = 0; j < dp.size(); ++j) {
+                for (int r = j + 1; r <= j + k && r < dp.size(); ++r) {
+                    (dpNew[r] += dp[j]) %= mod;
                 }
             }
-            dp = move(new_dp);
+            dp = dpNew;
         }
-
-        return dp.back();
+        return dp[target];
     }
 };
