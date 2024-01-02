fc3f46cfa24547586732073489ea6c083e538e3a
TH Huang <tzhsn.huang@gmail.com>
2024-01-02 09:45:27 -0600

    [leetcode] valid palindrome iii

--- solutions/leetcode/valid-palindrome-iii/main.py
+++ solutions/leetcode/valid-palindrome-iii/main.py
@@ -10,3 +10,21 @@ class Solution:
                 else:
                     dp[(l, r)] = 1 + min(dp[(l, r - 1)], dp[(l + 1, r)])
         return dp[(0, len(s) - 1)] <= k
+
+
+class Solution:
+
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        n = len(s)
+        dp1 = [0 for _ in range(n)]
+        dp2 = [0 for _ in range(n)]
+        for d in range(2, n + 1):
+            dp0 = [0 for _ in range(n)]
+            for l in range(0, n - (d - 1)):
+                r = l + d - 1
+                if s[l] == s[r]:
+                    dp0[l] = dp2[l + 1]
+                else:
+                    dp0[l] = 1 + min(dp1[l], dp1[l + 1])
+            dp1, dp2 = dp0, dp1
+        return dp0[0] <= k
