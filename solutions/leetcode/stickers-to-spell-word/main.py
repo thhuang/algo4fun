2fcb93b52ec5c4ce228e2235ae18db0afde6d1ee
TH Huang <tzhsn.huang@gmail.com>
2024-01-04 10:57:07 -0600

    [leetcode] stickers to spell word

---
+++ solutions/leetcode/stickers-to-spell-word/main.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def minStickers(self, stickers: List[str], target: str) -> int:
+        sticker_freqs = [Counter(sticker) for sticker in stickers]
+
+        @cache
+        def dfs(t: str) -> int:
+            if not t:
+                return 0
+
+            result = inf
+            freq = Counter(t)
+            for sticker_freq in sticker_freqs:
+                if t[0] not in sticker_freq:
+                    continue
+                new_t = "".join(
+                    [c * v for c, v in (freq - sticker_freq).items()])
+                result = min(result, 1 + dfs(new_t))
+            return result
+
+        result = dfs(target)
+        return result if result < inf else -1
