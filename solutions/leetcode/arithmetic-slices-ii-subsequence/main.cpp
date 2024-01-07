3554e5a27b53fb7b3b6fee6a1a9d5db668ce47f6
TH Huang <tzhsn.huang@gmail.com>
2024-01-06 23:14:22 -0600

    [leetcode] arithmetic slices ii subsequence

--- solutions/leetcode/arithmetic-slices-ii-subsequence/main.cpp
+++ solutions/leetcode/arithmetic-slices-ii-subsequence/main.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    using ll = long long;
+    typedef long long ll;
 
    public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-
         int result = 0;
         vector<unordered_map<ll, int>> dp(n);
         for (int r = 1; r < n; ++r) {
-            for (int m = 0; m < r; ++m) {
-                ll d = (ll)nums[r] - nums[m];
-                int s = dp[m][d];
-                dp[r][d] += s + 1;
-                result += s;
+            for (int l = 0; l < r; ++l) {
+                auto d = (ll)nums[r] - nums[l];
+                auto p = dp[l][d];
+                result += p;
+                dp[r][d] += p + 1;
             }
         }
-
         return result;
     }
 };
