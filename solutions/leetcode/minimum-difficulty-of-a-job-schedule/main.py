4b8b8b3122220084ad695ea4d37377fe7e83ff77
TH Huang <tzhsn.huang@gmail.com>
2024-01-06 23:37:03 -0600

    [leetcode] minimum difficulty of a job schedule

--- solutions/leetcode/minimum-difficulty-of-a-job-schedule/main.py
+++ solutions/leetcode/minimum-difficulty-of-a-job-schedule/main.py
@@ -23,3 +23,26 @@ class Solution:
         result = dp(0, d)
 
         return result if result < inf else -1
+
+
+class Solution:
+
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        n = len(jobDifficulty)
+
+        remain_max = jobDifficulty[:]
+        for i in reversed(range(0, n - 1)):
+            remain_max[i] = max(jobDifficulty[i], remain_max[i + 1])
+
+        dp = [[inf] * (d + 1) for _ in range(n + 1)]
+        for i in reversed(range(0, n)):
+            dp[i][1] = remain_max[i]
+            for days_left in range(2, d + 1):
+                result = inf
+                curr_max = 0
+                for j in range(i, n - days_left + 1):
+                    curr_max = max(curr_max, jobDifficulty[j])
+                    result = min(result, curr_max + dp[j + 1][days_left - 1])
+                dp[i][days_left] = result
+
+        return dp[0][d] if dp[0][d] < inf else -1
