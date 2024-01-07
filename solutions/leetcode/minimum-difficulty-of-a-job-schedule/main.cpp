3b14754352147d75e55e964a675f5c6b2b7c24bf
TH Huang <tzhsn.huang@gmail.com>
2024-01-06 23:45:14 -0600

    [leetcode] minimum difficulty of a job schedule

--- solutions/leetcode/minimum-difficulty-of-a-job-schedule/main.cpp
+++ solutions/leetcode/minimum-difficulty-of-a-job-schedule/main.cpp
@@ -26,3 +26,31 @@ class Solution {
         return dp[n][d];
     }
 };
+
+class Solution {
+    const int inf = 1e9;
+
+   public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        int n = jobDifficulty.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(d + 1, inf));
+        dp[n - 1][1] = jobDifficulty[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            dp[i][1] = max(jobDifficulty[i], dp[i + 1][1]);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int daysLeft = 2; daysLeft <= d; ++daysLeft) {
+                int result = inf;
+                for (int j = i, mx = 0; j < n - daysLeft + 1; ++j) {
+                    mx = max(mx, jobDifficulty[j]);
+                    result = min(result, mx + dp[j + 1][daysLeft - 1]);
+                }
+                dp[i][daysLeft] = result;
+            }
+        }
+
+        return dp[0][d] < inf ? dp[0][d] : -1;
+    }
+};
