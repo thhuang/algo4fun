e6644f56e0f9e5cfab9c0faa11c99ee89e1dc975
TH Huang <tzhsn.huang@gmail.com>
2024-01-09 11:36:16 -0600

    [leetcode] leftmost column with at least a one

--- solutions/leetcode/leftmost-column-with-at-least-a-one/main.cpp
+++ solutions/leetcode/leftmost-column-with-at-least-a-one/main.cpp
@@ -22,3 +22,29 @@ class Solution {
         return j;
     }
 };
+
+class Solution {
+   public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        auto dim = binaryMatrix.dimensions();
+        int rows = dim[0];
+        int cols = dim[1];
+
+        int result = cols;
+
+        for (int i = 0; i < rows; ++i) {
+            int l = -1, r = cols;
+            while (r - l > 1) {
+                int m = l + (r - l) / 2;
+                if (binaryMatrix.get(i, m) == 1) {
+                    r = m;
+                } else {
+                    l = m;
+                }
+            }
+            result = min(result, r);
+        }
+
+        return result < cols ? result : -1;
+    }
+};
