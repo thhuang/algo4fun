1505ec1fbf19cb5765a374e452d25f9aa7b1678f
TH Huang <tzhsn.huang@gmail.com>
2024-01-09 16:27:25 -0600

    [leetcode] string compression

--- solutions/leetcode/string-compression/main.cpp
+++ solutions/leetcode/string-compression/main.cpp
@@ -2,38 +2,17 @@ class Solution {
    public:
     int compress(vector<char>& chars) {
         int p = 0;
-        for (int l = 0, n = chars.size(); l < n;) {
-            int r = l + 1;
-            while (r < n && chars[r] == chars[l]) ++r;
-
-            if (r - l == 1) {
-                chars[p++] = chars[l++];
-                continue;
-            }
+        for (int l = 0; l < chars.size();) {
+            int r = l;
+            while (r < chars.size() && chars[r] == chars[l]) ++r;
 
             chars[p++] = chars[l];
-            for (auto c : to_string(r - l)) chars[p++] = c;
+
+            int v = r - l;
+            if (v > 1)
+                for (char c : to_string(v)) chars[p++] = c;
+
             l = r;
-        }
-        return p;
-    }
-};
-
-class Solution {
-   public:
-    int compress(vector<char>& chars) {
-        int n = chars.size();
-        int p = 0;
-        for (int l = 0; l < n;) {
-            int r = l + 1;
-            while (r < n && chars[l] == chars[r]) ++r;
-
-            int d = r - l;
-            chars[p++] = chars[l];
-            l = r;
-
-            if (d == 1) continue;
-            for (char c : to_string(d)) chars[p++] = c;
         }
         return p;
     }
