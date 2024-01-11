f6985f09c6b6a6a901d92248b10e4b24f9f8040c
TH Huang <tzhsn.huang@gmail.com>
2024-01-11 17:45:30 -0600

    [leetcode] n-th tribonacci number

---
+++ solutions/leetcode/n-th-tribonacci-number/main.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def tribonacci(self, n: int) -> int:
+        dp = [0, 1, 1]
+
+        if n < 3: return dp[n]
+
+        for _ in range(3, n + 1):
+            dp = [dp[1], dp[2], sum(dp)]
+
+        return dp[-1]
