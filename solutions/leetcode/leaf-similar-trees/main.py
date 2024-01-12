3ff42f978bf31d863a3e1b8adfbe4094078c20d9
TH Huang <tzhsn.huang@gmail.com>
2024-01-12 16:33:01 -0600

    [leetcode] leaf similar trees

--- solutions/leetcode/leaf-similar-trees/main.py
+++ solutions/leetcode/leaf-similar-trees/main.py
@@ -23,3 +23,21 @@ class Solution:
         collect(root2, leaves2)
 
         return leaves1 == leaves2
+
+
+class Solution:
+
+    def leafSimilar(self, root1: Optional[TreeNode],
+                    root2: Optional[TreeNode]) -> bool:
+
+        def collect(u: Optional[TreeNode], leaves: List[int]) -> None:
+            if u.left is None and u.right is None:
+                leaves.append(u.val)
+                return leaves
+            if u.left is not None:
+                collect(u.left, leaves)
+            if u.right is not None:
+                collect(u.right, leaves)
+            return leaves
+
+        return collect(root1, []) == collect(root2, [])
