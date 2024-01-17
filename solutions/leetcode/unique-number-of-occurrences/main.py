5d73014ed801fccef1c6c7e031a81530a4e6feb2
TH Huang <tzhsn.huang@gmail.com>
2024-01-16 23:56:52 -0600

    [leetcode] unique number of occurrences

--- solutions/leetcode/unique-number-of-occurrences/main.py
+++ solutions/leetcode/unique-number-of-occurrences/main.py
@@ -7,3 +7,14 @@ class Solution:
             if v > 1:
                 return False
         return True
+
+
+class Solution:
+
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        freq = collections.Counter(arr)
+        occurrence_freq = collections.Counter(freq.values())
+        occurrences = set(occurrence_freq.values())
+        if 1 not in occurrences or len(occurrences) > 1:
+            return False
+        return True
