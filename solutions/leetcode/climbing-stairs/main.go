848058baff5397b5f2c0e33114bb4f6e4d0e4173
TH Huang <tzhsn.huang@gmail.com>
2024-01-18 01:12:47 -0600

    [leetcode] climbing stairs

--- solutions/leetcode/climbing-stairs/main.go
+++ solutions/leetcode/climbing-stairs/main.go
@@ -1,9 +1,6 @@
 func climbStairs(n int) int {
-	if n <= 2 {
-		return n
-	}
-	dp := []int{1, 2}
-	for i := 3; i <= n; i++ {
+	dp := []int{1, 1}
+	for i := 2; i <= n; i++ {
 		dp[0], dp[1] = dp[1], dp[0]+dp[1]
 	}
 	return dp[1]
