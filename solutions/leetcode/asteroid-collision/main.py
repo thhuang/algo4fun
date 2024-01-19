398cc1d62d4e71b1ac1845ad7e30904f7aa173f9
TH Huang <tzhsn.huang@gmail.com>
2024-01-19 14:19:50 -0600

    [leetcode] asteroid collision

--- solutions/leetcode/asteroid-collision/main.py
+++ solutions/leetcode/asteroid-collision/main.py
@@ -4,16 +4,10 @@ class Solution:
         result = []
 
         for v in asteroids:
-            if v > 0:
-                result.append(v)
-                continue
-
-            while len(result) > 0:
-                if result[-1] < 0 or result[-1] + v >= 0:
-                    break
+            while len(result) > 0 and 0 < result[-1] < -v:
                 result.pop()
 
-            if len(result) == 0 or result[-1] < 0:
+            if v > 0 or len(result) == 0 or result[-1] < 0:
                 result.append(v)
             elif result[-1] + v == 0:
                 result.pop()
