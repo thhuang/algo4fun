721feee3d3279ecd1c2b0efcab145ed8bb0cea5a
TH Huang <tzhsn.huang@gmail.com>
2024-01-19 14:19:46 -0600

    [leetcode] asteroid collision

--- solutions/leetcode/asteroid-collision/main.cpp
+++ solutions/leetcode/asteroid-collision/main.cpp
@@ -16,3 +16,25 @@ class Solution {
         return result;
     }
 };
+
+class Solution {
+   public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> result;
+        for (int v : asteroids) {
+            if (v > 0) {
+                result.push_back(v);
+                continue;
+            }
+            while (!result.empty() && 0 < result.back() && result.back() < -v) {
+                result.pop_back();
+            }
+            if (result.empty() || result.back() < 0) {
+                result.push_back(v);
+            } else if (result.back() == -v) {
+                result.pop_back();
+            }
+        }
+        return result;
+    }
+};
