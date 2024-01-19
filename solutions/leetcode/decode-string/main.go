69ffec2425638be769afabaabc1387594402afe2
TH Huang <tzhsn.huang@gmail.com>
2024-01-19 16:32:37 -0600

    [leetcode] decode string

--- solutions/leetcode/decode-string/main.go
+++ solutions/leetcode/decode-string/main.go
@@ -32,3 +32,38 @@ func decodeRunes(rs []rune, i int) (int, []rune) {
 
 	return i + 1, result
 }
+
+
+import "unicode"
+
+func decodeString(s string) string {
+    var decode func(int) (int, []rune)
+    decode = func(i int) (int, []rune) {
+        result := []rune{}
+
+        for i < len(s) && s[i] != ']' {
+            if unicode.IsLetter(rune(s[i])) {
+                result = append(result, rune(s[i]))
+                i++
+                continue
+            }
+
+            k := 0
+            for unicode.IsDigit(rune(s[i])) {
+                k = k*10 + int(s[i]-'0')
+                i++
+            }
+            var rs []rune
+            i, rs = decode(i+1)
+            for k > 0 {
+                result = append(result, rs...)
+                k--
+            }
+        }
+
+        return i+1, result
+    }
+
+    _, rs := decode(0)
+    return string(rs)
+}
