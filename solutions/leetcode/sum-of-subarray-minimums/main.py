4f94305cd35965bac1c49548065978b759b6eb87
TH Huang <tzhsn.huang@gmail.com>
2024-01-19 20:05:15 -0600

    [leetcode] sum of subarray minimums

---
+++ solutions/leetcode/sum-of-subarray-minimums/main.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        mod = int(1e9 + 7)
+        n = len(arr)
+
+        asc = []
+
+        r = [None] * n
+        for i in range(n):
+            while len(asc) > 0 and arr[asc[-1]] > arr[i]:
+                r[asc[-1]] = i - 1
+                asc.pop()
+            asc.append(i)
+        while len(asc) > 0:
+            r[asc[-1]] = n - 1
+            asc.pop()
+
+        l = [None] * n
+        for i in range(n)[::-1]:
+            while len(asc) > 0 and arr[asc[-1]] >= arr[i]:
+                l[asc[-1]] = i + 1
+                asc.pop()
+            asc.append(i)
+        while len(asc) > 0:
+            l[asc[-1]] = 0
+            asc.pop()
+
+        result = 0
+        for i, v in enumerate(arr):
+            result = (result + v * (i - l[i] + 1) * (r[i] - i + 1)) % mod
+
+        return result
