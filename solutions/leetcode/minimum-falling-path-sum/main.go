670026d3bce93d572065ce73366e31e8e11a8231
TH Huang <tzhsn.huang@gmail.com>
2024-01-19 19:53:43 -0600

    [leetcode] minimum falling path sum

--- solutions/leetcode/minimum-falling-path-sum/main.go
+++ solutions/leetcode/minimum-falling-path-sum/main.go
@@ -27,3 +27,33 @@ func minFallingPathSum(matrix [][]int) int {
 
 	return result
 }
+
+import "math"
+
+func minFallingPathSum(matrix [][]int) int {
+    m := len(matrix)
+    n := len(matrix[0])
+
+    dp := matrix[0]
+    for i := 1; i < m; i++ {
+        dpNew := make([]int, n)
+        copy(dpNew, dp)
+        for j := 0; j < n; j++ {
+            if j - 1 >= 0 {
+                dpNew[j] = min(dpNew[j], dp[j - 1])
+            }
+            if j + 1 < n {
+                dpNew[j] = min(dpNew[j], dp[j + 1])
+            }
+            dpNew[j] += matrix[i][j]
+        }
+        dp = dpNew
+    }
+
+    result := math.MaxInt32
+    for _, v := range dp {
+        result = min(result, v)
+    }
+
+    return result
+}
