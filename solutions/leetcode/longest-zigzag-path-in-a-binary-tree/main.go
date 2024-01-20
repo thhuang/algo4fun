0113f3eb67805e8ddad121d2d75393841f80f9da
TH Huang <tzhsn.huang@gmail.com>
2024-01-20 14:47:36 -0600

    [leetcode] longest zigzag path in a binary tree

--- solutions/leetcode/longest-zigzag-path-in-a-binary-tree/main.go
+++ solutions/leetcode/longest-zigzag-path-in-a-binary-tree/main.go
@@ -31,3 +31,28 @@ func longestZigZag(root *TreeNode) int {
 
 	return result
 }
+
+func longestZigZag(root *TreeNode) int {
+	result := 0
+
+	var dfs func(*TreeNode, bool) int
+	dfs = func(u *TreeNode, isLeft bool) int {
+		if u == nil {
+			return 0
+		}
+
+		l := dfs(u.Left, true)
+		r := dfs(u.Right, false)
+
+		result = max(result, max(l, r))
+
+		if isLeft {
+			return r + 1
+		}
+		return l + 1
+	}
+
+	dfs(root, true)
+
+	return result
+}
