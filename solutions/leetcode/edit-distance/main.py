546ae28e9c70a949ed786e008a244e6a84518ffe
TH Huang <tzhsn.huang@gmail.com>
2024-01-26 17:13:53 -0600

    [leetcode] edit distance

--- solutions/leetcode/edit-distance/main.py
+++ solutions/leetcode/edit-distance/main.py
@@ -18,3 +18,25 @@ class Solution:
                                        dp[i + 1][j + 1])
 
         return dp[0][0]
+
+
+class Solution:
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+
+        dp = [0] * (n + 1)
+        for j in reversed(range(n)):
+            dp[j] = n - j
+
+        for i in reversed(range(m)):
+            dp_new = [0] * (n + 1)
+            dp_new[n] = m - i
+            for j in reversed(range(n)):
+                if word1[i] == word2[j]:
+                    dp_new[j] = dp[j + 1]
+                else:
+                    dp_new[j] = 1 + min(dp_new[j + 1], dp[j], dp[j + 1])
+            dp = dp_new
+
+        return dp[0]
