2427688dc8edcf327e3800b301e3d1e8b2f0d5d0
TH Huang <tzhsn.huang@gmail.com>
2024-01-26 13:49:34 -0600

    [leetcode] longest common subsequence

--- solutions/leetcode/longest-common-subsequence/main.py
+++ solutions/leetcode/longest-common-subsequence/main.py
@@ -13,3 +13,19 @@ class Solution:
                     dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
 
         return dp[0][0]
+
+
+class Solution:
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+        dp = [0] * (n + 1)
+        for i in reversed(range(m)):
+            new_dp = [0] * (n + 1)
+            for j in reversed(range(n)):
+                if text1[i] == text2[j]:
+                    new_dp[j] = 1 + dp[j + 1]
+                else:
+                    new_dp[j] = max(new_dp[j + 1], dp[j])
+            dp = new_dp
+        return dp[0]
