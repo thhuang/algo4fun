a13e0e9f041593ac090c6bbaf32112706706b591
TH Huang <tzhsn.huang@gmail.com>
2024-01-25 19:56:12 -0600

    [leetcode] out of boundary paths

--- solutions/leetcode/out-of-boundary-paths/main.py
+++ solutions/leetcode/out-of-boundary-paths/main.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 class Solution:
 
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int,
@@ -24,3 +21,26 @@ class Solution:
             return result % (10**9 + 7)
 
         return dfs(startRow, startColumn, maxMove)
+
+
+class Solution:
+
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int,
+                  startColumn: int) -> int:
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        dp = [[[0 for _ in range(maxMove + 1)] for _ in range(n)]
+              for _ in range(m)]
+
+        for k in range(1, maxMove + 1):
+            for i in range(m):
+                for j in range(n):
+                    for di, dj in directions:
+                        ii, jj = i + di, j + dj
+                        if ii < 0 or m <= ii or jj < 0 or n <= jj:
+                            dp[i][j][k] += 1
+                        else:
+                            dp[i][j][k] += dp[ii][jj][k - 1]
+                    dp[i][j][k] %= 10**9 + 7
+
+        return dp[startRow][startColumn][maxMove]
