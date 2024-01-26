1ef0bf6863e4f2f31d2a326106ec20e684d2aa66
TH Huang <tzhsn.huang@gmail.com>
2024-01-26 13:02:55 -0600

    [leetcode] min cost climbing stairs

--- solutions/leetcode/min-cost-climbing-stairs/main.py
+++ solutions/leetcode/min-cost-climbing-stairs/main.py
@@ -2,6 +2,6 @@ class Solution:
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp = [0, 0]
-        for i in range(len(cost)):
-            dp[0], dp[1] = dp[1], min(dp) + cost[i]
+        for c in cost:
+            dp[0], dp[1] = dp[1], min(dp) + c
         return min(dp)
