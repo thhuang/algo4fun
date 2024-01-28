fa445bf181119d09308bd4446d33eed58bbde9a4
TH Huang <tzhsn.huang@gmail.com>
2024-01-28 16:13:00 -0600

    [leetcode] counting bits

--- solutions/leetcode/counting-bits/main.py
+++ solutions/leetcode/counting-bits/main.py
@@ -13,3 +13,29 @@ class Solution:
             b = b << 1
 
         return []
+
+
+class Solution:
+
+    def countBits(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+
+        b = 1
+        for r in range(1, n + 1):
+            if r >= b * 2:
+                b *= 2
+            l = r - b
+            result[r] = result[l] + 1
+
+        return result
+
+
+class Solution:
+
+    def countBits(self, n: int) -> List[int]:
+        result = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            result[i] = result[i & (i - 1)] + 1
+
+        return result
