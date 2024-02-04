26f222dc7ff7c7380c9182f3207d4c2f84eed812
TH Huang <tzhsn.huang@gmail.com>
2024-02-04 16:13:11 -0600

    [leetcode] sequential digits

--- solutions/leetcode/sequential-digits/main.py
+++ solutions/leetcode/sequential-digits/main.py
@@ -16,3 +16,19 @@ class Solution:
                 r += 1
 
         return result
+
+
+class Solution:
+
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        seq = "123456789"
+
+        result = []
+
+        for d in range(len(str(low)), len(str(high)) + 1):
+            for l in range(len(seq) - d + 1):
+                v = int(seq[l:l + d])
+                if low <= v <= high:
+                    result.append(v)
+
+        return result
