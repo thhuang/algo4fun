73e506bc58331609e1159b244de6a1fa4e502693
TH Huang <tzhsn.huang@gmail.com>
2024-02-04 17:30:01 -0600

    [leetcode] number of submatrices that sum to target

--- solutions/leetcode/number-of-submatrices-that-sum-to-target/main.py
+++ solutions/leetcode/number-of-submatrices-that-sum-to-target/main.py
@@ -23,3 +23,30 @@ class Solution:
                     prefix_sum[v] += 1
 
         return result
+
+
+class Solution:
+
+    def numSubmatrixSumTarget(self, matrix: List[List[int]],
+                              target: int) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        suffix = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in reversed(range(m)):
+            s = 0
+            for j in reversed(range(n)):
+                s += matrix[i][j]
+                suffix[i][j] = suffix[i + 1][j] + s
+
+        result = 0
+        for l in range(n):
+            for r in range(l, n):
+                prefix_sum = defaultdict(int)
+                prefix_sum[0] = 1
+                for i in reversed(range(m)):
+                    v = suffix[i][l] - suffix[i][r + 1]
+                    result += prefix_sum[v - target]
+                    prefix_sum[v] += 1
+
+        return result
