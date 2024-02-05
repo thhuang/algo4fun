758a0963d8584461fad991d07a9c4a9564856571
TH Huang <tzhsn.huang@gmail.com>
2024-02-04 21:45:27 -0600

    [leetcode] k inverse pairs array

--- solutions/leetcode/k-inverse-pairs-array/main.py
+++ solutions/leetcode/k-inverse-pairs-array/main.py
@@ -27,3 +27,20 @@ class Solution:
                     v -= dp[i - 1][j - i]
                 dp[i][j] = (dp[i][j] + v) % mod
         return dp[n][k]
+
+
+class Solution:
+
+    def kInversePairs(self, n: int, k: int) -> int:
+        mod = 10**9 + 7
+        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
+        for i in range(1, n + 1):
+            v = 0
+            for r in range(k + 1):
+                l = r - i
+                v += dp[i - 1][r]
+                if l >= 0:
+                    v -= dp[i - 1][l]
+                dp[i][r] = (dp[i][r] + v) % mod
+        return dp[n][k]
