7dd5e9d689b82c57d04b440e3e95d128567d5352
TH Huang <tzhsn.huang@gmail.com>
2024-02-07 22:26:00 -0600

    [leetcode] perfect squares

---
+++ solutions/leetcode/perfect-squares/main.py
@@ -0,0 +1,13 @@
+class Solution:
+
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n + 1)
+        dp[0] = 0
+
+        for i in range(1, n + 1):
+            j = 1
+            while j * j <= i:
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
+
+        return dp[-1]
