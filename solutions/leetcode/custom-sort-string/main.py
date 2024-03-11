8bb02cf1040740b22dd9e7acc06696b50285a340
TH Huang <tzhsn.huang@gmail.com>
2024-03-10 22:11:11 -0500

    [leetcode] custom sort string

--- solutions/leetcode/custom-sort-string/main.py
+++ solutions/leetcode/custom-sort-string/main.py
@@ -5,3 +5,23 @@ class Solution:
         for i, c in enumerate(order):
             score[c] = i
         return "".join(sorted(s, key=lambda c: score[c]))
+
+
+class Solution:
+
+    def customSortString(self, order: str, s: str) -> str:
+        freq = defaultdict(int)
+        for c in s:
+            freq[c] += 1
+
+        result = []
+        for c in order:
+            for i in range(freq[c]):
+                result.append(c)
+            freq[c] = 0
+
+        for c, f in freq.items():
+            for i in range(f):
+                result.append(c)
+
+        return "".join(result)
