e1a8924518bd391431781ec807031f6b24a35107
TH Huang <tzhsn.huang@gmail.com>
2024-03-14 20:34:46 -0500

    [leetcode] product of array except self

--- solutions/leetcode/product-of-array-except-self/main.py
+++ solutions/leetcode/product-of-array-except-self/main.py
@@ -12,3 +12,21 @@ class Solution:
             v *= nums[-1 - i]
 
         return result
+
+
+class Solution:
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+
+        prefix = 1
+        for v in nums:
+            result.append(prefix)
+            prefix *= v
+
+        suffix = 1
+        for i, v in enumerate(reversed(nums)):
+            result[-i - 1] *= suffix
+            suffix *= v
+
+        return result
