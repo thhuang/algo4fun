2779658a5fdd7a8b74c1a9c39223b39486c00abe
TH Huang <tzhsn.huang@gmail.com>
2024-04-04 19:42:13 -0500

    [leetcode] make the string great

--- solutions/leetcode/make-the-string-great/main.py
+++ solutions/leetcode/make-the-string-great/main.py
@@ -14,3 +14,17 @@ class Solution:
                 result.append(c)
 
         return "".join(result)
+
+
+class Solution:
+
+    def makeGood(self, s: str) -> str:
+        result = []
+        for c in s:
+            if len(result) > 0 and ((c.isupper() and c.lower() == result[-1]) or
+                                    c.islower() and c.upper() == result[-1]):
+                result.pop()
+            else:
+                result.append(c)
+
+        return "".join(result)
