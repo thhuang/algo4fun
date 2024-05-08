fb359583e57c3e5841637ddbdf5202b997169271
TH Huang <tzhsn.huang@gmail.com>
2024-05-07 21:45:08 -0500

    [leetcode] number of wonderful substrings

---
+++ solutions/leetcode/number-of-wonderful-substrings/main.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def wonderfulSubstrings(self, word: str) -> int:
+        freq = {0: 1}
+
+        result = 0
+
+        mask = 0
+        for c in word:
+            shift = ord(c) - ord("a")
+            mask ^= (1 << shift)
+
+            if mask in freq:
+                result += freq[mask]
+                freq[mask] += 1
+            else:
+                freq[mask] = 1
+
+            for i in range(10):
+                v = mask ^ (1 << i)
+                if v in freq:
+                    result += freq[v]
+
+        return result
