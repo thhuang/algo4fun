31c3d7a87c398523ef75ed5dc1f0395b54640d13
TH Huang <tzhsn.huang@gmail.com>
2024-06-03 19:35:43 -0500

    [leetcode] longest palindrome

---
+++ solutions/leetcode/longest-palindrome/main.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def longestPalindrome(self, s: str) -> int:
+        result = 0
+
+        has_odd = False
+        for v in Counter(s).values():
+            if v % 2 == 1:
+                has_odd = True
+            result += v // 2 * 2
+
+        if has_odd:
+            result += 1
+
+        return result
