d598a59792c8f5e6f91b81ea69c93f28eb96f58e
TH Huang <tzhsn.huang@gmail.com>
2024-06-04 19:55:59 -0500

    [leetcode] find common characters

--- solutions/leetcode/find-common-characters/main.py
+++ solutions/leetcode/find-common-characters/main.py
@@ -22,3 +22,9 @@ class Solution:
         for c in [chr(i + ord("a")) for i in range(26)]:
             result += [c] * min([w.count(c) for w in words])
         return result
+
+
+class Solution:
+
+    def commonChars(self, words: List[str]) -> List[str]:
+        return [c for c in [chr(i + ord("a")) for i in range(26)] for _ in range(min([w.count(c) for w in words]))]
