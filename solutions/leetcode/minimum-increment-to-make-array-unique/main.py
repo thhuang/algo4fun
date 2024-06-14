a3189f94e42ee21da6ce7b49089f4bb87ea98b8a
TH Huang <tzhsn.huang@gmail.com>
2024-06-14 14:37:47 -0500

    [leetcode] minimum increment to make array unique

--- solutions/leetcode/minimum-increment-to-make-array-unique/main.py
+++ solutions/leetcode/minimum-increment-to-make-array-unique/main.py
@@ -13,3 +13,20 @@ class Solution:
             result += diff
 
         return result
+
+
+class Solution:
+
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        freq = dict()
+        for v in nums:
+            freq[v] = freq.get(v, 0) + 1
+
+        result = 0
+
+        for v in range(min(nums), max(nums) + len(nums)):
+            if freq.get(v, 0) > 1:
+                freq[v + 1] = freq.get(v + 1, 0) + freq[v] - 1
+                result += freq[v] - 1
+
+        return result
