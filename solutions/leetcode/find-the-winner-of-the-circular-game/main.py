340aa0670f90843eabbbb34d7246f4ec0f92a348
TH Huang <tzhsn.huang@gmail.com>
2024-07-07 22:41:15 -0500

    [leetcode] find the winer of the circular game

--- solutions/leetcode/find-the-winner-of-the-circular-game/main.py
+++ solutions/leetcode/find-the-winner-of-the-circular-game/main.py
@@ -8,3 +8,12 @@ class Solution:
             return (solve(n - 1, k) + k) % n
 
         return solve(n, k) + 1
+
+
+class Solution:
+
+    def findTheWinner(self, n: int, k: int) -> int:
+        dp = 0
+        for i in range(1, n):
+            dp = (dp + k) % (i + 1)
+        return dp + 1
