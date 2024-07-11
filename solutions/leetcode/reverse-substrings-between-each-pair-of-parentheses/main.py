49331b66d5863e015a09df70ae41d937094d3594
TH Huang <tzhsn.huang@gmail.com>
2024-07-10 23:55:24 -0500

    [leetcode] reverse substrings between each pair of parentheses

--- solutions/leetcode/reverse-substrings-between-each-pair-of-parentheses/main.py
+++ solutions/leetcode/reverse-substrings-between-each-pair-of-parentheses/main.py
@@ -14,3 +14,28 @@ class Solution:
         for l, r in parentheses:
             chars[l:r] = chars[l:r][::-1]
         return "".join(chars)
+
+
+class Solution:
+
+    def reverseParentheses(self, s: str) -> str:
+        pairs = {}
+        opens = []
+        for i, c in enumerate(s):
+            if c == "(":
+                opens.append(i)
+            elif c == ")":
+                l, r = opens.pop(), i
+                pairs[l], pairs[r] = r, l
+
+        result = []
+        i, direction = 0, 1
+        for _ in range(len(s)):
+            if i in pairs:
+                i = pairs[i]
+                direction = -direction
+            else:
+                result.append(s[i])
+            i += direction
+
+        return "".join(result)
