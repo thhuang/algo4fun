16dd62a44b2b3106db146714af957b48125a7149
TH Huang <tzhsn.huang@gmail.com>
2024-07-30 00:03:06 -0500

    [leetcode] circular array loop

---
+++ circular-array-loop/main.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+
+        def next(u: int) -> int:
+            n = len(nums)
+            return (u + nums[u] + n) % n
+
+        def valid(u: int, v: int) -> bool:
+            return u * v > 0
+
+        vis = set()
+
+        for i in range(len(nums)):
+            if i in vis:
+                continue
+            slow = i
+            fast = next(i)
+            while valid(nums[slow], nums[fast]) and valid(nums[slow], nums[next(fast)]):
+                vis.add(slow)
+                slow = next(slow)
+                fast = next(next(fast))
+                if slow == fast:
+                    if slow == next(slow):
+                        break
+                    return True
+
+        return False
