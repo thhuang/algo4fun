dc74db54b0695bf6c69822c2b2132990e42857c5
TH Huang <tzhsn.huang@gmail.com>
2024-08-19 20:53:00 -0500

    [leetcode] 2 keys keyboard

--- solutions/leetcode/2-keys-keyboard/main.py
+++ solutions/leetcode/2-keys-keyboard/main.py
@@ -1,6 +1,19 @@
 class Solution:
 
     def minSteps(self, n: int) -> int:
+        dp = [i for i in range(n + 1)]
+        dp[1] = 0
+        for l in range(2, n // 2):
+            d = dp[l] + 2
+            for r in range(l * 2, n + 1, l):
+                dp[r] = min(dp[r], d)
+                d += 1
+        return dp[-1]
+
+
+class Solution:
+
+    def minSteps(self, n: int) -> int:
         result = 0
         p = 2
         while p <= n:
