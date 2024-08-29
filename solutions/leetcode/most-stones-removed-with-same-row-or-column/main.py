51a1e5fd3e67cdc37a0ddef6d07e1684f6763d1c
TH Huang <tzhsn.huang@gmail.com>
2024-08-29 14:31:10 -0700

    [leetcode] most stones removed with same row or column

---
+++ solutions/leetcode/most-stones-removed-with-same-row-or-column/main.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        group, rank = [i for i in range(n)], [0 for _ in range(n)]
+
+        def find(k: int) -> int:
+            if k == group[k]:
+                return k
+            group[k] = find(group[k])
+            return group[k]
+
+        def unite(a: int, b: int):
+            ga, gb = find(a), find(b)
+            if ga == gb:
+                return
+            if rank[ga] < rank[gb]:
+                group[ga] = gb
+            elif rank[gb] < rank[ga]:
+                group[gb] = ga
+            else:
+                group[gb] = ga
+                rank[ga] += 1
+
+        for i in range(n):
+            for j in range(n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    unite(i, j)
+
+        return n - len(set([find(u) for u in range(n)]))
