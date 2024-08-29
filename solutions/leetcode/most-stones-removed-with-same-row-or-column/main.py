b7cf20d929855906ef01d78c26e0a77be66c60a5
TH Huang <tzhsn.huang@gmail.com>
2024-08-29 14:38:30 -0700

    [leetcode] most stones removed with same row or column

--- solutions/leetcode/most-stones-removed-with-same-row-or-column/main.py
+++ solutions/leetcode/most-stones-removed-with-same-row-or-column/main.py
@@ -2,6 +2,29 @@ class Solution:
 
     def removeStones(self, stones: List[List[int]]) -> int:
         n = len(stones)
+        vis = [False] * n
+
+        def dfs(i: int):
+            if vis[i]:
+                return
+            vis[i] = True
+            for j in range(n):
+                if stones[i][0] == stones[j][0] or stones[i][1] == stones[j][1]:
+                    dfs(j)
+
+        result = n
+        for i in range(n):
+            if not vis[i]:
+                dfs(i)
+                result -= 1
+
+        return result
+
+
+class Solution:
+
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
         group, rank = [i for i in range(n)], [0 for _ in range(n)]
 
         def find(k: int) -> int:
