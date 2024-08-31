9b4bc20ddfd5c6f561b09d48ac16abf48d29bdbf
TH Huang <tzhsn.huang@gmail.com>
2024-08-31 14:18:18 -0700

    [leetcode] path with maximum probability

--- solutions/leetcode/path-with-maximum-probability/main.py
+++ solutions/leetcode/path-with-maximum-probability/main.py
@@ -26,3 +26,31 @@ class Solution:
                 heapq.heappush(pq, (neg_prob * p, v))
 
         return 0
+
+
+class Solution:
+
+    def maxProbability(self, n: int, edges: List[List[int]], succ_prob: List[float], start_node: int,
+                       end_node: int) -> float:
+        adj = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(edges):
+            adj[a].append((b, succ_prob[i]))
+            adj[b].append((a, succ_prob[i]))
+
+        vis = [False] * n
+        pq = [(-1, start_node)]
+
+        while len(pq) > 0:
+            nprob, u = heapq.heappop(pq)
+
+            if u == end_node:
+                return -nprob
+
+            if vis[u]:
+                continue
+            vis[u] = True
+
+            for v, p in adj[u]:
+                heapq.heappush(pq, (nprob * p, v))
+
+        return 0
