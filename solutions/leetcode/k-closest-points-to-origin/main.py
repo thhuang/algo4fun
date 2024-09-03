df5402cb0286dd33b47d8384b27948c623a5078b
TH Huang <tzhsn.huang@gmail.com>
2024-09-03 15:16:52 -0700

    [leetcode] k closest points to origin

---
+++ solutions/leetcode/k-closest-points-to-origin/main.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        n = len(points)
+
+        def dist2(point: List[int]) -> int:
+            return point[0] * point[0] + point[1] * point[1]
+
+        def partition(l: int, r: int, t: int) -> int:
+            points[t], points[r] = points[r], points[t]
+            p = l
+            d = dist2(points[r])
+            for i in range(l, r):
+                if dist2(points[i]) < d:
+                    points[i], points[p] = points[p], points[i]
+                    p += 1
+            points[r], points[p] = points[p], points[r]
+            return p
+
+        l, r = 0, n - 1
+        while l <= r:
+            t = l + (r - l) // 2
+            t = partition(l, r, t)
+            if t >= k:
+                r = t - 1
+            else:
+                l = t + 1
+
+        return points[:l]
