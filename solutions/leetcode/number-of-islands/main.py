828f1d3c2be2a5f46c0bcede4f6155f947ee218a
TH Huang <tzhsn.huang@gmail.com>
2024-09-03 16:34:53 -0700

    [leetcode] number of islands

--- solutions/leetcode/number-of-islands/main.py
+++ solutions/leetcode/number-of-islands/main.py
@@ -22,3 +22,29 @@ class Solution:
                     result += 1
 
         return result
+
+
+class Solution:
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+        m, n = len(grid), len(grid[0])
+        vis = [[False for _ in range(n)] for _ in range(m)]
+
+        def dfs(i: int, j: int):
+            if i < 0 or m <= i or j < 0 or n <= j or vis[i][j] or grid[i][j] != "1":
+                return
+            vis[i][j] = True
+            for di, dj in directions:
+                dfs(i + di, j + dj)
+
+        result = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and not vis[i][j]:
+                    result += 1
+                    dfs(i, j)
+
+        return result
