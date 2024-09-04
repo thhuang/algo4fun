63d012465e9e424f95ea800b2f1dbbfcce2cc0d0
TH Huang <tzhsn.huang@gmail.com>
2024-09-03 17:53:44 -0700

    [leetcode] binary tree right side view

--- solutions/leetcode/binary-tree-right-side-view/main.py
+++ solutions/leetcode/binary-tree-right-side-view/main.py
@@ -24,3 +24,26 @@ class Solution:
                     q.append(u.left)
 
         return result
+
+
+class Solution:
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        result = []
+        q = deque()
+        q.append(root)
+
+        while len(q) > 0:
+            n = len(q)
+            result.append(q[0].val)
+            for _ in range(n):
+                u = q.popleft()
+                if u.right is not None:
+                    q.append(u.right)
+                if u.left is not None:
+                    q.append(u.left)
+
+        return result
