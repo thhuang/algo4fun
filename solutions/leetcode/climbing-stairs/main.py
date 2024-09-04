7cefbf3eee10e8af9790b727b3315eb869844b5c
TH Huang <tzhsn.huang@gmail.com>
2024-09-03 22:34:56 -0700

    [leetcode] climbing stairs

--- solutions/leetcode/climbing-stairs/main.py
+++ solutions/leetcode/climbing-stairs/main.py
@@ -5,3 +5,12 @@ class Solution:
         for _ in range(2, n + 1):
             dp[0], dp[1] = dp[1], dp[0] + dp[1]
         return dp[1]
+
+
+class Solution:
+
+    def climbStairs(self, n: int) -> int:
+        dp = (1, 1)
+        for _ in range(1, n):
+            dp = (dp[1], dp[0] + dp[1])
+        return dp[1]
