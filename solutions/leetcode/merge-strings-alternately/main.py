e50168a22e1702191b55647d3cdc03576e3c9b6c
TH Huang <tzhsn.huang@gmail.com>
2024-09-03 22:11:51 -0700

    [leetcode] merge strings alternately

--- solutions/leetcode/merge-strings-alternately/main.py
+++ solutions/leetcode/merge-strings-alternately/main.py
@@ -15,3 +15,19 @@ class Solution:
             result.append(word2[i])
 
         return ''.join(result)
+
+
+class Solution:
+
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        m, n = len(word1), len(word2)
+        i, j = 0, 0
+        result = []
+        while i < m or j < n:
+            if i < m:
+                result.append(word1[i])
+                i += 1
+            if j < n:
+                result.append(word2[j])
+                j += 1
+        return "".join(result)
