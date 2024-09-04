7f70e07f63093c9e0d597e8897a1b64b88eacbfb
TH Huang <tzhsn.huang@gmail.com>
2024-09-04 12:38:40 -0700

    [leetcode] continuous subarray sum

--- solutions/leetcode/continuous-subarray-sum/main.py
+++ solutions/leetcode/continuous-subarray-sum/main.py
@@ -1,8 +1,22 @@
 class Solution:
 
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        prefix = {0}
+        s = nums[0] % k
+        prefix_sum = {0}
+        for i in range(1, len(nums)):
+            r = (s + nums[i]) % k
+            if r in prefix_sum:
+                return True
+            prefix_sum.add(s)
+            s = r
+        return False
+
+
+class Solution:
+
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         s = 0
+        prefix = {0}
         for i in range(1, len(nums)):
             s = (s + nums[i - 1]) % k
             if (s + nums[i]) % k in prefix:
