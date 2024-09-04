1254cf30366e93dd88500dfd77d483040d328056
TH Huang <tzhsn.huang@gmail.com>
2024-09-04 12:08:10 -0700

    [leetcode] top k frequent elements

--- solutions/leetcode/top-k-frequent-elements/main.py
+++ solutions/leetcode/top-k-frequent-elements/main.py
@@ -2,6 +2,14 @@ class Solution:
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = Counter(nums)
+        sorted_freq = sorted(freq.items(), key=lambda v: -v[1])
+        return [v[0] for v in sorted_freq[:k]]
+
+
+class Solution:
+
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
 
         mn, mx = min(freq.values()), max(freq.values())
         buckets = [[] for _ in range(mx - mn + 1)]
