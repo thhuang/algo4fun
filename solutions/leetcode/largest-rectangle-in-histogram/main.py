e253b95d46567368e3b10e000b66e15c85905b51
TH Huang <tzhsn.huang@gmail.com>
2024-09-04 09:21:22 -0700

    [leetcode] largest rectangle in histogram

--- solutions/leetcode/largest-rectangle-in-histogram/main.py
+++ solutions/leetcode/largest-rectangle-in-histogram/main.py
@@ -15,3 +15,24 @@ class Solution:
             incr.append((h, l))
 
         return result
+
+
+class Solution:
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        Rect = namedtuple("Rect", ["h", "i"])
+
+        heights.append(0)
+        incr = [Rect(0, -1)]
+        result = 0
+
+        for i, h in enumerate(heights):
+            l = i
+            while len(incr) > 0 and incr[-1].h > h:
+                bar = incr.pop()
+                l = bar.i
+                w = i - bar.i
+                result = max(result, bar.h * w)
+            incr.append(Rect(h, l))
+
+        return result
