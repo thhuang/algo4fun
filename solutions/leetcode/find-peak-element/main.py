582ac57a5a499f585627c79dcea7eefd4925ce38
TH Huang <tzhsn.huang@gmail.com>
2024-09-05 10:57:34 -0700

    [leetcode] find peak element

--- solutions/leetcode/find-peak-element/main.py
+++ solutions/leetcode/find-peak-element/main.py
@@ -9,3 +9,22 @@ class Solution:
             else:
                 r = m
         return l
+
+
+class Solution:
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        inf = float('inf')
+        n = len(nums)
+
+        l, r = -1, n  # [l, r)
+        while r - l > 1:
+            m = (l + r) // 2
+            a = nums[m - 1] if m - 1 >= 0 else -inf
+            b = nums[m] if m < n else -inf
+            if a < b:
+                l = m
+            else:  # a > b
+                r = m
+
+        return l
