b6d44432b6793d30e55bde51444d8aa54a08ef4d
TH Huang <tzhsn.huang@gmail.com>
2024-09-05 14:33:33 -0700

    [leetcode] maximum swap

--- solutions/leetcode/maximum-swap/main.py
+++ solutions/leetcode/maximum-swap/main.py
@@ -10,3 +10,31 @@ class Solution:
                 t = s[0:i] + s[j] + s[i + 1:j] + s[i] + s[j + 1:]
                 result = max(result, int(t))
         return result
+
+
+class Solution:
+
+    def maximumSwap(self, num: int) -> int:
+        s = [c for c in str(num)]
+
+        l = None
+        r_val = None
+
+        mx = 0
+        for i in range(len(s))[::-1]:
+            v = int(s[i])
+            if v < mx:
+                l = i
+                r_val = mx
+
+            mx = max(mx, v)
+
+        if l is None:
+            return num
+
+        for r in range(len(s))[::-1]:
+            if int(s[r]) == r_val:
+                s[l], s[r] = s[r], s[l]
+                break
+
+        return int("".join(s))
