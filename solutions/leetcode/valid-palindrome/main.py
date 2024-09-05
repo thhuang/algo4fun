fd63471450aafd5fc7b0c745b50c68c0672955c5
TH Huang <tzhsn.huang@gmail.com>
2024-09-05 09:51:24 -0700

    [leetcode] valid palindrome

---
+++ solutions/leetcode/valid-palindrome/main.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+            elif not s[r].isalnum():
+                r -= 1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
