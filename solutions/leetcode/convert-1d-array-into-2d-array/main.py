d5b0268d0bfd4aeb889c95060d55305db5c5f6ab
TH Huang <tzhsn.huang@gmail.com>
2024-09-05 12:17:44 -0700

    [leetcode] convert 1d array into 2d array

--- solutions/leetcode/convert-1d-array-into-2d-array/main.py
+++ solutions/leetcode/convert-1d-array-into-2d-array/main.py
@@ -3,4 +3,4 @@ class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
         if m * n != len(original):
             return []
-        return [[original[n * i + j] for j in range(n)] for i in range(m)]
+        return [[original[i * n + j] for j in range(n)] for i in range(m)]
