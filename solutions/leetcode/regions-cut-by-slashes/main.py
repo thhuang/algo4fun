3e88f90bb86acc1533ea2b58e1f38986c8a3c4e3
TH Huang <tzhsn.huang@gmail.com>
2024-09-05 20:34:51 -0700

    [leetcode] regions cut by slashes

--- solutions/leetcode/regions-cut-by-slashes/main.py
+++ solutions/leetcode/regions-cut-by-slashes/main.py
@@ -57,3 +57,53 @@ class Solution:
                     ds.unite(convert(i, j, 2), convert(i, j, 3))
 
         return ds.groups()
+
+
+class Solution:
+
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        group = [i for i in range(m * n * 4)]
+        rank = [0] * (m * n * 4)
+
+        def find(k: int) -> int:
+            if group[k] == k:
+                return k
+            group[k] = find(group[k])
+            return group[k]
+
+        def unite(a: int, b: int):
+            ga = find(a)
+            gb = find(b)
+            if ga == gb:
+                return
+            if rank[ga] < rank[gb]:
+                group[ga] = gb
+            elif rank[gb] < rank[ga]:
+                group[gb] = ga
+            else:
+                group[gb] = ga
+                rank[ga] += 1
+
+        def idx(i: int, j: int, k: int) -> int:
+            return (i * n + j) * 4 + k
+
+        for i in range(m):
+            for j in range(n):
+                if i > 0:
+                    unite(idx(i, j, 0), idx(i - 1, j, 2))
+                if j > 0:
+                    unite(idx(i, j, 3), idx(i, j - 1, 1))
+                if grid[i][j] == "/":
+                    unite(idx(i, j, 0), idx(i, j, 3))
+                    unite(idx(i, j, 1), idx(i, j, 2))
+                elif grid[i][j] == "\\":
+                    unite(idx(i, j, 0), idx(i, j, 1))
+                    unite(idx(i, j, 2), idx(i, j, 3))
+                else:  # grid[i][j] == " ":
+                    unite(idx(i, j, 0), idx(i, j, 1))
+                    unite(idx(i, j, 1), idx(i, j, 2))
+                    unite(idx(i, j, 2), idx(i, j, 3))
+
+        return len(set([find(idx(i, j, k)) for i in range(m) for j in range(n) for k in range(4)]))
