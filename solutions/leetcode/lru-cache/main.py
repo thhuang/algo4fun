24fcec3814a33737f868155b059a15dcdf6d8d4b
TH Huang <tzhsn.huang@gmail.com>
2024-09-17 10:17:57 -0700

    [leetcode] lru cache

--- solutions/leetcode/lru-cache/main.py
+++ solutions/leetcode/lru-cache/main.py
@@ -81,6 +81,25 @@ class LRUCache:
             self.cache.popitem(last=False)
 
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
