7dacfb5a5390dc876cb82dadae71f91a66033c75
TH Huang <tzhsn.huang@gmail.com>
2024-09-17 19:39:28 -0700

    [leetcode] buildings with an ocean view

--- solutions/leetcode/buildings-with-an-ocean-view/main.py
+++ solutions/leetcode/buildings-with-an-ocean-view/main.py
@@ -7,3 +7,13 @@ class Solution:
                 result.pop()
             result.append(i)
         return result
+
+
+class Solution:
+
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        rresult = []
+        for i, h in reversed(list(enumerate(heights))):
+            if len(rresult) == 0 or heights[rresult[-1]] < h:
+                rresult.append(i)
+        return reversed(rresult)
