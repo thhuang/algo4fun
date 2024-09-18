6c2466d248c887db8d6d35cdbca8d4d427abda92
TH Huang <tzhsn.huang@gmail.com>
2024-09-17 23:10:42 -0700

    [leetcode] valid word abbreviation

--- solutions/leetcode/valid-word-abbreviation/main.py
+++ solutions/leetcode/valid-word-abbreviation/main.py
@@ -25,3 +25,32 @@ class Solution:
                 j += 1
 
         return i == m and j == n
+
+
+class Solution:
+
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        m = len(word)
+        n = len(abbr)
+
+        i = 0
+        j = 0
+
+        while i < m and j < n:
+            if abbr[j].isalpha():
+                if word[i] != abbr[j]:
+                    return False
+                i += 1
+                j += 1
+            elif abbr[j] == "0":
+                return False
+            else:
+                v = 0
+                while j < n and abbr[j].isdigit():
+                    v = v * 10 + int(abbr[j])
+                    j += 1
+                if m - i < v:
+                    return False
+                i += v
+
+        return i == m and j == n
