ec4a6c6bc1e0cb21b1c1e4bb24c074b76a8f0468
TH Huang <tzhsn.huang@gmail.com>
2024-09-17 19:48:16 -0700

    [leetcode] goat latin

--- solutions/leetcode/goat-latin/main.py
+++ solutions/leetcode/goat-latin/main.py
@@ -21,3 +21,19 @@ class Solution:
             words.append(word)
 
         return " ".join(["".join(word) for word in words])
+
+
+class Solution:
+
+    def __init__(self):
+        self.vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+
+    def toGoatLatin(self, sentence: str) -> str:
+        words = []
+        for i, w in enumerate(sentence.split()):
+            if w[0] in self.vowels:
+                words.append(w + "ma" + "a" * (i + 1))
+            else:
+                words.append(w[1:] + w[0] + "ma" + "a" * (i + 1))
+
+        return " ".join(words)
