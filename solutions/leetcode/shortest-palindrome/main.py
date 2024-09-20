d93ba95f989d29933d4216f0eca58ce63e226443
TH Huang <tzhsn.huang@gmail.com>
2024-09-19 20:42:17 -0700

    [leetcode] shortest palindrome

---
+++ solutions/leetcode/shortest-palindrome/main.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def shortestPalindrome(self, s: str) -> str:
+        n = len(s)
+
+        for i in range(n, 0, -1):
+            if s[:i] == s[:i][::-1]:
+                return s[i:][::-1] + s
+
+        return ""
