b955d45861a850e68a1414469e0ee2de2a5accd7
TH Huang <tzhsn.huang@gmail.com>
2024-09-20 18:44:41 -0700

    [leetcode] merge sorted array

--- solutions/leetcode/merge-sorted-array/main.py
+++ solutions/leetcode/merge-sorted-array/main.py
@@ -18,3 +18,28 @@ class Solution:
             else:
                 nums1[p] = nums2[j]
                 j -= 1
+
+
+class Solution:
+
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        for p in reversed(range(m + n)):
+            if i < 0:
+                nums1[p] = nums2[j]
+                j -= 1
+            elif j < 0:
+                break
+            else:
+                a = nums1[i]
+                b = nums2[j]
+                if a > b:
+                    nums1[p] = a
+                    i -= 1
+                else:  # b >= a
+                    nums1[p] = b
+                    j -= 1
