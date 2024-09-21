f5ba926bf774e0a215c8cbedec0a3a434e860c95
TH Huang <tzhsn.huang@gmail.com>
2024-09-20 20:14:57 -0700

    [leetcode] lowest common ancestor of a binary tree

--- solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/Solution.kt
+++ solutions/leetcode/lowest-common-ancestor-of-a-binary-tree/Solution.kt
@@ -17,3 +17,13 @@ class Solution {
         return null
     }
 }
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null || root == p || root == q) return root
+        val l = lowestCommonAncestor(root.left, p, q)
+        val r = lowestCommonAncestor(root.right, p, q)
+        if (l != null && r != null) return root
+        return l ?: r
+    }
+}
