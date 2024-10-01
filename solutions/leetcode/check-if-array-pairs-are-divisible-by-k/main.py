fd203839696b8f6eed2a519b6c78dfb7755d7b7b
TH Huang <tzhsn.huang@gmail.com>
2024-09-30 18:06:12 -0700

    [leetcode] check if array pairs are divisible by k

--- solutions/leetcode/check-if-array-pairs-are-divisible-by-k/main.py
+++ solutions/leetcode/check-if-array-pairs-are-divisible-by-k/main.py
@@ -14,3 +14,21 @@ class Solution:
                 mp[v % k] += 1
 
         return n == 0
+
+
+class Solution:
+
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        freq = [0] * k
+
+        for v in arr:
+            freq[v % k] += 1
+
+        if freq[0] % 2 != 0:
+            return False
+
+        for i in range(1, k // 2 + 1):
+            if freq[i] != freq[k - i]:
+                return False
+
+        return True
