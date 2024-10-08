71d1e9e474631107809714f1800f5cbabb1b4899
TH Huang <tzhsn.huang@gmail.com>
2024-10-07 20:56:52 -0700

    [leetcode] minimum number of swaps to make the string balanced

--- solutions/leetcode/minimum-number-of-swaps-to-make-the-string-balanced/main.py
+++ solutions/leetcode/minimum-number-of-swaps-to-make-the-string-balanced/main.py
@@ -23,3 +23,17 @@ class Solution:
                     result += 1
 
         return result
+
+
+class Solution:
+
+    def minSwaps(self, s: str) -> int:
+        unmatched = 0
+
+        for c in s:
+            if c == "[":
+                unmatched += 1
+            elif unmatched > 0:
+                unmatched -= 1
+
+        return (unmatched + 1) // 2
