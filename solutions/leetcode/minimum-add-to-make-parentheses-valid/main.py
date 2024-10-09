d352dfefe399f3ddda888921f779264622072859
TH Huang <tzhsn.huang@gmail.com>
2024-10-08 19:58:01 -0700

    [leetcode] minimum add to make parentheses valid

--- solutions/leetcode/minimum-add-to-make-parentheses-valid/main.py
+++ solutions/leetcode/minimum-add-to-make-parentheses-valid/main.py
@@ -16,3 +16,22 @@ class Solution:
                 close = max(0, close - 1)
 
         return open + close
+
+
+class Solution:
+
+    def minAddToMakeValid(self, s: str) -> int:
+        result = 0
+
+        opens = 0
+        for c in s:
+            if c == "(":
+                opens += 1
+            elif opens == 0:
+                result += 1
+            else:
+                opens -= 1
+
+        result += opens
+
+        return result
