a952401d64b7650a59ea24be1bd9a456e005cf4d
TH Huang <tzhsn.huang@gmail.com>
2024-10-10 09:57:01 -0700

    [leetcode] maximum width ramp

--- solutions/leetcode/maximum-width-ramp/main.py
+++ solutions/leetcode/maximum-width-ramp/main.py
@@ -1,16 +1,16 @@
 class Solution:
 
     def maxWidthRamp(self, nums: List[int]) -> int:
-        incr = []
+        decr = []
         for i in range(len(nums)):
-            if len(incr) == 0 or nums[incr[-1]] > nums[i]:
-                incr.append(i)
+            if len(decr) == 0 or nums[decr[-1]] > nums[i]:
+                decr.append(i)
 
         result = 0
 
         for i in reversed(range(len(nums))):
-            while len(incr) > 0 and nums[incr[-1]] <= nums[i]:
-                result = max(result, i - incr[-1])
-                incr.pop()
+            while len(decr) > 0 and nums[decr[-1]] <= nums[i]:
+                result = max(result, i - decr[-1])
+                decr.pop()
 
         return result
