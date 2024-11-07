644589f351bde053e387e05dab0b7a09cd10c3a6
TH Huang <tzhsn.huang@gmail.com>
2024-11-06 18:32:04 -0700

    [leetcode] find if array can be sorted

--- solutions/leetcode/find-if-array-can-be-sorted/main.py
+++ solutions/leetcode/find-if-array-can-be-sorted/main.py
@@ -13,3 +13,30 @@ class Solution:
                     bits[i - 1], bits[i] = bits[i], bits[i - 1]
 
         return True
+
+
+class Solution:
+
+    def canSortArray(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        prev_max = -float('inf')
+        l = 0
+
+        while l < n:
+            mn = nums[l]
+            mx = nums[l]
+            r = l + 1
+
+            while r < n and nums[l].bit_count() == nums[r].bit_count():
+                mn = min(mn, nums[r])
+                mx = max(mx, nums[r])
+                r += 1
+
+            if prev_max > mn:
+                return False
+            prev_max = mx
+
+            l = r
+
+        return True
