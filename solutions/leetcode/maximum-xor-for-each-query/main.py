da99b805f8a9ac5fc5c4524ca661d9569ad4e334
TH Huang <tzhsn.huang@gmail.com>
2024-11-09 11:30:10 -0800

    [leetcode] maximum xor for each query

--- solutions/leetcode/maximum-xor-for-each-query/main.py
+++ solutions/leetcode/maximum-xor-for-each-query/main.py
@@ -12,3 +12,17 @@ class Solution:
             cur ^= v
 
         return result
+
+
+class Solution:
+
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        cur = reduce(lambda a, b: a ^ b, nums)
+
+        result = []
+        for v in reversed(nums):
+            b = (1 << maximumBit) - 1
+            result.append((cur & b) ^ b)
+            cur ^= v
+
+        return result
