0ed39d26c155b167e8f53ea3d5f13d9a5fc9bccf
TH Huang <tzhsn.huang@gmail.com>
2024-11-11 22:54:25 -0800

    [leetcode] prime subtraction operation

--- solutions/leetcode/prime-subtraction-operation/main.py
+++ solutions/leetcode/prime-subtraction-operation/main.py
@@ -29,3 +29,34 @@ class Solution:
                 return False
 
         return True
+
+
+class Solution:
+
+    def getPrimes(self, mx: int) -> List[int]:
+        primes = [2]
+        for i in range(3, mx + 1, 2):
+            is_prime = True
+            for p in primes:
+                if p * p > i:
+                    break
+                if i % p == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                primes.append(i)
+        return primes
+
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        mx = max(nums)
+        primes = self.getPrimes(mx)
+
+        nums = [0] + nums
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                return False
+            j = bisect_left(primes, nums[i] - nums[i - 1]) - 1
+            if j >= 0:
+                nums[i] -= primes[j]
+
+        return True
