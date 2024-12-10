fd78c18f3530915e4227a4ea0828fb794d705d89
TH Huang <thhuang@meta.com>
2024-12-09 21:12:14 -0800

    [leetcode] find longest special substring that occurs thrice i

--- solutions/leetcode/find-longest-special-substring-that-occurs-thrice-i/main.py
+++ solutions/leetcode/find-longest-special-substring-that-occurs-thrice-i/main.py
@@ -15,3 +15,30 @@ class Solution:
         if result == 0:
             return -1
         return result
+
+
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        result = 0
+
+        for length in range(1, len(s) + 1):
+            freq = defaultdict(int)
+
+            window = defaultdict(int)
+
+            for r in range(length - 1):
+                window[s[r]] += 1
+
+            for r in range(length - 1, len(s)):
+                l = r - length + 1
+                window[s[r]] += 1
+                if window[s[r]] == length:
+                    freq[s[r]] += 1
+                window[s[l]] -= 1
+
+            if len(freq) > 0 and max(freq.values()) >= 3:
+                result = length
+
+        if result == 0:
+            return -1
+        return result
