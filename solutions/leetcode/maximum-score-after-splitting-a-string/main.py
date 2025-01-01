7f03d6cab5a281a90864f0dc01231980622fde08
TH Huang <thhuang@meta.com>
2024-12-31 21:12:58 -0800

    [leetcode] maximum score after splitting a string

--- solutions/leetcode/maximum-score-after-splitting-a-string/main.py
+++ solutions/leetcode/maximum-score-after-splitting-a-string/main.py
@@ -14,3 +14,18 @@ class Solution:
             result = max(result, left_zeros + right_ones)
 
         return result
+
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        n = len(s)
+        result = 0
+        state = n - s.count("0")
+        for c in s[: n - 1]:
+            if c == "0":
+                state += 1
+            else:  # c == "1"
+                state -= 1
+            result = max(result, state)
+
+        return result
