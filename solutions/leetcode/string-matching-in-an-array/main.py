e52a5615c90ac49d3aef9a1a4294401caea792d2
TH Huang <tzhsn.huang@gmail.com>
2025-01-07 04:26:07 +0000

    [leetcode] string matching in an array

---
+++ solutions/leetcode/string-matching-in-an-array/main.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def stringMatching(self, words: List[str]) -> List[str]:
+        words.sort(key=lambda w: len(w))
+        result = []
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if words[i] in words[j]:
+                    result.append(words[i])
+                    break
+        return result
