66678d00cc8889ee3a1d970c135ae01cdf6e815c
TH Huang <tzhsn.huang@gmail.com>
2025-01-10 11:23:38 -0800

    [leetcode] word subsets

--- solutions/leetcode/word-subsets/main.py
+++ solutions/leetcode/word-subsets/main.py
@@ -20,3 +20,32 @@ class Solution:
                 result.append(words1[i])
 
         return result
+
+
+class Solution:
+
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        words_char_freq_1 = [Counter(list(w)) for w in words1]
+
+        def merge_counters(a: dict, b: dict) -> dict:
+            c = defaultdict(int)
+            for k, v in a.items():
+                c[k] = max(c[k], v)
+            for k, v in b.items():
+                c[k] = max(c[k], v)
+            return c
+
+        f2 = reduce(merge_counters, [Counter(w) for w in words2])
+
+        result = []
+        for i in range(len(words1)):
+            is_subset = True
+            f1 = words_char_freq_1[i]
+            for k, v in f2.items():
+                if f1.get(k, 0) < v:
+                    is_subset = False
+                    break
+            if is_subset:
+                result.append(words1[i])
+
+        return result
