0ca57dec94265df659bddf589ea8739f3ef1fa68
TH Huang <tzhsn.huang@gmail.com>
2025-01-14 23:53:55 -0800

    [leetcode] minimize xor

--- solutions/leetcode/minimize-xor/main.rs
+++ solutions/leetcode/minimize-xor/main.rs
@@ -1,10 +1,10 @@
 impl Solution {
     pub fn minimize_xor(num1: i32, num2: i32) -> i32 {
-        let mut n = (num2.count_ones() as i32) - (num1.count_ones() as i32);
+        let mut n = num2.count_ones() as i32 - num1.count_ones() as i32;
         let mut result = num1;
 
         for i in 0..32 {
-            let mask = 1i32 << i;
+            let mask = 1 << i;
             if n > 0 && result & mask == 0 {
                 n -= 1;
                 result |= mask;
