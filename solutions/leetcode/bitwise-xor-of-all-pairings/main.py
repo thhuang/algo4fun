492551fa8b5bcb53262f78648b2622fbaec46061
TH Huang <tzhsn.huang@gmail.com>
2025-01-15 22:48:35 -0800

    [leetcode] bitwise xor of all pairings

--- solutions/leetcode/bitwise-xor-of-all-pairings/main.py
+++ solutions/leetcode/bitwise-xor-of-all-pairings/main.py
@@ -1,13 +1,9 @@
 class Solution:
 
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        xors = lambda nums: reduce(lambda acc, x: acc ^ x, nums, 0)
-
         result = 0
-
         if len(nums2) % 2 == 1:
-            result ^= xors(nums1)
+            result ^= reduce(lambda acc, x: acc ^ x, nums1, 0)
         if len(nums1) % 2 == 1:
-            result ^= xors(nums2)
-
+            result ^= reduce(lambda acc, x: acc ^ x, nums2, 0)
         return result
