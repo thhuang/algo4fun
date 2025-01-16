924c2b7a41ad737b43dab414492e2146b6130c63
TH Huang <tzhsn.huang@gmail.com>
2025-01-15 22:47:12 -0800

    [leetcode] bitwise xor of all pairings

--- solutions/leetcode/bitwise-xor-of-all-pairings/main.rs
+++ solutions/leetcode/bitwise-xor-of-all-pairings/main.rs
@@ -14,3 +14,16 @@ impl Solution {
         result
     }
 }
+
+impl Solution {
+    pub fn xor_all_nums(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        let mut result = 0;
+        if nums2.len() % 2 == 1 {
+            result ^= nums1.iter().fold(0, |acc, &x| acc ^ x);
+        }
+        if nums1.len() % 2 == 1 {
+            result ^= nums2.iter().fold(0, |acc, &x| acc ^ x);
+        }
+        result
+    }
+}
