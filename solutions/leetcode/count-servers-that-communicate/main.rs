45456c602fdd7fd57ef25a9601f308ce56e4bc72
TH Huang <tzhsn.huang@gmail.com>
2025-01-22 20:24:08 -0800

    [leetcode] count servers that communicate

--- solutions/leetcode/count-servers-that-communicate/main.rs
+++ solutions/leetcode/count-servers-that-communicate/main.rs
@@ -47,3 +47,34 @@ impl Solution {
         result
     }
 }
+
+impl Solution {
+    pub fn count_servers(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+
+        let mut row_count = vec![0; m];
+        let mut col_count = vec![0; n];
+
+        for i in 0..m {
+            for j in 0..n {
+                if grid[i][j] == 1 {
+                    row_count[i] += 1;
+                    col_count[j] += 1;
+                }
+            }
+        }
+
+        let mut result = 0;
+
+        for i in 0..m {
+            for j in 0..n {
+                if grid[i][j] == 1 && (row_count[i] > 1 || col_count[j] > 1) {
+                    result += 1;
+                }
+            }
+        }
+
+        result
+    }
+}
