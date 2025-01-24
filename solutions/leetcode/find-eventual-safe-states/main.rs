93e41e744615c1323e3b3b0b37f15638c03518a2
TH Huang <tzhsn.huang@gmail.com>
2025-01-23 21:01:51 -0800

    [leetcode] find eventual safe states

--- solutions/leetcode/find-eventual-safe-states/main.rs
+++ solutions/leetcode/find-eventual-safe-states/main.rs
@@ -41,3 +41,36 @@ impl Solution {
         result
     }
 }
+
+impl Solution {
+    fn dfs(u: usize, graph: &Vec<Vec<i32>>, states: &mut Vec<State>) -> State {
+        if states[u] != State::Unknown {
+            return states[u];
+        }
+        states[u] = State::Unsafe;
+
+        let is_safe = graph[u].iter().fold(true, |acc, &v| {
+            Solution::dfs(v as usize, graph, states) == State::Safe && acc
+        });
+
+        if is_safe {
+            states[u] = State::Safe;
+        }
+
+        states[u]
+    }
+
+    pub fn eventual_safe_nodes(graph: Vec<Vec<i32>>) -> Vec<i32> {
+        let n = graph.len();
+        let mut states = vec![State::Unknown; n];
+        let mut result: Vec<i32> = Vec::new();
+
+        for u in 0..n {
+            if Solution::dfs(u, &graph, &mut states) == State::Safe {
+                result.push(u as i32);
+            }
+        }
+
+        result
+    }
+}
