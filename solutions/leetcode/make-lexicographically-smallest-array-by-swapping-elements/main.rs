c687e5fb903e91cd7d25f39074981dad93c0d5d9
TH Huang <tzhsn.huang@gmail.com>
2025-01-24 21:31:34 -0800

    [leetcode] make lexicographically smallest array by swapping elements

--- solutions/leetcode/make-lexicographically-smallest-array-by-swapping-elements/main.rs
+++ solutions/leetcode/make-lexicographically-smallest-array-by-swapping-elements/main.rs
@@ -3,12 +3,12 @@ use std::collections::{HashMap, VecDeque};
 impl Solution {
     pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
         let n = nums.len();
+
         let mut sorted_nums = nums.clone();
         sorted_nums.sort();
 
-        let mut group_nums: Vec<VecDeque<i32>> = vec![VecDeque::from(vec![sorted_nums[0]])];
-        let mut num_to_group: HashMap<i32, usize> = HashMap::new();
-        num_to_group.insert(sorted_nums[0], 0);
+        let mut group_nums: Vec<VecDeque<i32>> = vec![VecDeque::from([sorted_nums[0]])];
+        let mut num_to_group: HashMap<i32, usize> = HashMap::from([(sorted_nums[0], 0)]);
 
         for i in 1..n {
             let num = sorted_nums[i];
@@ -19,11 +19,10 @@ impl Solution {
             num_to_group.insert(num, group_nums.len() - 1);
         }
 
-        let mut result = vec![];
+        let mut result = Vec::with_capacity(n);
         for num in &nums {
-            let group = num_to_group.get(num).unwrap();
-            let v = group_nums[*group].pop_front().unwrap();
-            result.push(v);
+            let group = num_to_group[&num];
+            result.push(group_nums[group].pop_front().unwrap());
         }
 
         result
