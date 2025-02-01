b53ed84d1fedf522e94b8bd0314c7beaab33171a
TH Huang <thhuang@meta.com>
2025-01-31 16:26:48 -0800

    [leetcode] special array i

---
+++ solutions/leetcode/special-array-i/main.rs
@@ -0,0 +1,7 @@
+impl Solution {
+    pub fn is_array_special(nums: Vec<i32>) -> bool {
+        (1..nums.len()).fold(
+            true,
+            |acc, i| acc && nums[i - 1] % 2 + nums[i] % 2 == 1)
+    }
+}
