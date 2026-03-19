0e1a4771a3b0528a8253f921d8dca7b38fce5322
TH Huang <tzhsn.huang@gmail.com>
2026-03-18 22:46:36 -0700

    [leetcode] validate binary search tree

---
+++ solutions/leetcode/validate-binary-search-tree/main.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        prev = -inf
+
+        def inorder(u: Optional[TreeNode]):
+            nonlocal prev
+
+            if u is None:
+                return True
+
+            if not inorder(u.left):
+                return False
+
+            if u.val <= prev:
+                return False
+            prev = u.val
+
+            if not inorder(u.right):
+                return False
+
+            return True
+
+        return inorder(root)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        q = [(root, -inf, inf)]
+
+        while len(q) > 0:
+            nq = []
+
+            for (u, l, r) in q:
+                if u.left is not None:
+                    if l < u.left.val < u.val:
+                        nq.append((u.left, l, u.val))
+                    else:
+                        return False
+
+                if u.right is not None:
+                    if u.val < u.right.val < r:
+                        nq.append((u.right, u.val, r))
+                    else:
+                        return False
+
+            q = nq
+
+        return True
