f1344d1feb5178c01150812a85e1bcda3ab42c76
TH Huang <tzhsn.huang@gmail.com>
2026-03-19 19:51:23 -0700

    [leetcode] house robber

--- solutions/leetcode/house-robber/main.py
+++ solutions/leetcode/house-robber/main.py
@@ -5,3 +5,12 @@ class Solution:
         for v in nums:
             dp[0], dp[1] = dp[1], max(dp[0] + v, dp[1])
         return dp[1]
+
+
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        dp = (0, 0)
+        for v in nums:
+            dp = (dp[1], max(dp[1], dp[0] + v))
+        return dp[1]
