2aa0e12a45151a8c817d131076b8e13942b31f09
TH Huang <tzhsn.huang@gmail.com>
2026-03-19 21:18:23 -0700

    [leetcode] longest increasing subsequence

--- solutions/leetcode/longest-increasing-subsequence/main.py
+++ solutions/leetcode/longest-increasing-subsequence/main.py
@@ -9,3 +9,17 @@ class Solution:
             else:
                 seq[i] = v
         return len(seq)
+
+
+class Solution:
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+
+        for r in range(1, n):
+            for l in range(0, r):
+                if nums[l] < nums[r]:
+                    dp[r] = max(dp[r], dp[l] + 1)
+
+        return max(dp)
