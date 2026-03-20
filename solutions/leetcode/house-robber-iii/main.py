a9fba21d75ac4524f0d99bc40894b69248c82813
TH Huang <tzhsn.huang@gmail.com>
2026-03-19 20:10:23 -0700

    [leetcode] house robber iii

--- solutions/leetcode/house-robber-iii/main.py
+++ solutions/leetcode/house-robber-iii/main.py
@@ -17,3 +17,27 @@ class Solution:
 
     def rob(self, root: Optional[TreeNode]) -> int:
         return max(self.search(root))
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def search(self, root: Optional[TreeNode]) -> Tuple[int, int]:
+        if root is None:
+            return [0, 0]
+
+        l_rob, l_skip = self.search(root.left)
+        r_rob, r_skip = self.search(root.right)
+
+        return [
+            l_skip + r_skip + root.val,
+            max(l_rob, l_skip) + max(r_rob, r_skip)
+        ]
+
+    def rob(self, root: Optional[TreeNode]) -> int:
+        return max(self.search(root))
