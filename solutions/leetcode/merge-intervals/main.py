284a6af810d85f06a2a07ccff6de85d55b7b2277
TH Huang <tzhsn.huang@gmail.com>
2026-03-20 22:39:38 -0700

    [leetcode] merge intervals

--- solutions/leetcode/merge-intervals/main.py
+++ solutions/leetcode/merge-intervals/main.py
@@ -9,3 +9,19 @@ class Solution:
             else:
                 result[-1][1] = max(result[-1][1], interval[1])
         return result
+
+
+class Solution:
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+
+        result = intervals[0:1]
+
+        for interval in intervals:
+            if result[-1][1] >= interval[0]:
+                result[-1][1] = max(result[-1][1], interval[1])
+            else:
+                result.append(interval)
+
+        return result
