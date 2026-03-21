c33e131b7c97db7a726335bfdafdfd189faf9bc2
TH Huang <tzhsn.huang@gmail.com>
2026-03-20 23:52:56 -0700

    [leetcode] permutations

---
+++ solutions/leetcode/permutations/main.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+
+        result = []
+
+        for i in range(len(nums)):
+            for p in self.permute(nums[:i] + nums[i + 1:]):
+                result.append([nums[i]] + p)
+
+        return result
+
+
+class Solution:
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def search(i):
+            if i == len(nums):
+                result.append(nums.copy())
+                return
+
+            for j in range(i, len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                search(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        search(0)
+
+        return result
