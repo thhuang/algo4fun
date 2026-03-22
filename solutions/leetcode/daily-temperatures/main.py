e5b66c9815bcd692395c80f1bbcf73af843fd002
TH Huang <tzhsn.huang@gmail.com>
2026-03-21 23:38:58 -0700

    [leetcode] daily temperatures

--- solutions/leetcode/daily-temperatures/main.py
+++ solutions/leetcode/daily-temperatures/main.py
@@ -1,11 +1,10 @@
 class Solution:
 
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        result = [0 for _ in range(n)]
+        result = [0] * len(temperatures)
         desc = []
         for r, t in enumerate(temperatures):
-            while len(desc) > 0 and temperatures[desc[-1]] < t:
+            while desc and temperatures[desc[-1]] < t:
                 l = desc.pop()
                 result[l] = r - l
             desc.append(r)
