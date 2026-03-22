793135bddb681a06418642ae70a628d05e4cd01b
TH Huang <tzhsn.huang@gmail.com>
2026-03-22 10:22:59 -0700

    [leetcode] house robber ii

--- solutions/leetcode/house-robber-ii/main.py
+++ solutions/leetcode/house-robber-ii/main.py
@@ -10,3 +10,16 @@ class Solution:
         if len(nums) < 2:
             return nums[0]
         return max(self.robLinear(nums[:-1]), self.robLinear(nums[1:]))
+
+
+class Solution:
+
+    def robLinear(self, nums: List[int]) -> int:
+        dp = (0, 0)
+        for v in nums:
+            dp = (dp[1], max(dp[1], dp[0] + v))
+        return dp[1]
+
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.robLinear(nums[1:]),
+                   self.robLinear(nums[:-1]))
