3934cc07cdf9c39f26848e96eecdf32808fdeff0
TH Huang <tzhsn.huang@gmail.com>
2026-03-22 14:24:32 -0700

    [leetcode] network delay time

--- solutions/leetcode/network-delay-time/main.py
+++ solutions/leetcode/network-delay-time/main.py
@@ -1,7 +1,7 @@
 class Solution:
 
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        visited = [inf] * (n + 1)
+        dist = [inf] * (n + 1)
 
         adj = [[] for _ in range(n + 1)]
         for u, v, w in times:
@@ -12,12 +12,12 @@ class Solution:
         while len(h) > 0:
             t, u = heappop(h)
 
-            if visited[u] <= t:
+            if dist[u] <= t:
                 continue
-            visited[u] = t
+            dist[u] = t
 
             for v, w in adj[u]:
                 heappush(h, (t + w, v))
 
-        result = max(visited[1:])
+        result = max(dist[1:])
         return -1 if result == inf else result
