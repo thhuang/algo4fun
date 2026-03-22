968c31838b891dbf6d3486e31a4261b4f076e41f
TH Huang <tzhsn.huang@gmail.com>
2026-03-21 22:19:02 -0700

    [leetcode] lru cache

--- solutions/leetcode/lru-cache/main.py
+++ solutions/leetcode/lru-cache/main.py
@@ -52,6 +52,12 @@ class LRUCache:
             self.removeNode(self.head.nxt.key)
 
 
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -77,6 +83,12 @@ class LRUCache:
             self.cache.popitem(last=False)
 
 
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -94,6 +106,81 @@ class LRUCache:
         self.cache.move_to_end(key)
         if len(self.cache) > self.cap:
             self.cache.popitem(last=False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
+class Node:
+
+    def __init__(self, key: int = -1, value: int = -1) -> None:
+        self.key = key
+        self.value = value
+        self.prv = None
+        self.nxt = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.head = Node()
+        self.tail = Node()
+        self.head.nxt = self.tail
+        self.tail.prv = self.head
+        self.key_to_node = {}
+
+    def get(self, key: int) -> int:
+        # check if the key exists
+        #   false: return -1
+        #   true:  move the node to the front then return the value
+        #
+        # return the value
+
+        if key not in self.key_to_node:
+            return -1
+
+        self._move_node_to_front(key)
+
+        return self.key_to_node[key].value
+
+    def put(self, key: int, value: int) -> None:
+        # check if the key exists
+        #   true: remove the node
+        #
+        # add the node to the front
+        #
+        # check if the size > capacity
+        #   true: remove the last node
+
+        if key in self.key_to_node:
+            self._remove_node(key)
+
+        self._add_front(key, value)
+
+        if len(self.key_to_node) > self.capacity:
+            self._remove_node(self.tail.prv.key)
+
+    def _remove_node(self, key: int) -> None:
+        node = self.key_to_node[key]
+        prv, nxt = node.prv, node.nxt
+        prv.nxt, nxt.prv = node.nxt, node.prv
+        del self.key_to_node[key]
+
+    def _add_front(self, key: int, value: int) -> None:
+        node = Node(key=key, value=value)
+        prv, nxt = self.head, self.head.nxt
+        prv.nxt, nxt.prv = node, node
+        node.prv, node.nxt = prv, nxt
+        self.key_to_node[key] = node
+
+    def _move_node_to_front(self, key: int) -> None:
+        value = self.key_to_node[key].value
+        self._remove_node(key)
+        self._add_front(key, value)
 
 
 # Your LRUCache object will be instantiated and called as such:
