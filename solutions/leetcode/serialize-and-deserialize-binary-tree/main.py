b9201061f2cc4703a83a9204e6e44f62118818fa
TH Huang <tzhsn.huang@gmail.com>
2026-03-22 17:40:01 -0700

    [leetcode] serialize and deserialize binary tree

--- solutions/leetcode/serialize-and-deserialize-binary-tree/main.py
+++ solutions/leetcode/serialize-and-deserialize-binary-tree/main.py
@@ -97,3 +97,65 @@ class Codec:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        result = []
+
+        q = deque([root])
+
+        while q:
+            u = q.popleft()
+
+            if u is None:
+                result.append(",")
+                continue
+
+            result.append(f",{u.val}")
+            q.append(u.left)
+            q.append(u.right)
+
+        return "".join(result)[1:]
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if data == "":
+            return None
+
+        tokens = iter(data.split(","))
+
+        token = next(tokens)
+        root = TreeNode(int(token))
+
+        q = [root]
+
+        while q:
+            nq = []
+
+            for u in q:
+                l = next(tokens)
+                if l:
+                    u.left = TreeNode(int(l))
+                    nq.append(u.left)
+
+                r = next(tokens)
+                if r:
+                    u.right = TreeNode(int(r))
+                    nq.append(u.right)
+
+            q = nq
+
+        return root
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
