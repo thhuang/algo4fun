d6f1d57ab6a39edcfd4e9bd28b0692bb3c6204ed
TH Huang <tzhsn.huang@gmail.com>
2026-03-22 17:16:47 -0700

    [leetcode] serialize and deserialize binary tree

--- solutions/leetcode/serialize-and-deserialize-binary-tree/main.py
+++ solutions/leetcode/serialize-and-deserialize-binary-tree/main.py
@@ -4,8 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-
 class Codec:
 
     def serialize(self, root: Optional[TreeNode]) -> str:
@@ -39,6 +37,54 @@ class Codec:
             u = TreeNode(int(nodes[p]))
             p += 1
 
+            u.left = construct()
+            u.right = construct()
+
+            return u
+
+        return construct()
+
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Codec:
+
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        result = []
+
+        def preorder(u: Optional[TreeNode]) -> None:
+            if u is None:
+                result.append(",")
+                return
+
+            result.append(f",{u.val}")
+            preorder(u.left)
+            preorder(u.right)
+
+        preorder(root)
+
+        return "".join(result)[1:]
+
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        tokens = iter(data.split(","))
+
+        def construct() -> Optional[TreeNode]:
+            token = next(tokens)
+
+            if token == "":
+                return None
+
+            u = TreeNode(int(token))
             u.left = construct()
             u.right = construct()
 
