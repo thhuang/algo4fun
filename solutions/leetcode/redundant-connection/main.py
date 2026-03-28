556bbebcd4ec9f02769e87f37579c164f59870b3
TH Huang <tzhsn.huang@gmail.com>
2026-03-28 16:55:51 -0700

    [leetcode] redundant connection

--- solutions/leetcode/redundant-connection/main.py
+++ solutions/leetcode/redundant-connection/main.py
@@ -33,3 +33,37 @@ class Solution:
                 return e
 
         return None
+
+
+class Solution:
+
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+
+        parent = [i for i in range(n + 1)]
+        rank = [0] * (n + 1)
+
+        def find(x: int) -> int:
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def unite(a: int, b: int) -> bool:
+            pa, pb = find(a), find(b)
+            if pa == pb:
+                return False
+
+            if rank[pa] < rank[pb]:
+                pa, pb = pb, pa
+            elif rank[pa] == rank[pb]:
+                rank[pa] += 1
+
+            parent[pb] = pa
+
+            return True
+
+        for a, b in edges:
+            if not unite(a, b):
+                return [a, b]
+
+        return []
