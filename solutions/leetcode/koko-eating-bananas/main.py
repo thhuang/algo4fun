ea435b827c717b9cc8ee5f734e7bacb9e4553527
TH Huang <tzhsn.huang@gmail.com>
2026-03-28 17:16:01 -0700

    [leetcode] koko eating bananas

--- solutions/leetcode/koko-eating-bananas/main.py
+++ solutions/leetcode/koko-eating-bananas/main.py
@@ -14,3 +14,26 @@ class Solution:
                 r = m
 
         return r
+
+
+class Solution:
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def feasible(v: int) -> bool:
+            t = 0
+            for p in piles:
+                t += ceil(p / v)
+            return t <= h
+
+        l = 1
+        r = max(piles)
+
+        while l < r:
+            m = (l + r) // 2
+            if feasible(m):
+                r = m
+            else:
+                l = m + 1
+
+        return r
